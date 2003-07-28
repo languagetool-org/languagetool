4a73d5e74972b67df15d9ede61bd6c7102ade137
Daniel Naber <naber@danielnaber.de>
2003-07-28 01:43:39 +0000

    test config

--- trunk/languagetool/client.py
+++ trunk/languagetool/client.py
@@ -2,15 +2,14 @@
 # daniel.naber@t-online.de, 2003-05-02
 # This is just a test how a TextChecker server can be called
 
-# TODO?: add options as XML attributes to <text>
-
 import socket
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.connect(("127.0.0.1", 50100))
 
 print "Connected"
-s.sendall("<text>A sentence bigger then a short one.</text>")
+cfg = '<config textlanguage="en" mothertongue="de" grammar="COMP_THAN" />\n'
+s.sendall("%s<text>A sentence bigger then a short one.</text>" % cfg)
 print "Data sent"
 data = ""
 while 1:
