1f9072b986d0dce06319fc43fd5eb42569c24004
Daniel Naber <naber@danielnaber.de>
2004-06-13 13:04:16 +0000

    change license to LGPL

--- trunk/languagetool/src/SentenceSplitterTest.py
+++ trunk/languagetool/src/SentenceSplitterTest.py
@@ -1,21 +1,20 @@
 # -*- coding: iso-8859-1 -*-
 #!/usr/bin/python
-# Copyright (C) 2003 Daniel Naber <daniel.naber@t-online.de>
+# Copyright (C) 2003,2004 Daniel Naber <daniel.naber@t-online.de>
 
-# This program is free software; you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation; either version 2 of the License, or (at
-# your option) any later version.
+# This library is free software; you can redistribute it and/or
+# modify it under the terms of the GNU Lesser General Public
+# License as published by the Free Software Foundation; either
+# version 2.1 of the License, or (at your option) any later version.
 #
-# This program is distributed in the hope that it will be useful, but
-# WITHOUT ANY WARRANTY; without even the implied warranty of
+# This library is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-# General Public License for more details.
+# Lesser General Public License for more details.
 #
-# You should have received a copy of the GNU General Public License
-# along with this program; if not, write to the Free Software
-# Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307
-# USA
+# You should have received a copy of the GNU Lesser General Public
+# License along with this library; if not, write to the Free Software
+# Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 
 import SentenceSplitter
 import unittest
