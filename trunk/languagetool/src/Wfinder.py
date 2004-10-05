4b5845c9fe7f529d1843bf67290689ccb56c539c
Eleonora <tyuk@users.sourceforge.net>
2004-10-05 10:37:40 +0000

    added Wfhun.py and Wfdeu.py to isolate language pecularities

--- trunk/languagetool/src/Wfinder.py
+++ trunk/languagetool/src/Wfinder.py
@@ -50,6 +50,8 @@ import array
 import codecs
 import os
 import Tagger
+import Wfdeu
+import Wfhun
 from string import *
 import time
 import sys
@@ -83,6 +85,8 @@ class Wfinder:
 		self.is_initialized = 0
 		self.is_secondflag = 0
 		self.textlanguage = textlanguage
+		self.wfdeu = Wfdeu.Wfdeu()
+		self.wfhun = Wfhun.Wfhun()
 		return
 
 	def aff_read(self):
@@ -549,94 +553,10 @@ class Wfinder:
 # Here are the language specific rules of each language
 #
 			if self.textlanguage == 'de':
-				if typ != "":
-					if typ == 'V' or typ == 'HV':
-						if oword[-4:] == 'ende' or oword[-5:-1] == 'ende':
-							typ = 'ADJV'
-					if typ == 'V' or typ == 'HV':
-						if oword[-1:] == 'e':
-							typ =  typ + '11'
-						elif oword[-2:] == 'st':
-							typ = typ + '12'
-						elif oword[-2:] == 'en':
-							typ = typ + '14'
-						elif oword[-2:] == 'et':
-							typ = typ + '15'
-						elif oword[-1:] == 't':
-							typ = typ + '13'
-					elif typ == 'ADJ':
-						if oword[-2:] == 'er':
-							typ = 'ADJER'
-						elif oword[-2:] == 'en':
-							typ = 'ADJEN'
-						elif oword[-2:] == 'em':
-							typ = 'ADJEM'
-						elif oword[-2:] == 'es':
-							typ = 'ADJES'
-						elif oword[-1:] == 'e':
-							typ = 'ADJE'
-					elif typ == 'NMS':
-						if oword[-2:] == 'in':
-							typ = 'NFS'
-						elif oword[-5:] == 'innen':
-							typ = 'NF'
-					if typ[0] == 'N':
-						if word != oword and typ[-1:] == 'S':
-							typ = typ[0:-1]
+				typ = self.wfdeu.getTyp(typ, oword, word)
 			elif self.textlanguage == 'hu':
 #				print word+" "+oword+" "+typ
-				dif = len(oword) - len(word)
-				if (typ[0] == 'V' or typ[0:2] == 'SI') and word != oword:
-					ik = ''
-					telo = 'SI'
-					if typ[0] == 'V':
-						telo = 'V'
-					if oword[0:2] != word[0:2]:
-						ik = 'IK'
-					if oword[-3:]  in (u'iük','iuk', 'nak', 'nek','tak', 'tek') or oword[-2:] in (u'ák', u'ék'):
-						typ = ik + telo + '6'
-					elif oword[-3:]  in ('tok','tek', u'tök'):
-						typ = ik + telo + '5'
-					elif oword[-3:]  in (u'ünk','unk', u'ánk', u'énk') or oword[-2:] in ('uk', u'ük'):
-						typ = ik + telo + '4'
-					elif oword[-2:]  in ('sz','od', 'ed', u'öd',u'éd','ad',u'ád'):
-						typ = ik + telo + '2'
-					elif oword[-2:]  in ('ok','ek',u'ök','om','em',u'öm', u'ám', u'ém', 'am'):
-						typ = ik + telo + '1'
-					elif oword[-2:] in ('va', 've') or oword[-3:] in (u'ván', u'vén'):
-						typ = 'ADV'
-					elif oword[-2:]  == 'ni':
-						typ = 'INF'
-					else:
-						typ = ik + telo + '3'
-				elif typ[0:3] == 'PP4':
-					if oword != 'mi':
-						typ = 'ADV'
-				elif typ[0:3] == 'ADJ':
-					if oword[-2:]  in ('ek','ok', 'ak', u'ék', u'ák') and dif > 0 and (dif < 3 or ((word[0:1] != oword[0:1]) and dif < 9)):
-						typ = 'ADJP'
-					elif oword[-1:]  in (u'é',u'á') and dif > 0 and (dif < 5 or ((word[0:1] != oword[0:1]) and dif < 12)):
-						typ = 'ADV'
-					elif oword[-2:] in ('an', 'en', 'bb','ul',u'ül') and dif == 2:
-						typ = 'ADV'
-					elif dif != 0:
-						typ = 'ADV'
-				elif typ[0] == 'N':
-					if oword[-1] == 'k' and oword[-2] in ('a',u'á', 'e',u'é','i',u'í','o',u'ó',u'ö',u'õ','u',u'ú',u'ü',u'û') and dif > 0 and dif < 3 :
-						typ = 'NP'
-					elif oword[-1:] == 'i' and dif == 1:
-						typ = 'DNA'
-					elif (oword[-1:] in(u'ú', u'û') and dif == 1) or (oword[-2:] in (u'jú', u'jû')  and dif == 2):
-						typ = 'ADJS'
-					elif typ == 'N':
-						if oword[-1] == 'k' and oword == word:
-							typ = 'NP'
-						else:
-							typ = 'NS'
-					elif  dif >= 2:
-						typ = 'N'
-				if typ[0] == 'N' and oword == word and word[-1] != 'k':
-						typ = typ+'N'
+				typ = self.wfhun.getTyp(typ, oword, word)
 #
 # end of language specific rules for new languages
 #
