b3bf6f4b8787b6d171e1bb0805d0965fa4d8be11
Daniel Naber <naber@danielnaber.de>
2005-09-04 21:48:37 +0000

    new tool to check BNC files

---
+++ trunk/JLanguageTool/src/test/de/danielnaber/languagetool/BNCCheck.java
@@ -0,0 +1,69 @@
+/* JLanguageTool, a natural language style checker 
+ * Copyright (C) 2005 Daniel Naber (http://www.danielnaber.de)
+ * 
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301
+ * USA
+ */
+
+package de.danielnaber.languagetool;
+
+import java.io.File;
+import java.io.IOException;
+
+import de.danielnaber.languagetool.tools.StringTools;
+
+/**
+ * Uses JLanguageTol on the files of the BNC (British National Corpus).
+ * 
+ * @author Daniel Naber
+ */
+public class BNCCheck {
+
+  public static void main(String[] args) throws IOException {
+    if (args.length != 1) {
+      System.out.println("Usage: BNCTest <directory>");
+      System.exit(1);
+    }
+    BNCCheck prg = new BNCCheck();
+    prg.run(new File(args[0]));
+  }
+
+  private Main prg;
+  
+  private BNCCheck() throws IOException {
+    prg = new Main(false, Language.ENGLISH);
+  }
+
+  private void run(File file) throws IOException {
+    if (file.isDirectory()) {
+      File[] files = file.listFiles();
+      for (int i = 0; i < files.length; i++) {
+        run(new File(file, files[i].getName()));
+      }
+    } else {
+      System.out.println("Checking " + file.getAbsolutePath());
+      String text = StringTools.readFile(file.getAbsolutePath());
+      text = text.replaceAll("(?s)<header.*?>.*?</header>", "");
+      text = text.replaceAll("<w.*?>", "");
+      text = text.replaceAll("<c.*?>", "");
+      text = text.replaceAll("<.*?>", "");
+      text = text.replaceAll(" +", " ");
+      text = text.replaceAll("&bquo|&equo", "\"");
+      //System.out.println(text);
+      prg.checkText(text);
+    }
+  }
+
+}
