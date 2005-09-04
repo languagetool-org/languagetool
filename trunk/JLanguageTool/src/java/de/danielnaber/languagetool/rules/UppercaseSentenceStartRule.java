57950daa49449659bceda5749e43a9041969bda4
Daniel Naber <naber@danielnaber.de>
2005-09-04 21:45:54 +0000

    new rule that checks whether a sentence starts with an uppercase letter

---
+++ trunk/JLanguageTool/src/java/de/danielnaber/languagetool/rules/UppercaseSentenceStartRule.java
@@ -0,0 +1,67 @@
+/* JLanguageTool, a natural language style checker 
+ * Copyright (C) 2005 Daniel Naber (http://www.danielnaber.de)
+ * 
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301
+ * USA
+ */
+package de.danielnaber.languagetool.rules;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import de.danielnaber.languagetool.AnalyzedSentence;
+import de.danielnaber.languagetool.AnalyzedToken;
+import de.danielnaber.languagetool.Language;
+
+/**
+ * Checks that a sentence starts with an uppercase letter.
+ * 
+ * @author Daniel Naber
+ */
+public class UppercaseSentenceStartRule extends Rule {
+
+  public String getId() {
+    return "UPPERCASE_SENTENCE_START";
+  }
+
+  public String getDescription() {
+    return "Checks that a sentence starts with an uppercase letter";
+  }
+
+  public Language[] getLanguages() {
+    return new Language[] { Language.ENGLISH, Language.GERMAN };
+  }
+
+  public RuleMatch[] match(AnalyzedSentence text) {
+    List ruleMatches = new ArrayList();
+    AnalyzedToken[] tokens = text.getTokensWithoutWhitespace();
+    if (tokens.length < 2)
+      return toRuleMatchArray(ruleMatches);
+    AnalyzedToken token = tokens[1];        // 0 is the artifical sentence start token
+    String firstToken = token.getToken();
+    char firstChar = firstToken.charAt(0);
+    if (Character.isLowerCase(firstChar)) {
+      String msg = "This sentence does not start with an uppercase letter";
+      RuleMatch ruleMatch = new RuleMatch(this, token.getStartPos(), 
+          token.getStartPos()+token.getToken().length(), msg);
+      ruleMatches.add(ruleMatch);
+    }
+    return toRuleMatchArray(ruleMatches);
+  }
+
+  public void reset() {
+  }
+
+}
---
+++ trunk/JLanguageTool/src/test/de/danielnaber/languagetool/rules/UppercaseSentenceStartRuleTest.java
@@ -0,0 +1,54 @@
+/* JLanguageTool, a natural language style checker 
+ * Copyright (C) 2005 Daniel Naber (http://www.danielnaber.de)
+ * 
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301
+ * USA
+ */
+package de.danielnaber.languagetool.rules;
+
+import java.io.IOException;
+import java.util.List;
+
+import junit.framework.TestCase;
+import de.danielnaber.languagetool.JLanguageTool;
+import de.danielnaber.languagetool.Language;
+
+/**
+ * @author Daniel Naber
+ */
+public class UppercaseSentenceStartRuleTest extends TestCase {
+
+  public void testRule() throws IOException {
+    JLanguageTool langTool = new JLanguageTool(Language.GERMAN);
+    List matches;
+    
+    matches = langTool.check("Dies ist ein Satz. Und hier kommt noch einer");
+    assertEquals(0, matches.size());
+    matches = langTool.check("Dies ist ein Satz. Ätsch, noch einer mit Umlaut.");
+    assertEquals(0, matches.size());
+    matches = langTool.check("Dieser Satz ist bspw. okay so.");
+    assertEquals(0, matches.size());
+    matches = langTool.check("Dieser Satz ist z.B. okay so.");
+    assertEquals(0, matches.size());
+    matches = langTool.check("In Nov. next year.");
+    assertEquals(0, matches.size());
+
+    matches = langTool.check("Dies ist ein Satz. und hier kommt noch einer");
+    assertEquals(1, matches.size());
+    matches = langTool.check("Dies ist ein Satz. ätsch, noch einer mit Umlaut.");
+    assertEquals(1, matches.size());
+  }
+  
+}
