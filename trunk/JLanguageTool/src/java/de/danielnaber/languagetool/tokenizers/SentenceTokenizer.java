324899164c8c8f56f48c6eead043dea7e1c82186
Daniel Naber <naber@danielnaber.de>
2005-10-31 21:18:36 +0000

    adding abbreviations

--- trunk/JLanguageTool/src/java/de/danielnaber/languagetool/tokenizers/SentenceTokenizer.java
+++ trunk/JLanguageTool/src/java/de/danielnaber/languagetool/tokenizers/SentenceTokenizer.java
@@ -66,6 +66,8 @@ public class SentenceTokenizer implements Tokenizer {
   private static final String[] ABBREV_LIST = { "Mr", "Mrs", "No", "pp", "St", "no", 
       "Sr", "Bros", "etc", "vs", "esp", "Fig", "fig", "Jan", "Feb", "Mar", "Apr", "Jun", "Jul",
       "Aug", "Sep", "Sept", "Oct", "Okt", "Nov", "Dec", "Ph.D", "PhD",
+      "al",  // in "et al."
+      "cf",
       // German:
       "ggf", "Dr", "Prof", "bspw", "etc", "ca", "lt", "Chr", "n", "Anm", "d", "Übers", "usw"
       };
