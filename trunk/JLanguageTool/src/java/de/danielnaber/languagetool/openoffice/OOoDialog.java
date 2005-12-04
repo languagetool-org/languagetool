1b75f54cae109ec007f5277e96634d8eed4b64e1
Daniel Naber <naber@danielnaber.de>
2005-12-04 01:30:13 +0000

    close dialog when user presses Escape key

--- trunk/JLanguageTool/src/java/de/danielnaber/languagetool/openoffice/OOoDialog.java
+++ trunk/JLanguageTool/src/java/de/danielnaber/languagetool/openoffice/OOoDialog.java
@@ -24,17 +24,21 @@ import java.awt.GridBagLayout;
 import java.awt.Insets;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
 
 import javax.swing.JButton;
+import javax.swing.JComponent;
 import javax.swing.JDialog;
 import javax.swing.JList;
 import javax.swing.JOptionPane;
+import javax.swing.JRootPane;
 import javax.swing.JScrollPane;
 import javax.swing.JTextPane;
+import javax.swing.KeyStroke;
 import javax.xml.parsers.ParserConfigurationException;
 
 import org.xml.sax.SAXException;
@@ -101,13 +105,22 @@ public class OOoDialog implements ActionListener {
   }
   
   void show() {
-    // TODO: escape = close dialog
     if (ruleMatches.size() == 0) {
       JOptionPane.showMessageDialog(null, COMPLETE_TEXT);
       return;
     }
     dialog = new JDialog();
-    //dialog.setDefaultCloseOperation(JDialog.EXIT_ON_CLOSE);
+    // close when user presses Escape key:
+    KeyStroke stroke = KeyStroke.getKeyStroke(KeyEvent.VK_ESCAPE, 0);
+    ActionListener actionListener = new ActionListener() {
+      public void actionPerformed(ActionEvent actionEvent) {
+        if (actionEvent == null) actionEvent = null;    // avoid compiler warning
+         dialog.setVisible(false);
+      }
+    };
+    JRootPane rootPane = dialog.getRootPane();
+    rootPane.registerKeyboardAction(actionListener, stroke, JComponent.WHEN_IN_FOCUSED_WINDOW);
+    
     dialog.setTitle("JLanguageTool/OOo ALPHA Version");
     Container contentPane = dialog.getContentPane();
     contentPane.setLayout(new GridBagLayout());
