2c0357e69e848869e80fa352cd3d299151da2ed8
Daniel Naber <naber@danielnaber.de>
2005-12-27 13:36:40 +0000

    adding copyright; fix window title

--- trunk/JLanguageTool/src/java/de/danielnaber/languagetool/openoffice/ProgressDialog.java
+++ trunk/JLanguageTool/src/java/de/danielnaber/languagetool/openoffice/ProgressDialog.java
@@ -1,5 +1,20 @@
-/*
- * Created on 24.12.2005
+/* LanguageTool, a natural language style checker 
+ * Copyright (C) 2005 Daniel Naber (http://www.danielnaber.de)
+ * 
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301
+ * USA
  */
 package de.danielnaber.languagetool.openoffice;
 
@@ -14,7 +29,7 @@ import javax.swing.JProgressBar;
 class ProgressDialog extends JFrame {
 
   public ProgressDialog() {
-    setTitle("-Dmitri v3- Starting LanguageTool...");
+    setTitle("Starting LanguageTool...");
     JPanel progressPanel = new JPanel(new FlowLayout(FlowLayout.CENTER));
     JProgressBar progressBar = new JProgressBar(0, 100);
     progressBar.setIndeterminate(true);
