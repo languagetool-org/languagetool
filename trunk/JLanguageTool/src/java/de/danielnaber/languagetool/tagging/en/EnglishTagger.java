4436818fddfae53ac4c6e7e8c1297d5445c16ae9
Daniel Naber <naber@danielnaber.de>
2006-01-07 21:48:03 +0000

    fix contracted forms manually as the tagger seems to get confused easily

--- trunk/JLanguageTool/src/java/de/danielnaber/languagetool/tagging/en/EnglishTagger.java
+++ trunk/JLanguageTool/src/java/de/danielnaber/languagetool/tagging/en/EnglishTagger.java
@@ -54,11 +54,30 @@ public class EnglishTagger implements Tagger {
     List analyzedTokens = new ArrayList();
     int i = 0;
     int pos = 0;
+    AnalyzedToken nextAnalyzedToken = null;
     for (Iterator iter = taggerTokens.iterator(); iter.hasNext();) {
       String posTag = (String) iter.next();
       String token = (String)tokens.get(i);
-      // startPos will be set in JLanguageTool:
-      analyzedTokens.add(new AnalyzedToken(token, posTag, 0));
+      String nextToken = null;
+      if (i < tokens.size()-1)
+        nextToken = (String)tokens.get(i+1);
+      // the tagger has problems with contracted forms (not only because we turn "don't" into "don", "t"),
+      // so fix it manually:
+      if (nextAnalyzedToken != null) {
+        analyzedTokens.add(nextAnalyzedToken);
+        nextAnalyzedToken = null;
+      } else if (token.equals("don") && "t".equals(nextToken)) {
+        analyzedTokens.add(new AnalyzedToken(token, "VBP", 0));
+        nextAnalyzedToken = new AnalyzedToken(nextToken, "RB", 0);
+      } else if (token.equals("won") && "t".equals(nextToken)) {
+        analyzedTokens.add(new AnalyzedToken(token, "MD", 0));
+        nextAnalyzedToken = new AnalyzedToken(nextToken, "RB", 0);
+      } else if (token.equals("they") && "ll".equals(nextToken)) {
+        analyzedTokens.add(new AnalyzedToken(token, "PRP", 0));
+        nextAnalyzedToken = new AnalyzedToken(nextToken, "MD", 0);
+      } else {
+        analyzedTokens.add(new AnalyzedToken(token, posTag, 0));    // startPos will be set in JLanguageTool
+      }
       i++;
       pos += token.length();
     }
@@ -69,4 +88,17 @@ public class EnglishTagger implements Tagger {
     return new AnalyzedToken(token, null, startPos);
   }
 
+  /* testing only:
+  public static void main(String[] args) {
+    File resourceFile = JLanguageTool.getAbsoluteFile(RESOURCE_FILENAME);
+    PosTagger tagger = new PosTagger(resourceFile.getAbsolutePath(), (Dictionary)null);
+    String[] l = new String[] {"Then",  "don", "'t",  "take", "us", "there", "."};
+    String[] taggerTokens = tagger.tag(l);
+    //String taggerTokens = tagger.tag("He will not listen to me. Then don 't say so! We 'll show you there 's no way out. Do not say so.");
+    for (int i = 0; i < taggerTokens.length; i++) {
+      System.err.println(l[i] + " "+ taggerTokens[i]);
+    }
+  }
+  */
+  
 }
