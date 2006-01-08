896fbd370839d731120863cfefa81ab9917ffa55
Daniel Naber <naber@danielnaber.de>
2006-01-08 15:57:04 +0000

    validate false friends XML

--- trunk/JLanguageTool/src/test/de/danielnaber/languagetool/ValidateXMLTest.java
+++ trunk/JLanguageTool/src/test/de/danielnaber/languagetool/ValidateXMLTest.java
@@ -37,29 +37,29 @@ import de.danielnaber.languagetool.tools.StringTools;
 
 public class ValidateXMLTest extends TestCase {
 
-  public void test() {
-    try {
-      for (int i = 0; i < Language.LANGUAGES.length; i++) {
-        Language lang = Language.LANGUAGES[i];
-        validate("rules" + File.separator + lang.getShortName() + File.separator + "grammar.xml");
-      }
-    } catch (Exception e) {
-      e.printStackTrace();
-      fail();
+  public void testPatternFile() throws SAXException, IOException, ParserConfigurationException {
+    for (int i = 0; i < Language.LANGUAGES.length; i++) {
+      Language lang = Language.LANGUAGES[i];
+      validate("rules" + File.separator + lang.getShortName() + File.separator + "grammar.xml", "rules/rules.dtd");
     }
   }
 
-  private void validate(String filename) throws SAXException, IOException, ParserConfigurationException {
+  public void testFalseFriendsXML() throws SAXException, IOException, ParserConfigurationException {
+    validate("rules" + File.separator + "false-friends.xml", "rules/false-friends.dtd");
+  }
+
+  private void validate(String filename, String dtdFile) throws SAXException, IOException, ParserConfigurationException {
     SAXParserFactory factory = SAXParserFactory.newInstance();
     factory.setValidating(true);
     SAXParser saxParser = factory.newSAXParser();
     String xml = StringTools.readFile(filename);
     final String decl = "<?xml version=\"1.0\" encoding=\"utf-8\"?>";
-    final String dtd = "<!DOCTYPE rules PUBLIC \"-//W3C//DTD GrammarRules 0.1//EN\" \"file:rules/rules.dtd\">";
+    final String dtd = "<!DOCTYPE rules PUBLIC \"-//W3C//DTD Rules 0.1//EN\" \"file:" +dtdFile+ "\">";
     int pos = xml.indexOf(decl);
     if (pos == -1)
       fail("No XML declaration found in " + filename);
     String newXML = xml.substring(0, pos+decl.length()) + "\r\n" + dtd + xml.substring(pos+decl.length());
+    //System.err.println(newXML);
     InputSource is = new InputSource(new StringReader(newXML));
     saxParser.parse(is, new MyHandler());
   }
