e1dd06f5f78411d6b093a1e82d50e1c1f66b8061
Daniel Naber <naber@danielnaber.de>
2006-01-23 20:24:05 +0000

    more abbreviations, thanks to Wolfgang Lenhard

--- trunk/JLanguageTool/src/java/de/danielnaber/languagetool/tokenizers/SentenceTokenizer.java
+++ trunk/JLanguageTool/src/java/de/danielnaber/languagetool/tokenizers/SentenceTokenizer.java
@@ -63,14 +63,42 @@ public class SentenceTokenizer implements Tokenizer {
   private static final Pattern repair3 = Pattern.compile("([ap]\\.m\\.\\s+)([\\p{Lu}])");
 
   // some German and English abbreviations:
-  private static final String[] ABBREV_LIST = { "Mr", "Mrs", "No", "pp", "St", "no", 
+  private static final String[] ABBREV_LIST = {
+      // English:
+      "Mr", "Mrs", "No", "pp", "St", "no", 
       "Sr", "Bros", "etc", "vs", "esp", "Fig", "fig", "Jan", "Feb", "Mar", "Apr", "Jun", "Jul",
       "Aug", "Sep", "Sept", "Oct", "Okt", "Nov", "Dec", "Ph.D", "PhD",
       "al",  // in "et al."
       "cf",
       // German:
-      "ggf", "Dr", "Prof", "bspw", "etc", "ca", "lt", "Chr", "n", "Anm", "d", "Übers", "usw", "bzw"
-      };
+      "d", "Übers", "usw", "bzw", "Abh", "Abk", "Abt", "ahd", "Akk",
+      "allg", "alltagsspr", "altdt", "alttest", "amerikan", "Anh",
+      "Ank", "Anm", "Art", "Az", "Bat", "bayr", "Bd", "Bde", "Bed",
+      "Bem", "bes", "bez", "Bez", "Bhf", "bspw", "btto", "bw", "bzw",
+      "cts", "ct", "ca", "chem", "chin", "Chr", "cresc", "dat", "Dat",
+      "desgl", "ders", "dgl", "Di", "Dipl", "Dir", "Do", "Doz", "Dr",
+      "dt", "ebd", "Ed", "eigtl", "engl", "Erg", "al", "etc", "etw",
+      "ev", "evtl", "exkl", "Expl", "Exz", "ff", "Fa", "fachspr", "fam",
+      "fem", "Fem", "Fr", "fr", "franz", "frz", "frdl", "Frl",
+      "Fut", "Gd", "geb", "gebr", "Gebr", "geh", "geh", "geleg", "gen",
+      "Gen", "germ", "gesch", "ges", "get", "ggf", "Ggs", "ggT",
+      "griech", "hebr", "hg", "Hrsg", "Hg", "hist", "hochd", "hochspr",
+      "Hptst", "Hr", "Allg", "ill", "inkl", "incl", "Ind", "Inf", "Ing",
+      "ital", "Tr", "Jb", "Jg", "Jh", "jmd", "jmdm", "jmdn", "jmds",
+      "jur", "Kap", "kart", "kath", "kfm", "kaufm", "Kfm", "kgl",
+      "Kl", "Konj", "Krs", "Kr", "Kto", "lat", "lfd", "Lit", "lt",
+      "Lz", "Mask", "mask", "Mrd", "mdal", "med", "met", "mhd", "Mi",
+      "Mio", "min", "Mo", "mod", "nachm", "nördlBr", "neutr",
+      "Nhd", "Nom", "Nr", "Nrn", "Num", "Obj", "od", "dgl", "offz",
+      "Part", "Pass", "Perf", "Pers", "Pfd", "Pl", "Plur",
+      "pl", "Plusq", "Pos", "pp", "Präp", "Präs", "Prät", "Prov", "Prof",
+      "rd", "reg", "resp", "Rhld", "rit", "Sa", "südl", "Br",
+      "sel", "sen", "Sept", "Sing", "sign", "So", "sog", "Sp", "St",
+      "St", "St", "Std", "stacc", "Str", "stud", "Subst", "sva", "svw",
+      "sZ", "Temp", "trans", "Tsd", "übertr", "übl", "ff", "ugs", "univ",
+      "urspr", "usw", "vgl", "Vol", "vorm", "vorm", "Vp", "Vs",
+      "vs", "wg", "Hd", "Ztr", "zus", "Zus", "zzt", "zz", "Zz", "Zt"      
+  };
 
   // einige deutsche Monate, vor denen eine Zahl erscheinen kann,
   // ohne dass eine Satzgrenze erkannt wird (z.B. "am 13. Dezember" -> keine Satzgrenze)
