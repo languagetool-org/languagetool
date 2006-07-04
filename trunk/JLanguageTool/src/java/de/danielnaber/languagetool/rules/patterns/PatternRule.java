b65edd44f9d7a6e80497414be63d7e08ed63d011
Marcin Milkowski <milek_pl@o2.pl>
2006-07-04 15:48:47 +0000

    Oops, a typo

--- trunk/JLanguageTool/src/java/de/danielnaber/languagetool/rules/patterns/PatternRule.java
+++ trunk/JLanguageTool/src/java/de/danielnaber/languagetool/rules/patterns/PatternRule.java
@@ -167,7 +167,7 @@ public class PatternRule extends Rule {
     				 Match = true;
     			 }
     			 allElementsMatch = Match;
-    		 }    		 }
+    		 }
     		 if (!allElementsMatch) {
           break;
         } else {
