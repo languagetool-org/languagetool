2313fc14af5f6dbddaa547332a9ce2d30282cb4e
Daniel Naber <naber@danielnaber.de>
2006-07-07 19:04:48 +0000

    reformat only, no functional change

--- trunk/JLanguageTool/src/java/de/danielnaber/languagetool/rules/patterns/PatternRule.java
+++ trunk/JLanguageTool/src/java/de/danielnaber/languagetool/rules/patterns/PatternRule.java
@@ -30,8 +30,8 @@ import de.danielnaber.languagetool.rules.RuleMatch;
 import de.danielnaber.languagetool.tools.StringTools;
 
 /**
- * A Rule that drescribes a language error as a simple pattern of words or their
- * part-of-speech tags.
+ * A Rule that drescribes a language error as a simple pattern of words or their part-of-speech
+ * tags.
  * 
  * @author Daniel Naber
  */
@@ -44,10 +44,10 @@ public class PatternRule extends Rule {
   private String message;
   private int startPositionCorrection = 0;
   private int endPositionCorrection = 0;
-
+  
   private boolean caseSensitive = false;
   private boolean regExp = false;
-
+  
   private Element[] patternElements;
 
   PatternRule(String id, Language language, String pattern, String description, String message) {
@@ -65,7 +65,7 @@ public class PatternRule extends Rule {
     this.description = description;
     this.message = message;
   }
-  
+
   public String getId() {
     return id;
   }
@@ -93,19 +93,19 @@ public class PatternRule extends Rule {
   public boolean getCaseSensitive() {
     return caseSensitive;
   }
-  
+
   public void setCaseSensitive(boolean caseSensitive) {
-	    this.caseSensitive = caseSensitive;
-	  }
-  
+    this.caseSensitive = caseSensitive;
+  }
+
   public boolean getregExpSetting() {
-	    return regExp;
-	  }
+    return regExp;
+  }
 
   public void setregExpSetting(boolean regExp) {
     this.regExp = regExp;
   }
-  
+
   public int getStartPositionCorrection() {
     return startPositionCorrection;
   }
@@ -123,52 +123,50 @@ public class PatternRule extends Rule {
   }
 
   public void addPatternElements(List elements) {
-	  List<Element> elems = new ArrayList<Element>();
-	  if (this.patternElements!=null)
-	  for (int i = 0; i < this.patternElements.length; i++) {
-		elems.add(this.patternElements[i]);  
-	  }
-	  for(int i=0;i<elements.size();i++)
-	  {
-		  elems.add((Element)elements.get(i));
-	  }
-	  this.patternElements=(Element[])elems.toArray(new Element[0]);
+    List<Element> elems = new ArrayList<Element>();
+    if (this.patternElements != null)
+      for (int i = 0; i < this.patternElements.length; i++) {
+        elems.add(this.patternElements[i]);
+      }
+    for (int i = 0; i < elements.size(); i++) {
+      elems.add((Element) elements.get(i));
+    }
+    this.patternElements = (Element[]) elems.toArray(new Element[0]);
   }
-  
+
   public RuleMatch[] match(AnalyzedSentence text) {
-    List<RuleMatch> ruleMatches = new ArrayList<RuleMatch>(); 
+    List<RuleMatch> ruleMatches = new ArrayList<RuleMatch>();
     AnalyzedTokenReadings[] tokens = text.getTokensWithoutWhitespace();
-    if (patternElements == null) {      // lazy init
-      patternElements = getPatternElements(pattern); 
+    if (patternElements == null) { // lazy init
+      patternElements = getPatternElements(pattern);
     }
     int tokenPos = 0;
-    
+
     int firstMatchToken = -1;
     int lastMatchToken = -1;
 
     for (int i = 0; i < tokens.length; i++) {
-         	 boolean allElementsMatch = true;
-    	 int matchingTokens = 0;
-    	 for (int k = 0; k < patternElements.length; k++) {
-    		 Element elem = patternElements[k];
-    		 int nextPos = tokenPos+k;
-    		 if (nextPos >= tokens.length) {
-    			 allElementsMatch = false;
-    			 break;
-    		 }
-    		 boolean Match = false;
-    		 for (int l = 0; l < tokens[nextPos].getReadingslength(); l++) {
-    			 AnalyzedToken matchToken = tokens[nextPos].getAnalyzedToken(l);
-    			 //Logical OR (cannot be AND):
-    			 if (!elem.match(matchToken)) {
-    				 Match = Match || false;
-    			 }
-    			 else {
-    				 Match = true;
-    			 }
-    			 allElementsMatch = Match;
-    		 }
-    		 if (!allElementsMatch) {
+      boolean allElementsMatch = true;
+      int matchingTokens = 0;
+      for (int k = 0; k < patternElements.length; k++) {
+        Element elem = patternElements[k];
+        int nextPos = tokenPos + k;
+        if (nextPos >= tokens.length) {
+          allElementsMatch = false;
+          break;
+        }
+        boolean Match = false;
+        for (int l = 0; l < tokens[nextPos].getReadingslength(); l++) {
+          AnalyzedToken matchToken = tokens[nextPos].getAnalyzedToken(l);
+          // Logical OR (cannot be AND):
+          if (!elem.match(matchToken)) {
+            Match = Match || false;
+          } else {
+            Match = true;
+          }
+          allElementsMatch = Match;
+        }
+        if (!allElementsMatch) {
           break;
         } else {
           matchingTokens++;
@@ -181,15 +179,14 @@ public class PatternRule extends Rule {
         String errMessage = message;
         // replace back references like \1 in message:
         for (int j = 0; j < matchingTokens; j++) {
-          errMessage = errMessage.replaceAll("\\\\"+(j+1),
-              tokens[firstMatchToken+j].getToken());
+          errMessage = errMessage.replaceAll("\\\\" + (j + 1), tokens[firstMatchToken + j]
+              .getToken());
         }
-        boolean startsWithUppercase = 
-          StringTools.startsWithUppercase(tokens[firstMatchToken+startPositionCorrection].toString());
-        RuleMatch ruleMatch = new RuleMatch(this,
-            tokens[firstMatchToken+startPositionCorrection].getStartPos(), 
-            tokens[lastMatchToken+endPositionCorrection].getStartPos()+
-            tokens[lastMatchToken+endPositionCorrection].getToken().length(), errMessage,
+        boolean startsWithUppercase = StringTools.startsWithUppercase(tokens[firstMatchToken
+            + startPositionCorrection].toString());
+        RuleMatch ruleMatch = new RuleMatch(this, tokens[firstMatchToken + startPositionCorrection]
+            .getStartPos(), tokens[lastMatchToken + endPositionCorrection].getStartPos()
+            + tokens[lastMatchToken + endPositionCorrection].getToken().length(), errMessage,
             startsWithUppercase);
         ruleMatches.add(ruleMatch);
       } else {
@@ -198,28 +195,29 @@ public class PatternRule extends Rule {
       }
       tokenPos++;
     }
-  
-    return (RuleMatch[])ruleMatches.toArray(new RuleMatch[0]);
+
+    return (RuleMatch[]) ruleMatches.toArray(new RuleMatch[0]);
   }
 
   private Element[] getPatternElements(String pattern) {
     List<Element> elements = new ArrayList<Element>();
-    pattern = pattern.replaceAll("[\\(\\)]", "");       // just ignore parentheses
+    pattern = pattern.replaceAll("[\\(\\)]", ""); // just ignore parentheses
     String[] parts = pattern.split("\\s+");
     for (int i = 0; i < parts.length; i++) {
       String element = parts[i];
       boolean negation = false;
       if (element.startsWith("^")) {
         negation = true;
-        element = element.substring(1);     // cut off "^"
+        element = element.substring(1); // cut off "^"
       }
-      if ((element.startsWith("\"") && !element.endsWith("\"")) || (element.endsWith("\"") && !element.startsWith("\""))) {
+      if ((element.startsWith("\"") && !element.endsWith("\""))
+          || (element.endsWith("\"") && !element.startsWith("\""))) {
         throw new IllegalArgumentException("Invalid pattern '" + pattern + "': unbalanced quote");
       }
-      if (element.startsWith("\"") && element.endsWith("\"")) {         // cut off quotes
-        element = element.substring(1, element.length()-1);
+      if (element.startsWith("\"") && element.endsWith("\"")) { // cut off quotes
+        element = element.substring(1, element.length() - 1);
         String tokenParts[] = element.split("\\|");
-        StringElement stringElement = new StringElement(tokenParts, caseSensitive, false, false); 
+        StringElement stringElement = new StringElement(tokenParts, caseSensitive, false, false);
         stringElement.setNegation(negation);
         elements.add(stringElement);
       } else if (Character.isUpperCase(element.charAt(0))) {
@@ -229,7 +227,7 @@ public class PatternRule extends Rule {
         POSElement posElement;
         if (element.indexOf("^") != -1) {
           tokenParts = element.substring(0, element.indexOf("^")).split("\\|");
-          exceptions = element.substring(element.indexOf("^")+1).split("\\|");
+          exceptions = element.substring(element.indexOf("^") + 1).split("\\|");
         } else {
           tokenParts = element.split("\\|");
         }
@@ -240,9 +238,9 @@ public class PatternRule extends Rule {
         throw new IllegalArgumentException("Unknown type " + element + " in pattern: " + pattern);
       }
     }
-    return (Element[])elements.toArray(new Element[0]);
+    return (Element[]) elements.toArray(new Element[0]);
   }
-  
+
   public void reset() {
     // nothing
   }
