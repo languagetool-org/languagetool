1faaa1739c5a5afbd68a1f387bb7c598166b16e3
Daniel Naber <naber@danielnaber.de>
2006-07-11 21:53:44 +0000

    fix file name

--- trunk/JLanguageTool/resource/de/make-dict-de.sh
+++ trunk/JLanguageTool/resource/de/make-dict-de.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
 OUTPUT=resource/de/german.dict
-cat resource/de/morphy.txt resource/de/morphy_additions.txt | sort -u | gawk -f s_fsa/morph_data.awk | s_fsa/fsa_ubuild -O -o $OUTPUT
+cat resource/de/morphy.txt resource/de/added.txt | sort -u | gawk -f s_fsa/morph_data.awk | s_fsa/fsa_ubuild -O -o $OUTPUT
 echo "Output written to $OUTPUT"
