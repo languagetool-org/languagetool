3130999945be0ac7d878832a4cae0577adb910c9
Daniel Naber <naber@danielnaber.de>
2006-07-18 21:59:11 +0000

    set LANG=POSIX

--- trunk/JLanguageTool/resource/de/make-dict-de.sh
+++ trunk/JLanguageTool/resource/de/make-dict-de.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 
+LANG=POSIX
 OUTPUT=resource/de/german.dict
-cat resource/de/morphy.txt resource/de/added.txt | sort -u | gawk -f s_fsa/morph_data.awk | s_fsa/fsa_ubuild -O -o $OUTPUT
+cat resource/de/morphy_fsa.txt resource/de/added.txt | sort -u | gawk -f s_fsa/morph_data.awk | s_fsa/fsa_ubuild -O -o $OUTPUT
 echo "Output written to $OUTPUT"
