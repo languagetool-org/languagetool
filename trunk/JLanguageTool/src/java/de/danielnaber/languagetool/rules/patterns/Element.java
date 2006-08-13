b8b222f767871fcc6c4c4b584d05847f8ab360d0
Daniel Naber <naber@danielnaber.de>
2006-08-13 22:19:31 +0000

    re-indent, no functional change

--- trunk/JLanguageTool/src/java/de/danielnaber/languagetool/rules/patterns/Element.java
+++ trunk/JLanguageTool/src/java/de/danielnaber/languagetool/rules/patterns/Element.java
@@ -32,8 +32,10 @@ public class Element {
   private String stringToken;
   private String posToken;
   private boolean posRegExp = false;
+
   boolean negation = false;
   boolean posNegation = false;
+  
   private boolean caseSensitive = false;
   private boolean stringRegExp = false;
   private boolean inflected = false;
@@ -46,201 +48,200 @@ public class Element {
   private boolean exceptionRegExp = false;
   private boolean exceptionInflected = false;
   private boolean exceptionSet = false;
-  
+
   int skip = 0;
 
   boolean match(AnalyzedToken token) {
-    return (matchStringToken(token) != negation)
-    && (matchPosToken(token) != posNegation)
-    && !exceptionMatch(token);
+    return (matchStringToken(token) != negation) && (matchPosToken(token) != posNegation)
+        && !exceptionMatch(token);
   }
 
   boolean exceptionMatch(AnalyzedToken token) {
-    if (exceptionSet) { 
+    if (exceptionSet) {
       return (matchExceptionStringToken(token) != exceptionNegation)
-      && (matchExceptionPosToken(token) != exceptionPosNegation);
+          && (matchExceptionPosToken(token) != exceptionPosNegation);
     } else {
       return false;
     }
   }
-  
+
   Element(String token, boolean caseSensitive, boolean regExp, boolean inflected) {
-	  this.stringToken = token;
-	  this.caseSensitive = caseSensitive;
-	  this.stringRegExp = regExp;
-	  this.inflected = inflected;
+    this.stringToken = token;
+    this.caseSensitive = caseSensitive;
+    this.stringRegExp = regExp;
+    this.inflected = inflected;
   }
-  
+
   public String toString() {
-    if (posToken!=null) {
-	   return stringToken.concat("/").concat(posToken);
-	  }
-	  else return stringToken;
+    if (posToken != null) {
+      return stringToken.concat("/").concat(posToken);
+    } else {
+      return stringToken;
+    }
   }
-  
+
   public void setPosElement(String posToken, boolean regExp, boolean negation) {
-    this.posToken=posToken;
-    this.posNegation=negation;
-    posRegExp=regExp;
+    this.posToken = posToken;
+    this.posNegation = negation;
+    posRegExp = regExp;
   }
 
   public void setPosException(String posToken, boolean regExp, boolean negation) {
-	    exceptionPosToken=posToken;
-	    exceptionPosNegation=negation;
-	    exceptionPosRegExp=regExp;
-	    exceptionSet=true;
-	  }
-  
+    exceptionPosToken = posToken;
+    exceptionPosNegation = negation;
+    exceptionPosRegExp = regExp;
+    exceptionSet = true;
+  }
+
   public void setStringException(String token, boolean regExp, boolean inflected, boolean negation) {
-	    exceptionStringToken = token;
-	    exceptionRegExp = regExp;
-	    exceptionInflected = inflected;
-	    exceptionNegation = negation;
-	    exceptionSet=true;
-	  }
-  
+    exceptionStringToken = token;
+    exceptionRegExp = regExp;
+    exceptionInflected = inflected;
+    exceptionNegation = negation;
+    exceptionSet = true;
+  }
+
   boolean matchExceptionPosToken(AnalyzedToken token) {
-	  // if no POS set
-	  // defaulting to true
-	  if (exceptionPosToken==null) {
-		  return true;
-	  }
-	  boolean match = false;
-	  if (!exceptionPosRegExp)
-	  {
-		  if (exceptionPosToken.equals(token.getPOSTag())) {
-			  match = true;
-		  }
-	  } else
-		  //changed to match regexps
-		  if (token.getPOSTag()!=null)
-			  if (Pattern.matches(exceptionPosToken, token.getPOSTag()))	{	    
-				  match = true;
-			  }
-	  return match;
+    // if no POS set
+    // defaulting to true
+    if (exceptionPosToken == null) {
+      return true;
+    }
+    boolean match = false;
+    if (!exceptionPosRegExp) {
+      if (exceptionPosToken.equals(token.getPOSTag())) {
+        match = true;
+      }
+    } else
+    // changed to match regexps
+    if (token.getPOSTag() != null) {
+      if (Pattern.matches(exceptionPosToken, token.getPOSTag())) {
+        match = true;
+      }
+    }
+    return match;
   }
-  
+
   boolean matchExceptionStringToken(AnalyzedToken token) {
-	  
-	  //if no string set
-	  //defaulting to true
-	  if (exceptionStringToken == null) {
-		  return true;
-	  }
-	  if (exceptionStringToken.equals("")){
-		  return true;
-	  }
-	  
-	  String testToken = null;
-	  if (exceptionInflected)
-		  testToken = token.getLemma();
-	  else
-		  testToken = token.getToken();
-	  
-	  if (caseSensitive) {
-		  if (exceptionRegExp) {
-			  if (token.getToken() != null)
-				  if (Pattern.matches(exceptionStringToken, testToken))
-					  return true;
-		  } else {
-			  if (exceptionStringToken.equals(testToken))
-				  return true;
-		  }      
-	  } else {
-		  if (exceptionRegExp) {
-			  if (testToken != null)
-				  //(?u) - regex matching 
-				  //case insensitive in Unicode
-				  if (Pattern.matches("(?u)".concat(exceptionStringToken), testToken))
-					  return true;
-		  } else {
-			  if (exceptionStringToken.equalsIgnoreCase(testToken))
-				  return true;
-		  }	      
-	  }
-	  return false;
+
+    // if no string set
+    // defaulting to true
+    if (exceptionStringToken == null) {
+      return true;
+    }
+    if (exceptionStringToken.equals("")) {
+      return true;
+    }
+
+    String testToken = null;
+    if (exceptionInflected)
+      testToken = token.getLemma();
+    else
+      testToken = token.getToken();
+
+    if (caseSensitive) {
+      if (exceptionRegExp) {
+        if (token.getToken() != null)
+          if (Pattern.matches(exceptionStringToken, testToken))
+            return true;
+      } else {
+        if (exceptionStringToken.equals(testToken))
+          return true;
+      }
+    } else {
+      if (exceptionRegExp) {
+        if (testToken != null)
+          // (?u) - regex matching
+          // case insensitive in Unicode
+          if (Pattern.matches("(?u)".concat(exceptionStringToken), testToken))
+            return true;
+      } else {
+        if (exceptionStringToken.equalsIgnoreCase(testToken))
+          return true;
+      }
+    }
+    return false;
   }
-  
+
   boolean matchPosToken(AnalyzedToken token) {
-	  // if no POS set
-	  // defaulting to true
-	  if (posToken==null) {
-		  return true;
-	  }
-	  boolean match = false;
-	  if (!posRegExp)
-	  {
-		  if (posToken.equals(token.getPOSTag())) {
-			  match = true;
-		  }
-	  } else
-		  //changed to match regexps
-		  if (token.getPOSTag()!=null)
-			  if (Pattern.matches(posToken, token.getPOSTag()))	{	    
-				  match = true;
-			  }
-	  return match;
+    // if no POS set
+    // defaulting to true
+    if (posToken == null) {
+      return true;
+    }
+    boolean match = false;
+    if (!posRegExp) {
+      if (posToken.equals(token.getPOSTag())) {
+        match = true;
+      }
+    } else
+    // changed to match regexps
+    if (token.getPOSTag() != null) {
+      if (Pattern.matches(posToken, token.getPOSTag())) {
+        match = true;
+      }
+    }
+    return match;
   }
 
   boolean matchStringToken(AnalyzedToken token) {
-	  
-	  //if no string set
-	  //defaulting to true
-	  if (stringToken == null) {
-		  return true;
-	  }
-	  if (stringToken.equals("")){
-		  return true;
-	  }
-	  
-	  String testToken = null;
-	  if (inflected)
-		  testToken = token.getLemma();
-	  else
-		  testToken = token.getToken();
-	  
-	  if (caseSensitive) {
-		  if (stringRegExp) {
-			  if (token.getToken() != null)
-				  if (Pattern.matches(stringToken, testToken))
-					  return true;
-		  } else {
-			  if (stringToken.equals(testToken))
-				  return true;
-		  }      
-	  } else {
-		  if (stringRegExp) {
-			  if (testToken != null)
-				  //(?u) - regex matching 
-				  //case insensitive in Unicode
-				  if (Pattern.matches("(?u)".concat(stringToken), testToken))
-					  return true;
-		  } else {
-			  if (stringToken.equalsIgnoreCase(testToken))
-				  return true;
-		  }	      
-	  }
-	  return false;
+
+    // if no string set
+    // defaulting to true
+    if (stringToken == null) {
+      return true;
+    }
+    if (stringToken.equals("")) {
+      return true;
+    }
+
+    String testToken = null;
+    if (inflected)
+      testToken = token.getLemma();
+    else
+      testToken = token.getToken();
+
+    if (caseSensitive) {
+      if (stringRegExp) {
+        if (token.getToken() != null)
+          if (Pattern.matches(stringToken, testToken))
+            return true;
+      } else {
+        if (stringToken.equals(testToken))
+          return true;
+      }
+    } else {
+      if (stringRegExp) {
+        if (testToken != null)
+          // (?u) - regex matching
+          // case insensitive in Unicode
+          if (Pattern.matches("(?u)".concat(stringToken), testToken))
+            return true;
+      } else {
+        if (stringToken.equalsIgnoreCase(testToken))
+          return true;
+      }
+    }
+    return false;
   }
 
-  
-  public int getSkipNext(){
-	  return skip;
+  public int getSkipNext() {
+    return skip;
   }
-  
+
   public void setSkipNext(int i) {
-	  skip = i;
+    skip = i;
   }
-  
+
   String getTokens() {
     return stringToken;
   }
-  
+
   /**
-   * Negates the meaning of match(). 
+   * Negates the meaning of match().
    */
   void setNegation(boolean negation) {
     this.negation = negation;
   }
-  
+
 }
