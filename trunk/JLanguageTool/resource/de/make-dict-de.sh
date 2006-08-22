2a9eadecc4fc6a5b87dc451784db35de6349cb4e
Marcin Milkowski <milek_pl@o2.pl>
2006-08-22 09:15:43 +0000

    updated Polish rule

--- trunk/JLanguageTool/resource/de/make-dict-de.sh
+++ trunk/JLanguageTool/resource/de/make-dict-de.sh
@@ -2,5 +2,5 @@
 
 LANG=POSIX
 OUTPUT=resource/de/german.dict
-cat resource/de/morphy_fsa.txt resource/de/added.txt | sort -u | gawk -f s_fsa/morph_data.awk | s_fsa/fsa_ubuild -O -o $OUTPUT
+cat resource/de/morphy_fsa.txt resource/de/added.txt | sort -u | gawk -f ~/fsa/morph_data.awk | fsa_ubuild -O -o $OUTPUT
 echo "Output written to $OUTPUT"
