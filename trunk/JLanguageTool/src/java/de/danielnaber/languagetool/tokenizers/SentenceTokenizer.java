73427cd677e8d356490220c66165d5474819ee80
Marcin Milkowski <milek_pl@o2.pl>
2006-09-06 23:41:39 +0000

    enhanced Polish punctuation rules, new pl abbrevs

--- trunk/JLanguageTool/src/java/de/danielnaber/languagetool/tokenizers/SentenceTokenizer.java
+++ trunk/JLanguageTool/src/java/de/danielnaber/languagetool/tokenizers/SentenceTokenizer.java
@@ -104,7 +104,8 @@ public class SentenceTokenizer implements Tokenizer {
       //Polish:
       "np", "p.n.e", "m.in", "itd", "itp", "pt","cdn", "czyt", "dyr","hab",
       "inż","jw", "lek","n.e","nb","rys", "tj", "tzw", "tzn", "zob" , "ds", "ang",
-      "ul", "pl", "al", "prof", "gen", "k", "n", "ks", "ok", "tys", "r", "proc"
+      "ul", "pl", "al", "prof", "gen", "k", "n", "ks", "ok", "tys", "r", "proc",
+      "ww", "M.in"
   };
 
   // einige deutsche Monate, vor denen eine Zahl erscheinen kann,
