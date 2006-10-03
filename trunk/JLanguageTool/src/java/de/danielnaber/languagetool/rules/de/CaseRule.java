84c9cfdf82b5e892f0b441a4497ca493d30b4c5d
Daniel Naber <naber@danielnaber.de>
2006-10-03 18:54:36 +0000

    add exceptions

--- trunk/JLanguageTool/src/java/de/danielnaber/languagetool/rules/de/CaseRule.java
+++ trunk/JLanguageTool/src/java/de/danielnaber/languagetool/rules/de/CaseRule.java
@@ -74,6 +74,8 @@ public class CaseRule extends GermanRule {
   
   private final static Set<String> exceptions = new HashSet<String>();
   static {
+    exceptions.add("Dr");
+    exceptions.add("Prof");
     exceptions.add("Mr");
     exceptions.add("Mrs");
     exceptions.add("Le");    // "Le Monde" etc
