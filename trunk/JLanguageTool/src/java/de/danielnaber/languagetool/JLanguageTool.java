29103091f72c4fe0ffd355b649cd9c541335596c
Daniel Naber <naber@danielnaber.de>
2006-10-30 22:51:40 +0000

    0.8.5 is done, rename to 0.8.6-dev

--- trunk/JLanguageTool/src/java/de/danielnaber/languagetool/JLanguageTool.java
+++ trunk/JLanguageTool/src/java/de/danielnaber/languagetool/JLanguageTool.java
@@ -67,7 +67,7 @@ import de.danielnaber.languagetool.tokenizers.Tokenizer;
  */
 public class JLanguageTool {
 
-  public static final String VERSION = "0.8.5";      // keep in sync with build.xml!
+  public static final String VERSION = "0.8.6-dev";      // keep in sync with build.xml!
 
   public static final String RULES_DIR = "rules";
   public static final String PATTERN_FILE = "grammar.xml";
