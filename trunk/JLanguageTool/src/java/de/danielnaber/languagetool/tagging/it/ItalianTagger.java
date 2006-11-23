f7b29ce76a5ad65923effb29bbd2ca4503745a50
Daniel Naber <naber@danielnaber.de>
2006-11-23 20:22:46 +0000

    javadoc and license update; whitespace cleanup

--- trunk/JLanguageTool/src/java/de/danielnaber/languagetool/tagging/it/ItalianTagger.java
+++ trunk/JLanguageTool/src/java/de/danielnaber/languagetool/tagging/it/ItalianTagger.java
@@ -1,3 +1,21 @@
+/* LanguageTool, a natural language style checker 
+ * Copyright (C) 2006 Daniel Naber (http://www.danielnaber.de)
+ * 
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301
+ * USA
+ */
 package de.danielnaber.languagetool.tagging.it;
 
 import de.danielnaber.languagetool.tagging.Tagger;
@@ -8,7 +26,6 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
-
 
 import com.dawidweiss.stemmers.Lametyzator;
 
@@ -24,66 +41,66 @@ import de.danielnaber.languagetool.JLanguageTool;
  * see resource/it/readme-morph-it.txt for tagset
  * 
  * @author Marcin Milkowski
- *
  */
-
 public class ItalianTagger implements Tagger {
 
-  private static final String RESOURCE_FILENAME = "resource" +File.separator+ "it" +File.separator+
-  "italian.dict"; 
-    private Lametyzator morfologik = null;
-    
-  public List<AnalyzedTokenReadings> tag(final List<String> sentenceTokens)
-      throws IOException {
+  private static final String RESOURCE_FILENAME = "resource" + File.separator + "it"
+      + File.separator + "italian.dict";
+
+  private Lametyzator morfologik = null;
+
+  public List<AnalyzedTokenReadings> tag(final List<String> sentenceTokens) throws IOException {
     String[] taggerTokens;
     List<AnalyzedTokenReadings> tokenReadings = new ArrayList<AnalyzedTokenReadings>();
     int pos = 0;
     //caching Lametyzator instance - lazy init
-    if (morfologik == null) {   
-       File resourceFile = JLanguageTool.getAbsoluteFile(RESOURCE_FILENAME); 
-       //System.setProperty(Lametyzator.PROPERTY_NAME_LAMETYZATOR_DICT, resourceFile.getAbsolutePath());
-       morfologik = new Lametyzator(Tools.getInputStream(resourceFile.getAbsolutePath()), "iso8859-15", '+');
+    if (morfologik == null) {
+      File resourceFile = JLanguageTool.getAbsoluteFile(RESOURCE_FILENAME);
+      //System.setProperty(Lametyzator.PROPERTY_NAME_LAMETYZATOR_DICT, resourceFile.getAbsolutePath());
+      morfologik = new Lametyzator(Tools.getInputStream(resourceFile.getAbsolutePath()),
+          "iso8859-15", '+');
     }
-    
+
     for (Iterator<String> iter = sentenceTokens.iterator(); iter.hasNext();) {
       String word = iter.next();
-      List<AnalyzedToken> l = new ArrayList<AnalyzedToken>();      
+      List<AnalyzedToken> l = new ArrayList<AnalyzedToken>();
       String[] lowerTaggerTokens = null;
-        taggerTokens = morfologik.stemAndForm(word);
-        if (word != word.toLowerCase()) {
+      taggerTokens = morfologik.stemAndForm(word);
+      if (word != word.toLowerCase()) {
         lowerTaggerTokens = morfologik.stemAndForm(word.toLowerCase());
-        }
-                
-    if (taggerTokens != null) {
-       int i = 0;
+      }
+
+      if (taggerTokens != null) {
+        int i = 0;
         while (i < taggerTokens.length) {
-            //Lametyzator returns data as String[]
-            //first lemma, then annotations
-            l.add(new AnalyzedToken(word, taggerTokens[i + 1], taggerTokens[i]));
-            i = i + 2;
-        } 
-      }     
-    if (lowerTaggerTokens != null) {
-      int i = 0;
-       while (i < lowerTaggerTokens.length) {
-           //Lametyzator returns data as String[]
-           //first lemma, then annotations
-           l.add(new AnalyzedToken(word, lowerTaggerTokens[i + 1], lowerTaggerTokens[i]));
-           i = i + 2;
-       } 
-     }        
-    
-    if (lowerTaggerTokens == null && taggerTokens == null) {
-            l.add(new AnalyzedToken(word, null, pos));                       
-    }
+          //Lametyzator returns data as String[]
+          //first lemma, then annotations
+          l.add(new AnalyzedToken(word, taggerTokens[i + 1], taggerTokens[i]));
+          i = i + 2;
+        }
+      }
+      if (lowerTaggerTokens != null) {
+        int i = 0;
+        while (i < lowerTaggerTokens.length) {
+          //Lametyzator returns data as String[]
+          //first lemma, then annotations
+          l.add(new AnalyzedToken(word, lowerTaggerTokens[i + 1], lowerTaggerTokens[i]));
+          i = i + 2;
+        }
+      }
+
+      if (lowerTaggerTokens == null && taggerTokens == null) {
+        l.add(new AnalyzedToken(word, null, pos));
+      }
       pos += word.length();
-      tokenReadings.add(new AnalyzedTokenReadings((AnalyzedToken[])l.toArray(new AnalyzedToken[0])));
-   }
-    
+      tokenReadings
+          .add(new AnalyzedTokenReadings((AnalyzedToken[]) l.toArray(new AnalyzedToken[0])));
+    }
+
     return tokenReadings;
 
-  }  
-  
+  }
+
   /** 
    * @see de.danielnaber.languagetool.tagging.Tagger#createNullToken(java.lang.String, int)
    * @return AnalyzedTokenReadings
@@ -91,5 +108,5 @@ public class ItalianTagger implements Tagger {
   public final Object createNullToken(final String token, final int startPos) {
     return new AnalyzedTokenReadings(new AnalyzedToken(token, null, startPos));
   }
-  
-  }
+
+}
