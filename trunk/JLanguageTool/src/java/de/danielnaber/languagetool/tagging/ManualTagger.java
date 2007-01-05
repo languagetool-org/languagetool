5fddb9b7a9ac2bfedf9dee1f410a2ff8ae61599b
Daniel Naber <naber@danielnaber.de>
2007-01-05 19:13:24 +0000

    a "tagger" that takes its information from a plain text file so the *.dict doesn't need to be recompiled for every dictionary change

---
+++ trunk/JLanguageTool/src/java/de/danielnaber/languagetool/tagging/ManualTagger.java
@@ -0,0 +1,119 @@
+/* LanguageTool, a natural language style checker 
+ * Copyright (C) 2007 Daniel Naber (http://www.danielnaber.de)
+ * 
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301
+ * USA
+ */
+package de.danielnaber.languagetool.tagging;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * A tagger that reads the POS information from a plain (UTF-8) text file.
+ * This makes it possible for the user to edit the text file to let the system
+ * know about new words or missing readings in the *.dict file.
+ *
+ * <p>File Format: <tt>fullform baseform postags</tt> (tab separated)
+ * 
+ * @author Daniel Naber
+ */
+public class ManualTagger {
+
+  private Map<String, List<LookedUpTerm>> mapping = null;
+  
+  public ManualTagger(File file) throws IOException {
+    mapping = loadMapping(file, "utf8");
+  }
+  
+  /**
+   * Look up a word's baseform and POS information.
+   * 
+   * @param term
+   * @return an array with the baseform (at psoitino 0, 2, ...) and the
+   *  POS information (at psoitino 1, 3, ...) or <code>null</code> if
+   *  the word is unknown
+   */
+  public String[] lookup(final String term) {
+    List<LookedUpTerm> l = mapping.get(term);
+    if (l == null)
+      return null;
+    List<String> plainResult = new ArrayList<String>();
+    for (Iterator iter = l.iterator(); iter.hasNext();) {
+      LookedUpTerm lookedUpTerm = (LookedUpTerm) iter.next();
+      plainResult.add(lookedUpTerm.baseform);
+      plainResult.add(lookedUpTerm.postags);
+    }
+    if (plainResult.size() == 0)
+      return null;
+    return plainResult.toArray(new String[]{});
+  }
+  
+  private Map<String, List<LookedUpTerm>> loadMapping(final File file, final String encoding) throws IOException {
+    Map<String, List<LookedUpTerm>> map = new HashMap<String, List<LookedUpTerm>>();
+    InputStreamReader isr = null;
+    BufferedReader br = null;
+    FileInputStream fis = null;
+    try {
+      fis = new FileInputStream(file);
+      isr = new InputStreamReader(fis, encoding);
+      br = new BufferedReader(isr);
+      String line;
+      while ((line = br.readLine()) != null) {
+        if (line.startsWith("#"))
+          continue;
+        String[] parts = line.split("\t");
+        if (parts.length != 3) {
+          throw new IOException("Unknown format in " +file.getAbsolutePath()+ ": " + line);
+        }
+        if (map.containsKey(parts[0])) {
+          List<LookedUpTerm> l = map.get(parts[0]);
+          l.add(new LookedUpTerm(parts[1], parts[2]));
+          map.put(parts[0], l);
+        } else {
+          List<LookedUpTerm> l = new ArrayList<LookedUpTerm>();
+          l.add(new LookedUpTerm(parts[1], parts[2]));
+          map.put(parts[0], l);
+        }
+      }
+    } finally {
+      if (br != null) br.close();
+      if (isr != null) isr.close();
+      if (fis != null) fis.close();
+    }
+    return map;
+  }
+
+}
+
+class LookedUpTerm {
+  
+  String baseform;
+  String postags;
+  
+  LookedUpTerm(String baseform, String postags) {
+    this.baseform = baseform;
+    this.postags = postags;
+  }
+  
+}
---
+++ trunk/JLanguageTool/src/test/de/danielnaber/languagetool/tagging/ManualTaggerTest.java
@@ -0,0 +1,48 @@
+/* LanguageTool, a natural language style checker 
+ * Copyright (C) 2007 Daniel Naber (http://www.danielnaber.de)
+ * 
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301
+ * USA
+ */
+package de.danielnaber.languagetool.tagging;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Arrays;
+
+import junit.framework.TestCase;
+
+/**
+ * @author Daniel Naber
+ */
+public class ManualTaggerTest extends TestCase {
+
+  private static final String MANUAL_DICT_FILENAME = "resource" + File.separator + "de"
+    + File.separator + "added.txt";
+
+  public void testManualTagger() throws IOException {
+    ManualTagger mt = new ManualTagger(new File(MANUAL_DICT_FILENAME));
+    assertNull(mt.lookup(""));
+    assertNull(mt.lookup("gibtsnicht"));
+    
+    assertEquals("[Trotz, SUB:NOM:SIN:MAS]", Arrays.toString(mt.lookup("Trotz")));
+    // lookup is case sensitive:
+    assertNull(mt.lookup("trotz"));
+
+    assertEquals("[Interesse, SUB:NOM:PLU:NEU, Interesse, SUB:AKK:PLU:NEU, Interesse, SUB:GEN:PLU:NEU]",
+        Arrays.toString(mt.lookup("Interessen")));
+  }
+  
+}
