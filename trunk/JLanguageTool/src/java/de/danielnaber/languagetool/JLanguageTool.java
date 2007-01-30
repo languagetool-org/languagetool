dd2ae7ceeede9e396556f23f966d1caf74a05f68
Daniel Naber <naber@danielnaber.de>
2007-01-30 21:24:05 +0000

    prepare v0.8.8

--- trunk/JLanguageTool/src/java/de/danielnaber/languagetool/JLanguageTool.java
+++ trunk/JLanguageTool/src/java/de/danielnaber/languagetool/JLanguageTool.java
@@ -70,7 +70,7 @@ import de.danielnaber.languagetool.tokenizers.Tokenizer;
  */
 public class JLanguageTool {
 
-  public static final String VERSION = "0.8.8-dev";      // keep in sync with build.xml!
+  public static final String VERSION = "0.8.8";      // keep in sync with build.xml!
 
   public static final String RULES_DIR = "rules";
   public static final String PATTERN_FILE = "grammar.xml";
