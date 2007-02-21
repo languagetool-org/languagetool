177a058cd83c231ceaf0633612c5325b7e5bb074
Andriy Rysin <arysin@gmail.com>
2007-02-21 14:38:42 +0000

    fix Ukrainian rules

--- trunk/JLanguageTool/src/test/de/danielnaber/languagetool/rules/uk/SimpleReplaceRuleTest.java
+++ trunk/JLanguageTool/src/test/de/danielnaber/languagetool/rules/uk/SimpleReplaceRuleTest.java
@@ -8,22 +8,23 @@ import de.danielnaber.languagetool.TestTools;
 import de.danielnaber.languagetool.rules.RuleMatch;
 import junit.framework.TestCase;
 
+
 public class SimpleReplaceRuleTest extends TestCase {
 
-    public void testRule() throws IOException {
-        SimpleReplaceRule rule = new SimpleReplaceRule(TestTools.getEnglishMessages());
+	public void testRule() throws IOException {
+		SimpleReplaceRule rule = new SimpleReplaceRule(TestTools.getEnglishMessages());
 
-        RuleMatch[] matches;
-        JLanguageTool langTool = new JLanguageTool(Language.UKRAINIAN);
-        
-        // correct sentences:
-        matches = rule.match(langTool.getAnalyzedSentence("Ці рядки повинні збігатиѿя."));
-        assertEquals(0, matches.length);
+		RuleMatch[] matches;
+		JLanguageTool langTool = new JLanguageTool(Language.UKRAINIAN);
+		
+		// correct sentences:
+		matches = rule.match(langTool.getAnalyzedSentence("Ці рядки повинні збігатися."));
+		assertEquals(0, matches.length);
 
-        // incorrect sentences:
-        matches = rule.match(langTool.getAnalyzedSentence("Ці рядки повинні ѿпівпадати."));
-        assertEquals(1, matches.length);
-        assertEquals(1, matches[0].getSuggestedReplacements().size());
-        assertEquals("збігатиѿя", matches[0].getSuggestedReplacements().get(0));
-    }
+		// incorrect sentences:
+		matches = rule.match(langTool.getAnalyzedSentence("Ці рядки повинні співпадати."));
+		assertEquals(1, matches.length);
+		assertEquals(1, matches[0].getSuggestedReplacements().size());
+		assertEquals("збігатися", matches[0].getSuggestedReplacements().get(0));
+	}
 }
