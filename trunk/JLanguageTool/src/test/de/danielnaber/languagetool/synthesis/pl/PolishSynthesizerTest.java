7c99959918fe2b05f75cb4831d9918800c883911
Marcin Milkowski <milek_pl@o2.pl>
2007-04-23 16:22:50 +0000

    Polish synthesizer full test

--- trunk/JLanguageTool/src/test/de/danielnaber/languagetool/synthesis/pl/PolishSynthesizerTest.java
+++ trunk/JLanguageTool/src/test/de/danielnaber/languagetool/synthesis/pl/PolishSynthesizerTest.java
@@ -13,13 +13,12 @@ public class PolishSynthesizerTest extends TestCase {
     assertEquals("[Aaru]", Arrays.toString(synth.synthesize("Aar", "subst:sg:gen:m3")));
     assertEquals("[Abchazem]", Arrays.toString(synth.synthesize("Abchaz", "subst:sg:inst:m3")));
     assertEquals("[nieduży]", Arrays.toString(synth.synthesize("duży", "adj:sg:nom:m:neg")));
-//FIXME: lametyzator bug, will be fixed 
-    //assertEquals("[miała]", Arrays.toString(synth.synthesize("mieć", "verb:praet:sg:ter:f:?perf")));    
+    assertEquals("[miała]", Arrays.toString(synth.synthesize("mieć", "verb:praet:sg:ter:f:?perf")));    
     //with regular expressions
     assertEquals("[tonera]", Arrays.toString(synth.synthesize("toner", ".*sg:gen.*", true)));
     assertEquals("[niedużego, niedużemu, nieduży]", Arrays.toString(synth.synthesize("duży", "adj:sg.*m:neg", true)));    
-//FIXME: lametyzator bug, will be fixed    
-//    assertEquals("[miał]", Arrays.toString(synth.synthesize("mieć", ".*praet:sg.*", true)));
+    assertEquals("[miałabym, miałbym, miałabyś, miałbyś, miałaby, miałby, miałoby, miałam, miałem, miałaś, miałeś, miała, miał, miało]", 
+          Arrays.toString(synth.synthesize("mieć", ".*praet:sg.*", true)));
   }
 
 }
