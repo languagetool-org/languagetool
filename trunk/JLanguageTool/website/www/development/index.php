27d066e797813babbf01db46af36f13c80b862ce
Marcin Milkowski <milek_pl@o2.pl>
2007-09-16 18:56:57 +0000

    rewording..

--- trunk/JLanguageTool/website/www/development/index.php
+++ trunk/JLanguageTool/website/www/development/index.php
@@ -169,7 +169,7 @@ etc as an error:</p>
 	applied to avoid false alarms. In some cases, it's usefule to use 
 	<tt>scope="previous"</tt> in rules that already have <tt>skip="-1"</tt>.
 	This way, you can set an exception against a single token that immediately
-	preceeds that token that is matched. For example, we want to match "jak"
+	preceeds the matched token. For example, we want to match "jak"
 	which is not preceeded by a comma</p>:
 	
 	<? hl('<token>tak</token>
