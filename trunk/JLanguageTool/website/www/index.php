07fa433cbb1f69d8ba17ffc7e72d9c60556ab076
Daniel Naber <naber@danielnaber.de>
2007-09-17 20:08:28 +0000

    v 0.9.1

--- trunk/JLanguageTool/website/www/index.php
+++ trunk/JLanguageTool/website/www/index.php
@@ -14,15 +14,12 @@ up <em>there/their</em>, <em>no/now</em> etc. It can also detect some
 grammar mistakes. It does not include spell checking. See the <?=show_link("languages", "languages/", 0) ?> 
 page for a list of supported languages.</p>
 
-<p><strong>Update 2007-05-20:</strong> Released version 0.9:
-Supports easier rule editing, added more rules for French, improved recognition
-of German compound nouns, added XML disambiguation rules, made improvements
-to the rule syntax, added initial support for Slovenian, improved rules
-for Polish, English, and Spanish.
-See the <?=show_link("Changelog", "download/CHANGES.txt", 0) ?> for details.</p>
+<p><strong>Update 2007-09-17:</strong> Released version 0.9.1:
+Works with OpenOffice.org 2.3, improved rules (mostly Polish and English),
+bug fixes. See the <?=show_link("Changelog", "download/CHANGES.txt", 0) ?> for details.</p>
 
 <p><strong>Download:</strong><br />
-<strong><?=show_link("LanguageTool 0.9", "download/LanguageTool-0.9.zip", 0) ?></strong>, 9&nbsp;MB,
+<strong><?=show_link("LanguageTool 0.9.1", "download/LanguageTool-0.9.1.zip", 0) ?></strong>, 9&nbsp;MB,
 requires <?=show_link("Java", "http://www.java.com/en/download/manual.jsp", 1)?>&nbsp;5.0 (also called 1.5) or later<br />
 <!-- 
 <?=show_link("LanguageTool 0.9 with source code", "download/LanguageTool-0.9-src.zip", 0) ?>, 8.5&nbsp;MB<br />
@@ -31,17 +28,17 @@ requires <?=show_link("Java", "http://www.java.com/en/download/manual.jsp", 1)?>
  (includes latest version in CVS)<br />
 </p>
 
-<!-- -->
+<!-- 
 <p><strong>Try LanguageTool via Java WebStart:</strong><br />
 <?=show_link("Start LanguageTool (8&nbsp;MB)", "webstart/LanguageTool.jnlp", 0) ?></p>
-<!-- -->
+-->
     
 <p><strong>Installation and Usage:</strong>
 
 <ul>
 	<li><strong>In OpenOffice.org</strong>:
 	Do <em>not</em> unzip the archive, just call <em>Tools -&gt; Extension Manager -&gt; Add...</em>
-	to install <tt>LanguageTool-0.9.zip</tt> (note that the menu item is called <em>Package Manager</em>
+	to install <tt>LanguageTool-0.9.1.zip</tt> (note that the menu item is called <em>Package Manager</em>
 	in OpenOffice.org 2.0.x). Open a new window of OpenOffice.org (Ctrl-N)
 	and you'll see a new menu entry "LanguageTool" that will check the current text.</li>
 
