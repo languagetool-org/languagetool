46688de85dd69f4340d257190f370defb550aeed
Marcin Milkowski <milek_pl@o2.pl>
2007-09-17 20:41:35 +0000

    set to 0.9.2-dev

--- trunk/JLanguageTool/src/java/de/danielnaber/languagetool/JLanguageTool.java
+++ trunk/JLanguageTool/src/java/de/danielnaber/languagetool/JLanguageTool.java
@@ -63,7 +63,7 @@ import de.danielnaber.languagetool.tools.ReflectionUtils;
  */
 public final class JLanguageTool {
 
-  public static final String VERSION = "0.9.1";      // keep in sync with build.xml!
+  public static final String VERSION = "0.9.2-dev";      // keep in sync with build.xml!
 
   public static final String RULES_DIR = "/rules";
   public static final String PATTERN_FILE = "grammar.xml";
