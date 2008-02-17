5473f5a7a83f6e6dbe8754eb976d0c0dfd87fd2c
Daniel Naber <naber@danielnaber.de>
2008-02-17 19:15:31 +0000

    update for 0.9.2

--- trunk/JLanguageTool/website/www/languages/index.php
+++ trunk/JLanguageTool/website/www/languages/index.php
@@ -14,8 +14,8 @@ language is supported.</p>
 
 <!-- Output of Overview.java: -->
 
-<b>Rules in LanguageTool 0.9.1-dev</b><br />
-Date: 2007-05-24<br /><br />
+<b>Rules in LanguageTool 0.9.3-dev</b><br />
+Date: 2008-02-17<br /><br />
 
 <table>
 <tr>
@@ -23,18 +23,33 @@ Date: 2007-05-24<br /><br />
   <th align="right">XML rules</th>
   <th>&nbsp;&nbsp;</th>
   <th align="right">Java rules</th>
+  <th>&nbsp;&nbsp;</th>
+  <th align="right">False friends<th><td> (<a href="http://languagetool.cvs.sourceforge.net/*checkout*/languagetool/JLanguageTool/src/rules/false-friends.xml">show</a>)</td>
 </tr>
-<tr><td>Czech</td><td align="right">1 (<a href="http://languagetool.cvs.sourceforge.net/*checkout*/languagetool/JLanguageTool/src/rules/cs/grammar.xml">show</a>)</td><td></td><td align="right">0</td></tr>
-<tr><td>Dutch</td><td align="right">128 (<a href="http://languagetool.cvs.sourceforge.net/*checkout*/languagetool/JLanguageTool/src/rules/nl/grammar.xml">show</a>)</td><td></td><td align="right">0</td></tr>
-<tr><td>English</td><td align="right">220 (<a href="http://languagetool.cvs.sourceforge.net/*checkout*/languagetool/JLanguageTool/src/rules/en/grammar.xml">show</a>)</td><td></td><td align="right">1</td></tr>
-<tr><td>French</td><td align="right">53 (<a href="http://languagetool.cvs.sourceforge.net/*checkout*/languagetool/JLanguageTool/src/rules/fr/grammar.xml">show</a>)</td><td></td><td align="right">1</td></tr>
-<tr><td>German</td><td align="right">71 (<a href="http://languagetool.cvs.sourceforge.net/*checkout*/languagetool/JLanguageTool/src/rules/de/grammar.xml">show</a>)</td><td></td><td align="right">7</td></tr>
-<tr><td>Italian</td><td align="right">5 (<a href="http://languagetool.cvs.sourceforge.net/*checkout*/languagetool/JLanguageTool/src/rules/it/grammar.xml">show</a>)</td><td></td><td align="right">0</td></tr>
-<tr><td>Lithuanian</td><td align="right">4 (<a href="http://languagetool.cvs.sourceforge.net/*checkout*/languagetool/JLanguageTool/src/rules/lt/grammar.xml">show</a>)</td><td></td><td align="right">0</td></tr>
-<tr><td>Polish</td><td align="right">569 (<a href="http://languagetool.cvs.sourceforge.net/*checkout*/languagetool/JLanguageTool/src/rules/pl/grammar.xml">show</a>)</td><td></td><td align="right">1</td></tr>
-<tr><td>Slovenian</td><td align="right">5 (<a href="http://languagetool.cvs.sourceforge.net/*checkout*/languagetool/JLanguageTool/src/rules/sl/grammar.xml">show</a>)</td><td></td><td align="right">0</td></tr>
-<tr><td>Spanish</td><td align="right">3 (<a href="http://languagetool.cvs.sourceforge.net/*checkout*/languagetool/JLanguageTool/src/rules/es/grammar.xml">show</a>)</td><td></td><td align="right">0</td></tr>
-<tr><td>Ukrainian</td><td align="right">8 (<a href="http://languagetool.cvs.sourceforge.net/*checkout*/languagetool/JLanguageTool/src/rules/uk/grammar.xml">show</a>)</td><td></td><td align="right">1</td></tr>
+<tr><td>Czech</td><td align="right">1 (<a href="http://languagetool.cvs.sourceforge.net/*checkout*/languagetool/JLanguageTool/src/rules/cs/grammar.xml">show</a>)</td><td></td><td></td><td align="right">0</td><td></td><td></td>
+<td align="left">0</td></tr>
+<tr><td>Dutch</td><td align="right">129 (<a href="http://languagetool.cvs.sourceforge.net/*checkout*/languagetool/JLanguageTool/src/rules/nl/grammar.xml">show</a>)</td><td></td><td></td><td align="right">0</td><td></td><td></td>
+<td align="left">0</td></tr>
+<tr><td>English</td><td align="right">256 (<a href="http://languagetool.cvs.sourceforge.net/*checkout*/languagetool/JLanguageTool/src/rules/en/grammar.xml">show</a>)</td><td></td><td></td><td align="right">1</td><td></td><td></td>
+<td align="left">181</td></tr>
+<tr><td>French</td><td align="right">1669 (<a href="http://languagetool.cvs.sourceforge.net/*checkout*/languagetool/JLanguageTool/src/rules/fr/grammar.xml">show</a>)</td><td></td><td></td><td align="right">1</td><td></td><td></td>
+<td align="left">1</td></tr>
+<tr><td>German</td><td align="right">75 (<a href="http://languagetool.cvs.sourceforge.net/*checkout*/languagetool/JLanguageTool/src/rules/de/grammar.xml">show</a>)</td><td></td><td></td><td align="right">7</td><td></td><td></td>
+<td align="left">84</td></tr>
+<tr><td>Italian</td><td align="right">5 (<a href="http://languagetool.cvs.sourceforge.net/*checkout*/languagetool/JLanguageTool/src/rules/it/grammar.xml">show</a>)</td><td></td><td></td><td align="right">0</td><td></td><td></td>
+<td align="left">0</td></tr>
+<tr><td>Lithuanian</td><td align="right">4 (<a href="http://languagetool.cvs.sourceforge.net/*checkout*/languagetool/JLanguageTool/src/rules/lt/grammar.xml">show</a>)</td><td></td><td></td><td align="right">0</td><td></td><td></td>
+<td align="left">0</td></tr>
+<tr><td>Polish</td><td align="right">690 (<a href="http://languagetool.cvs.sourceforge.net/*checkout*/languagetool/JLanguageTool/src/rules/pl/grammar.xml">show</a>)</td><td></td><td></td><td align="right">1</td><td></td><td></td>
+<td align="left">124</td></tr>
+<tr><td>Slovenian</td><td align="right">5 (<a href="http://languagetool.cvs.sourceforge.net/*checkout*/languagetool/JLanguageTool/src/rules/sl/grammar.xml">show</a>)</td><td></td><td></td><td align="right">0</td><td></td><td></td>
+<td align="left">0</td></tr>
+<tr><td>Spanish</td><td align="right">3 (<a href="http://languagetool.cvs.sourceforge.net/*checkout*/languagetool/JLanguageTool/src/rules/es/grammar.xml">show</a>)</td><td></td><td></td><td align="right">0</td><td></td><td></td>
+<td align="left">1</td></tr>
+<tr><td>Swedish</td><td align="right">4 (<a href="http://languagetool.cvs.sourceforge.net/*checkout*/languagetool/JLanguageTool/src/rules/sv/grammar.xml">show</a>)</td><td></td><td></td><td align="right">0</td><td></td><td></td>
+<td align="left">0</td></tr>
+<tr><td>Ukrainian</td><td align="right">8 (<a href="http://languagetool.cvs.sourceforge.net/*checkout*/languagetool/JLanguageTool/src/rules/uk/grammar.xml">show</a>)</td><td></td><td></td><td align="right">1</td><td></td><td></td>
+<td align="left">0</td></tr>
 </table>
 
 <!-- End Output of Overview.java -->
