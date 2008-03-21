8ecf80681ab8b918ba8d8871ffca00781a83b6c1
Marcin Milkowski <milek_pl@o2.pl>
2008-03-21 19:34:03 +0000

    removing FIXME line - it was a false alarm here

--- trunk/JLanguageTool/src/java/de/danielnaber/languagetool/Main.java
+++ trunk/JLanguageTool/src/java/de/danielnaber/languagetool/Main.java
@@ -165,14 +165,10 @@ class Main {
     if (!apiFormat)
       System.out.println("Working on " + filename + "...");
     String fileContents = StringTools.readFile(new FileInputStream(filename), encoding);
-    //FIXME: this breaks on files with XML escaped with slashes
-    //for example: Select \<emph\>New -> Select \New
-    //proposed fix: add a cmd line arg to turn off the behavior 
     return filterXML(fileContents);
   }
     
-  private String filterXML(String s) {
-    
+  private String filterXML(String s) {    
     Pattern pattern = Pattern.compile("<!--.*?-->", Pattern.DOTALL);
     Matcher matcher = pattern.matcher(s);
     s = matcher.replaceAll(" ");
