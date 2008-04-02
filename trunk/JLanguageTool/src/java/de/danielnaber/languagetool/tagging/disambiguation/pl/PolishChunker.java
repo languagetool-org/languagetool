cb15846b9c360a9bee96b9cbdf158c3eb5c03e0f
Marcin Milkowski <milek_pl@o2.pl>
2008-04-02 17:58:28 +0000

    small update to PL chunker

--- trunk/JLanguageTool/src/java/de/danielnaber/languagetool/tagging/disambiguation/pl/PolishChunker.java
+++ trunk/JLanguageTool/src/java/de/danielnaber/languagetool/tagging/disambiguation/pl/PolishChunker.java
@@ -33,6 +33,7 @@ import de.danielnaber.languagetool.tagging.disambiguation.Disambiguator;
         + "w oparciu o|PREP:ACC\n"
         + "w związku z|PREP:INST\n"
         + "w zgodzie z|PREP:INST\n"
+        + "zgodnie z|PREP:INST\n"
         + "w porównaniu z|PREP:INST\n"
         + "odnośnie do|PREP:GEN\n"
         + "za pomocą|PREP:GEN\n"      
