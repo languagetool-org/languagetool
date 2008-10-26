fd54651d001b93f9a209e561054283154b4962a4
Daniel Naber <naber@danielnaber.de>
2008-10-26 17:26:59 +0000

    native2ascii + better error message if assert fails

--- trunk/JLanguageTool/src/test/de/danielnaber/languagetool/server/HTTPServerTest.java
+++ trunk/JLanguageTool/src/test/de/danielnaber/languagetool/server/HTTPServerTest.java
@@ -44,7 +44,8 @@ public class HTTPServerTest extends TestCase {
       String result = check(Language.GERMAN, "ein kleiner test. Und wieder Erwarten noch was: \u00f6\u00e4\u00fc\u00df.");
       assertTrue(result.indexOf("UPPERCASE_SENTENCE_START") != -1);
       assertTrue(result.indexOf("WIEDER_WILLEN") != -1);
-      assertTrue(result.indexOf("\u00f6\u00e4\u00fc\u00df") != -1);   // special chars are intact
+      assertTrue("Expected special chars, got: '" + result+ "'",
+          result.indexOf("\u00f6\u00e4\u00fc\u00df") != -1);   // special chars are intact
       XMLValidator validator = new XMLValidator();
       validator.validateXMLString(result, "/resource/api-output.dtd", "matches");
       validator.checkSimpleXMLString(result);
