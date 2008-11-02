88677ba3d4ee506d112a50fe6ac6ba03e083a254
Daniel Naber <naber@danielnaber.de>
2008-11-02 23:28:33 +0000

    update version number

--- trunk/JLanguageTool/openoffice/deploy.sh
+++ trunk/JLanguageTool/openoffice/deploy.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
 echo "Deploying LanguageTool to OpenOffice.org..."
-unopkg add dist/LanguageTool-0.9.5-dev.oxt
+unopkg add dist/LanguageTool-0.9.6-dev.oxt
--- trunk/JLanguageTool/openoffice/undeploy.sh
+++ trunk/JLanguageTool/openoffice/undeploy.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
 echo "Undeploying LanguageTool from OpenOffice.org..."
-unopkg remove LanguageTool-0.9.5-dev.oxt
+unopkg remove LanguageTool-0.9.6-dev.oxt
