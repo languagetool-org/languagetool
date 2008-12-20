570dab6c3120a613e825766888167632eb226b68
Daniel Naber <naber@danielnaber.de>
2008-12-20 20:05:57 +0000

    adding license header

--- trunk/JLanguageTool/src/test/de/danielnaber/languagetool/rules/UnpairedQuotesBracketsRuleTest.java
+++ trunk/JLanguageTool/src/test/de/danielnaber/languagetool/rules/UnpairedQuotesBracketsRuleTest.java
@@ -1,3 +1,21 @@
+/* LanguageTool, a natural language style checker 
+ * Copyright (C) 2008 Daniel Naber (http://www.danielnaber.de)
+ * 
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301
+ * USA
+ */
 package de.danielnaber.languagetool.rules;
 
 import java.io.IOException;
@@ -65,7 +83,7 @@ public class UnpairedQuotesBracketsRuleTest extends TestCase {
     // correct sentences:
     matches = rule.match(langTool.getAnalyzedSentence("(To jest zdanie do testowania)."));
     assertEquals(0, matches.length);
-//  correct sentences:
+    // correct sentences:
     matches = rule.match(langTool.getAnalyzedSentence("Piosenka ta trafiła na wiele list \"Best of...\", włączając w to te, które zostały utworzone przez magazyn Rolling Stone."));
     assertEquals(0, matches.length);
     // incorrect sentences:
