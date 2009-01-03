074ccb47a6154188de6327cda5b2596b56badfc5
Marcin Milkowski <milek_pl@o2.pl>
2009-01-03 01:39:27 +0000

    redundant parantheses

--- trunk/JLanguageTool/src/java/de/danielnaber/languagetool/tagging/disambiguation/pl/PolishHybridDisambiguator.java
+++ trunk/JLanguageTool/src/java/de/danielnaber/languagetool/tagging/disambiguation/pl/PolishHybridDisambiguator.java
@@ -26,26 +26,24 @@ import de.danielnaber.languagetool.tagging.disambiguation.Disambiguator;
 import de.danielnaber.languagetool.tagging.disambiguation.rules.pl.PolishRuleDisambiguator;
 
 /**
- * Hybrid chunker-disambiguator for Polish. 
+ * Hybrid chunker-disambiguator for Polish.
  * 
  * @author Marcin Miłkowski
  */
 
 public class PolishHybridDisambiguator implements Disambiguator {
 
-
   private Disambiguator chunker = new PolishChunker();
   private Disambiguator disambiguator = new PolishRuleDisambiguator();
-  
-  /** 
-   * Calls two disambiguator classes: (1) a chunker;
-   * (2) a rule-based disambiguator.
+
+  /**
+   * Calls two disambiguator classes: (1) a chunker; (2) a rule-based
+   * disambiguator.
    */
   @Override
   public final AnalyzedSentence disambiguate(AnalyzedSentence input)
-  throws IOException {
-    return disambiguator.disambiguate
-    ((chunker.disambiguate(input)));
+      throws IOException {
+    return disambiguator.disambiguate(chunker.disambiguate(input));
   }
 
 }
