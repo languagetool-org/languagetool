8c20276bc968f6b8205efc9662acffe2d7fdcd45
Marcin Milkowski <milek_pl@o2.pl>
2009-03-17 21:47:57 +0000

    update French dictionary to real UTF-8

--- trunk/JLanguageTool/src/test/de/danielnaber/languagetool/tagging/disambiguation/rules/fr/FrenchRuleDisambiguatorTest.java
+++ trunk/JLanguageTool/src/test/de/danielnaber/languagetool/tagging/disambiguation/rules/fr/FrenchRuleDisambiguatorTest.java
@@ -48,25 +48,25 @@ public class FrenchRuleDisambiguatorTest extends TestCase {
         "/[null]SENT_START Je/[je]R pers suj 1 s  /[null]null ne/[ne]A  /[null]null suis/[suivre]V etre ind pres 1 s  /[null]null pas/[pas]A  /[null]null la/[le]D f s  /[null]null seule/[seul]J f s ./[null]null", 
         tokenizer, sentenceTokenizer, tagger, disambiguator);
     TestTools.myAssert("Je ne suis pas la seule.",
-        "/[null]SENT_START Je/[je]R pers suj 1 s  /[null]null ne/[ne]A  /[null]null suis/[suivre]V etre ind pres 1 s|suis/[suivre]V imp pres 2 s|suis/[suivre]V ind pres 1 s|suis/[suivre]V ind pres 2 s  /[null]null pas/[pas]A|pas/[pas]N m sp  /[null]null la/[la]N m sp|la/[la]R pers obj 3 f s|la/[le]D f s  /[null]null seule/[seul]D f s|seule/[seul]J f s ./[null]null", 
+        "/[null]SENT_START Je/[je]R pers suj 1 s  /[null]null ne/[ne]A  /[null]null suis/[suivre]V etre ind pres 1 s|suis/[suivre]V imp pres 2 s|suis/[suivre]V ind pres 1 s|suis/[suivre]V ind pres 2 s  /[null]null pas/[pas]A|pas/[pas]N m sp  /[null]null la/[le]D f s|la/[la]N m sp|la/[la]R pers obj 3 f s  /[null]null seule/[seul]D f s|seule/[seul]J f s ./[null]null", 
         tokenizer, sentenceTokenizer, tagger, disamb2); 
     TestTools.myAssert("Il a enfin publié son livre.",
         "/[null]SENT_START Il/[il]R pers suj 3 m s  /[null]null a/[avoir]V avoir ind pres 3 s  /[null]null enfin/[enfin]A  /[null]null publié/[publier]V ppa m s  /[null]null son/[son]D e s  /[null]null livre/[livre]N e s ./[null]null", 
         tokenizer, sentenceTokenizer, tagger, disambiguator);
     TestTools.myAssert("Il a enfin publié son livre.",
-        "/[null]SENT_START Il/[il]R pers suj 3 m s  /[null]null a/[a]N m sp|a/[avoir]V avoir ind pres 3 s  /[null]null enfin/[enfin]A  /[null]null publié/[publier]V ppa m s  /[null]null son/[son]D m s|son/[son]N m s  /[null]null livre/[livre]N e s|livre/[livrer]V imp pres 2 s|livre/[livrer]V ind pres 1 s|livre/[livrer]V ind pres 3 s|livre/[livrer]V sub pres 1 s|livre/[livrer]V sub pres 3 s ./[null]null", 
+        "/[null]SENT_START Il/[il]R pers suj 3 m s  /[null]null a/[a]N m sp|a/[avoir]V avoir ind pres 3 s  /[null]null enfin/[enfin]A  /[null]null publié/[publier]V ppa m s  /[null]null son/[son]D m s|son/[son]N m s  /[null]null livre/[livre]N e s|livre/[livrer]V sub pres 3 s|livre/[livrer]V sub pres 1 s|livre/[livrer]V imp pres 2 s|livre/[livrer]V ind pres 3 s|livre/[livrer]V ind pres 1 s ./[null]null", 
         tokenizer, sentenceTokenizer, tagger, disamb2);
     TestTools.myAssert("Je danse toutes les semaines au club.",
-        "/[null]SENT_START Je/[je]R pers suj 1 s  /[null]null danse/[danser]V ind pres 1 s  /[null]null toutes/[tout]D f p|toutes/[tous]R f p  /[null]null les/[le]D e p  /[null]null semaines/[semaine]N f p  /[null]null au/[au]D m s  /[null]null club/[club]N m s ./[null]null", 
+        "/[null]SENT_START Je/[je]R pers suj 1 s  /[null]null danse/[danser]V ind pres 1 s  /[null]null toutes/[tous]R f p|toutes/[tout]D f p  /[null]null les/[le]D e p  /[null]null semaines/[semaine]N f p  /[null]null au/[au]D m s  /[null]null club/[club]N m s ./[null]null", 
         tokenizer, sentenceTokenizer, tagger, disambiguator);
     TestTools.myAssert("Je danse toutes les semaines au club.",
-        "/[null]SENT_START Je/[je]R pers suj 1 s  /[null]null danse/[danse]N f s|danse/[danser]V imp pres 2 s|danse/[danser]V ind pres 1 s|danse/[danser]V ind pres 3 s|danse/[danser]V sub pres 1 s|danse/[danser]V sub pres 3 s  /[null]null toutes/[tout]D f p|toutes/[tous]R f p  /[null]null les/[les]R pers obj 3 p|les/[le]D e p  /[null]null semaines/[semaine]N f p  /[null]null au/[au]D m s  /[null]null club/[club]N m s ./[null]null", 
+        "/[null]SENT_START Je/[je]R pers suj 1 s  /[null]null danse/[danser]V sub pres 3 s|danse/[danser]V sub pres 1 s|danse/[danser]V imp pres 2 s|danse/[danser]V ind pres 3 s|danse/[danser]V ind pres 1 s|danse/[danse]N f s  /[null]null toutes/[tous]R f p|toutes/[tout]D f p  /[null]null les/[les]R pers obj 3 p|les/[le]D e p  /[null]null semaines/[semaine]N f p  /[null]null au/[au]D m s  /[null]null club/[club]N m s ./[null]null", 
         tokenizer, sentenceTokenizer, tagger, disamb2);
     TestTools.myAssert("Quand j'étais petit, je jouais au football.",
         "/[null]SENT_START Quand/[quand]C sub  /[null]null j/[je]R pers suj 1 s '/[null]null étais/[être]V etre ind impa 1 s  /[null]null petit/[petit]J m s ,/[null]null  /[null]null je/[je]R pers suj 1 s  /[null]null jouais/[jouer]V ind impa 1 s  /[null]null au/[au]D m s  /[null]null football/[football]N m s ./[null]null", 
         tokenizer, sentenceTokenizer, tagger, disambiguator);
     TestTools.myAssert("Quand j'étais petit, je jouais au football.",
-        "/[null]SENT_START Quand/[quand]A|Quand/[quand]C sub  /[null]null j/[j]N m sp|j/[je]R pers suj 1 s '/[null]null étais/[étai]N m p|étais/[être]V etre ind impa 1 s|étais/[être]V etre ind impa 2 s  /[null]null petit/[petit]A|petit/[petit]J m s|petit/[petit]N m s ,/[null]null  /[null]null je/[je]R pers suj 1 s  /[null]null jouais/[jouer]V ind impa 1 s|jouais/[jouer]V ind impa 2 s  /[null]null au/[au]D m s  /[null]null football/[football]N m s ./[null]null", 
+        "/[null]SENT_START Quand/[quand]A|Quand/[quand]C sub  /[null]null j/[je]R pers suj 1 s|j/[j]N m sp '/[null]null étais/[étai]N m p|étais/[être]V etre ind impa 1 s|étais/[être]V etre ind impa 2 s  /[null]null petit/[petit]A|petit/[petit]J m s|petit/[petit]N m s ,/[null]null  /[null]null je/[je]R pers suj 1 s  /[null]null jouais/[jouer]V ind impa 1 s|jouais/[jouer]V ind impa 2 s  /[null]null au/[au]D m s  /[null]null football/[football]N m s ./[null]null", 
         tokenizer, sentenceTokenizer, tagger, disamb2);
     TestTools.myAssert("Je suis petite.",
         "/[null]SENT_START Je/[je]R pers suj 1 s  /[null]null suis/[suivre]V etre ind pres 1 s  /[null]null petite/[petit]J f s ./[null]null", 
