9c9a9b96b508e71fc2e78a745f4eda77cd7d59f0
Marcin Milkowski <milek_pl@o2.pl>
2009-05-02 18:37:06 +0000

    remove unused import

--- trunk/JLanguageTool/src/java/de/danielnaber/languagetool/tokenizers/SRXSentenceTokenizer.java
+++ trunk/JLanguageTool/src/java/de/danielnaber/languagetool/tokenizers/SRXSentenceTokenizer.java
@@ -21,7 +21,6 @@ package de.danielnaber.languagetool.tokenizers;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.io.UnsupportedEncodingException;
 import java.util.ArrayList;
 import java.util.List;
 
