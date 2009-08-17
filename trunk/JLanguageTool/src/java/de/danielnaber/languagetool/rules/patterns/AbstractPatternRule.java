51d272f3d2d7355b020f60825d5a954bc521cbfe
Marcin Milkowski <milek_pl@o2.pl>
2009-08-17 17:57:15 +0000

    add simple short-circuiting to speed up a little, mostly to show how this should work in principle (we don't need to test the same lemma again with the same element if we know it didn't match, we don't need to test the same POS) -- more info should be available as a method of AnalyzedTokenReadings for preprocessing

--- trunk/JLanguageTool/src/java/de/danielnaber/languagetool/rules/patterns/AbstractPatternRule.java
+++ trunk/JLanguageTool/src/java/de/danielnaber/languagetool/rules/patterns/AbstractPatternRule.java
@@ -173,6 +173,10 @@ public abstract class AbstractPatternRule extends Rule {
         return false;
       }
       thisMatched = thisMatched || elem.isMatched(matchToken);
+      if (!thisMatched && 
+          l > 0 && !elem.isInflected() && elem.getPOStag() == null) {
+        return false; // the token is the same, we will not get a match
+      }
       if (groupsOrUnification) {
         thisMatched &= testUnificationAndGroups(thisMatched,
             l + 1 == numberOfReadings, matchToken, elem);
