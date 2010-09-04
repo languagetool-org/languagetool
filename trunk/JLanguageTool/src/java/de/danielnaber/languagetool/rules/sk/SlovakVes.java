b709ee8f8c011952fc823460f0275f9fe191d496
Daniel Naber <naber@danielnaber.de>
2010-09-04 17:33:16 +0000

    re-indent only

--- trunk/JLanguageTool/src/java/de/danielnaber/languagetool/rules/sk/SlovakVes.java
+++ trunk/JLanguageTool/src/java/de/danielnaber/languagetool/rules/sk/SlovakVes.java
@@ -30,117 +30,117 @@ import de.danielnaber.languagetool.rules.Category;
 import de.danielnaber.languagetool.rules.RuleMatch;
 
 
-public class SlovakVes extends SlovakRule { 
- 
-    public SlovakVes(final ResourceBundle messages) {
-        if (messages != null) {
-            super.setCategory(new Category(messages.getString("category_misc")));
-        }
-        setDefaultOff();
-    }
+public class SlovakVes extends SlovakRule {
 
-    @Override
-    public final String getId() {
-        return "SK_VES";
+  public SlovakVes(final ResourceBundle messages) {
+    if (messages != null) {
+      super.setCategory(new Category(messages.getString("category_misc")));
     }
+    setDefaultOff();
+  }
 
-    @Override
-    public final String getDescription() {
-        return "Názvy obcí, v ktorých je \"Ves\"";
-    }
+  @Override
+  public final String getId() {
+    return "SK_VES";
+  }
 
-    @Override
-    public final RuleMatch[] match(final AnalyzedSentence text) {
-        final List<RuleMatch> ruleMatches = new ArrayList<RuleMatch>();
-        final AnalyzedTokenReadings[] tokens = text.getTokensWithoutWhitespace();
-     // never read        boolean prve_uvodzovky;
-        boolean tag, tag2, tag3;
-        final String pomoc;
-        final char znak;
+  @Override
+  public final String getDescription() {
+    return "Názvy obcí, v ktorých je \"Ves\"";
+  }
+
+  @Override
+  public final RuleMatch[] match(final AnalyzedSentence text) {
+    final List<RuleMatch> ruleMatches = new ArrayList<RuleMatch>();
+    final AnalyzedTokenReadings[] tokens = text.getTokensWithoutWhitespace();
+    // never read        boolean prve_uvodzovky;
+    boolean tag, tag2, tag3;
+    final String pomoc;
+    final char znak;
 
 // never read         prve_uvodzovky = false;
-        tag = false;
-        tag2 = false;
-        tag3 = false;
+    tag = false;
+    tag2 = false;
+    tag3 = false;
 
-        pomoc = tokens[1].getToken();
-        if (pomoc.length() >= 1) {
-            znak = pomoc.charAt(0);
-        } else {
-            znak = '.';
-        }
+    pomoc = tokens[1].getToken();
+    if (pomoc.length() >= 1) {
+      znak = pomoc.charAt(0);
+    } else {
+      znak = '.';
+    }
 
-        if (znak == '?') {
+    if (znak == '?') {
 // never read  prve_uvodzovky = true;
-        }
-        for (int i = 1; i < tokens.length; i++) {
-            final String token = tokens[i].getToken();
+    }
+    for (int i = 1; i < tokens.length; i++) {
+      final String token = tokens[i].getToken();
 // never read           String premenna = token.toString();
-            final char pomocnik;
+      final char pomocnik;
 // never read           final int help; 
-            boolean bodka;
-            boolean pady;
+      boolean bodka;
+      boolean pady;
 
-            pady = false;
-            pomocnik = token.charAt(0);
-            bodka = false;
-            if (token.charAt(0) == '.' || token.charAt(0) == '?'
-                    || token.charAt(0) == '!') {
-                bodka = true;
-            }
+      pady = false;
+      pomocnik = token.charAt(0);
+      bodka = false;
+      if (token.charAt(0) == '.' || token.charAt(0) == '?'
+              || token.charAt(0) == '!') {
+        bodka = true;
+      }
 
-            if (tokens[i].hasPosTag("AAfs1x") || tokens[i].hasPosTag("AAfs2x")
-                    || tokens[i].hasPosTag("AAfs3x")
-                    || tokens[i].hasPosTag("AAfs4x")
-                    || tokens[i].hasPosTag("AAfs6x")
-                    || tokens[i].hasPosTag("AAfs7x")) {
-                pady = true;
-            }
-            if (pady && Character.isUpperCase(pomocnik)) {
-                tag = true;
-            }
+      if (tokens[i].hasPosTag("AAfs1x") || tokens[i].hasPosTag("AAfs2x")
+              || tokens[i].hasPosTag("AAfs3x")
+              || tokens[i].hasPosTag("AAfs4x")
+              || tokens[i].hasPosTag("AAfs6x")
+              || tokens[i].hasPosTag("AAfs7x")) {
+        pady = true;
+      }
+      if (pady && Character.isUpperCase(pomocnik)) {
+        tag = true;
+      }
 
-            if (tag && (tag2 == false)) {
-                if (pady && Character.isLowerCase(pomocnik)) {
-                    tag2 = true;
- //                   premenna = tokens[i].getToken();
-                }
-
-            }
-
-            if (tag2) { 
-                if (token.equals("Ves") || token.equals("Vsi")
-                        || token.equals("Vsou")) {
-                    tag3 = true; 
-                }
-            }
-            if (tag3 && !bodka) {
-                String spravne; 
-                char prve;
-
-                prve = tokens[i - 1].getToken().charAt(0);
-                prve = Character.toUpperCase(prve);
-                spravne = tokens[i - 1].getToken().substring(1,
-                        tokens[i - 1].getToken().length()); 
-
-                final String msg = "Zmeňte začiatočné písmeno na veľké: <suggestion> "
-                        + prve + spravne + " </suggestion>";
-                final int pos = tokens[i - 1].getStartPos();
-                final int pos2 = tokens[i - 1].getToken().length();
-                final RuleMatch ruleMatch = new RuleMatch(this, pos, pos + pos2,
-                        msg, "Zmeňte začiatočné písmeno na veľké: ");
-
-                ruleMatches.add(ruleMatch);
-
-            }
-
+      if (tag && (tag2 == false)) {
+        if (pady && Character.isLowerCase(pomocnik)) {
+          tag2 = true;
+          //                   premenna = tokens[i].getToken();
         }
-        return toRuleMatchArray(ruleMatches);
-    }
 
-    @Override
-    public void reset() {// nothing
+      }
+
+      if (tag2) {
+        if (token.equals("Ves") || token.equals("Vsi")
+                || token.equals("Vsou")) {
+          tag3 = true;
+        }
+      }
+      if (tag3 && !bodka) {
+        String spravne;
+        char prve;
+
+        prve = tokens[i - 1].getToken().charAt(0);
+        prve = Character.toUpperCase(prve);
+        spravne = tokens[i - 1].getToken().substring(1,
+                tokens[i - 1].getToken().length());
+
+        final String msg = "Zmeňte začiatočné písmeno na veľké: <suggestion> "
+                + prve + spravne + " </suggestion>";
+        final int pos = tokens[i - 1].getStartPos();
+        final int pos2 = tokens[i - 1].getToken().length();
+        final RuleMatch ruleMatch = new RuleMatch(this, pos, pos + pos2,
+                msg, "Zmeňte začiatočné písmeno na veľké: ");
+
+        ruleMatches.add(ruleMatch);
+
+      }
+
     }
+    return toRuleMatchArray(ruleMatches);
+  }
+
+  @Override
+  public void reset() {// nothing
+  }
 
 }
 
