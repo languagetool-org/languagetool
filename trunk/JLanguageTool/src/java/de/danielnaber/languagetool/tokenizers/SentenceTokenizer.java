d277e298ed1eec999edadc2f89d9b4d5abdc73c9
Daniel Naber <naber@danielnaber.de>
2010-09-25 16:49:26 +0000

    don't make abbreviations static - this caused all abbreviations of all languages to appear for any language

--- trunk/JLanguageTool/src/java/de/danielnaber/languagetool/tokenizers/SentenceTokenizer.java
+++ trunk/JLanguageTool/src/java/de/danielnaber/languagetool/tokenizers/SentenceTokenizer.java
@@ -83,7 +83,7 @@ public class SentenceTokenizer implements Tokenizer {
       "cf", "Inc", "Ms", "Gen", "Sen", "Prof", "Corp", "Co"
   };
 
-  private static Set<String> abbreviations = new HashSet<String>();  
+  private Set<String> abbreviations = new HashSet<String>();  
 
   /**
    * Month names like "Dezember" that should not be considered a sentence
