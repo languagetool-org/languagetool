92ce20da2f3e69da114bbf458935cbf378a7bb16
Marcin Milkowski <milek_pl@o2.pl>
2010-10-01 17:28:00 +0000

    fix classpath glitch

--- trunk/JLanguageTool/src/scripts/testrules.sh
+++ trunk/JLanguageTool/src/scripts/testrules.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
 java -cp junit.jar:LanguageTool.jar de.danielnaber.languagetool.rules.patterns.PatternRuleTest
-java -cp junit.jar;LanguageTool.jar de.danielnaber.languagetool.tagging.disambiguation.rules.DisambiguationRuleTest
+java -cp junit.jar:LanguageTool.jar de.danielnaber.languagetool.tagging.disambiguation.rules.DisambiguationRuleTest
 java -cp junit.jar:LanguageTool.jar de.danielnaber.languagetool.ValidateXMLTest
