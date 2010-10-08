ac904853286b9721638a837b0f93730c8fd289c5
Daniel Naber <naber@danielnaber.de>
2010-10-08 21:56:53 +0000

    initialize constants only once

--- trunk/JLanguageTool/src/java/de/danielnaber/languagetool/tagging/eo/EsperantoTagger.java
+++ trunk/JLanguageTool/src/java/de/danielnaber/languagetool/tagging/eo/EsperantoTagger.java
@@ -24,7 +24,6 @@ package de.danielnaber.languagetool.tagging.eo;
 
 import java.util.Arrays;
 import java.util.ArrayList;
-import java.util.Iterator;
 import java.util.List;
 import java.util.HashSet;
 import java.util.Set;
@@ -37,222 +36,229 @@ import de.danielnaber.languagetool.AnalyzedTokenReadings;
 import de.danielnaber.languagetool.tagging.Tagger;
 
 /**
- * A tagger for Esperanto.
+ * A part-of-speech tagger for Esperanto.
  *
  * @author Dominique Pellé
  */
 public class EsperantoTagger implements Tagger {
 
+  // These words don't need to be tagged.
+  private final static String wordsNotTagged[] = {
+    "ne", "nek", "pli", "malpli", "ol", "ju", "des"
+  };
+
+  private final static Set setWordsNotTagged = new HashSet<String>(Arrays.asList(wordsNotTagged));
+
+  // Following preposition are never followed by accusative.
+  private final static String prepositionsNoAccusative[] = {
+      "al", "anstataŭ", "apud", "cis", "da", "de", "dum", "el", "far",
+      "ĝis", "je", "kontraŭ", "krom", "kun", "laŭ", "malgraŭ", "na",
+      "per", "po", "post", "por", "pri", "pro", "sen", "super", "tra"
+  };
+
+  private final static Set setPrepositionsNoAccusative =
+    new HashSet<String>(Arrays.asList(prepositionsNoAccusative));
+
+  // Following preposition may be followed by accusative.
+  private final static String prepositionsAccusative[] = {
+    "en", "sur", "sub", "trans", "preter", "ĉirkaŭ", "antaŭ", "ekster",
+    "inter", "ĉe"
+  };
+
+  private final Set setPrepositionsAccusative =
+    new HashSet<String>(Arrays.asList(prepositionsAccusative));
+
+  // Conjunctions.
+  private final static String conjunctions[] = {
+    "ĉar", "kaj", "aŭ", "sed", "plus", "minus", "tamen"
+  };
+
+  private final static Set setConjunctions = new HashSet<String>(Arrays.asList(conjunctions));
+
+  // Numbers.
+  private final static String numbers[] = {
+    "nul", "unu", "du", "tri", "kvar", "kvin", "ses",
+    "sep", "ok", "naŭ", "dek", "cent", "mil"
+  };
+
+  private final static Set setNumbers = new HashSet<String>(Arrays.asList(numbers));
+
+  // Adverbs which do not end in -e
+  private final static String adverbs[] = {
+    "ankoraŭ", "almenaŭ", "apenaŭ", "baldaŭ", "preskaŭ", "eĉ",
+    "jam", "jen", "ĵus", "morgaŭ", "hodiaŭ", "hieraŭ", "nun",
+    "nur", "plu", "tre", "tro", "tuj", "for"
+  };
+
+  private final static Set setAdverbs = new HashSet<String>(Arrays.asList(adverbs));
+
+  // Stem of transitive and intransitive verbs.  A few verbs can be both
+  // transitive and intransitive. Such verbs may appear in both ntrVerb[]
+  // and trVerbs[].
+
+  // Intransitive verbs (TODO: this should be stored in a dictionary).
+  // No need to list verbs with suffix iĝ suffix since those are
+  // always intransitive.
+  private final static String ntrVerbs[] = {
+    "abort",
+    "abstin",
+    "ag",
+    "agoni",
+    "aĝi",
+    "akord",
+    "altern",
+    "aparten",
+    "apelaci",
+    "aper",
+    "aspekt",
+    "at",
+    "atut",
+    "aŭdac",
+    "aviad",
+    "balot",
+    "bankrot",
+    "barakt",
+    "batal",
+    "bicikl",
+    "blasfem",
+    "blek",
+    "blov",
+    "boj",
+    "boks",
+    "bol",
+    "bril",
+    "brokant",
+    "bru",
+    "brul",
+    "cirkul",
+    "daŭr",
+    "degel",
+    "dron",
+    "eksplod",
+    "est",
+    "evolu",
+    "fal",
+    "grimp",
+    "halt",
+    "ir",
+    "koler",
+    "kresk",
+    "krev",
+    "labor",
+    "mir",
+    "odor",
+    "okaz",
+    "parol",
+    "pend",
+    "porol",
+    "rid",
+    "sid",
+    "star",
+    "velk",
+    "ven",
+    "vetur",
+    "zorg",
+    "ĉes",
+    "ĝoj",
+    "ŝpruc",
+    "ŝrump",
+    "ŝvel",
+  };
+
+  private final static Set setNtrVerbs = new HashSet<String>(Arrays.asList(ntrVerbs));
+
+  // Verbs always end with this pattern.
+  private final static Pattern patternVerb1 = Pattern.compile("(.*)(as|os|is|us|u|i)$");
+  private final static Pattern patternVerb2 = Pattern.compile(".*(ig|iĝ)(.s|.)$");
+
+  // Transitive verbs (TODO: this should be stored in a dictionary).
+  // No need to list verbs with suffix ig suffix since those are
+  // always transitive.
+  private final static String trVerbs[] = {
+    "balanc",
+    "ban",
+    "bonven",
+    "bukl",
+    "dezir",
+    "difin",
+    "dolor",
+    "duŝ",
+    "enhav",
+    "etend",
+    "far",
+    "fend",
+    "ferm",
+    "fin",
+    "fleks",
+    "forges",
+    "hav",
+    "interes",
+    "kaŝ",
+    "kirl",
+    "klin",
+    "kolekt",
+    "komenc",
+    "komplik",
+    "komunik",
+    "konduk",
+    "korekt",
+    "lav",
+    "lig",
+    "lud",
+    "manĝ",
+    "memor",
+    "mov",
+    "mezur",
+    "miks",
+    "mov",
+    "nask",
+    "naŭz",
+    "pag",
+    "parol",
+    "paŝt",
+    "pes",
+    "perd",
+    "profit",
+    "renkont",
+    "renvers",
+    "romp",
+    "rul",
+    "sci",
+    "send",
+    "sent",
+    "sku",
+    "spekt",
+    "streĉ",
+    "sufok",
+    "sving",
+    "ted",
+    "tim",
+    "tir",
+    "tord",
+    "tranĉ",
+    "translok",
+    "tren",
+    "turn",
+    "uz",
+    "vek",
+    "vend",
+    "venĝ",
+    "verŝ",
+    "vest",
+    "vid",
+    "vind",
+    "vol",
+    "volv",
+    "ŝancel",
+    "ŝanĝ",
+    "ŝlos",
+    "ŝir",
+    "ŝut",
+  };
+
+  private final static Set setTrVerbs = new HashSet<String>(Arrays.asList(trVerbs));
+
   public List<AnalyzedTokenReadings> tag(final List<String> sentenceTokens) {
-
-    // These words don't need to be tagged.
-    final String wordsNotTagged[] = { 
-      "ne", "nek", "pli", "malpli", "ol", "ju", "des"
-    };
-    final Set setWordsNotTagged = 
-      new HashSet<String>(Arrays.asList(wordsNotTagged));
-
-    // Following preposition are never followed by accusative.
-    final String prepositionsNoAccusative[] = {
-        "al", "anstataŭ", "apud", "cis", "da", "de", "dum", "el", "far", 
-        "ĝis", "je", "kontraŭ", "krom", "kun", "laŭ", "malgraŭ", "na", 
-        "per", "po", "post", "por", "pri", "pro", "sen", "super", "tra"
-    };
-    final Set setPrepositionsNoAccusative = 
-      new HashSet<String>(Arrays.asList(prepositionsNoAccusative));
-
-    // Following preposition may be followed by accusative.
-    final String prepositionsAccusative[] = { 
-      "en", "sur", "sub", "trans", "preter", "ĉirkaŭ", "antaŭ", "ekster",
-      "inter", "ĉe"
-    };
-    final Set setPrepositionsAccusative = 
-      new HashSet<String>(Arrays.asList(prepositionsAccusative));
-
-    // Conjunctions.
-    final String conjunctions[] = { 
-      "ĉar", "kaj", "aŭ", "sed", "plus", "minus", "tamen"
-    };
-    final Set setConjunctions = new HashSet<String>(Arrays.asList(conjunctions));
-
-    // Numbers.
-    final String numbers[] = { 
-      "nul", "unu", "du", "tri", "kvar", "kvin", "ses", 
-      "sep", "ok", "naŭ", "dek", "cent", "mil"
-    };
-    final Set setNumbers = new HashSet<String>(Arrays.asList(numbers));
-
-    // Adverbs which do not end in -e
-    final String adverbs[] = {
-      "ankoraŭ", "almenaŭ", "apenaŭ", "baldaŭ", "preskaŭ", "eĉ",
-      "jam", "jen", "ĵus", "morgaŭ", "hodiaŭ", "hieraŭ", "nun", 
-      "nur", "plu", "tre", "tro", "tuj", "for"
-    };
-    final Set setAdverbs = new HashSet<String>(Arrays.asList(adverbs));
-
-    // Verbs always end with this pattern.
-    final Pattern patternVerb1 = Pattern.compile("(.*)(as|os|is|us|u|i)$");
-    final Pattern patternVerb2 = Pattern.compile(".*(ig|iĝ)(.s|.)$");
-
-    // Stem of transitive and intransitive verbs.  A few verbs can be both 
-    // transitive and intransitive. Such verbs may appear in both ntrVerb[]
-    // and trVerbs[].
-
-    // Intransitive verbs (TODO: this should be stored in a dictionary).
-    // No need to list verbs with suffix iĝ suffix since those are 
-    // always intransitive.  
-    final String ntrVerbs[] = {
-      "abort", 
-      "abstin", 
-      "ag", 
-      "agoni", 
-      "aĝi", 
-      "akord", 
-      "altern", 
-      "aparten", 
-      "apelaci", 
-      "aper", 
-      "aspekt", 
-      "at", 
-      "atut", 
-      "aŭdac", 
-      "aviad", 
-      "balot", 
-      "bankrot", 
-      "barakt", 
-      "batal", 
-      "bicikl", 
-      "blasfem", 
-      "blek", 
-      "blov", 
-      "boj", 
-      "boks", 
-      "bol", 
-      "bril",
-      "brokant",
-      "bru",
-      "brul",
-      "cirkul",
-      "daŭr",
-      "degel",
-      "dron",
-      "eksplod",
-      "est",
-      "evolu", 
-      "fal", 
-      "grimp",
-      "halt",
-      "ir",
-      "koler", 
-      "kresk", 
-      "krev",
-      "labor", 
-      "mir", 
-      "odor",
-      "okaz",
-      "parol",
-      "pend",
-      "porol", 
-      "rid", 
-      "sid",
-      "star",
-      "velk", 
-      "ven", 
-      "vetur",
-      "zorg",
-      "ĉes",
-      "ĝoj",
-      "ŝpruc",
-      "ŝrump",
-      "ŝvel",
-    };
-    final Set setNtrVerbs = new HashSet<String>(Arrays.asList(ntrVerbs));
- 
-    // Transitive verbs (TODO: this should be stored in a dictionary).
-    // No need to list verbs with suffix ig suffix since those are 
-    // always transitive.
-    final String trVerbs[] = { 
-      "balanc", 
-      "ban",
-      "bonven",
-      "bukl", 
-      "dezir",
-      "difin",
-      "dolor",
-      "duŝ", 
-      "enhav",
-      "etend", 
-      "far",
-      "fend", 
-      "ferm", 
-      "fin",
-      "fleks", 
-      "forges",
-      "hav",
-      "interes", 
-      "kaŝ",
-      "kirl",
-      "klin", 
-      "kolekt", 
-      "komenc", 
-      "komplik", 
-      "komunik",
-      "konduk", 
-      "korekt", 
-      "lav", 
-      "lig",
-      "lud",
-      "manĝ",
-      "memor", 
-      "mov",
-      "mezur",
-      "miks",
-      "mov",
-      "nask", 
-      "naŭz", 
-      "pag",
-      "parol",
-      "paŝt",
-      "pes",
-      "perd", 
-      "profit",
-      "renkont", 
-      "renvers",
-      "romp",
-      "rul",
-      "sci",
-      "send",
-      "sent",
-      "sku",
-      "spekt",
-      "streĉ",
-      "sufok",
-      "sving",
-      "ted",
-      "tim", 
-      "tir", 
-      "tord", 
-      "tranĉ", 
-      "translok", 
-      "tren",
-      "turn",
-      "uz",
-      "vek",
-      "vend",
-      "venĝ",
-      "verŝ",
-      "vest",
-      "vid",
-      "vind",
-      "vol",
-      "volv",
-      "ŝancel",
-      "ŝanĝ",
-      "ŝlos",
-      "ŝir",
-      "ŝut",
-    };
-    final Set setTrVerbs = new HashSet<String>(Arrays.asList(trVerbs));
 
     // Particips -ant-, -int, ont-, -it-, -it-, -ot-
     // TODO: this is not used yet.
@@ -273,67 +279,67 @@ public class EsperantoTagger implements Tagger {
     int pos = 0;
     for (String word : sentenceTokens) {
       final List<AnalyzedToken> l = new ArrayList<AnalyzedToken>();
-      final String lword = word.toLowerCase();
+      final String lWord = word.toLowerCase();
 
-      if (lword.equals(".")) {
+      if (lWord.equals(".")) {
         l.add(new AnalyzedToken(word, "M fino", null));
 
-      } else if (lword.equals("?")) {
+      } else if (lWord.equals("?")) {
         l.add(new AnalyzedToken(word, "M fino dem", null));
 
-      } else if (lword.equals("!")) {
+      } else if (lWord.equals("!")) {
         l.add(new AnalyzedToken(word, "M fino kri", null));
 
-      } else if (lword.equals("la")) {
+      } else if (lWord.equals("la")) {
         l.add(new AnalyzedToken(word, "D", null));
 
 
-      } else if (setAdverbs.contains(lword)) {
+      } else if (setAdverbs.contains(lWord)) {
         l.add(new AnalyzedToken(word, "E nak", null));
 
-      } else if (setWordsNotTagged.contains(lword)) {
+      } else if (setWordsNotTagged.contains(lWord)) {
         l.add(new AnalyzedToken(word, null, null));
 
       // Pronouns.
-      } else if (lword.equals("mi") || lword.equals("ci") 
-              || lword.equals("li")
-              || lword.equals("ŝi") || lword.equals("oni")) {
+      } else if (lWord.equals("mi") || lWord.equals("ci")
+              || lWord.equals("li")
+              || lWord.equals("ŝi") || lWord.equals("oni")) {
         l.add(new AnalyzedToken(word, "R nak np", null));
-      } else if (lword.equals("min") || lword.equals("cin")
-             ||  lword.equals("lin") || lword.equals("ŝin")) {
+      } else if (lWord.equals("min") || lWord.equals("cin")
+             ||  lWord.equals("lin") || lWord.equals("ŝin")) {
         l.add(new AnalyzedToken(word, "R akz np", null));
-      } else if (lword.equals("ni") || lword.equals("ili")) {
+      } else if (lWord.equals("ni") || lWord.equals("ili")) {
         l.add(new AnalyzedToken(word, "R nak pl", null));
-      } else if (lword.equals("nin") || lword.equals("ilin")) {
+      } else if (lWord.equals("nin") || lWord.equals("ilin")) {
         l.add(new AnalyzedToken(word, "R akz pl", null));
-      } else if (lword.equals("vi")) {
+      } else if (lWord.equals("vi")) {
         l.add(new AnalyzedToken(word, "R nak pn", null));
-      } else if (lword.equals("vin")) {
+      } else if (lWord.equals("vin")) {
         l.add(new AnalyzedToken(word, "R akz pn", null));
 
       // Conjunctions (kaj, sed, ...)
-      } else if (setConjunctions.contains(lword)) {
+      } else if (setConjunctions.contains(lWord)) {
         l.add(new AnalyzedToken(word, "K", null));
   
       // Prepositions.
-      } else if (setPrepositionsNoAccusative.contains(lword)) {
+      } else if (setPrepositionsNoAccusative.contains(lWord)) {
         l.add(new AnalyzedToken(word, "P sak", null));
-      } else if (setPrepositionsAccusative.contains(lword)) {
+      } else if (setPrepositionsAccusative.contains(lWord)) {
         l.add(new AnalyzedToken(word, "P kak", null));
 
-      } else if (setNumbers.contains(lword)) {
+      } else if (setNumbers.contains(lWord)) {
         l.add(new AnalyzedToken(word, "B", null));
 
       // Tiu, kiu (tabelvortoj).
-      } else if ((matcher = patternTabelvorto.matcher(lword)).find()) {
-        String type1Group = matcher.group(1).substring(0, 1).toLowerCase();
-        String type2Group = matcher.group(4);
-        String plGroup    = matcher.group(5);
-        String accGroup   = matcher.group(6);
-        String type3Group = matcher.group(7);
-        String type;
-        String plural;
-        String accusative;
+      } else if ((matcher = patternTabelvorto.matcher(lWord)).find()) {
+        final String type1Group = matcher.group(1).substring(0, 1).toLowerCase();
+        final String type2Group = matcher.group(4);
+        final String plGroup    = matcher.group(5);
+        final String accGroup   = matcher.group(6);
+        final String type3Group = matcher.group(7);
+        final String type;
+        final String plural;
+        final String accusative;
 
         if (accGroup == null) {
           accusative = "xxx";
@@ -351,43 +357,43 @@ public class EsperantoTagger implements Tagger {
           accusative + plural + type1Group + " " + type, null));
 
       // Words ending in .*oj?n? are nouns.
-      } else if (lword.endsWith("o")) {
+      } else if (lWord.endsWith("o")) {
         l.add(new AnalyzedToken(word, "O nak np", null));
-      } else if (lword.endsWith("oj")) {       
+      } else if (lWord.endsWith("oj")) {
         l.add(new AnalyzedToken(word, "O nak pl", null));
-      } else if (lword.endsWith("on")) {       
+      } else if (lWord.endsWith("on")) {
         l.add(new AnalyzedToken(word, "O akz np", null));
-      } else if (lword.endsWith("ojn")) {      
+      } else if (lWord.endsWith("ojn")) {
         l.add(new AnalyzedToken(word, "O akz pl", null));
 
       // Words ending in .*aj?n? are nouns.
-      } else if (lword.endsWith("a")) {
+      } else if (lWord.endsWith("a")) {
         l.add(new AnalyzedToken(word, "A nak np", null));
-      } else if (lword.endsWith("aj")) {    
+      } else if (lWord.endsWith("aj")) {
         l.add(new AnalyzedToken(word, "A nak pl", null));
-      } else if (lword.endsWith("an")) {    
+      } else if (lWord.endsWith("an")) {
         l.add(new AnalyzedToken(word, "A akz np", null));
-      } else if (lword.endsWith("ajn")) {
+      } else if (lWord.endsWith("ajn")) {
         l.add(new AnalyzedToken(word, "A akz pl", null));
 
       // Words ending in .*en? are adverbs.
-      } else if (lword.endsWith("e")) {
+      } else if (lWord.endsWith("e")) {
         l.add(new AnalyzedToken(word, "E nak", null));
-      } else if (lword.endsWith("en")) {
+      } else if (lWord.endsWith("en")) {
         l.add(new AnalyzedToken(word, "E akz", null));
 
       // Verbs.
-      } else if ((matcher = patternVerb1.matcher(lword)).find()) {
-        String tense = matcher.group(2);
-        String transitive;
+      } else if ((matcher = patternVerb1.matcher(lWord)).find()) {
+        final String tense = matcher.group(2);
+        final String transitive;
 
-        Matcher matcher2 = patternVerb2.matcher(lword);
+        final Matcher matcher2 = patternVerb2.matcher(lWord);
         if (matcher2.find()) {
           transitive = matcher2.group(1).equals("ig") ? "tr" : "nt";
         } else {
-          String verb = matcher.group(1);
-          boolean isTransitive   = setTrVerbs .contains(verb);
-          boolean isIntransitive = setNtrVerbs.contains(verb);
+          final String verb = matcher.group(1);
+          final boolean isTransitive   = setTrVerbs .contains(verb);
+          final boolean isIntransitive = setNtrVerbs.contains(verb);
 
           if (isTransitive) {
             transitive = isIntransitive ? "tn" : "tr";
