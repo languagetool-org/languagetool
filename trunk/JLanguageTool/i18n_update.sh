aa14450ff99c43817d98cf2bca0297b7abfd9872
Marcin Milkowski <milek_pl@o2.pl>
2011-03-20 12:47:25 +0000

    don't change the current directory

--- trunk/JLanguageTool/i18n_update.sh
+++ trunk/JLanguageTool/i18n_update.sh
@@ -3,7 +3,7 @@
 # Note that you need to trigger crowdin export via API if you need to make sure everything
 # is up-to-date (automatic export may only happen every 24 hours).
 
-cd ~/workspace/JLanguageTool
+#cd ~/workspace/JLanguageTool
 mkdir i18n-temp
 cd i18n-temp
 wget http://crowdin.net/download/project/languagetool.zip
