7e6fe5dadf38d0e8512527cbe2406c2a92d0d026
Marcin Milkowski <milek_pl@o2.pl>
2011-04-03 21:34:00 +0000

    Thunderbird Extension

--- trunk/JLanguageTool/website/www/links/index.php
+++ trunk/JLanguageTool/website/www/links/index.php
@@ -35,8 +35,8 @@ Daniel Naber (<strong>naber <span>a&#116;</span> danielnaber<span>.</span>de</st
 	<li><?=show_link("LanguageTool for LyX", "http://wiki.lyx.org/Tools/LyX-GrammarChecker", 1) ?></li>
 	<li><?=show_link("LanguageTool plugin for OmegaT", "https://sourceforge.net/projects/omegat-plugins/files/OmegaT-LanguageTool/", 1)?>
 		a plugin that enables grammar-checking in computer-aided translation tool OmegaT (open source)</li>
-	<li><?=show_link("LanguageTool in CheckMate", "http://www.opentag.com/okapi/wiki/index.php?title=CheckMate", 1)?> LanguageTool
-	can be used as a server in CheckMate, translation QA tool</li>
+	<li><?=show_link("LanguageTool in CheckMate", "http://www.opentag.com/okapi/wiki/index.php?title=CheckMate", 1)?> used as a server to enhance translation QA</li>
+	<li><?=show_link("LanguageTool for Thunderbird", "https://addons.mozilla.org/en-US/thunderbird/addon/14781", 1)?></li>
 </ul>
 		
 <p class="firstpara"><strong>Links</strong> to other Open Source language tools:</p>
