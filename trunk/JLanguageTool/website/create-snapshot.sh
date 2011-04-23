b073029dc01d5c5b6ba80a40e8b7f024e25cc0c1
Daniel Naber <naber@danielnaber.de>
2011-04-23 21:06:42 +0000

    script to create LT snapshots

---
+++ trunk/JLanguageTool/website/create-snapshot.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+# compiles the current LanguageTool code and moves the result to a snapshots directory
+
+cd /home/languagetool/languagetool.org
+svn up svn-checkout
+cd svn-checkout
+ant dist
+mv dist/LanguageTool-1.4-dev.oxt ../www/download/snapshots/LanguageTool-`date +%Y%m%d`.oxt
+
+# delete *.oxt files older than 5 days:
+rm `find ../www/download/snapshots/ -name "*.oxt" -ctime 5`
