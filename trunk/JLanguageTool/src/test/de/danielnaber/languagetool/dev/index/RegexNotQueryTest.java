b5eb833540ac5812e2838a77c99bb121ea1ad8e5
Tao Lin <taolin2011@users.sourceforge.net>
2011-06-01 03:04:44 +0000

    Unit test for RegexNotQuery and its span version

---
+++ trunk/JLanguageTool/src/test/de/danielnaber/languagetool/dev/index/RegexNotQueryTest.java
@@ -0,0 +1,134 @@
+package de.danielnaber.languagetool.dev.index;
+
+import org.apache.lucene.document.Document;
+import org.apache.lucene.document.Field;
+import org.apache.lucene.index.IndexReader;
+import org.apache.lucene.index.RandomIndexWriter;
+import org.apache.lucene.index.Term;
+import org.apache.lucene.index.TermEnum;
+import org.apache.lucene.search.IndexSearcher;
+import org.apache.lucene.search.regex.JakartaRegexpCapabilities;
+import org.apache.lucene.search.regex.JavaUtilRegexCapabilities;
+import org.apache.lucene.search.regex.RegexCapabilities;
+import org.apache.lucene.search.regex.SpanRegexQuery;
+import org.apache.lucene.search.spans.SpanNearQuery;
+import org.apache.lucene.search.spans.SpanQuery;
+import org.apache.lucene.store.Directory;
+import org.apache.lucene.util.LuceneTestCase;
+
+public class RegexNotQueryTest extends LuceneTestCase {
+  private IndexSearcher searcher;
+
+  private IndexReader reader;
+
+  private Directory directory;
+
+  private final String FN = "field";
+
+  @Override
+  public void setUp() throws Exception {
+    super.setUp();
+    directory = newDirectory();
+    RandomIndexWriter writer = new RandomIndexWriter(random, directory);
+    Document doc = new Document();
+    doc.add(newField(FN, "the quick brown fox jumps over the lazy dog", Field.Store.NO,
+        Field.Index.ANALYZED));
+    writer.addDocument(doc);
+    doc = new Document();
+    doc.add(newField(FN, "quick", Field.Store.NO, Field.Index.ANALYZED));
+    writer.addDocument(doc);
+    reader = writer.getReader();
+    writer.close();
+    searcher = newSearcher(reader);
+  }
+
+  @Override
+  public void tearDown() throws Exception {
+    searcher.close();
+    reader.close();
+    directory.close();
+    super.tearDown();
+  }
+
+  private Term newTerm(String value) {
+    return new Term(FN, value);
+  }
+
+  private int regexQueryNrHits(String regex, RegexCapabilities capability) throws Exception {
+    RegexNotQuery query = new RegexNotQuery(newTerm(regex));
+
+    if (capability != null)
+      query.setRegexImplementation(capability);
+
+    return searcher.search(query, null, 1000).totalHits;
+  }
+
+  private int spanRegexQueryNrHits(String regex1, String regex2, int slop, boolean ordered)
+      throws Exception {
+    SpanRegexNotQuery srq1 = new SpanRegexNotQuery(newTerm(regex1));
+    SpanRegexQuery srq2 = new SpanRegexQuery(newTerm(regex2));
+    SpanNearQuery query = new SpanNearQuery(new SpanQuery[] { srq1, srq2 }, slop, ordered);
+
+    return searcher.search(query, null, 1000).totalHits;
+  }
+
+  public void testMatchAll() throws Exception {
+    TermEnum terms = new RegexNotQuery(new Term(FN, "^q.[aeiou]c.*$")).getEnum(searcher
+        .getIndexReader());
+    do {
+      Term term = terms.term();
+      assertNotNull(term);
+    } while (terms.next());
+  }
+
+  public void testRegex1() throws Exception {
+    assertEquals(1, regexQueryNrHits("^q.[aeiou]c.*$", null));
+  }
+
+  public void testRegex2() throws Exception {
+    assertEquals(2, regexQueryNrHits("^.[aeiou]c.*$", null));
+  }
+
+  public void testRegex3() throws Exception {
+    assertEquals(2, regexQueryNrHits("^q.[aeiou]c$", null));
+  }
+
+  public void testSpanRegex1() throws Exception {
+    assertEquals(0, spanRegexQueryNrHits("^lazy$", "dog", 0, true));
+  }
+
+  public void testSpanRegex2() throws Exception {
+    assertEquals(1, spanRegexQueryNrHits("^lazy$", "dog", 1, true));
+  }
+
+  public void testEquals() throws Exception {
+    RegexNotQuery query1 = new RegexNotQuery(newTerm("foo.*"));
+    query1.setRegexImplementation(new JakartaRegexpCapabilities());
+
+    RegexNotQuery query2 = new RegexNotQuery(newTerm("foo.*"));
+    assertFalse(query1.equals(query2));
+  }
+
+  public void testJakartaCaseSensative() throws Exception {
+    assertEquals(2, regexQueryNrHits("^.*QUICK.*$", null));
+  }
+
+  public void testJavaUtilCaseSensative() throws Exception {
+    assertEquals(2, regexQueryNrHits("^.*QUICK.*$", null));
+  }
+
+  public void testJakartaCaseInsensative() throws Exception {
+    assertEquals(
+        1,
+        regexQueryNrHits("^.*QUICK.*$", new JakartaRegexpCapabilities(
+            JakartaRegexpCapabilities.FLAG_MATCH_CASEINDEPENDENT)));
+  }
+
+  public void testJavaUtilCaseInsensative() throws Exception {
+    assertEquals(
+        1,
+        regexQueryNrHits("^.*QUICK.*$", new JavaUtilRegexCapabilities(
+            JavaUtilRegexCapabilities.FLAG_CASE_INSENSITIVE)));
+  }
+
+}
