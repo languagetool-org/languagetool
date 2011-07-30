727659aed31f081ebf1aa240c8a235b96c55b759
Dominique Pelle <dominique.pelle@gmail.com>
2011-07-30 16:34:18 +0000

    - Java rule COMMA_PARENTHESIS_WHITESPACE now detects missing   space before open parenthesis as in:   "She eats fruits(apple, oranges, etc.)" -> "She eats fruits (apple, oranges, etc.)

--- trunk/JLanguageTool/src/java/de/danielnaber/languagetool/rules/CommaWhitespaceRule.java
+++ trunk/JLanguageTool/src/java/de/danielnaber/languagetool/rules/CommaWhitespaceRule.java
@@ -64,12 +64,18 @@ public class CommaWhitespaceRule extends Rule {
       || tokens[i].isFieldCode();
       pos += token.length();
       String msg = null;
-      int fixLen = 0;
+      int fixFromLen = 0;
+      int fixToLen = 0;
       String suggestionText = null;
       if (isWhite && isLeftBracket(prevToken)) {
         msg = messages.getString("no_space_after");
         suggestionText = prevToken;
-        fixLen = 1;
+        fixToLen = 1;
+      } else if (i > 0 && !prevWhite && isLeftBracket(token)) {
+        msg = messages.getString("missing_space_before");
+        suggestionText = " " + token;
+        fixFromLen = prevToken.length();
+        fixToLen = 1;
       } else if (!isWhite && prevToken.equals(",") 
           && isNotQuoteOrHyphen(token) 
           && containsNoNumber(prevPrevToken) 
@@ -81,11 +87,11 @@ public class CommaWhitespaceRule extends Rule {
         if (isRightBracket(token)) {
           msg = messages.getString("no_space_before");
           suggestionText = token;
-          fixLen = 1;
+          fixToLen = 1;
         } else if (token.equals(",")) {
           msg = messages.getString("space_after_comma");
           suggestionText = ",";
-          fixLen = 1;
+          fixToLen = 1;
           //exception for duplicated comma (we already have another rule for that)
           if (i + 1 < tokens.length
              && ",".equals(tokens[i + 1].getToken())) {
@@ -94,7 +100,7 @@ public class CommaWhitespaceRule extends Rule {
         } else if (token.equals(".")) {
           msg = messages.getString("no_space_before_dot");
           suggestionText = ".";
-          fixLen = 1;
+          fixToLen = 1;
           // exception case for figures such as ".5" and ellipsis
           if (i + 1 < tokens.length
               && isNumberOrDot(tokens[i + 1].getToken())) {
@@ -103,8 +109,8 @@ public class CommaWhitespaceRule extends Rule {
         }
       }
       if (msg != null) {
-        final int fromPos = tokens[i - 1].getStartPos();
-        final int toPos = tokens[i - 1].getStartPos() + fixLen + prevLen;
+        final int fromPos = tokens[i - 1].getStartPos() + fixFromLen;
+        final int toPos = tokens[i - 1].getStartPos() + fixToLen + prevLen;
         // TODO: add some good short comment here
         final RuleMatch ruleMatch = new RuleMatch(this, fromPos, toPos, msg);
         ruleMatch.setSuggestedReplacement(suggestionText);
