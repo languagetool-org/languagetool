74d7c7d8de98c743017a28aff00b94a62642ec13
Daniel Naber <naber@danielnaber.de>
2011-07-31 12:09:52 +0000

    no output to stdout in test case; adding final; re-indent with spaces

--- trunk/JLanguageTool/src/test/de/danielnaber/languagetool/tools/LanguageIdentifierToolsTest.java
+++ trunk/JLanguageTool/src/test/de/danielnaber/languagetool/tools/LanguageIdentifierToolsTest.java
@@ -6,13 +6,13 @@ import junit.framework.TestCase;
 
 public class LanguageIdentifierToolsTest extends TestCase {
 
-	public void testAddProfile() throws Exception {
-		LanguageIdentifier.initProfiles();
-		int numLanguagesBefore = LanguageIdentifier.getSupportedLanguages().size();
-		System.out.println(Integer.toString(numLanguagesBefore) + " languages before");
-		LanguageIdentifierTools.addLtProfiles();
-		int numLanguagesAfter = LanguageIdentifier.getSupportedLanguages().size();
-		assertNotSame(numLanguagesAfter, numLanguagesBefore);		
-	}
-	
+  public void testAddProfile() throws Exception {
+    LanguageIdentifier.initProfiles();
+    final int numLanguagesBefore = LanguageIdentifier.getSupportedLanguages().size();
+    assertEquals(18, numLanguagesBefore);
+    LanguageIdentifierTools.addLtProfiles();
+    final int numLanguagesAfter = LanguageIdentifier.getSupportedLanguages().size();
+    assertNotSame(numLanguagesAfter, numLanguagesBefore);
+  }
+
 }
