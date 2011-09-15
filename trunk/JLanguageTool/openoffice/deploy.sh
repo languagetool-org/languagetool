2f5e588e9f864db9d3ca4fcc84e75bd73200f145
Daniel Naber <naber@danielnaber.de>
2011-09-15 19:08:50 +0000

    update version number

--- trunk/JLanguageTool/openoffice/deploy.sh
+++ trunk/JLanguageTool/openoffice/deploy.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
+# install LanguageTool as an add-on for OpenOffice.org/LibreOffice
 
-echo "Deploying LanguageTool to OpenOffice.org..."
-unopkg add dist/LanguageTool-1.0.1-dev.oxt
+echo "Deploying LanguageTool to OpenOffice.org/LibreOffice..."
+unopkg add dist/LanguageTool-1.5-dev.oxt
--- trunk/JLanguageTool/openoffice/undeploy.sh
+++ trunk/JLanguageTool/openoffice/undeploy.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
+# de-install LanguageTool as an add-on from OpenOffice.org/LibreOffice
 
-echo "Undeploying LanguageTool from OpenOffice.org..."
-unopkg remove LanguageTool-1.0.1-dev.oxt
+echo "Undeploying LanguageTool from OpenOffice.org/LibreOffice..."
+unopkg remove LanguageTool-1.5-dev.oxt
