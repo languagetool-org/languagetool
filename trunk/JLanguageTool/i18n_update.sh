09d770fda07c98c4d662fc1f531fc4f8e35c877d
Daniel Naber <naber@danielnaber.de>
2011-09-17 20:02:42 +0000

    new script to export i18n from Transifex instead of crowdin

--- trunk/JLanguageTool/i18n_update.sh
+++ trunk/JLanguageTool/i18n_update.sh
@@ -1,25 +1,26 @@
 #!/bin/sh
-# Download latest translations from crowdin and copy them over the existing local files.
-# Note that you need to trigger crowdin export via API if you need to make sure everything
-# is up-to-date (automatic export may only happen every 24 hours).
+# Download latest translations from Transifex and copy them over the existing local files.
 
-#cd ~/workspace/JLanguageTool
+# Transifex username and password
+USERNAME=dnaber
+PASSWORD=fixme
+
+rm -I i18n-temp
 mkdir i18n-temp
 cd i18n-temp
-wget http://crowdin.net/download/project/languagetool.zip
-unzip languagetool.zip
 
-for lang in es-ES sv-SE
+for lang in ast be br ca cs da de eo es fr gl is it km lt nl pl ro ru sk sl sv tl uk zh
 do
-	mv $lang `expr substr $lang 1 2`
-done
-
-for lang in be  ca  cs  da  de  es  fr  gl  is  it  km lt  nl  pl  ro  ru  sk  sl  sv  uk
-do
-  SOURCE="$lang/MessagesBundle.properties"
-	TARGET="../src/java/de/danielnaber/languagetool/MessagesBundle_${lang}.properties"
-	echo "Copying $SOURCE to $TARGET"
-	cp $SOURCE $TARGET
+  SOURCE=downloaded.tmp
+  # download and hackish JSON cleanup:
+  curl --user $USERNAME:$PASSWORD http://www.transifex.net/api/2/project/languagetool/resource/messagesbundleproperties/translation/$lang/ \
+    | grep "\"content\"" | sed 's/    "content": "//' |  sed -e 's/",$//' | sed -e 's/\\n/\n/g' | sed -e 's/\\\\/\\/g' \
+    | sed -e 's/\\"/"/g' >$SOURCE
+  TARGET="../src/java/de/danielnaber/languagetool/MessagesBundle_${lang}.properties"
+  SOURCE2=downloaded.tmp.ascii
+  native2ascii $SOURCE >$SOURCE2
+  echo "Moving $SOURCE2 to $TARGET"
+  mv $SOURCE2 $TARGET
 done
 
 cd ..
