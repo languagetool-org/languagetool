9f381e94c71c2663113d832e11b6a14d64f44b33
Daniel Naber <naber@danielnaber.de>
2011-09-25 14:04:36 +0000

    trunk is now version 1.6-dev

--- trunk/JLanguageTool/src/java/de/danielnaber/languagetool/JLanguageTool.java
+++ trunk/JLanguageTool/src/java/de/danielnaber/languagetool/JLanguageTool.java
@@ -58,7 +58,7 @@ import java.util.*;
 @SuppressWarnings({"UnusedDeclaration"})
 public final class JLanguageTool {
 
-  public static final String VERSION = "1.5-dev"; // keep in sync with build.properties!
+  public static final String VERSION = "1.6-dev"; // keep in sync with build.properties!
 
   public static final String PATTERN_FILE = "grammar.xml";
   public static final String FALSE_FRIEND_FILE = "false-friends.xml";
