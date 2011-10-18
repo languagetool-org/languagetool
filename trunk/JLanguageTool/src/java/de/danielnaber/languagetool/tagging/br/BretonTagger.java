7a4191c543b0cd56d1b902bcd88e5de9751f038a
Dominique Pelle <dominique.pelle@gmail.com>
2011-10-18 22:08:46 +0000

    [br] detect errors in things like: *ar kador-mañ* -> ar gador-mañ,      *ar kador-se* -> ar gardor-se, *ar kador-hont -> ar gador-hont.

--- trunk/JLanguageTool/src/java/de/danielnaber/languagetool/tagging/br/BretonTagger.java
+++ trunk/JLanguageTool/src/java/de/danielnaber/languagetool/tagging/br/BretonTagger.java
@@ -1,6 +1,6 @@
-/* LanguageTool, a natural language style checker 
+/* LanguageTool, a natural language style checker
  * Copyright (C) 2006 Daniel Naber (http://www.danielnaber.de)
- * 
+ *
  * This library is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public
  * License as published by the Free Software Foundation; either
@@ -18,13 +18,25 @@
  */
 package de.danielnaber.languagetool.tagging.br;
 
+import java.io.IOException;
+import java.net.URL;
 import java.util.Locale;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
+import morfologik.stemming.Dictionary;
+import morfologik.stemming.DictionaryLookup;
+import morfologik.stemming.IStemmer;
 import de.danielnaber.languagetool.JLanguageTool;
 import de.danielnaber.languagetool.tagging.BaseTagger;
+import de.danielnaber.languagetool.AnalyzedToken;
+import de.danielnaber.languagetool.AnalyzedTokenReadings;
+import de.danielnaber.languagetool.tools.StringTools;
 
 /** Breton Tagger.
- * 
+ *
  * Based on Breton diction diction from apertium:
  *
  *  Copyright (C) 2008--2010 Francis Tyers <ftyers@prompsit.com>
@@ -36,18 +48,99 @@ import de.danielnaber.languagetool.tagging.BaseTagger;
  *  * Grup Transducens, Universitat d'Alacant
  *
  * Implemented in FSA.
- * 
- * @author Dominique Pelle
+ *
+ * @author Dominique Pellé <dominique.pelle@gmail.com>
  */
 public class BretonTagger extends BaseTagger {
 
+  private static final Pattern patternSuffix = Pattern.compile("(?iu)(..+)-(mañ|se|hont)$");
+  private Locale conversionLocale = Locale.getDefault();
+  private IStemmer dictLookup;
+
   @Override
   public final String getFileName() {
-    return JLanguageTool.getDataBroker().getResourceDir() + "/br/breton.dict";    
+    return JLanguageTool.getDataBroker().getResourceDir() + "/br/breton.dict";
   }
-  
+
   public BretonTagger() {
     super();
     setLocale(new Locale("br"));
+  }
+
+  // This method is almost the same as the 'tag' method in
+  // BaseTagger class, except that when probing the
+  // dictionary fails, it retry without the suffixes
+  // -mañ, -se, -hont.
+  @Override
+  public List<AnalyzedTokenReadings> tag(final List<String> sentenceTokens)
+  throws IOException {
+    List<AnalyzedToken> taggerTokens;
+    List<AnalyzedToken> lowerTaggerTokens;
+    List<AnalyzedToken> upperTaggerTokens;
+    final List<AnalyzedTokenReadings> tokenReadings = new ArrayList<AnalyzedTokenReadings>();
+    int pos = 0;
+    // caching IStemmer instance - lazy init
+    if (dictLookup == null) {
+      final URL url = this.getClass().getResource(getFileName());
+      dictLookup = new DictionaryLookup(Dictionary.read(url));
+    }
+
+    Matcher matcher;
+    for (String word : sentenceTokens) {
+      String probeWord = word;
+
+      // This loop happens when we need to retry probing the dictionary
+      // which happens rarely when trying to remove suffixes -mañ, -se, etc.
+      for (;;) {
+        final List<AnalyzedToken> l = new ArrayList<AnalyzedToken>();
+        final String lowerWord = probeWord.toLowerCase(conversionLocale);
+        taggerTokens = asAnalyzedTokenList(word, dictLookup.lookup(probeWord));
+        lowerTaggerTokens = asAnalyzedTokenList(word, dictLookup.lookup(lowerWord));
+        final boolean isLowercase = probeWord.equals(lowerWord);
+
+        // Normal case.
+        addTokens(taggerTokens, l);
+
+        if (!isLowercase) {
+          // Lowercase.
+          addTokens(lowerTaggerTokens, l);
+        }
+
+        // Uppercase.
+        if (lowerTaggerTokens.isEmpty() && taggerTokens.isEmpty()) {
+          if (isLowercase) {
+            upperTaggerTokens = asAnalyzedTokenList(word,
+                dictLookup.lookup(StringTools.uppercaseFirstChar(probeWord)));
+            if (!upperTaggerTokens.isEmpty()) {
+              addTokens(upperTaggerTokens, l);
+            }
+          }
+          if (l.isEmpty()) {
+            if ((matcher = patternSuffix.matcher(probeWord)).find()) {
+              // Remove the suffix and probe dictionary again.
+              // So given a word such as "xxx-mañ", we're going to
+              // try to probe the dictionary again with "xxx" this time.
+              probeWord = matcher.group(1);
+              continue;
+            }
+            l.add(new AnalyzedToken(word, null, null));
+          }
+        }
+        tokenReadings.add(new AnalyzedTokenReadings(l, pos));
+        pos += word.length();
+        break;
+      }
+    }
+
+    return tokenReadings;
+
+  }
+
+  private void addTokens(final List<AnalyzedToken> taggedTokens, final List<AnalyzedToken> l) {
+    if (taggedTokens != null) {
+      for (AnalyzedToken at : taggedTokens) {
+        l.add(at);
+      }
+    }
   }
 }
