16c229790b6d8708f07cb5cc6a86438e8777cded
Yakov Reztsov <yakovru@users.sourceforge.net>
2012-01-22 16:59:13 +0000

    [ru] page in Russian

---
+++ trunk/JLanguageTool/website/www/ru/index.php
@@ -0,0 +1,66 @@
+<?php
+$page = "ru";
+$title = "LanguageTool";
+$title2 = "Программа для проверки грамматики и стиля";
+$lastmod = "2012-01-22 16:24:00 CET";
+include("../../include/header.php");
+?>
+
+<p>LanguageTool это <a href="http://ru.wikipedia.org/wiki/Свободное_программное_обеспечение" target="_blank">свободное программное обеспечение</a> для проверки грамматики и стиля.
+
+<h2>Функциональность</h2>
+
+В состав LanguageTool входит <a href="http://community.languagetool.org/rule/list?lang=ru">около 150 правил</a> для проверки русскоязычных текстов:<br/><br/>
+<!--
+<small>(Наведите курсор мыши на ошибки для отображения сообщения программы.)</small>
+-->
+<ul>
+    <li>Грамматика
+        <ul>
+        </ul>
+    </li>
+    <li>Стиль
+        <ul>
+        </ul>
+    </li>
+    <li>Пунктуация
+        <ul>
+        </ul>
+    </li>
+    <li>Заглавные/строчные буквы
+        <ul>
+        </ul>
+    </li>
+    <li>Типографика
+        <ul>
+        </ul>
+    </li>
+    <li>Прочие правила (настройки)
+        <ul>
+        </ul>
+    </li>
+
+
+LanguageTool не предназначен для проверки орфографии. Но в группе настроек "Стиль" для русского языка есть правило для поиска  слов, которых нет в словаре.
+
+<h2>Демонстрация возможностей LanguageTool</h2>
+
+Запустить LanguageTool <a href="http://www.languagetool.org/webstart/web/LanguageTool.jnlp">через Java WebStart (30 Mb)!</a>.
+ <a href="http://community.languagetool.org/?lang=ru"/>Здесь<a> можно проверить  LanguageTool непосредственно в браузере.
+
+<h2>Скачать</h2>
+
+<p>LanguageTool можно скачать <a href="../">с главной страницы</a> сайта.</p>
+
+<!-- TODO: установка для LibO/OOo -->
+
+
+<h2>Контакты</h2>
+<!-- TODO:  -->
+
+<p><a href="../forum">Форум</a> или  <a href="http://myooo.ru/content/view/83/43/">домашняя страничка расширения для OpenOffice.org/LibreOffice</a></p>
+
+
+<?php
+include("../../include/footer.php");
+?>
