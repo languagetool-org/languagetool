d9177d1da830e86c7a9478eff7333c4e5e2bc3a6
Jan Schreiber <janschreiber@users.sourceforge.net>
2012-01-24 20:53:30 +0000

    Wikipedia script: updated syntax plus file for disabling some of the rules by default

--- trunk/JLanguageTool/src/scripts/testwikipedia.bat
+++ trunk/JLanguageTool/src/scripts/testwikipedia.bat
@@ -1,6 +1,7 @@
 @ECHO off
 :: Check command line parameters
 IF (%3)==() GOTO usage
+IF (%2)==() GOTO usage
 IF (%1)==() GOTO usage
 IF (%1)==(?) GOTO usage
 IF (%1)==(/?) GOTO usage
@@ -10,16 +11,18 @@ IF (%1)==(/help) GOTO usage
 IF (%1)==(-help) GOTO usage
 IF (%1)==(--help) GOTO usage
 
-java -Xmx512M -cp commons-lang-2.4.jar;bliki-3.0.3.jar;LanguageTool.jar org.languagetool.dev.wikipedia.CheckWikipediaDump - %1 %2 %3
+java -Xmx512M -cp commons-lang-2.4.jar;bliki-3.0.3.jar;LanguageTool.jar org.languagetool.dev.wikipedia.CheckWikipediaDump - disabled_rules.txt %1 %2 %3 %4
 GOTO eof
 
 :usage
-ECHO Usage: %0 lang wikipediaXmlDump ruleIds
+ECHO Usage: %0 lang wikipediaXmlDump ruleIds [maxNumArticles]
 ECHO Where:
 ECHO - lang is a language code such as 'en' or 'de'
 ECHO - wikipediaXmlDump is the path to an unpacked Wikipedia XML dump
 ECHO - ruleIds is a comma-separated list of rules to be activated, or '-' for the default rules
-ECHO Example:
-ECHO - %0 de dewiki-20101012-pages-articles-partly.xml -
+ECHO - maxNumArticles is the maximum nuber of articles to check (optional)
+ECHO Examples:
+ECHO - %0 de dewiki-20111012-pages-articles-partly.xml -
+ECHO - %0 de dewiki-20111012-pages-articles-partly.xml GROSSER_STIEL 50
 
 :eof
