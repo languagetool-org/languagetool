b8860a754b1ed4a52449af9a91d3d12cc492d0f1
Daniel Naber <naber@danielnaber.de>
2012-02-21 20:51:59 +0000

    [de] avoid some false alarms

--- trunk/JLanguageTool/src/java/org/languagetool/rules/de/CaseRule.java
+++ trunk/JLanguageTool/src/java/org/languagetool/rules/de/CaseRule.java
@@ -79,6 +79,8 @@ public class CaseRule extends GermanRule {
   
   private static final Set<String> exceptions = new HashSet<String>();
   static {
+    exceptions.add("Hochdeutsch");
+    exceptions.add("Pest");
     exceptions.add("Schwinge");
     exceptions.add("Verlies");
     exceptions.add("Nachfolge");
