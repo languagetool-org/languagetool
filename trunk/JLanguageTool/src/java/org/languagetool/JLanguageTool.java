322e7a60a3f21c4c4c8c9a7df954c2cf1b79ebf6
Daniel Naber <naber@danielnaber.de>
2012-03-25 19:35:43 +0000

    start development of release 1.8

--- trunk/JLanguageTool/src/java/org/languagetool/JLanguageTool.java
+++ trunk/JLanguageTool/src/java/org/languagetool/JLanguageTool.java
@@ -61,7 +61,7 @@ import java.util.jar.Manifest;
 @SuppressWarnings({"UnusedDeclaration"})
 public final class JLanguageTool {
 
-  public static final String VERSION = "1.7-dev"; // keep in sync with build.properties!
+  public static final String VERSION = "1.8-dev"; // keep in sync with build.properties!
   public static final String BUILD_DATE = getBuildDate();
 
   public static final String PATTERN_FILE = "grammar.xml";
