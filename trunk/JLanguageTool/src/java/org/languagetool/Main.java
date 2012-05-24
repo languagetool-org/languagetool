a22c213f9dcfbdd7ba67859aa12b1c2dea01dcdd
Marcin Milkowski <milek_pl@o2.pl>
2012-05-24 15:57:57 +0000

    organize imports

--- trunk/JLanguageTool/src/java/org/languagetool/Main.java
+++ trunk/JLanguageTool/src/java/org/languagetool/Main.java
@@ -21,12 +21,9 @@ package org.languagetool;
 import java.io.*;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Collections;
-import java.util.HashSet;
 import java.util.List;
 import java.util.Locale;
-import java.util.Set;
 
 import javax.xml.parsers.ParserConfigurationException;
 
