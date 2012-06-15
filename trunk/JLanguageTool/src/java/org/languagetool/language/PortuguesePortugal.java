783408eddee5b432108d1f0a8373c16c0e874c11
Daniel Naber <naber@danielnaber.de>
2012-06-15 12:47:45 +0000

    [pt] fix language name

--- trunk/JLanguageTool/src/java/org/languagetool/language/PortuguesePortugal.java
+++ trunk/JLanguageTool/src/java/org/languagetool/language/PortuguesePortugal.java
@@ -1,5 +1,5 @@
 /* LanguageTool, a natural language style checker 
- * Copyright (C) 2007 Daniel Naber (http://www.danielnaber.de)
+ * Copyright (C) 2012 Daniel Naber (http://www.danielnaber.de)
  * 
  * This library is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public
@@ -22,7 +22,7 @@ public class PortuguesePortugal extends Portuguese {
 
 	  @Override
 	  public String getName() {
-	    return "Portuguese (Brazil)";
+	    return "Portuguese (Portugal)";
 	  }
 
 	  @Override
