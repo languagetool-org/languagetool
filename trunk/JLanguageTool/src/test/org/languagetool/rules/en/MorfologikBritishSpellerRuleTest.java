b7fb7e7a6839be4aec576d2368c7f9c3ca158603
Marcin Milkowski <milek_pl@o2.pl>
2012-06-22 11:28:30 +0000

    and a Junit test...

---
+++ trunk/JLanguageTool/src/test/org/languagetool/rules/en/MorfologikBritishSpellerRuleTest.java
@@ -0,0 +1,45 @@
+package org.languagetool.rules.en;
+
+import static org.junit.Assert.*;
+
+import java.io.IOException;
+
+import org.junit.Test;
+import org.languagetool.JLanguageTool;
+import org.languagetool.Language;
+import org.languagetool.TestTools;
+import org.languagetool.rules.RuleMatch;
+import org.languagetool.rules.spelling.morfologik.pl.MorfologikPolishSpellerRule;
+
+public class MorfologikBritishSpellerRuleTest {
+
+    @Test
+    public void testMorfologikSpeller() throws IOException {
+        MorfologikBritishSpellerRule rule =
+                new MorfologikBritishSpellerRule (TestTools.getMessages("English"), Language.BRITISH_ENGLISH);
+
+        RuleMatch[] matches;
+        JLanguageTool langTool = new JLanguageTool(Language.BRITISH_ENGLISH);
+
+
+        // correct sentences:
+        assertEquals(0, rule.match(langTool.getAnalyzedSentence("This is an example: we get behaviour as a dictionary word.")).length);
+        assertEquals(0, rule.match(langTool.getAnalyzedSentence("Why don't we speak today.")).length);
+        assertEquals(0, rule.match(langTool.getAnalyzedSentence(",")).length);
+        assertEquals(0, rule.match(langTool.getAnalyzedSentence("123454")).length);
+
+        //incorrect sentences:
+
+        matches = rule.match(langTool.getAnalyzedSentence("Behavior"));
+        // check match positions:
+        assertEquals(1, matches.length);
+        assertEquals(0, matches[0].getFromPos());
+        assertEquals(8, matches[0].getToPos());
+        assertEquals("behaviour", matches[0].getSuggestedReplacements().get(0));
+
+        assertEquals(1, rule.match(langTool.getAnalyzedSentence("aõh")).length);
+        assertEquals(0, rule.match(langTool.getAnalyzedSentence("a")).length);
+
+    }
+
+}
