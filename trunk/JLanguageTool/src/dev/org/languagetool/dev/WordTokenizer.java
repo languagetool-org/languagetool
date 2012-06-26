2c70675411717c9e66ceace90059274a2389c6fd
Marcin Milkowski <milek_pl@o2.pl>
2012-06-26 10:51:19 +0000

    heh, forgot to flush and close the buffer

--- trunk/JLanguageTool/src/dev/org/languagetool/dev/WordTokenizer.java
+++ trunk/JLanguageTool/src/dev/org/languagetool/dev/WordTokenizer.java
@@ -1,21 +1,21 @@
 /* LanguageTool, a natural language style checker 
-* Copyright (C) 2006 Daniel Naber (http://www.danielnaber.de)
-* 
-* This library is free software; you can redistribute it and/or
-* modify it under the terms of the GNU Lesser General Public
-* License as published by the Free Software Foundation; either
-* version 2.1 of the License, or (at your option) any later version.
-*
-* This library is distributed in the hope that it will be useful,
-* but WITHOUT ANY WARRANTY; without even the implied warranty of
-* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-* Lesser General Public License for more details.
-*
-* You should have received a copy of the GNU Lesser General Public
-* License along with this library; if not, write to the Free Software
-* Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301
-* USA
-*/
+ * Copyright (C) 2006 Daniel Naber (http://www.danielnaber.de)
+ * 
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301
+ * USA
+ */
 
 package org.languagetool.dev;
 
@@ -38,23 +38,24 @@ import org.languagetool.Language;
 public final class WordTokenizer {
 
 
-     public static void main(final String[] args) throws IOException {
+    public static void main(final String[] args) throws IOException {
         final WordTokenizer prg = new WordTokenizer();
         if (args.length != 1) {
             System.err.println("Please supply the language code as the only argument.");
             System.exit(-1);
         }
         prg.run(args[0]);
-      }
+    }
 
-      private void run(final String lang) throws IOException {
-        
+    private void run(final String lang) throws IOException {
+
         JLanguageTool langTool = new JLanguageTool(
                 Language.getLanguageForShortName(lang));
         BufferedReader in = null;
+        BufferedWriter out = null;
         try {
             in = new BufferedReader(new InputStreamReader(System.in));
-            BufferedWriter out = new BufferedWriter(new OutputStreamWriter(System.out));
+            out = new BufferedWriter(new OutputStreamWriter(System.out));
             String line;
             while ((line = in.readLine()) != null) {
                 AnalyzedTokenReadings[] atr = langTool.getRawAnalyzedSentence(line).
@@ -62,8 +63,7 @@ public final class WordTokenizer {
                 for (AnalyzedTokenReadings a : atr) {
                     out.write(a.getToken());
                     out.write("\n");
-                }
-                
+                }                                
             }
         }
         catch (IOException e) {
@@ -73,6 +73,10 @@ public final class WordTokenizer {
         finally {
             if (in != null) {
                 in.close();
+            }
+            if (out != null) {
+                out.flush();
+                out.close();
             }
         }      
     }
