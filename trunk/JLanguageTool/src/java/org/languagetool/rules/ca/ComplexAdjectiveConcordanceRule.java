02542f23d9ceb7e41aa690abe5ce75ab8da83391
Jaume Ortola i Font <jaumeortola@gmail.com>
2012-07-27 23:45:40 +0000

    [ca] Fixed some false alarms.

--- trunk/JLanguageTool/src/java/org/languagetool/rules/ca/ComplexAdjectiveConcordanceRule.java
+++ trunk/JLanguageTool/src/java/org/languagetool/rules/ca/ComplexAdjectiveConcordanceRule.java
@@ -76,7 +76,7 @@ public class ComplexAdjectiveConcordanceRule extends CatalanRule {
 	private static final Pattern COORDINACIO = Pattern.compile(",|i|o");
 	private static final Pattern COORDINACIO_IONI = Pattern.compile("i|o|ni");
 	private static final Pattern KEEP_COUNT = Pattern.compile("A.*|N.*|D[NAID].*|SPS.*|R.*|V.P.*");
-	private static final Pattern KEEP_COUNT2 = Pattern.compile(",|i|o|ni");
+	private static final Pattern KEEP_COUNT2 = Pattern.compile(",|i|o|ni|\\d+");
 	private static final Pattern PREPOSICIONS = Pattern.compile("SPS.*");
 	private static final Pattern VERB_AUXILIAR = Pattern.compile("V[AS].*");
 	private static final Pattern EXCEPCIONS_PARTICIPI = Pattern.compile("atès|atés|atesa|atesos|ateses|donat|donats|donada|donades");
