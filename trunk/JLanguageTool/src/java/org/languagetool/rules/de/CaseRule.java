c557b46cb65abcd257b96e4a21f2bc0f06b72ce6
Daniel Naber <naber@danielnaber.de>
2012-07-29 13:24:30 +0000

    [de] avoiding some false alarms ("der Auftrieb" etc)

--- trunk/JLanguageTool/src/java/org/languagetool/rules/de/CaseRule.java
+++ trunk/JLanguageTool/src/java/org/languagetool/rules/de/CaseRule.java
@@ -80,6 +80,9 @@ public class CaseRule extends GermanRule {
   private static final Set<String> exceptions = new HashSet<String>();
   static {
     exceptions.add("Hr");   // Hr. = Abkürzung für Herr
+    exceptions.add("Auftrieb");
+    exceptions.add("Vortrieb");
+    exceptions.add("Abtrieb");
     exceptions.add("Gesandter");
     exceptions.add("Durchfahrt");
     exceptions.add("Durchgriff");
