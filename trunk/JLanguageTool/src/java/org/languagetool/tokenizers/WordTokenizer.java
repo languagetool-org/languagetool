46b9cbe0b36ea77f480f32a8b985fb03d3824c09
Dominique Pelle <dominique.pelle@gmail.com>
2012-08-13 19:52:26 +0000

    - The word tokenizer now considers the pipe | and backtick ` characters   as word separators.  I intended to make the star * as word separator too   but it would currently break 2 rules: German rule LEERZEICHEN_RECHENZEICHEN   and Italian rule GR_09.

--- trunk/JLanguageTool/src/java/org/languagetool/tokenizers/WordTokenizer.java
+++ trunk/JLanguageTool/src/java/org/languagetool/tokenizers/WordTokenizer.java
@@ -37,7 +37,7 @@ public class WordTokenizer implements Tokenizer {
   public List<String> tokenize(final String text) {
     final List<String> l = new ArrayList<String>();
     final StringTokenizer st = new StringTokenizer(text, 
-        "\u0020\u00A0\u115f\u1160\u1680" 
+        "\u0020\u0060\u007c\u00A0\u115f\u1160\u1680" 
         + "\u2000\u2001\u2002\u2003\u2004\u2005\u2006\u2007" 
         + "\u2008\u2009\u200A\u200B\u200c\u200d\u200e\u200f"
         + "\u2028\u2029\u202a\u202b\u202c\u202d\u202e\u202f"
