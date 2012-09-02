e3f4b29bfd1dfd89856a274fb66582c655b54cca
Daniel Naber <naber@danielnaber.de>
2012-09-02 21:01:02 +0000

    adapt to move of i18n files

--- trunk/JLanguageTool/i18n_update.sh
+++ trunk/JLanguageTool/i18n_update.sh
@@ -16,7 +16,7 @@ do
   # download and hackish JSON cleanup:
   curl --user $USERNAME:$PASSWORD https://www.transifex.net/api/2/project/languagetool/resource/messagesbundleproperties/translation/$lang/?file >$SOURCE
   recode latin1..utf8 $SOURCE
-  TARGET="../src/java/org/languagetool/MessagesBundle_${lang}.properties"
+  TARGET="../src/main/resources/org/languagetool/MessagesBundle_${lang}.properties"
   SOURCE2=downloaded.tmp.ascii
   native2ascii $SOURCE >$SOURCE2
   echo "Moving $SOURCE2 to $TARGET"
