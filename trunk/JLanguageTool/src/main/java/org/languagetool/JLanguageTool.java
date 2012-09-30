d3fd93776e903a1d8186ca129f242c82e3cd7ac5
Daniel Naber <naber@danielnaber.de>
2012-09-30 14:35:29 +0000

    set version to 2.0-dev

--- trunk/JLanguageTool/src/main/java/org/languagetool/JLanguageTool.java
+++ trunk/JLanguageTool/src/main/java/org/languagetool/JLanguageTool.java
@@ -75,7 +75,7 @@ import org.xml.sax.SAXException;
 @SuppressWarnings({"UnusedDeclaration"})
 public final class JLanguageTool {
 
-  public static final String VERSION = "1.9"; // keep in sync with build.properties!
+  public static final String VERSION = "2.0-dev"; // keep in sync with build.properties!
   public static final String BUILD_DATE = getBuildDate();
 
   public static final String PATTERN_FILE = "grammar.xml";
