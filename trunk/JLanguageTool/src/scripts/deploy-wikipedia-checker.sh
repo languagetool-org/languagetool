643e2afadfa6f267e8ee6deb1f9060405dd01a1a
Daniel Naber <naber@danielnaber.de>
2012-10-01 11:32:30 +0000

    update the release instructions to use the current path names etc.

--- trunk/JLanguageTool/src/scripts/deploy-wikipedia-checker.sh
+++ trunk/JLanguageTool/src/scripts/deploy-wikipedia-checker.sh
@@ -8,5 +8,5 @@ echo ""
 sleep 1
 
 ant dist-standalone && \
-  scp -i /home/dnaber/.ssh/openthesaurus dist/LanguageTool-[1-9].[0-9]*.zip languagetool@83.169.5.38: && \
-  ssh -i /home/dnaber/.ssh/openthesaurus languagetool@83.169.5.38 "unzip -d /home/languagetool/ltcommunity/corpus/code ~/LanguageTool-[1-9].[0-9]*.zip"
+  scp -i /home/dnaber/.ssh/openthesaurus dist/LanguageTool-[1-9].[0-9]*.zip languagetool@languagetool.org: && \
+  ssh -i /home/dnaber/.ssh/openthesaurus languagetool@languagetool.org "unzip -d /home/languagetool/ltcommunity/corpus ~/LanguageTool-[1-9].[0-9]*.zip"
