2ef7250d3ed85b20ed77fe1a722656613cff366d
Daniel Naber <naber@danielnaber.de>
2012-10-10 20:02:19 +0000

    fix: adapt scripts to modified usage of CheckWikipediaDump (could not test the .bat)

--- trunk/JLanguageTool/src/scripts/testwikipedia.sh
+++ trunk/JLanguageTool/src/scripts/testwikipedia.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-: ${3?"Usage: $0 <lang> <wikipediaXmlDump> <ruleIds> (where <lang> is a language code like 'en' or 'de', <ruleIds> is a comma-separated list of rules to be activated or '-' for the default rules)"}
+: ${5?"Usage: $0 <lang> <wikipediaXmlDump> <ruleIds> <maxArticles> <maxErrors> (where <lang> is a language code like 'en' or 'de', <ruleIds> is a comma-separated list of rules to be activated or '-' for the default rules)"}
 
-java -Xmx512M -cp commons-lang-2.4.jar:bliki-3.0.3.jar:LanguageTool.jar org.languagetool.dev.wikipedia.CheckWikipediaDump - - $1 $2 $3
+java -Xmx512M -cp commons-lang-2.4.jar:bliki-3.0.3.jar:LanguageTool.jar org.languagetool.dev.wikipedia.CheckWikipediaDump - - $1 $2 $3 $4 $5
