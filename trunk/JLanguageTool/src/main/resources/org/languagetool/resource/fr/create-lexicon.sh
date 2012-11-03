e346a976a3c2bf34fc7d6182472450321a75c3e5
Dominique Pelle <dominique.pelle@gmail.com>
2012-11-03 07:26:10 +0000

    [fr] added script to create the French POS tag dictionary.

---
+++ trunk/JLanguageTool/src/main/resources/org/languagetool/resource/fr/create-lexicon.sh
@@ -0,0 +1,32 @@
+#!/bin/sh
+#
+# How to create the POS tag 'french.dict' dictionary:
+#
+# 1) Download morfologik-stemming-1.4.0.zip from
+#    http://sourceforge.net/projects/morfologik/files/morfologik-stemming/1.4.0/
+#    $ unzip morfologik-stemming-1.4.0.zip
+#    This creates morfologik-stemming-nodict-1.4.0.jar
+# 4) Run the script:
+#    $ ./create-lexicon.pl
+#    This creates the dictionary 'french.dict'.
+#
+# Author: Dominique Pelle <dominique.pelle@gmail.com>
+#
+
+INPUT=lexique-dicollecte-fr-v4.6
+
+if [ ! -f $INPUT.txt ]; then
+  wget http://www.dicollecte.org/download/fr/$INPUT.zip
+  unzip $INPUT.zip
+fi
+
+chmod a+x DL2LT.py
+vim  -c 'set fileformat=unix' -c 1s/python3.1/python3 -c wq DL2LT.py
+./DL2LT.py $INPUT.txt
+
+java -jar morfologik-stemming-nodict-1.4.0.jar tab2morph \
+     -i $INPUT.txt.LT.txt \
+     -o output.txt
+java -jar morfologik-stemming-nodict-1.4.0.jar fsa_build \
+     -i output.txt \
+     -o french.dict
