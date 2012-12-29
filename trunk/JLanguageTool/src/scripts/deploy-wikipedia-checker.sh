299b4dbde4f27e464305f25d38c177193fe4b62f
Daniel Naber <naber@danielnaber.de>
2012-12-29 20:38:03 +0000

    more verbose usage message for deployment script

--- trunk/JLanguageTool/src/scripts/deploy-wikipedia-checker.sh
+++ trunk/JLanguageTool/src/scripts/deploy-wikipedia-checker.sh
@@ -3,7 +3,12 @@
 echo ""
 echo "###"
 echo "### Admin only - you will need the server password to deploy the code ###"
+echo "### This will deploy code for automatic Wikipedia checking ###"
+echo "### as seen on http://community.languagetool.org/corpusMatch/list?lang=en ###"
+echo "### but it will not deploy the web-app ###"
 echo "###"
+echo ""
+echo "### NOTE: You still need to call update-all.sh manually on the server ###"
 echo ""
 sleep 1
 
