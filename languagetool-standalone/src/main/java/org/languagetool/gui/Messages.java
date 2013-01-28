0d665d83fcf4a6d547ce519dc3165a3588caa914
Daniel Naber <naber@danielnaber.de>
2013-01-28 21:33:16 +0000

    add missing license header; whitespace cleanup (tab -> spaces)

--- languagetool-standalone/src/main/java/org/languagetool/gui/Messages.java
+++ languagetool-standalone/src/main/java/org/languagetool/gui/Messages.java
@@ -1,3 +1,21 @@
+/* LanguageTool, a natural language style checker
+ * Copyright (C) 2011 Marcin Miłkowski
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301
+ * USA
+ */
 package org.languagetool.gui;
 
 import org.languagetool.JLanguageTool;
@@ -7,17 +25,17 @@ import java.util.ResourceBundle;
 
 public class Messages {
 
-	private static final ResourceBundle RESOURCE_BUNDLE = ResourceBundle
-			.getBundle(JLanguageTool.MESSAGE_BUNDLE);
-  
-	private Messages() {
-	}
+  private static final ResourceBundle RESOURCE_BUNDLE = ResourceBundle
+          .getBundle(JLanguageTool.MESSAGE_BUNDLE);
 
-	public static String getString(String key) {
-		try {
-			return RESOURCE_BUNDLE.getString(key);
-		} catch (MissingResourceException e) {
-			return '!' + key + '!';
-		}
-	}
+  private Messages() {
+  }
+
+  public static String getString(String key) {
+    try {
+      return RESOURCE_BUNDLE.getString(key);
+    } catch (MissingResourceException e) {
+      return '!' + key + '!';
+    }
+  }
 }
