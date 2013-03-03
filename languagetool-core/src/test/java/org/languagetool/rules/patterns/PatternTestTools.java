79dadd822ceb5b1310931be48eadc68da60c7d78
Daniel Naber <naber@danielnaber.de>
2013-03-03 17:31:56 +0000

    add license header

--- languagetool-core/src/test/java/org/languagetool/rules/patterns/PatternTestTools.java
+++ languagetool-core/src/test/java/org/languagetool/rules/patterns/PatternTestTools.java
@@ -1,3 +1,21 @@
+/* LanguageTool, a natural language style checker 
+ * Copyright (C) 2013 Marcin Miłkowski (www.languagetool.org)
+ * 
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301
+ * USA
+ */
 package org.languagetool.rules.patterns;
 
 import java.util.ArrayList;
@@ -10,11 +28,9 @@ import java.util.regex.Pattern;
 import org.languagetool.Language;
 
 /**
+ * Common pattern test routines (usable for Disambiguation rules as well). 
  * 
  * @author Marcin Miłkowski
- * 
- * Common pattern test routines (usable for Disambiguation rules as well). 
- *
  */
 public class PatternTestTools {
 
