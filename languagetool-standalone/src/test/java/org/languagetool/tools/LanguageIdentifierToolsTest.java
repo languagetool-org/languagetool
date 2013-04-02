9ea55b26064f3dff2c46278e1315a0ff30b69845
Daniel Naber <naber@danielnaber.de>
2013-04-02 20:55:11 +0000

    update to Tika 1.3 and remove the resources for languages that Tika can now detect itself

--- languagetool-standalone/src/test/java/org/languagetool/tools/LanguageIdentifierToolsTest.java
+++ languagetool-standalone/src/test/java/org/languagetool/tools/LanguageIdentifierToolsTest.java
@@ -27,7 +27,7 @@ public class LanguageIdentifierToolsTest extends TestCase {
   public void testAddProfile() throws Exception {
     LanguageIdentifier.initProfiles();
     final int numLanguagesBefore = LanguageIdentifier.getSupportedLanguages().size();
-    assertEquals(18, numLanguagesBefore);
+    assertEquals(27, numLanguagesBefore);
     LanguageIdentifierTools.addLtProfiles();
     final int numLanguagesAfter = LanguageIdentifier.getSupportedLanguages().size();
     assertNotSame(numLanguagesAfter, numLanguagesBefore);
