1189896d572e4f8c83edfe2f6b1fc90294b0486d
Daniel Naber <naber@danielnaber.de>
2013-04-02 19:01:56 +0000

    update upload helper script

--- languagetool-standalone/scripts/deploy-wikipedia-checker.sh
+++ languagetool-standalone/scripts/deploy-wikipedia-checker.sh
@@ -8,14 +8,13 @@ echo "### as seen on http://community.languagetool.org/corpusMatch/list?lang=en 
 echo "### but it will not deploy the web-app ###"
 echo "###"
 echo ""
-echo "### NOTE: You still need to call update-all.sh manually on the server ###"
-echo ""
 sleep 1
 
-echo "FIXME - still needs to be test after the switch to Maven" 
-
-#cd .. &&
-#  mvn clean package -DskipTests &&
-#  cd - &&
-#  scp -i /home/dnaber/.ssh/openthesaurus target/LanguageTool-[1-9].[0-9]*.zip languagetool@languagetool.org: &&
-#  ssh -i /home/dnaber/.ssh/openthesaurus languagetool@languagetool.org "unzip -d /home/languagetool/ltcommunity/corpus ~/LanguageTool-[1-9].[0-9]*.zip"
+cd ../.. &&
+  mvn --projects languagetool-wikipedia --also-make clean package -DskipTests &&
+  scp languagetool-wikipedia/target/LanguageTool-wikipedia-[1-9].[0-9]*.zip languagetool@languagetool.org:/home/languagetool/ltcommunity/corpus/ &&
+  cd - &&
+  echo "###" &&
+  echo "### NOTE: You still need to unpack the ZIP and call update-all.sh manually on the server ###" &&
+  echo "###" &&
+  echo ""
