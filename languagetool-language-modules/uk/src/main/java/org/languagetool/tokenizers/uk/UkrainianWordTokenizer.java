eac29a5acb9dd9b39401da227cf895a3eaf667fe
Andriy Rysin <arysin@gmail.com>
2013-04-23 02:58:21 +0000

    New Ukrainian tag dictionary and new rules

--- languagetool-language-modules/uk/src/main/java/org/languagetool/tokenizers/uk/UkrainianWordTokenizer.java
+++ languagetool-language-modules/uk/src/main/java/org/languagetool/tokenizers/uk/UkrainianWordTokenizer.java
@@ -31,6 +31,13 @@ import org.languagetool.tokenizers.Tokenizer;
  * @author Andriy Rysin
  */
 public class UkrainianWordTokenizer implements Tokenizer {
+  private static final String SPLIT_CHARS = "\u0020\u00A0\u115f\u1160\u1680" 
+        + "\u2000\u2001\u2002\u2003\u2004\u2005\u2006\u2007" 
+        + "\u2008\u2009\u200A\u200B\u200c\u200d\u200e\u200f"
+        + "\u2028\u2029\u202a\u202b\u202c\u202d\u202e\u202f"
+        + "\u205F\u2060\u2061\u2062\u2063\u206A\u206b\u206c\u206d"
+        + "\u206E\u206F\u3000\u3164\ufeff\uffa0\ufff9\ufffa\ufffb" 
+        + ",.;()[]{}<>!?:/|\\\"«»„”“`´‘‛′…¿¡\t\n\r";
 
   public UkrainianWordTokenizer() {
   }
@@ -38,18 +45,17 @@ public class UkrainianWordTokenizer implements Tokenizer {
   @Override
   public List<String> tokenize(final String text) {
     final List<String> l = new ArrayList<String>();
-    final StringTokenizer st = new StringTokenizer(text, 
-        "\u0020\u00A0\u115f\u1160\u1680" 
-        + "\u2000\u2001\u2002\u2003\u2004\u2005\u2006\u2007" 
-        + "\u2008\u2009\u200A\u200B\u200c\u200d\u200e\u200f"
-        + "\u2028\u2029\u202a\u202b\u202c\u202d\u202e\u202f"
-        + "\u205F\u2060\u2061\u2062\u2063\u206A\u206b\u206c\u206d"
-        + "\u206E\u206F\u3000\u3164\ufeff\uffa0\ufff9\ufffa\ufffb" 
-        + ",.;()[]{}<>!?:/|\\\"«»„”“`´‘‛′…¿¡\t\n\r", true);
+    final StringTokenizer st = new StringTokenizer(text, SPLIT_CHARS, true);
+        
     while (st.hasMoreElements()) {
-      l.add(st.nextToken());
+      l.add( clean(st.nextToken()) );
     }
+    
     return l;
+  }
+  
+  private static String clean(String token) {
+    return token.replace("\u0301", "").replace('’', '\'').replace('ʼ', '\'');
   }
   
 }
