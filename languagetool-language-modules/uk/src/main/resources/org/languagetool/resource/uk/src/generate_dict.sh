4e4427530b0dc9a0040c1af4827ca46c35ce7e8b
Andriy Rysin <arysin@gmail.com>
2013-05-29 03:20:31 +0000

    Ukrainian dictionary and rules update

--- languagetool-language-modules/uk/src/main/resources/org/languagetool/resource/uk/src/generate_dict.sh
+++ languagetool-language-modules/uk/src/main/resources/org/languagetool/resource/uk/src/generate_dict.sh
@@ -1,10 +1,11 @@
 #!/bin/sh
 
 spell_uk_dir="/home/arysin/work/ukr/spelling/spell-uk"
+FLAGS="-f"
 
 make -C $spell_uk_dir/src/Dictionary uk_words.tag && \
 make -C $spell_uk_dir/src/Affix uk_affix.tag && \
 ./parse.py && \
-./make-dict-uk-mfl.sh $1 && \
+./make-dict-uk-mfl.sh $FLAGS && \
 mv -f *.dict ukrainian_tags.txt ../
 
