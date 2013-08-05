c0d96d6e0b2effcd74aae3b222b5b4212582cd73
Daniel Naber <naber@danielnaber.de>
2013-08-05 06:27:01 +0000

    [de] add words to avoid false alarms

--- languagetool-language-modules/de/src/main/java/org/languagetool/rules/de/CaseRule.java
+++ languagetool-language-modules/de/src/main/java/org/languagetool/rules/de/CaseRule.java
@@ -81,6 +81,8 @@ public class CaseRule extends GermanRule {
      * solution is to add all those to our Morphy data, but as a simple
      * workaround to avoid false alarms, these words can be added here.
      */
+    exceptions.add("Vorfahre");
+    exceptions.add("Mittler");
     exceptions.add("Hr");   // Hr. = Abkürzung für Herr
     exceptions.add("Schwarz");
     exceptions.add("Genese");
