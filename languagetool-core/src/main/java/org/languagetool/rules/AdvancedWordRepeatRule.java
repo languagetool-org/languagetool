0e4f92e5a45c0a851c0049060222ebb946af0a77
Daniel Naber <naber@danielnaber.de>
2013-08-27 17:13:15 +0200

    move common code to AdvancedWordRepeatRule

---
+++ languagetool-core/src/main/java/org/languagetool/rules/AdvancedWordRepeatRule.java
@@ -0,0 +1,158 @@
+/* LanguageTool, a natural language style checker 
+ * Copyright (C) 2005 Daniel Naber (http://www.danielnaber.de)
+ * 
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301
+ * USA
+ */
+package org.languagetool.rules;
+
+import org.languagetool.AnalyzedSentence;
+import org.languagetool.AnalyzedToken;
+import org.languagetool.AnalyzedTokenReadings;
+import org.languagetool.JLanguageTool;
+import org.languagetool.tools.StringTools;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.ResourceBundle;
+import java.util.TreeSet;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+/**
+ * Rule for detecting same words in the sentence but not just in a row
+ *
+ * @author Marcin Miłkowski
+ */
+public abstract class AdvancedWordRepeatRule extends Rule {
+
+  public AdvancedWordRepeatRule(final ResourceBundle messages) {
+    if (messages != null) {
+      super.setCategory(new Category(messages.getString("category_misc")));
+    }
+    setDefaultOff();
+  }
+  
+  protected abstract Pattern getExcludedWordsPattern();
+  protected abstract Pattern getExcludedNonWordsPattern();
+  protected abstract Pattern getExcludedPos();
+  protected abstract String getMessage();
+  protected abstract String getShortMessage();
+
+  /*
+   * Tests if any word form is repeated in the sentence.
+   */
+  @Override
+  public final RuleMatch[] match(final AnalyzedSentence text) {
+    final List<RuleMatch> ruleMatches = new ArrayList<>();
+    final AnalyzedTokenReadings[] tokens = text.getTokensWithoutWhitespace();
+    boolean repetition = false;
+    final TreeSet<String> inflectedWords = new TreeSet<>();
+    String prevLemma;
+    int curToken = 0;
+    // start from real token, 0 = SENT_START
+    for (int i = 1; i < tokens.length; i++) {
+      final String token = tokens[i].getToken();
+      // avoid "..." etc. to be matched:
+      boolean isWord = true;
+      boolean hasLemma = true;
+
+      if (token.length() < 2) {
+        isWord = false;
+      }
+
+      for (AnalyzedToken analyzedToken : tokens[i]) {
+        final String posTag = analyzedToken.getPOSTag();
+        if (posTag != null) {
+          if (StringTools.isEmpty(posTag)) {
+            isWord = false;
+            break;
+          }
+          // FIXME: too many false alarms here:
+          final String lemma = analyzedToken.getLemma();
+          if (lemma == null) {
+            hasLemma = false;
+            break;
+          }
+          final Matcher m1 = getExcludedWordsPattern().matcher(lemma);
+          if (m1.matches()) {
+            isWord = false;
+            break;
+          }
+
+          final Matcher m2 = getExcludedPos().matcher(posTag);
+          if (m2.matches()) {
+            isWord = false;
+            break;
+          }
+        } else {
+          hasLemma = false;
+        }
+
+      }
+
+      final Matcher m1 = getExcludedNonWordsPattern().matcher(tokens[i].getToken());
+      if (m1.matches()) {
+        isWord = false;
+      }
+
+      prevLemma = "";
+      if (isWord) {
+        boolean notSentEnd = false;
+        for (AnalyzedToken analyzedToken : tokens[i]) {
+          final String pos = analyzedToken.getPOSTag();
+          if (pos != null) {
+            notSentEnd |= JLanguageTool.SENTENCE_END_TAGNAME.equals(pos);
+          }
+          if (hasLemma) {
+            final String curLemma = analyzedToken.getLemma();
+            if (!prevLemma.equals(curLemma) && !notSentEnd) {
+              if (inflectedWords.contains(curLemma) && curToken != i) {
+                repetition = true;
+              } else {
+                inflectedWords.add(analyzedToken.getLemma());
+                curToken = i;
+              }
+            }
+            prevLemma = curLemma;
+          } else {
+            if (inflectedWords.contains(tokens[i].getToken()) && !notSentEnd) {
+              repetition = true;
+            } else {
+              inflectedWords.add(tokens[i].getToken());
+            }
+          }
+
+        }
+      }
+
+      if (repetition) {
+        final int pos = tokens[i].getStartPos();
+        final RuleMatch ruleMatch = new RuleMatch(this, pos, pos
+                + token.length(), getMessage(), getShortMessage());
+        ruleMatches.add(ruleMatch);
+        repetition = false;
+      }
+
+    }
+    return toRuleMatchArray(ruleMatches);
+  }
+
+  @Override
+  public void reset() {
+    // nothing
+  }
+
+}
--- languagetool-language-modules/pl/src/main/java/org/languagetool/rules/pl/PolishWordRepeatRule.java
+++ languagetool-language-modules/pl/src/main/java/org/languagetool/rules/pl/PolishWordRepeatRule.java
@@ -18,27 +18,15 @@
  */
 package org.languagetool.rules.pl;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.ResourceBundle;
-import java.util.TreeSet;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
+import org.languagetool.rules.AdvancedWordRepeatRule;
 
-import org.languagetool.AnalyzedSentence;
-import org.languagetool.AnalyzedToken;
-import org.languagetool.AnalyzedTokenReadings;
-import org.languagetool.rules.Category;
-import org.languagetool.rules.RuleMatch;
-import org.languagetool.tools.StringTools;
+import java.util.ResourceBundle;
+import java.util.regex.Pattern;
 
 /**
  * @author Marcin Miłkowski
- * 
- *         Rule for detecting same words in the sentence but not just in a row
- * 
  */
-public class PolishWordRepeatRule extends PolishRule {
+public class PolishWordRepeatRule extends AdvancedWordRepeatRule {
 
   /**
    * Excluded dictionary words.
@@ -62,10 +50,7 @@ public class PolishWordRepeatRule extends PolishRule {
           + "M*(D?C{0,3}|C[DM])(L?X{0,3}|X[LC])(V?I{0,3}|I[VX])$");
 
   public PolishWordRepeatRule(final ResourceBundle messages) {
-    if (messages != null) {
-      super.setCategory(new Category(messages.getString("category_misc")));
-    }
-    setDefaultOff();
+    super(messages);
   }
 
   @Override
@@ -78,109 +63,29 @@ public class PolishWordRepeatRule extends PolishRule {
     return "Powtórzenia wyrazów w zdaniu (monotonia stylistyczna)";
   }
 
-  /*
-   * Tests if any word form is repeated in the sentence.
-   */
   @Override
-  public final RuleMatch[] match(final AnalyzedSentence text) {
-    final List<RuleMatch> ruleMatches = new ArrayList<>();
-    final AnalyzedTokenReadings[] tokens = text.getTokensWithoutWhitespace();
-    boolean repetition = false;
-    final TreeSet<String> inflectedWords = new TreeSet<>();
-    String prevLemma, curLemma;
-    int curToken = 0;
-    // start from real token, 0 = SENT_START
-    for (int i = 1; i < tokens.length; i++) {
-      final String token = tokens[i].getToken();
-      // avoid "..." etc. to be matched:
-      boolean isWord = true;
-      boolean hasLemma = true;
-
-      if (token.length() < 2) {
-        isWord = false;
-      }
-
-      for (AnalyzedToken analyzedToken : tokens[i]) {
-        final String posTag = analyzedToken.getPOSTag();
-        if (posTag != null) {
-          if (StringTools.isEmpty(posTag)) {
-            isWord = false;
-            break;
-          }
-          // FIXME: too many false alarms here:
-          final String lemma = analyzedToken.getLemma();
-          if (lemma == null) {
-            hasLemma = false;
-            break;
-          }
-          final Matcher m1 = EXC_WORDS.matcher(lemma);
-          if (m1.matches()) {
-            isWord = false;
-            break;
-          }
-
-          final Matcher m2 = EXC_POS.matcher(posTag);
-          if (m2.matches()) {
-            isWord = false;
-            break;
-          }
-        } else {
-          hasLemma = false;
-        }
-
-      }
-
-      final Matcher m1 = EXC_NONWORDS.matcher(tokens[i].getToken());
-      if (m1.matches()) {
-        isWord = false;
-      }
-
-      prevLemma = "";
-      if (isWord) {
-        boolean notSentEnd = false;
-        for (AnalyzedToken analyzedToken : tokens[i]) {
-          final String pos = analyzedToken.getPOSTag();
-          if (pos != null) {
-            notSentEnd |= "SENT_END".equals(pos);
-          }
-          if (hasLemma) {
-            curLemma = analyzedToken.getLemma();
-            if (!prevLemma.equals(curLemma) && !notSentEnd) {
-              if (inflectedWords.contains(curLemma) && curToken != i) {
-                repetition = true;
-              } else {
-                inflectedWords.add(analyzedToken.getLemma());
-                curToken = i;
-              }
-            }
-            prevLemma = curLemma;
-          } else {
-            if (inflectedWords.contains(tokens[i].getToken()) && !notSentEnd) {
-              repetition = true;
-            } else {
-              inflectedWords.add(tokens[i].getToken());
-            }
-          }
-
-        }
-      }
-
-      if (repetition) {
-        final String msg = "Powtórzony wyraz w zdaniu";
-        final int pos = tokens[i].getStartPos();
-        final RuleMatch ruleMatch = new RuleMatch(this, pos, pos
-            + token.length(), msg, "Powtórzenie wyrazu");
-        ruleMatches.add(ruleMatch);
-        repetition = false;
-      }
-
-    }
-    return toRuleMatchArray(ruleMatches);
+  protected Pattern getExcludedWordsPattern() {
+    return EXC_WORDS;
   }
 
   @Override
-  public void reset() {
-    // nothing
+  protected Pattern getExcludedNonWordsPattern() {
+    return EXC_NONWORDS;
+  }
+
+  @Override
+  protected Pattern getExcludedPos() {
+    return EXC_POS;
+  }
+
+  @Override
+  public final String getMessage() {
+    return "Powtórzony wyraz w zdaniu";
+  }
+
+  @Override
+  public final String getShortMessage() {
+    return "Powtórzenie wyrazu";
   }
 
 }
