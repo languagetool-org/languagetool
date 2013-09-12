595a51dadba63e41f99a850537a2c25620e2eecf
jaumeortola <jaumeortola@gmail.com>
2013-09-12 16:12:46 +0200

    [ca] Fix false alarms and add test for CatalanWordRepeatRule

---
+++ languagetool-language-modules/ca/src/test/java/org/languagetool/rules/ca/CatalanWordRepeatRuleTest.java
@@ -0,0 +1,56 @@
+/* LanguageTool, a natural language style checker 
+ * Copyright (C) 2005 Daniel Naber (http://www.danielnaber.de)
+ * 
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301
+ * USA
+ */
+
+package org.languagetool.rules.ca;
+
+import junit.framework.TestCase;
+
+import org.languagetool.JLanguageTool;
+import org.languagetool.TestTools;
+import org.languagetool.language.Catalan;
+import org.languagetool.rules.RuleMatch;
+
+import java.io.IOException;
+
+public class CatalanWordRepeatRuleTest extends TestCase {
+
+  /*
+   * Test method for 'org.languagetool.rules.ca.CatalanWordRepeatRule.match(AnalyzedSentence)'
+   */
+  public void testRule() throws IOException {
+    final CatalanWordRepeatRule rule = new CatalanWordRepeatRule(TestTools.getMessages("Catalan"), new Catalan());
+    RuleMatch[] matches;
+    JLanguageTool langTool = new JLanguageTool(new Catalan());
+    //correct
+    matches = rule.match(langTool.getAnalyzedSentence("Sempre pensa en en Joan."));
+    assertEquals(0, matches.length);
+    matches = rule.match(langTool.getAnalyzedSentence("Els els portaré aviat."));
+    assertEquals(0, matches.length);
+    matches = rule.match(langTool.getAnalyzedSentence("Maximilià I i Maria de Borgonya"));
+    assertEquals(0, matches.length);
+    matches = rule.match(langTool.getAnalyzedSentence("De la A a la z"));
+    assertEquals(0, matches.length);
+    matches = rule.match(langTool.getAnalyzedSentence("Entre I i II."));
+    assertEquals(0, matches.length);
+    //incorrect
+    matches = rule.match(langTool.getAnalyzedSentence("Tots els els homes són iguals."));
+    assertEquals(1, matches.length);
+  }
+
+}
