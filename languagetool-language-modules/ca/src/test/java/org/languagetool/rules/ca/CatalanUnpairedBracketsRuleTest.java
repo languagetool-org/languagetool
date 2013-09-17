683e5e835797b98fcb2f74ec8703b8fe0f86ca0d
jaumeortola <jaumeortola@gmail.com>
2013-09-17 12:19:43 +0200

    [ca] Disable exception for minutes and seconds in UnpairedBracketRule

--- languagetool-language-modules/ca/src/test/java/org/languagetool/rules/ca/CatalanUnpairedBracketsRuleTest.java
+++ languagetool-language-modules/ca/src/test/java/org/languagetool/rules/ca/CatalanUnpairedBracketsRuleTest.java
@@ -57,8 +57,10 @@ public class CatalanUnpairedBracketsRuleTest extends TestCase {
     assertCorrect("—\"Club dels llagoters\" –va repetir en Ron.");
     assertCorrect("»Això em porta a demanar-t'ho.");
     assertCorrect("»Això em porta (sí) a demanar-t'ho.");
-    assertCorrect("el grau en 60 parts iguals, tenim el minut (1'):");
-    assertCorrect("el minut en 60 parts iguals, tenim el segon (1\"):");
+    assertCorrect("al capítol 12 \"Llavors i fruits oleaginosos\"");
+    assertCorrect("\"Per què serveixen les forquilles?\" i aquest respon \"per menjar\".");
+    //assertCorrect("el grau en 60 parts iguals, tenim el minut (1'):");
+    //assertCorrect("el minut en 60 parts iguals, tenim el segon (1\"):");
     
     //assertCorrect("The screen is 20\" wide.");
     assertCorrect("This is a [test] sentence...");
