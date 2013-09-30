22525bad051942c366c46d85089a7eb6c3123f71
Daniel Naber <naber@danielnaber.de>
2013-09-30 21:29:32 +0200

    try again to make a release for Maven Central, using version 2.3.1

--- languagetool-core/src/main/java/org/languagetool/JLanguageTool.java
+++ languagetool-core/src/main/java/org/languagetool/JLanguageTool.java
@@ -83,7 +83,7 @@ import org.xml.sax.SAXException;
  */
 public class JLanguageTool {
 
-  public static final String VERSION = "2.4-SNAPSHOT";
+  public static final String VERSION = "2.3.1";
   public static final String BUILD_DATE = getBuildDate();
 
   public static final String PATTERN_FILE = "grammar.xml";
