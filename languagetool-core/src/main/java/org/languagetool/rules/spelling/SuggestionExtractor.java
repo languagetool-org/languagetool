f6eb738546a9a1d01d3600c934c2674921303869
Daniel Naber <naber@danielnaber.de>
2013-10-01 12:53:25 +0200

    move parts of SuggestionExtractor out of core as they are not needed at runtime

--- languagetool-core/src/main/java/org/languagetool/rules/spelling/SuggestionExtractor.java
+++ languagetool-core/src/main/java/org/languagetool/rules/spelling/SuggestionExtractor.java
@@ -18,14 +18,10 @@
  */
 package org.languagetool.rules.spelling;
 
-import org.languagetool.AnalyzedSentence;
-import org.languagetool.JLanguageTool;
 import org.languagetool.Language;
 import org.languagetool.rules.Rule;
-import org.languagetool.rules.RuleMatch;
 import org.languagetool.rules.patterns.PatternRule;
 
-import java.io.*;
 import java.util.*;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
@@ -92,122 +88,6 @@ public class SuggestionExtractor {
       }
     }
     return tokens;
-  }
-
-  private void writeIgnoreTokensForLanguages() throws IOException {
-    final Map<Language, Set<String>> map = getLanguageToIgnoreTokensMapping();
-    for (Map.Entry<Language, Set<String>> entry : map.entrySet()) {
-      final Language language = entry.getKey();
-      final File langDir = getLanguageDir(language);
-      final File hunspellDir = new File(langDir, "hunspell");
-      if (!hunspellDir.exists()) {
-        System.out.println("No directory " + hunspellDir + " found, ignoring language " + language);
-        continue;
-      }
-      final File ignoreFile = new File(hunspellDir, "ignore.txt");
-      final Set<String> tokens = entry.getValue();
-      try (FileOutputStream fos = new FileOutputStream(ignoreFile)) {
-        try (OutputStreamWriter writer = new OutputStreamWriter(fos, "utf-8")) {
-          writeIntro(writer, language);
-          for (String token : tokens) {
-            writer.write(token);
-            writer.write("\n");
-          }
-        }
-      }
-      System.out.println("Wrote " + tokens.size() + " words to " + ignoreFile);
-    }
-  }
-
-  private void writeIntro(Writer writer, Language language) throws IOException {
-    writer.write("# words to be ignored by the spellchecker (auto-generated)\n");
-    writeArtificialTestCaseItems(writer, language);
-  }
-
-  private void writeArtificialTestCaseItems(Writer writer, Language language) throws IOException {
-    if (language.getShortName().equals("en-US")) {
-      writer.write("anArtificialTestWordForLanguageTool\n");
-    } else if (language.getShortName().equals("de-DE")) {
-      writer.write("einPseudoWortFürLanguageToolTests\n");
-    }
-  }
-
-  /**
-   * We don't support sub-language resources yet, so collect all variants for one language.
-   */
-  private Map<Language, Set<String>> getLanguageToIgnoreTokensMapping() throws IOException {
-    final Map<Language, Set<String>> langToIgnoreTokens = new HashMap<>();
-    for (Language lang : Language.REAL_LANGUAGES) {
-      final Set<String> suggestionTokens = new HashSet<>();
-      final JLanguageTool languageTool = new JLanguageTool(lang);
-      final Rule spellcheckRule = getSpellcheckRule(languageTool);
-      if (spellcheckRule == null) {
-        System.out.println("No spellchecker rule found for " + lang);
-        continue;
-      }
-      languageTool.activateDefaultPatternRules();
-      final List<Rule> rules = languageTool.getAllRules();
-      int tokenCount = 0;
-      int noErrorCount = 0;
-      for (Rule rule : rules) {
-        final List<String> tokens = getSuggestionTokens(rule, lang);
-        tokenCount += tokens.size();
-        for (String token : tokens) {
-          final AnalyzedSentence sentence = languageTool.getAnalyzedSentence(token);
-          final RuleMatch[] matches = spellcheckRule.match(sentence);
-          if (matches.length > 0) {
-            suggestionTokens.add(token);
-          } else {
-            //System.out.println("No error matches for " + lang + ": " + token);
-            noErrorCount++;
-          }
-        }
-      }
-      System.out.println(lang + ": " + noErrorCount + " out of " + tokenCount + " words ignored because they are known to spellchecker anyway");
-      final Language noVariantLanguage = lang.getDefaultLanguageVariant() == null ? lang : lang.getDefaultLanguageVariant();
-      final Set<String> existingTokens = langToIgnoreTokens.get(noVariantLanguage);
-      if (existingTokens != null) {
-        existingTokens.addAll(suggestionTokens);
-      } else {
-        langToIgnoreTokens.put(noVariantLanguage, suggestionTokens);
-      }
-    }
-    return langToIgnoreTokens;
-  }
-
-  private File getLanguageDir(Language language) {
-    final String langCode = language.getShortName();
-    final File dir = new File("org/languagetool/resource", langCode);
-    if (dir.exists()) {
-      return dir;
-    } else {
-      // during development (in SVN):
-      return new File(langCode + "/src/main/resources/org/languagetool/resource/", langCode);
-    }
-  }
-
-  private Rule getSpellcheckRule(JLanguageTool languageTool) {
-    final List<Rule> allActiveRules = languageTool.getAllActiveRules();
-    for (Rule activeRule : allActiveRules) {
-      if (activeRule instanceof SpellingCheckRule) {
-        ((SpellingCheckRule) activeRule).setConsiderIgnoreWords(false);
-        return activeRule;
-      }
-    }
-    return null;
-  }
-
-  public static void main(String[] args) throws IOException {
-    if (Language.REAL_LANGUAGES.length < 5) {
-      throw new RuntimeException("Found only " + Language.REAL_LANGUAGES.length + " languages in classpath. " +
-              "Please run this class with the classpath of 'languagetool-standalone' to have access to all languages.");
-    }
-    final List<String> dirs = Arrays.asList(new File(".").list());
-    if (!dirs.contains("en") || !dirs.contains("de")) {
-      throw new RuntimeException("Please set the working directory to 'languagetool-language-modules' when running this class");
-    }
-    final SuggestionExtractor extractor = new SuggestionExtractor();
-    extractor.writeIgnoreTokensForLanguages();
   }
 
 }
