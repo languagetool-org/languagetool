0a7ddd6528b14a9e9ebd955582b1d24c1d151e66
Daniel Naber <naber@danielnaber.de>
2013-10-06 09:21:59 +0200

    maintainer update

--- languagetool-standalone/src/main/java/org/languagetool/dev/RuleOverview.java
+++ languagetool-standalone/src/main/java/org/languagetool/dev/RuleOverview.java
@@ -40,9 +40,9 @@ import java.util.*;
 public final class RuleOverview {
 
   private static final List<String> LANGUAGES_WITH_NEW_MAINTAINER_NEED = 
-          Arrays.asList("en", "ja", "is", "sv", "lt", "gl", "ro", "ml");
+          Arrays.asList("en", "ja", "is", "sv", "lt", "ro", "ml");
   private static final List<String> LANGUAGES_WITH_CO_MAINTAINER_NEED = 
-          Arrays.asList("da", "be", "zh");
+          Arrays.asList("da", "be", "zh", "gl");
 
   public static void main(final String[] args) throws IOException {
     if (args.length != 1) {
