c38f4facda6fb0b13fdccc49798a111e80cf0685
Daniel Naber <naber@danielnaber.de>
2013-10-23 22:24:14 +0200

    fix logic for token count

--- languagetool-wikipedia/src/main/java/org/languagetool/dev/dumpcheck/SentenceSource.java
+++ languagetool-wikipedia/src/main/java/org/languagetool/dev/dumpcheck/SentenceSource.java
@@ -32,7 +32,7 @@ import java.util.List;
 abstract class SentenceSource implements Iterator<Sentence> {
 
   private static final int MIN_SENTENCE_SIZE = 10;
-  private static final int MIN_SENTENCE_TOKEN_COUNT = 3;
+  private static final int MIN_SENTENCE_TOKEN_COUNT = 4;
   private static final int MAX_SENTENCE_LENGTH = 300;
 
   private final Tokenizer wordTokenizer;
@@ -73,7 +73,7 @@ abstract class SentenceSource implements Iterator<Sentence> {
     int realTokens = 0;
     List<String> allTokens = wordTokenizer.tokenize(sentence);
     for (String token : allTokens) {
-      if (token.trim().isEmpty()) {
+      if (!token.trim().isEmpty()) {
         realTokens++;
       }
     }
