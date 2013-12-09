7332b5624832182b0a379c229c1b5c5e4d45014f
Daniel Naber <naber@danielnaber.de>
2013-12-09 16:40:59 +0100

    script to upload Wikipedia Atom feed checker

---
+++ languagetool-standalone/scripts/deploy-feed-checker.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+CURRENT_DIR=`pwd`
+CURRENT_BASE=`basename $CURRENT_DIR`
+if [ "$(basename $CURRENT_DIR)" != 'scripts' ]; then
+    echo "Error: Please start this script from inside the 'scripts' directory";
+    exit 1;
+fi
+
+TARGET_FILE=LanguageTool-wikipedia-2.4-SNAPSHOT.zip
+
+cd ../..
+mvn clean package -DskipTests
+scp languagetool-wikipedia/target/$TARGET_FILE languagetool@languagetool.org:feed-checker/languagetool/
+echo "Now log on to the server and unzip feed-checker/languagetool/$TARGET_FILE"
