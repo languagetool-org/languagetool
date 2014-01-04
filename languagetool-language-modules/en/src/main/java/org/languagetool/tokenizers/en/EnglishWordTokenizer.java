bac3a8debe7bbd665a9df6a6c463c15e0b963146
milekpl <milek_pl@o2.pl>
2014-01-04 11:36:16 +0100

    [en] I hate git

--- languagetool-language-modules/en/src/main/java/org/languagetool/tokenizers/en/EnglishWordTokenizer.java
+++ languagetool-language-modules/en/src/main/java/org/languagetool/tokenizers/en/EnglishWordTokenizer.java
@@ -18,7 +18,7 @@
  */
 package org.languagetool.tokenizers.en;
 
- import java.util.ArrayList;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.StringTokenizer;
 
@@ -37,16 +37,16 @@ public class EnglishWordTokenizer extends WordTokenizer {
   public List<String> tokenize(final String text) {
     final List<String> l = new ArrayList<>();
     final StringTokenizer st = new StringTokenizer(text,
-         "\u0020\u00A0\u115f\u1160\u1680"
-             + "\u2000\u2001\u2002\u2003\u2004\u2005\u2006\u2007"
-             + "\u2008\u2009\u200A\u200B\u200c\u200d\u200e\u200f"
-             + "\u2028\u2029\u202a\u202b\u202c\u202d\u202e\u202f"
-             + "\u205F\u2060\u2061\u2062\u2063\u206A\u206b\u206c\u206d"
-             + "\u206E\u206F\u3000\u3164\ufeff\uffa0\ufff9\ufffa\ufffb"
-             + ",.;=#()[]{}<>!?:/|\\\"'«»„”“`´‘’‛′…¿¡\t\n\r", true);
+        "\u0020\u00A0\u115f\u1160\u1680"
+            + "\u2000\u2001\u2002\u2003\u2004\u2005\u2006\u2007"
+            + "\u2008\u2009\u200A\u200B\u200c\u200d\u200e\u200f"
+            + "\u2028\u2029\u202a\u202b\u202c\u202d\u202e\u202f"
+            + "\u205F\u2060\u2061\u2062\u2063\u206A\u206b\u206c\u206d"
+            + "\u206E\u206F\u3000\u3164\ufeff\uffa0\ufff9\ufffa\ufffb"
+            + ",.;=#()[]{}<>!?:/|\\\"'«»„”“`´‘’‛′…¿¡\t\n\r", true);
     while (st.hasMoreElements()) {
       final String token = st.nextToken();
-      if (token.endsWith("-")) {
+      if (token.length() > 1 && token.endsWith("-")) {
         l.add(token.substring(0, token.length() - 1));
         l.add("-");
       } else {
