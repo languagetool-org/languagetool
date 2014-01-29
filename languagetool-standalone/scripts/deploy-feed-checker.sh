837d028d1e2502d1af5f92a3a8fbc6d38cc81e5b
Daniel Naber <naber@danielnaber.de>
2014-01-29 16:35:52 +0100

    update script to provide detailed re-deploy instructions

--- languagetool-standalone/scripts/deploy-feed-checker.sh
+++ languagetool-standalone/scripts/deploy-feed-checker.sh
@@ -7,9 +7,19 @@ if [ "$(basename $CURRENT_DIR)" != 'scripts' ]; then
     exit 1;
 fi
 
-TARGET_FILE=LanguageTool-wikipedia-2.5-SNAPSHOT.zip
+BASE_NAME=LanguageTool-wikipedia-2.5-SNAPSHOT
+TARGET_FILE=$BASE_NAME.zip
 
 cd ../..
 mvn clean package -DskipTests
 scp languagetool-wikipedia/target/$TARGET_FILE languagetool@languagetool.org:feed-checker/languagetool/
-echo "Now log on to the server and unzip feed-checker/languagetool/$TARGET_FILE"
+echo "Now log on to the server and execute:"
+echo "  cd /home/languagetool/feed-checker/languagetool && rm -r LanguageTool-wikipedia_bak ; mv LanguageTool-wikipedia LanguageTool-wikipedia_bak && unzip $TARGET_FILE && mv $BASE_NAME LanguageTool-wikipedia"
+echo "Then kill the three running processes:"
+echo "  ps aux | grep java | grep AtomFeedCheckerCmd"
+echo "  => Kill those PIDs"
+echo "Restart the processes:"
+echo "  cd /home/languagetool/feed-checker"
+echo "  nohup ./check-wikipedia-feed-en.sh &"
+echo "  nohup ./check-wikipedia-feed-de.sh &"
+echo "  nohup ./check-wikipedia-feed-fr.sh &"
