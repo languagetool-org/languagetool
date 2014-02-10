7e82d1ae8a6b12ee51dfceae90340f66dfd1f0fa
Marcin Miłkowski <milek_pl@o2.pl>
2014-02-10 12:23:24 +0100

    handle <token min="2"/> and similar, with a test :)

--- languagetool-core/src/main/java/org/languagetool/rules/patterns/XMLRuleHandler.java
+++ languagetool-core/src/main/java/org/languagetool/rules/patterns/XMLRuleHandler.java
@@ -528,9 +528,8 @@ public class XMLRuleHandler extends DefaultHandler {
       tokenElement.setSkipNext(skipPos);
       skipPos = 0;
     }
-    if (minOccurrence > 1) {
-      tokenElement.setMinOccurrence(1);
-    } else if (minOccurrence == 0) {
+
+    if (minOccurrence == 0) {
       tokenElement.setMinOccurrence(0);
     }
     if (maxOccurrence != 1) {
@@ -555,10 +554,10 @@ public class XMLRuleHandler extends DefaultHandler {
     } else if (inOrGroup && orGroupCounter > 0) {
       elementList.get(elementList.size() - 1).setOrGroupElement(tokenElement);
     } else {
-      if (minOccurrence <= 1) {
+      if (minOccurrence < 1) {
         elementList.add(tokenElement);
       }
-      for (int i = 1; i < minOccurrence; i ++) {
+      for (int i = 1; i <= minOccurrence; i ++) {
         elementList.add(tokenElement);
       }
       minOccurrence = 1;
