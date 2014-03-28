460e6d2225f03a54f6881faab8856c30e6924357
Andriy Rysin <arysin@gmail.com>
2014-03-27 21:06:46 -0400

    [uk] dictionary updates; rule cleanup

--- languagetool-language-modules/uk/src/main/resources/org/languagetool/resource/uk/src/generate_dict.sh
+++ languagetool-language-modules/uk/src/main/resources/org/languagetool/resource/uk/src/generate_dict.sh
@@ -5,6 +5,7 @@ FLAGS="-f"
 
 mv -f tagged.main.txt tagged.main.txt.bak
 mv -f tagged.main.dups.txt tagged.main.dups.txt.bak
+mv -f tagged.main.uniq.txt tagged.main.uniq.txt.bak
 
 make -C $spell_uk_dir/src/Dictionary uk_words.tag && \
 make -C $spell_uk_dir/src/Affix uk_affix.tag && \
@@ -13,6 +14,7 @@ make -C $spell_uk_dir/src/Affix uk_affix.tag && \
 mv -f *.dict ukrainian_tags.txt ../
 
 sort tagged.main.txt | uniq -d > tagged.main.dups.txt
+sort tagged.main.txt | uniq -u > tagged.main.uniq.txt
 
-diff tagged.main.txt.bak tagged.main.txt > t.diff
+diff tagged.main.uniq.txt.bak tagged.main.uniq.txt > tu.diff
 diff tagged.main.dups.txt.bak tagged.main.dups.txt > td.diff
