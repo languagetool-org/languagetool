3062f25c092ada33ee870bf9938ac34348696d8e
Daniel Naber <naber@danielnaber.de>
2014-03-31 19:25:56 +0200

    fix second suggestion in case multiple strings need to be merged together (occurred with German rule ART_ADJ_SOL)

--- languagetool-core/src/main/java/org/languagetool/rules/patterns/PatternRuleMatcher.java
+++ languagetool-core/src/main/java/org/languagetool/rules/patterns/PatternRuleMatcher.java
@@ -332,7 +332,7 @@ class PatternRuleMatcher extends AbstractPatternRulePerformer {
     String suggestionRight = "";
     String rightSideNew = rightSide;
     final int sPos = leftSide.lastIndexOf(SUGGESTION_START_TAG);
-    if (sPos > 0) {
+    if (sPos >= 0) {
       suggestionLeft = leftSide.substring(sPos + SUGGESTION_START_TAG.length());
     }
     if (StringTools.isEmpty(suggestionLeft)) {
@@ -341,7 +341,7 @@ class PatternRuleMatcher extends AbstractPatternRulePerformer {
       errorMessage = leftSide.substring(0, leftSide.lastIndexOf(SUGGESTION_START_TAG)) + SUGGESTION_START_TAG;
     }
     final int rPos = rightSide.indexOf(SUGGESTION_END_TAG);
-    if (rPos > 0) {
+    if (rPos >= 0) {
       suggestionRight = rightSide.substring(0, rPos);
     }
     if (!StringTools.isEmpty(suggestionRight)) {
