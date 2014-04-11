10d65d2890b11e5d195cb554515ca0e0f59bb63e
yakovru <yakovr_st@mail.ru>
2014-04-11 23:44:47 +0400

    [ru] remove typo

--- languagetool-language-modules/ru/src/main/resources/org/languagetool/resource/ru/hunspell/make-freq-wordlist-ru.sh
+++ languagetool-language-modules/ru/src/main/resources/org/languagetool/resource/ru/hunspell/make-freq-wordlist-ru.sh
@@ -1,7 +1,5 @@
 #!/bin/sh
-
 echo "Make wordlist. You must convert WordData.txt to UTF-8 encoding first!"
 #Make wordlist frequency data from AOT's WordData.txt for LT spellcheck dictionary
-echo "Make dict" > f.txt
 gawk -f freq-wordlist.awk WordData.txt   > wordlist_ru.txt
 echo "Done!"
