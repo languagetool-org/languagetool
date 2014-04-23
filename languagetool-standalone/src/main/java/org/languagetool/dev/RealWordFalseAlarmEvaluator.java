28d085828afbbb7ffc12771e43ff3bf94b5d03ce
Daniel Naber <naber@danielnaber.de>
2014-04-23 15:11:24 +0200

    false alarm evaluation for the confusion rule

---
+++ languagetool-standalone/src/main/java/org/languagetool/dev/RealWordFalseAlarmEvaluator.java
@@ -0,0 +1,107 @@
+/* LanguageTool, a natural language style checker 
+ * Copyright (C) 2014 Daniel Naber (http://www.danielnaber.de)
+ * 
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301
+ * USA
+ */
+package org.languagetool.dev;
+
+import org.apache.tika.io.IOUtils;
+import org.languagetool.JLanguageTool;
+import org.languagetool.language.BritishEnglish;
+import org.languagetool.rules.Rule;
+import org.languagetool.rules.RuleMatch;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.List;
+
+/**
+ * Runs LanguageTool's confusion rule on Wikipedia-extracted sentences that we assume to be correct.
+ * @since 2.6
+ */
+class RealWordFalseAlarmEvaluator {
+
+  private static final int MAX_SENTENCES = 1000;
+  
+  private final JLanguageTool langTool;
+  
+  private int globalSentenceCount;
+  private int globalRuleMatches;
+
+  RealWordFalseAlarmEvaluator() throws IOException {
+    langTool = new JLanguageTool(new BritishEnglish());
+    langTool.activateDefaultPatternRules();
+    List<Rule> rules = langTool.getAllActiveRules();
+    for (Rule rule : rules) {
+      langTool.disableRule(rule.getId());
+    }
+    langTool.enableRule("CONFUSION_RULE");
+  }
+
+  void run(File dir) throws IOException {
+    File[] files = dir.listFiles();
+    //noinspection ConstantConditions
+    for (File file : files) {
+      if (!file.getName().endsWith(".txt")) {
+        System.out.println("Ignoring " + file + ", does not match *.txt");
+        continue;
+      }
+      try (FileInputStream fis = new FileInputStream(file)) {
+        System.out.println("===== Working on " + file.getName() + " =====");
+        checkLines(IOUtils.readLines(fis), file.getName().replace(".txt", ""));
+      }
+    }
+    System.out.println(globalSentenceCount + " sentences checked");
+    System.out.println(globalRuleMatches + " errors found");
+  }
+
+  private void checkLines(List<String> lines, String name) throws IOException {
+    int sentenceCount = 0;
+    int ruleMatches = 0;
+    for (String line : lines) {
+      List<RuleMatch> matches = langTool.check(line);
+      sentenceCount++;
+      globalSentenceCount++;
+      if (matches.size() > 0) {
+        System.out.println("[" + name + "] " + line + " => " + matches.size());
+        for (RuleMatch match : matches) {
+          System.out.println("    " + match + ": " + match.getSuggestedReplacements());
+          ruleMatches++;
+          globalRuleMatches++;
+        }
+      }
+      if (sentenceCount > MAX_SENTENCES) {
+        System.out.println("Max sentences (" + MAX_SENTENCES + ") reached, stopping");
+        break;
+      }
+    }
+    System.out.println(sentenceCount + " sentences checked");
+    System.out.println(ruleMatches + " errors found");
+    float percentage = ((float)ruleMatches/(float)sentenceCount*100);
+    System.out.printf("%.2f%% of sentences have a match\n\n", percentage);
+  }
+
+  public static void main(String[] args) throws IOException {
+    if (args.length != 1) {
+      System.out.println("Usage: " + RealWordFalseAlarmEvaluator.class.getSimpleName() + " <sentenceDirectory>");
+      System.exit(1);
+    }
+    RealWordFalseAlarmEvaluator evaluator = new RealWordFalseAlarmEvaluator();
+    evaluator.run(new File(args[0]));
+  }
+
+}
