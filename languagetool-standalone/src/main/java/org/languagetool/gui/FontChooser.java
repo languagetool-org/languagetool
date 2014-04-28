7434d0d8945c5cdd2c8133f0921319f415b46ab8
Daniel Naber <naber@danielnaber.de>
2014-04-28 09:00:42 +0200

    add license header; tiny code cleanup

--- languagetool-standalone/src/main/java/org/languagetool/gui/FontChooser.java
+++ languagetool-standalone/src/main/java/org/languagetool/gui/FontChooser.java
@@ -1,7 +1,20 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+/* LanguageTool, a natural language style checker 
+ * Copyright (C) 2014 Panagiotis Minos
+ * 
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301
+ * USA
  */
 package org.languagetool.gui;
 
@@ -46,7 +59,7 @@ import org.languagetool.JLanguageTool;
 class FontChooser extends JDialog implements ActionListener,
         DocumentListener, ListSelectionListener {
 
-  private static final Integer[] fontSizesArray = new Integer[]{
+  private static final Integer[] fontSizesArray = {
     6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 18, 20, 22, 24, 26, 28, 32
   };
   private String[] fontStylesArray;
@@ -304,7 +317,7 @@ class FontChooser extends JDialog implements ActionListener,
 
   @Override
   public void valueChanged(ListSelectionEvent e) {
-    if (e.getValueIsAdjusting() == true) {
+    if (e.getValueIsAdjusting()) {
       return;
     }
 
