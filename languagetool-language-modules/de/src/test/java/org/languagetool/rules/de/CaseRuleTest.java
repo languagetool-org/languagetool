ba9bcae3d7aab61a38ac5a2f97a15015def716f9
Daniel Naber <naber@danielnaber.de>
2014-05-22 21:03:17 +0200

    simplify test code

--- languagetool-language-modules/de/src/test/java/org/languagetool/rules/de/CaseRuleTest.java
+++ languagetool-language-modules/de/src/test/java/org/languagetool/rules/de/CaseRuleTest.java
@@ -30,139 +30,149 @@ import org.languagetool.language.German;
  */
 public class CaseRuleTest extends TestCase {
 
+  private CaseRule rule;
+  private JLanguageTool langTool;
+
+  @Override
+  public void setUp() throws IOException {
+    rule = new CaseRule(null, new German());
+    langTool = new JLanguageTool(new German());
+  }
+
   public void testRuleActivation() throws IOException {
     CaseRule rule = new CaseRule(null, new German());
     assertTrue(rule.supportsLanguage(new German()));
   }
 
   public void testRule() throws IOException {
-    CaseRule rule = new CaseRule(null, new German());
-    JLanguageTool langTool = new JLanguageTool(new German());
 
     // correct sentences:
-    assertEquals(0, rule.match(langTool.getAnalyzedSentence("Dem Hund Futter geben")).length);
-    assertEquals(0, rule.match(langTool.getAnalyzedSentence("Heute spricht Frau Stieg.")).length);
-    assertEquals(0, rule.match(langTool.getAnalyzedSentence("Ein einfacher Satz zum Testen.")).length);
-    assertEquals(0, rule.match(langTool.getAnalyzedSentence("Das Laufen fällt mir leicht.")).length);
-    assertEquals(0, rule.match(langTool.getAnalyzedSentence("Das Winseln stört.")).length);
-    assertEquals(0, rule.match(langTool.getAnalyzedSentence("Das schlägt nicht so zu Buche.")).length);
-    assertEquals(0, rule.match(langTool.getAnalyzedSentence("Dirk Hetzel ist ein Name.")).length);
-    assertEquals(0, rule.match(langTool.getAnalyzedSentence("Sein Verhalten war okay.")).length);
-    assertEquals(0, rule.match(langTool.getAnalyzedSentence("Hier ein Satz. \"Ein Zitat.\"")).length);
-    assertEquals(0, rule.match(langTool.getAnalyzedSentence("Hier ein Satz. 'Ein Zitat.'")).length);
-    assertEquals(0, rule.match(langTool.getAnalyzedSentence("Hier ein Satz. «Ein Zitat.»")).length);
-    assertEquals(0, rule.match(langTool.getAnalyzedSentence("Hier ein Satz. »Ein Zitat.«")).length);
-    assertEquals(0, rule.match(langTool.getAnalyzedSentence("Hier ein Satz. (Noch einer.)")).length);
-    assertEquals(0, rule.match(langTool.getAnalyzedSentence("Hier geht es nach Tel Aviv.")).length);
-    assertEquals(0, langTool.check("Karten werden vom Auswahlstapel gezogen. […] Der Auswahlstapel gehört zum Inhalt.").size());
-//     assertEquals(1, langTool.check("Karten werden vom Auswahlstapel gezogen. [...] Der Auswahlstapel gehört zum Inhalt.").size());
+    assertGood("Dem Hund Futter geben");
+    assertGood("Heute spricht Frau Stieg.");
+    assertGood("Ein einfacher Satz zum Testen.");
+    assertGood("Das Laufen fällt mir leicht.");
+    assertGood("Das Winseln stört.");
+    assertGood("Das schlägt nicht so zu Buche.");
+    assertGood("Dirk Hetzel ist ein Name.");
+    assertGood("Sein Verhalten war okay.");
+    assertGood("Hier ein Satz. \"Ein Zitat.\"");
+    assertGood("Hier ein Satz. 'Ein Zitat.'");
+    assertGood("Hier ein Satz. «Ein Zitat.»");
+    assertGood("Hier ein Satz. »Ein Zitat.«");
+    assertGood("Hier ein Satz. (Noch einer.)");
+    assertGood("Hier geht es nach Tel Aviv.");
     // "NIL" reading in Morphy that used to confuse CaseRule:
-    assertEquals(0, rule.match(langTool.getAnalyzedSentence("Ein Menschenfreund.")).length);
+    assertGood("Ein Menschenfreund.");
     // works only thanks to addex.txt:
-    assertEquals(0, rule.match(langTool.getAnalyzedSentence("Der Nachfahre.")).length);
+    assertGood("Der Nachfahre.");
     // both can be correct:
-    assertEquals(0, rule.match(langTool.getAnalyzedSentence("Hier ein Satz, \"Ein Zitat.\"")).length);
-    assertEquals(0, rule.match(langTool.getAnalyzedSentence("Hier ein Satz, \"ein Zitat.\"")).length);
+    assertGood("Hier ein Satz, \"Ein Zitat.\"");
+    assertGood("Hier ein Satz, \"ein Zitat.\"");
     // Exception 'Le':
-    assertEquals(0, rule.match(langTool.getAnalyzedSentence("Schon Le Monde schrieb das.")).length);
+    assertGood("Schon Le Monde schrieb das.");
     // unknown word:
-    assertEquals(0, rule.match(langTool.getAnalyzedSentence("In Blubberdorf macht man das so.")).length);
+    assertGood("In Blubberdorf macht man das so.");
 
     // sentences that used to trigger an error because of incorrect compound tokenization:
-    assertEquals(0, rule.match(langTool.getAnalyzedSentence("Das sind Euroscheine.")).length);
-    assertEquals(0, rule.match(langTool.getAnalyzedSentence("John Stallman isst.")).length);
-    assertEquals(0, rule.match(langTool.getAnalyzedSentence("Das ist die neue Gesellschafterin hier.")).length);
-    assertEquals(0, rule.match(langTool.getAnalyzedSentence("Das ist die neue Dienerin hier.")).length);
-    assertEquals(0, rule.match(langTool.getAnalyzedSentence("Das ist die neue Geigerin hier.")).length);
-    assertEquals(0, rule.match(langTool.getAnalyzedSentence("Die ersten Gespanne erreichen Köln.")).length);
+    assertGood("Das sind Euroscheine.");
+    assertGood("John Stallman isst.");
+    assertGood("Das ist die neue Gesellschafterin hier.");
+    assertGood("Das ist die neue Dienerin hier.");
+    assertGood("Das ist die neue Geigerin hier.");
+    assertGood("Die ersten Gespanne erreichen Köln.");
     
     // used to trigger error because of wrong POS tagging:
-    assertEquals(0, rule.match(langTool.getAnalyzedSentence("Die Schlinge zieht sich zu.")).length);
-    assertEquals(0, rule.match(langTool.getAnalyzedSentence("Die Schlingen ziehen sich zu.")).length);
+    assertGood("Die Schlinge zieht sich zu.");
+    assertGood("Die Schlingen ziehen sich zu.");
     
     // used to trigger error because of "abbreviation"
-    assertEquals(0, rule.match(langTool.getAnalyzedSentence("Sie fällt auf durch ihre hilfsbereite Art. Zudem zeigt sie soziale Kompetenz.")).length);
+    assertGood("Sie fällt auf durch ihre hilfsbereite Art. Zudem zeigt sie soziale Kompetenz.");
     
     // TODO: nach dem Doppelpunkt wird derzeit nicht auf groß/klein getestet:
-    assertEquals(0, rule.match(langTool.getAnalyzedSentence("Das ist es: kein Satz.")).length);
-    assertEquals(0, rule.match(langTool.getAnalyzedSentence("Das ist es: Kein Satz.")).length);
+    assertGood("Das ist es: kein Satz.");
+    assertGood("Das ist es: Kein Satz.");
 
     // incorrect sentences:
-    assertEquals(1, rule.match(langTool.getAnalyzedSentence("Die Schöne Tür")).length);
-    assertEquals(1, rule.match(langTool.getAnalyzedSentence("Das Blaue Auto.")).length);
-    assertEquals(1, rule.match(langTool.getAnalyzedSentence("Der Grüne Baum.")).length);
-    assertEquals(1, rule.match(langTool.getAnalyzedSentence("Ein Einfacher Satz zum Testen.")).length);
-    assertEquals(1, rule.match(langTool.getAnalyzedSentence("Das Winseln Stört.")).length);
-    assertEquals(1, rule.match(langTool.getAnalyzedSentence("Sein verhalten war okay.")).length);
+    assertBad("Die Schöne Tür");
+    assertBad("Das Blaue Auto.");
+    assertBad("Der Grüne Baum.");
+    assertBad("Ein Einfacher Satz zum Testen.");
+    assertBad("Das Winseln Stört.");
+    assertBad("Sein verhalten war okay.");
     assertEquals(1, langTool.check("Karten werden vom Auswahlstapel gezogen. Auch […] Der Auswahlstapel gehört zum Inhalt.").size());
-//     assertEquals(2, langTool.check("Karten werden vom Auswahlstapel gezogen. Auch [...] Der Auswahlstapel gehört zum Inhalt.").size());
+    //assertEquals(2, langTool.check("Karten werden vom Auswahlstapel gezogen. Auch [...] Der Auswahlstapel gehört zum Inhalt.").size());
+
+    assertEquals(0, langTool.check("Karten werden vom Auswahlstapel gezogen. […] Der Auswahlstapel gehört zum Inhalt.").size());
+    //assertEquals(1, langTool.check("Karten werden vom Auswahlstapel gezogen. [...] Der Auswahlstapel gehört zum Inhalt.").size());
+  }
+
+  private void assertGood(String input) throws IOException {
+    assertEquals("Did not expect error in: '" + input + "'", 0, rule.match(langTool.getAnalyzedSentence(input)).length);
+  }
+
+  private void assertBad(String input) throws IOException {
+    assertEquals("Did not find expected error in: '" + input + "'", 1, rule.match(langTool.getAnalyzedSentence(input)).length);
   }
 
   public void testSubstantivierteVerben() throws IOException {
-    CaseRule rule = new CaseRule(null, new German());
-    JLanguageTool langTool = new JLanguageTool(new German());
-
     // correct sentences:
-    assertEquals(0, rule.match(langTool.getAnalyzedSentence("Das fahrende Auto.")).length);
-    assertEquals(0, rule.match(langTool.getAnalyzedSentence("Das können wir so machen.")).length);
-    assertEquals(0, rule.match(langTool.getAnalyzedSentence("Denn das Fahren ist einfach.")).length);
-    assertEquals(0, rule.match(langTool.getAnalyzedSentence("Das Fahren ist einfach.")).length);
-    assertEquals(0, rule.match(langTool.getAnalyzedSentence("Das Gehen fällt mir leicht.")).length);
-    assertEquals(0, rule.match(langTool.getAnalyzedSentence("Das Ernten der Kartoffeln ist mühsam.")).length);
-    assertEquals(0, rule.match(langTool.getAnalyzedSentence("Entschuldige das späte Weiterleiten.")).length);
-    assertEquals(0, rule.match(langTool.getAnalyzedSentence("Ich liebe das Lesen.")).length);
-    assertEquals(0, rule.match(langTool.getAnalyzedSentence("Das Betreten des Rasens ist verboten.")).length);
-    assertEquals(0, rule.match(langTool.getAnalyzedSentence("Das haben wir aus eigenem Antrieb getan.")).length);
-    assertEquals(0, rule.match(langTool.getAnalyzedSentence("Das haben wir.")).length);
-    assertEquals(0, rule.match(langTool.getAnalyzedSentence("Das haben wir schon.")).length);
-    assertEquals(0, rule.match(langTool.getAnalyzedSentence("Das lesen sie doch sicher in einer Minute durch.")).length);
-    assertEquals(0, rule.match(langTool.getAnalyzedSentence("Das lesen Sie doch sicher in einer Minute durch!")).length);
+    assertGood("Das fahrende Auto.");
+    assertGood("Das können wir so machen.");
+    assertGood("Denn das Fahren ist einfach.");
+    assertGood("Das Fahren ist einfach.");
+    assertGood("Das Gehen fällt mir leicht.");
+    assertGood("Das Ernten der Kartoffeln ist mühsam.");
+    assertGood("Entschuldige das späte Weiterleiten.");
+    assertGood("Ich liebe das Lesen.");
+    assertGood("Das Betreten des Rasens ist verboten.");
+    assertGood("Das haben wir aus eigenem Antrieb getan.");
+    assertGood("Das haben wir.");
+    assertGood("Das haben wir schon.");
+    assertGood("Das lesen sie doch sicher in einer Minute durch.");
+    assertGood("Das lesen Sie doch sicher in einer Minute durch!");
 
     // Source of the following examples: http://www.canoo.net/services/GermanSpelling/Amtlich/GrossKlein/pgf57-58.html
-    assertEquals(0, rule.match(langTool.getAnalyzedSentence("Das Lesen fällt mir schwer.")).length);
-    assertEquals(0, rule.match(langTool.getAnalyzedSentence("Sie hörten ein starkes Klopfen.")).length);
-    assertEquals(0, rule.match(langTool.getAnalyzedSentence("Wer erledigt das Fensterputzen?")).length);
-    assertEquals(0, rule.match(langTool.getAnalyzedSentence("Viele waren am Zustandekommen des Vertrages beteiligt.")).length);
-    assertEquals(0, rule.match(langTool.getAnalyzedSentence("Die Sache kam ins Stocken.")).length);
-    assertEquals(0, rule.match(langTool.getAnalyzedSentence("Das ist zum Lachen.")).length);
-    assertEquals(0, rule.match(langTool.getAnalyzedSentence("Euer Fernbleiben fiel uns auf.")).length);
-    assertEquals(0, rule.match(langTool.getAnalyzedSentence("Uns half nur noch lautes Rufen.")).length);
-    assertEquals(0, rule.match(langTool.getAnalyzedSentence("Die Mitbewohner begnügten sich mit Wegsehen und Schweigen.")).length);
-    assertEquals(0, rule.match(langTool.getAnalyzedSentence("Sie wollte auf Biegen und Brechen gewinnen.")).length);
-    assertEquals(0, rule.match(langTool.getAnalyzedSentence("Er klopfte mit Zittern und Zagen an.")).length);
-    assertEquals(0, rule.match(langTool.getAnalyzedSentence("Ich nehme die Tabletten auf Anraten meiner Ärztin.")).length);
-    assertEquals(0, rule.match(langTool.getAnalyzedSentence("Sie hat ihr Soll erfüllt.")).length);
-    assertEquals(0, rule.match(langTool.getAnalyzedSentence("Dies ist ein absolutes Muss.")).length);
-    assertEquals(0, rule.match(langTool.getAnalyzedSentence("Das Lesen fällt mir schwer.")).length);
+    assertGood("Das Lesen fällt mir schwer.");
+    assertGood("Sie hörten ein starkes Klopfen.");
+    assertGood("Wer erledigt das Fensterputzen?");
+    assertGood("Viele waren am Zustandekommen des Vertrages beteiligt.");
+    assertGood("Die Sache kam ins Stocken.");
+    assertGood("Das ist zum Lachen.");
+    assertGood("Euer Fernbleiben fiel uns auf.");
+    assertGood("Uns half nur noch lautes Rufen.");
+    assertGood("Die Mitbewohner begnügten sich mit Wegsehen und Schweigen.");
+    assertGood("Sie wollte auf Biegen und Brechen gewinnen.");
+    assertGood("Er klopfte mit Zittern und Zagen an.");
+    assertGood("Ich nehme die Tabletten auf Anraten meiner Ärztin.");
+    assertGood("Sie hat ihr Soll erfüllt.");
+    assertGood("Dies ist ein absolutes Muss.");
+    assertGood("Das Lesen fällt mir schwer.");
 
     // incorrect sentences:
-    assertEquals(1, rule.match(langTool.getAnalyzedSentence("Das fahren ist einfach.")).length);
-    assertEquals(1, rule.match(langTool.getAnalyzedSentence("Denn das fahren ist einfach.")).length);
-    assertEquals(1, rule.match(langTool.getAnalyzedSentence("Denn das laufen ist einfach.")).length);
-    assertEquals(1, rule.match(langTool.getAnalyzedSentence("Denn das essen ist einfach.")).length);
-    assertEquals(1, rule.match(langTool.getAnalyzedSentence("Denn das gehen ist einfach.")).length);
-    assertEquals(1, rule.match(langTool.getAnalyzedSentence("Das Große Auto wurde gewaschen.")).length);
-    assertEquals(1, rule.match(langTool.getAnalyzedSentence("Ich habe ein Neues Fahrrad.")).length);
+    assertBad("Das fahren ist einfach.");
+    assertBad("Denn das fahren ist einfach.");
+    assertBad("Denn das laufen ist einfach.");
+    assertBad("Denn das essen ist einfach.");
+    assertBad("Denn das gehen ist einfach.");
+    assertBad("Das Große Auto wurde gewaschen.");
+    assertBad("Ich habe ein Neues Fahrrad.");
     // TODO: detect all the cases not preceded with 'das'
   }
 
   public void testPhraseExceptions() throws IOException {
-    CaseRule rule = new CaseRule(null, new German());
-    JLanguageTool langTool = new JLanguageTool(new German());
-
     // correct sentences:
-    assertEquals(0, rule.match(langTool.getAnalyzedSentence("Das gilt ohne Wenn und Aber.")).length);
-    assertEquals(0, rule.match(langTool.getAnalyzedSentence("ohne Wenn und Aber")).length);
-    assertEquals(0, rule.match(langTool.getAnalyzedSentence("Das gilt ohne Wenn und Aber bla blubb.")).length);
+    assertGood("Das gilt ohne Wenn und Aber.");
+    assertGood("ohne Wenn und Aber");
+    assertGood("Das gilt ohne Wenn und Aber bla blubb.");
     // as long as phrase exception isn't complete, there's no error:
-    assertEquals(0, rule.match(langTool.getAnalyzedSentence("Das gilt ohne wenn")).length);
-    assertEquals(0, rule.match(langTool.getAnalyzedSentence("Das gilt ohne wenn und")).length);
-    assertEquals(0, rule.match(langTool.getAnalyzedSentence("wenn und aber")).length);
-    assertEquals(0, rule.match(langTool.getAnalyzedSentence("und aber")).length);
-    assertEquals(0, rule.match(langTool.getAnalyzedSentence("aber")).length);
+    assertGood("Das gilt ohne wenn");
+    assertGood("Das gilt ohne wenn und");
+    assertGood("wenn und aber");
+    assertGood("und aber");
+    assertGood("aber");
     // incorrect sentences:
     // error not found here as it's in the XML rules:
-    //assertEquals(1, rule.match(langTool.getAnalyzedSentence("Das gilt ohne wenn und aber.")).length);
+    //assertBad("Das gilt ohne wenn und aber.");
   }
   
 }
