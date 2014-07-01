458a3dc3416e3c7a7bc45f79a0386c98ce484dd7
Daniel Naber <naber@danielnaber.de>
2014-07-01 14:17:01 +0200

    fix error vs. correction

--- languagetool-dev/src/main/java/org/languagetool/dev/errorcorpus/TrainingDataGenerator.java
+++ languagetool-dev/src/main/java/org/languagetool/dev/errorcorpus/TrainingDataGenerator.java
@@ -30,7 +30,7 @@ import java.util.List;
 /**
  * Takes a gold-standard corpus and creates training data
  * for it by looking up the available ngram numbers and the expected result
- * (0 = no error, 1 = error). For example, these inputs
+ * (1 = error, 1 = no error). For example, these inputs
  * 
  * <pre>"This is there report."
  *   is there = 0.2
@@ -43,8 +43,8 @@ import java.util.List;
  * ...would produce training data like this:
  * 
  * <pre>
- * 0.2 0.0001 1
- * 0.2 0.3    0
+ * 0.2 0.0001 0
+ * 0.2 0.3    1
  * </pre>
  * @author Daniel Naber 
  */
@@ -65,11 +65,11 @@ class TrainingDataGenerator {
       for (Error error : errors) {
         try {
           List<String> errorContext = get2gramContext(sentence, error, false);
-          writeTrainingData(errorContext);
+          writeTrainingData(errorContext, false);
           List<String> correctedContext = get2gramContext(sentence, error, true);
-          writeTrainingData(correctedContext);
+          writeTrainingData(correctedContext, true);
         } catch (AmbiguousErrorPositionException e) {
-          System.err.println("Ignoring sentence due to limited algorithm: '" + sentence + "'");
+          System.err.println("Ignoring sentence due to limited algorithm: " + e.getMessage());
           problemCount++;
         }
       }
@@ -81,11 +81,12 @@ class TrainingDataGenerator {
     String plainText = sentence.getAnnotatedText().getPlainText();
     List<String> tokens = removeWhitespaceTokens(tokenizer.tokenize(plainText));
     String markupText = sentence.getMarkupText();
-    String errorOrCorrection = useCorrection ? error.getCorrection() : markupText.substring(error.getStartPos(), error.getEndPos());
+    String errorToken = markupText.substring(error.getStartPos(), error.getEndPos());
+    String errorOrCorrection = useCorrection ? error.getCorrection() : errorToken;
     List<Integer> errorTokens = new ArrayList<>();
     int i = 0;
     for (String token : tokens) {
-      if (token.equals(errorOrCorrection)) {
+      if (token.equals(errorToken)) {
         errorTokens.add(i);
       }
       i++;
@@ -97,25 +98,26 @@ class TrainingDataGenerator {
     List<String> contextTokens = new ArrayList<>();
     if (errorTokenPos == 0) {
       contextTokens.add(LanguageModel.GOOGLE_SENTENCE_START);
-      contextTokens.add(tokens.get(errorTokenPos));
+      contextTokens.add(errorOrCorrection);
       contextTokens.add(tokens.get(errorTokenPos + 1));
     } else if (errorTokenPos >= tokens.size()) {
       contextTokens.add(tokens.get(errorTokenPos - 1));
-      contextTokens.add(tokens.get(errorTokenPos));
+      contextTokens.add(errorOrCorrection);
       contextTokens.add(LanguageModel.GOOGLE_SENTENCE_END);
     } else {
       contextTokens.add(tokens.get(errorTokenPos - 1));
-      contextTokens.add(tokens.get(errorTokenPos));
+      contextTokens.add(errorOrCorrection);
       contextTokens.add(tokens.get(errorTokenPos + 1));
     }
     return contextTokens;
   }
 
-  private void writeTrainingData(List<String> context) {
+  private void writeTrainingData(List<String> context, boolean isCorrected) {
     long leftCount = languageModel.getCount(context.get(0), context.get(1));
     long rightCount = languageModel.getCount(context.get(1), context.get(2));
     // the final part is just for debugging:
-    System.out.println(leftCount + "\t" + rightCount + "\t" + "01?" + "\t" + context);  // TODO: 0 or 1
+    int targetValue = isCorrected ? 1 : 0;
+    System.out.println(leftCount + "\t" + rightCount + "\t" + targetValue + "\t" + context);
   }
 
   private List<String> removeWhitespaceTokens(List<String> tokens) {
