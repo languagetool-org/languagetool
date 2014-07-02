ed5ce9d8b42a7849f94a96d09035218c67e1eb99
Daniel Naber <naber@danielnaber.de>
2014-07-02 15:34:12 +0200

    cleanup

--- languagetool-dev/src/test/java/org/languagetool/dev/errorcorpus/TrainingDataGeneratorTest.java
+++ languagetool-dev/src/test/java/org/languagetool/dev/errorcorpus/TrainingDataGeneratorTest.java
@@ -18,39 +18,58 @@
  */
 package org.languagetool.dev.errorcorpus;
 
+import org.encog.Encog;
+import org.encog.engine.network.activation.ActivationSigmoid;
+import org.encog.ml.data.MLData;
+import org.encog.ml.data.MLDataPair;
+import org.encog.ml.data.MLDataSet;
+import org.encog.ml.data.basic.BasicMLDataSet;
+import org.encog.neural.networks.BasicNetwork;
+import org.encog.neural.networks.layers.BasicLayer;
+import org.encog.neural.networks.training.propagation.resilient.ResilientPropagation;
 import org.junit.Ignore;
 import org.junit.Test;
-import org.neuroph.core.NeuralNetwork;
-import org.neuroph.core.learning.DataSet;
-import org.neuroph.core.learning.DataSetRow;
-import org.neuroph.nnet.Perceptron;
-
-import java.util.Arrays;
 
 public class TrainingDataGeneratorTest {
   
   @Test
   @Ignore("Interactive use only")
-  public void testCreateNetworkWithNeuroph() {
-    NeuralNetwork neuralNetwork = new Perceptron(2, 1);
-    DataSet trainingSet = new DataSet(2, 1);
-    trainingSet.addRow(new DataSetRow(new double[]{0, 0}, new double[]{0}));
-    trainingSet.addRow(new DataSetRow(new double[]{0, 1}, new double[]{1}));
-    trainingSet.addRow(new DataSetRow(new double[]{1, 0}, new double[]{1}));
-    trainingSet.addRow(new DataSetRow(new double[]{1, 1}, new double[]{1}));
-    neuralNetwork.learn(trainingSet);
-    neuralNetwork.save("/tmp/or_perceptron.nnet");
+  public void testEncog() {
+
+    double[][] XOR_INPUT = { { 0.0, 0.0 }, { 1.0, 0.0 }, { 0.0, 1.0 }, { 1.0, 1.0 } };
+    double[][] XOR_IDEAL = { { 0.0 }, { 1.0 }, { 1.0 }, { 0.0 } };
+    
+    BasicNetwork network = new BasicNetwork();
+    network.addLayer(new BasicLayer(null,true,2));
+    network.addLayer(new BasicLayer(new ActivationSigmoid(),true,3));
+    network.addLayer(new BasicLayer(new ActivationSigmoid(),false,1));
+    network.getStructure().finalizeStructure();
+    network.reset();
+
+    MLDataSet trainingSet = new BasicMLDataSet(XOR_INPUT, XOR_IDEAL);
+
+    final ResilientPropagation train = new ResilientPropagation(network, trainingSet);
+    int epoch = 1;
+    do {
+      train.iteration();
+      System.out.println("Epoch #" + epoch + " Error:" + train.getError());
+      epoch++;
+    } while(train.getError() > 0.01);
+    train.finishTraining();
+    
+    System.out.println("dump:");
+    System.out.println(network.dumpWeights());
+    System.out.println("-----");
+    
+    // test the neural network:
+    System.out.println("Neural Network Results:");
+    for(MLDataPair pair: trainingSet ) {
+      final MLData output = network.compute(pair.getInput());
+      System.out.println(pair.getInput().getData(0) + "," + pair.getInput().getData(1)
+              + ", actual=" + output.getData(0) + ",ideal=" + pair.getIdeal().getData(0));
+    }
+
+    Encog.getInstance().shutdown();
   }
 
-  @Test
-  @Ignore("Interactive use only")
-  public void testUseNeurophNetwork() {
-    NeuralNetwork neuralNetwork = NeuralNetwork.load("/tmp/or_perceptron.nnet");
-    //neuralNetwork2.setInput(1, 1); // 1.0
-    //neuralNetwork2.setInput(0, 0); // 0.0
-    neuralNetwork.setInput(1, 0); // 1.0 
-    neuralNetwork.calculate();
-    double[] networkOutput = neuralNetwork.getOutput();
-    System.out.println(Arrays.toString(networkOutput));
-  }
 }
