ad809fc755e5870b387c1382142d196eb058666b
Daniel Naber <naber@danielnaber.de>
2014-07-11 16:13:41 +0200

    initial support for Tamil

---
+++ languagetool-language-modules/ta/src/main/java/org/languagetool/language/Tamil.java
@@ -0,0 +1,71 @@
+/* LanguageTool, a natural language style checker 
+ * Copyright (C) 2014 Daniel Naber (http://www.danielnaber.de)
+ * 
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301
+ * USA
+ */
+package org.languagetool.language;
+
+import org.languagetool.Language;
+import org.languagetool.rules.Rule;
+import org.languagetool.tokenizers.SentenceTokenizer;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class Tamil extends Language {
+
+  private SentenceTokenizer sentenceTokenizer;
+  private String name = "Tamil";
+
+  @Override
+  public String getName() {
+    return name;
+  }
+
+  @Override
+  public void setName(final String name) {
+    this.name = name;
+  }
+
+  @Override
+  public final String getShortName() {
+    return "ta";
+  }
+
+  @Override
+  public final String[] getCountries() {
+    return new String[]{"IN"};
+  }
+  
+  /*@Override
+  public SentenceTokenizer getSentenceTokenizer() {
+    if (sentenceTokenizer == null) {
+      sentenceTokenizer = new SRXSentenceTokenizer(this);
+    }
+    return sentenceTokenizer;
+  }*/
+
+  @Override
+  public final Contributor[] getMaintainers() {
+    return new Contributor[] {new Contributor("Elanjelian Venugopal")};
+  }
+
+  @Override
+  public List<Class<? extends Rule>> getRelevantRules() {
+    return Arrays.asList();
+  }
+
+}
