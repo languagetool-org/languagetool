19d99ec804b7957036d6cf8309e1f0dcfa2314e9
Daniel Naber <naber@danielnaber.de>
2014-08-17 11:11:26 +0200

    re-indent only

--- languagetool-language-modules/fr/src/test/java/org/languagetool/rules/spelling/hunspell/TestFrenchDisambiguator.java
+++ languagetool-language-modules/fr/src/test/java/org/languagetool/rules/spelling/hunspell/TestFrenchDisambiguator.java
@@ -9,36 +9,35 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.util.List;
 
-
 public class TestFrenchDisambiguator implements Disambiguator {
 
-    @Override
-    public AnalyzedSentence disambiguate(AnalyzedSentence input) throws IOException {
-        AnalyzedSentence sentence = input;
-        List<DisambiguationPatternRule> disambiguationRules = null;
-        InputStream inputStream = null;
-        final String filePath = "/disambiguator.xml";
+  @Override
+  public AnalyzedSentence disambiguate(AnalyzedSentence input) throws IOException {
+    AnalyzedSentence sentence = input;
+    List<DisambiguationPatternRule> disambiguationRules = null;
+    InputStream inputStream = null;
+    final String filePath = "/disambiguator.xml";
+    try {
+      final DisambiguationRuleLoader ruleLoader = new DisambiguationRuleLoader();
+      inputStream = getClass().getResourceAsStream(filePath);
+      disambiguationRules = ruleLoader.getRules(inputStream);
+      for (final DisambiguationPatternRule patternRule : disambiguationRules) {
+        sentence = patternRule.replace(sentence);
+      }
+
+    } catch (Exception e) {
+      throw new RuntimeException("Problems with loading disambiguation file: " + filePath, e);
+    } finally {
+      if (inputStream != null) {
         try {
-            final DisambiguationRuleLoader ruleLoader = new DisambiguationRuleLoader();
-            inputStream = getClass().getResourceAsStream(filePath);
-            disambiguationRules = ruleLoader.getRules(inputStream);
-            for (final DisambiguationPatternRule patternRule : disambiguationRules) {
-                sentence = patternRule.replace(sentence);
-            }
-
-        } catch (Exception e) {
-            throw new RuntimeException("Problems with loading disambiguation file: " + filePath, e);
-        } finally {
-            if (inputStream != null) {
-                try {
-                    inputStream.close();
-                } catch (IOException e) {
-                    // nothing to do
-                }
-            }
+          inputStream.close();
+        } catch (IOException e) {
+          // nothing to do
         }
-
-        return sentence;
+      }
     }
+
+    return sentence;
+  }
 
 }
