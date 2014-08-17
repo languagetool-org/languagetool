babfa6712a0191b8e7333984ae6a23d4fe05d5de
Daniel Naber <naber@danielnaber.de>
2014-08-17 11:24:30 +0200

    code cleanup

--- languagetool-language-modules/fr/src/test/java/org/languagetool/rules/spelling/hunspell/TestFrenchDisambiguator.java
+++ languagetool-language-modules/fr/src/test/java/org/languagetool/rules/spelling/hunspell/TestFrenchDisambiguator.java
@@ -27,34 +27,21 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.util.List;
 
-public class TestFrenchDisambiguator implements Disambiguator {
+class TestFrenchDisambiguator implements Disambiguator {
 
   @Override
   public AnalyzedSentence disambiguate(AnalyzedSentence input) throws IOException {
     AnalyzedSentence sentence = input;
-    List<DisambiguationPatternRule> disambiguationRules = null;
-    InputStream inputStream = null;
-    final String filePath = "/disambiguator.xml";
-    try {
+    String filePath = "/disambiguator.xml";
+    try (InputStream inputStream = getClass().getResourceAsStream(filePath)) {
       final DisambiguationRuleLoader ruleLoader = new DisambiguationRuleLoader();
-      inputStream = getClass().getResourceAsStream(filePath);
-      disambiguationRules = ruleLoader.getRules(inputStream);
+      List<DisambiguationPatternRule> disambiguationRules = ruleLoader.getRules(inputStream);
       for (final DisambiguationPatternRule patternRule : disambiguationRules) {
         sentence = patternRule.replace(sentence);
       }
-
     } catch (Exception e) {
       throw new RuntimeException("Problems with loading disambiguation file: " + filePath, e);
-    } finally {
-      if (inputStream != null) {
-        try {
-          inputStream.close();
-        } catch (IOException e) {
-          // nothing to do
-        }
-      }
     }
-
     return sentence;
   }
 
