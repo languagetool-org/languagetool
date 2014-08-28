64adb94dfb563947fa7a57f8c650231f07e2dc1a
Daniel Naber <naber@danielnaber.de>
2014-08-28 15:50:50 +0200

    179 avoid code duplication (#179)

--- languagetool-language-modules/fa/src/main/java/org/languagetool/rules/fa/WordCoherencyRule.java
+++ languagetool-language-modules/fa/src/main/java/org/languagetool/rules/fa/WordCoherencyRule.java
@@ -18,49 +18,33 @@
  */
 package org.languagetool.rules.fa;
 
-import java.io.IOException;
-import java.io.InputStream;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-import java.util.ResourceBundle;
-import java.util.Scanner;
+import org.languagetool.rules.AbstractWordCoherencyRule;
 
-import org.languagetool.AnalyzedSentence;
-import org.languagetool.AnalyzedToken;
-import org.languagetool.AnalyzedTokenReadings;
-import org.languagetool.JLanguageTool;
-import org.languagetool.rules.Category;
-import org.languagetool.rules.Example;
-import org.languagetool.rules.RuleMatch;
+import java.io.IOException;
+import java.util.ResourceBundle;
 
 /**
- * A rule that matches words for which two different spellings are used
- * throughout the document. Currently only implemented for German. Loads
- * the relevant words from <code>rules/fa/coherency.txt</code>.
- * 
- * <p>Note that this should not be used for language variations like
- * American English vs. British English or German "alte Rechtschreibung"
- * vs. "neue Rechtschreibung" -- that's the task of a spell checker.
+ * Persian version of {@link org.languagetool.rules.AbstractWordCoherencyRule}.
  * 
  * @since 2.7
  */
-public class WordCoherencyRule extends PersianRule {
-
-  private static final String FILE_NAME = "/fa/coherency.txt";
-  private static final String FILE_ENCODING = "utf-8";
-  
-  private final Map<String, String> relevantWords;        // e.g. "aufwendig -> aufwändig"
-  private Map<String, RuleMatch> shouldNotAppearWord = new HashMap<>();  // e.g. aufwändig -> RuleMatch of aufwendig
+public class WordCoherencyRule extends AbstractWordCoherencyRule {
 
   public WordCoherencyRule(ResourceBundle messages) throws IOException {
-    if (messages != null) {
-      super.setCategory(new Category(messages.getString("category_misc")));
-    }
-    relevantWords = loadWords(JLanguageTool.getDataBroker().getFromRulesDirAsStream(FILE_NAME));
+    super(messages);
+    // TODO:
     //addExamplePair(Example.wrong("من در <marker>اطاق</marker> تو را دیدم."),
     //               Example.fixed("من در <marker>اتاق</marker> تو را دیدم."));
+  }
+
+  @Override
+  protected String getFilePath() {
+    return "/fa/coherency.txt";
+  }
+
+  @Override
+  protected String getMessage(String word1, String word2) {
+    return "'" + word1 + "' و '"  + word2 + "' نباید در یک جا استفاده شوند";
   }
   
   @Override
@@ -71,68 +55,6 @@ public class WordCoherencyRule extends PersianRule {
   @Override
   public String getDescription() {
     return "چند املا برای یک کلمه که یکی از آنها اولویت بیشتری دارد";
-  }
-
-  @Override
-  public RuleMatch[] match(AnalyzedSentence sentence) {
-    final List<RuleMatch> ruleMatches = new ArrayList<>();
-    final AnalyzedTokenReadings[] tokens = sentence.getTokensWithoutWhitespace();
-    int pos = 0;
-    for (AnalyzedTokenReadings tmpToken : tokens) {
-      String token = tmpToken.getToken();
-      final String origToken = token;
-      final List<AnalyzedToken> readings = tmpToken.getReadings();
-      // TODO: in theory we need to care about the other readings, too (affects e.g. German "Schenke" as a noun):
-      if (readings != null && readings.size() > 0) {
-        final String baseform = readings.get(0).getLemma();
-        if (baseform != null) {
-          token = baseform;
-        }
-      }
-      if (shouldNotAppearWord.containsKey(token)) {
-        final RuleMatch otherMatch = shouldNotAppearWord.get(token);
-        final String otherSpelling = otherMatch.getMessage();
-        final String msg = "'" + token + "' و '" + otherSpelling +
-                "' نباید در یک جا استفاده شوند";
-        final RuleMatch ruleMatch = new RuleMatch(this, pos, pos + origToken.length(), msg);
-        ruleMatch.setSuggestedReplacement(otherSpelling);
-        ruleMatches.add(ruleMatch);
-      } else if (relevantWords.containsKey(token)) {
-        final String shouldNotAppear = relevantWords.get(token);
-        // only used to display this spelling variation if the other one really occurs:
-        final RuleMatch potentialRuleMatch = new RuleMatch(this, pos, pos + origToken.length(), token);
-        shouldNotAppearWord.put(shouldNotAppear, potentialRuleMatch);
-      }
-      pos += tmpToken.getToken().length();
-    }
-    return toRuleMatchArray(ruleMatches);
-  }
-
-  private Map<String, String> loadWords(InputStream stream) throws IOException {
-    final Map<String, String> map = new HashMap<>();
-    try (Scanner scanner = new Scanner(stream, FILE_ENCODING)) {
-      while (scanner.hasNextLine()) {
-        final String line = scanner.nextLine().trim();
-        if (line.length() < 1) {
-          continue;
-        }
-        if (line.charAt(0) == '#') {      // ignore comments
-          continue;
-        }
-        final String[] parts = line.split(";");
-        if (parts.length != 2) {
-          throw new IOException("خطای ساختاری در پروندهٔ  " + JLanguageTool.getDataBroker().getFromRulesDirAsUrl(FILE_NAME) + "، خط: " + line);
-        }
-        map.put(parts[0], parts[1]);
-        map.put(parts[1], parts[0]);
-      }
-    }
-    return map;
-  }
-  
-  @Override
-  public void reset() {
-    shouldNotAppearWord = new HashMap<>();
   }
 
 }
