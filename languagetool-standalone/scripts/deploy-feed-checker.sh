3ae1a138229848bc93b99e05afc812bbfa297cbb
Daniel Naber <naber@danielnaber.de>
2014-09-22 15:26:30 +0200

    fix script

--- languagetool-standalone/scripts/deploy-feed-checker.sh
+++ languagetool-standalone/scripts/deploy-feed-checker.sh
@@ -17,7 +17,7 @@ scp -i ~/.ssh/wikipedia/toollabs languagetool-wikipedia/target/$TARGET_FILE tool
 echo "Now log on to the server and execute:"
 echo "  mv $TARGET_FILE /data/project/languagetool/"
 echo "  become languagetool"
-echo "  cd feedchecker && mv LanguageTool-wikipedia LanguageTool-wikipedia_bak && unzip ../$TARGET_FILE && mv $BASE_NAME LanguageTool-wikipedia"
+echo "  cd feedchecker && rm -r LanguageTool-wikipedia_bak && mv LanguageTool-wikipedia LanguageTool-wikipedia_bak && unzip ../$TARGET_FILE && mv $BASE_NAME LanguageTool-wikipedia"
 echo "  ./stop-all.sh"
 echo "Wait a bit until the processes are gone (see 'qstat')..."
 echo "  ./start-all.sh"
