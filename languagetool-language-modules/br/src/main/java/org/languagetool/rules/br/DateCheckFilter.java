13494e6533f1d7b825e8e561133de460c492cbe8
Dominique Pelle <dominique.pelle@gmail.com>
2014-10-19 19:21:59 +0200

    [br] detect invalid dates with days of month written with letters (which      is fairly common in Breton).

--- languagetool-language-modules/br/src/main/java/org/languagetool/rules/br/DateCheckFilter.java
+++ languagetool-language-modules/br/src/main/java/org/languagetool/rules/br/DateCheckFilter.java
@@ -36,6 +36,43 @@ public class DateCheckFilter extends AbstractDateCheckFilter {
 
   @SuppressWarnings("ControlFlowStatementWithoutBraces")
   @Override
+  protected int getDayOfMonth(String dayStr) {
+    String day = dayStr.toLowerCase();
+    if (day.charAt(0) == 't') day = 'd' + day.substring(1);
+    if (day.charAt(0) == 'p') day = 'b' + day.substring(1);
+    if (day.endsWith("vet")) {
+      // Removing final vet if any.
+      day = day.substring(0, day.length() - 3);
+    }
+
+    if (day.equals("c’hentañ") || day.equals("unan"))    return 1;
+    if (day.equals("daou")     || day.equals("eil"))     return 2;
+    if (day.equals("dri")      || day.equals("drede") || day.equals("deir")) return 3;
+    if (day.equals("bevar"))                             return 4;
+    // bemp or bemvet (vet has been removed).
+    if (day.equals("bemp")     || day.equals("bem"))     return 5;
+    if (day.equals("c’hwerc’h"))                         return 6;
+    if (day.equals("seizh"))                             return 7;
+    if (day.equals("eizh"))                              return 8;
+    // nav and navet (vet has been removed).
+    if (day.equals("nav")      || day.equals("na"))     return 9;
+    if (day.equals("dek"))                              return 10;
+    if (day.equals("unnek"))                            return 11;
+    if (day.equals("daouzek"))                          return 12;
+    if (day.equals("drizek"))                           return 13;
+    if (day.equals("bevazek"))                          return 14;
+    if (day.equals("bemzek"))                           return 15;
+    if (day.equals("c’hwezek"))                         return 16;
+    if (day.equals("seitek"))                           return 17;
+    if (day.equals("driwec’h"))                         return 18;
+    if (day.equals("naontek"))                          return 19;
+    if (day.equals("ugent"))                            return 20;
+    if (day.equals("dregont"))                          return 30;
+    return 0;
+  }
+
+  @SuppressWarnings("ControlFlowStatementWithoutBraces")
+  @Override
   protected int getDayOfWeek(String dayStr) {
     String day = dayStr.toLowerCase();
     if (day.endsWith("sul"))      return Calendar.SUNDAY;
