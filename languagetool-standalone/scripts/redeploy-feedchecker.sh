fc01fcb396420424eaff51546e7b15f091452797
Daniel Naber <naber@danielnaber.de>
2014-10-20 13:49:40 +0200

    auto-deploy the WikiCheck Feed Checker command line apps at Tool Labs

---
+++ languagetool-standalone/scripts/redeploy-feedchecker.sh
@@ -0,0 +1,28 @@
+#!/bin/sh
+# dnaber, 2014-10-20
+# This script is running on WikiCheck Tool Labs.
+# Re-deploy the command-line app to check Wikipedia Recent Changes.
+# This is script is to be called by create-snapshot.sh from the main LanguageTool server.
+
+WHOAMI=`whoami`
+if [ $WHOAMI != "tools.languagetool" ]
+then
+  echo "This script is supposed to be run on Tool Labs as user 'languagetool'. Stopping."
+  exit
+fi
+
+DOWNLOAD_DATE=`date +%Y%m%d`
+#DOWNLOAD_DATE=20141019
+
+cd ~/feedchecker && \
+  wget https://languagetool.org/download/snapshots/LanguageTool-wikipedia-$DOWNLOAD_DATE-snapshot.zip && \
+  rm -r LanguageTool-wikipedia_bak ; \
+  mv LanguageTool-wikipedia LanguageTool-wikipedia_bak && \
+  unzip LanguageTool-wikipedia-$DOWNLOAD_DATE-snapshot.zip && \
+  rm LanguageTool-wikipedia-$DOWNLOAD_DATE-snapshot.zip && \
+  mv LanguageTool-wikipedia-*-SNAPSHOT LanguageTool-wikipedia && \
+  echo "Stopping all command line feed checker apps..." && \
+  ./stop-all.sh && \
+  echo "Sleeping 30 seconds to wait for shutdown..." && \
+  sleep 30 && \
+  ./start-all.sh
