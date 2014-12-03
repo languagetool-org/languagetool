97ca289c332a5809462583911b61a3577f28175e
Andriy Rysin <arysin@gmail.com>
2014-12-02 20:24:03 -0500

    [uk] new rule for numeric/noun agreement; dictionary update

--- languagetool-language-modules/uk/src/main/resources/org/languagetool/resource/uk/src/generate_dict.sh
+++ languagetool-language-modules/uk/src/main/resources/org/languagetool/resource/uk/src/generate_dict.sh
@@ -11,9 +11,10 @@ make -C $spell_uk_dir/src/Dictionary uk_words.tag && \
 make -C $spell_uk_dir/src/Affix uk_affix.tag && \
 $spell_uk_dir/bin/tag/mk_pos_dict.py && \
 echo "Running regression diffs..." && \
-sort tagged.main.txt | uniq -d > tagged.main.dups.txt && \
-sort tagged.main.txt | uniq -u > tagged.main.uniq.txt && \
+sort tagged.main.txt | tee tagged.main.sorted.tmp | uniq -d > tagged.main.dups.txt && \
+cat tagged.main.sorted.tmp | uniq -u > tagged.main.uniq.txt && \
 (diff tagged.main.uniq.txt.bak tagged.main.uniq.txt > tu.diff || /bin/true) && \
 (diff tagged.main.dups.txt.bak tagged.main.dups.txt > td.diff || /bin/true) && \
 ./make-dict-uk-mfl.sh $FLAGS && \
 mv -f *.dict ukrainian_tags.txt ../
+rm -f tagged.main.sorted.tmp
