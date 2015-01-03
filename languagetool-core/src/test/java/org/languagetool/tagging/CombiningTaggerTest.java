dbe7dc5ea5cbbfdec6877df88ee9c39bf9a3425a
Daniel Naber <naber@danielnaber.de>
2015-01-03 11:02:38 +0100

    tagger make test cover overwrite mode

--- languagetool-core/src/test/java/org/languagetool/tagging/CombiningTaggerTest.java
+++ languagetool-core/src/test/java/org/languagetool/tagging/CombiningTaggerTest.java
@@ -13,13 +13,33 @@ import static org.junit.Assert.assertTrue;
 public class CombiningTaggerTest {
 
   @Test
-  public void testTag() throws Exception {
-    ManualTagger tagger1 = new ManualTagger(JLanguageTool.getDataBroker().getFromResourceDirAsStream("/xx/added1.txt"));
-    ManualTagger tagger2 = new ManualTagger(JLanguageTool.getDataBroker().getFromResourceDirAsStream("/xx/added2.txt"));
-    CombiningTagger tagger = new CombiningTagger(tagger1, tagger2, false);
+  public void testTagNoOverwrite() throws Exception {
+    CombiningTagger tagger = getCombiningTagger(false);
     assertThat(tagger.tag("nosuchword").size(), is(0));
     List<TaggedWord> result = tagger.tag("fullform");
     assertThat(result.size(), is(2));
+    String asString = getAsString(result);
+    assertTrue(asString.contains("baseform1/POSTAG1"));
+    assertTrue(asString.contains("baseform2/POSTAG2"));
+  }
+
+  @Test
+  public void testTagOverwrite() throws Exception {
+    CombiningTagger tagger = getCombiningTagger(true);
+    assertThat(tagger.tag("nosuchword").size(), is(0));
+    List<TaggedWord> result = tagger.tag("fullform");
+    assertThat(result.size(), is(1));
+    String asString = getAsString(result);
+    assertTrue(asString.contains("baseform2/POSTAG2"));
+  }
+
+  private CombiningTagger getCombiningTagger(boolean overwrite) throws IOException {
+    ManualTagger tagger1 = new ManualTagger(JLanguageTool.getDataBroker().getFromResourceDirAsStream("/xx/added1.txt"));
+    ManualTagger tagger2 = new ManualTagger(JLanguageTool.getDataBroker().getFromResourceDirAsStream("/xx/added2.txt"));
+    return new CombiningTagger(tagger1, tagger2, overwrite);
+  }
+
+  private String getAsString(List<TaggedWord> result) {
     StringBuilder sb = new StringBuilder();
     for (TaggedWord taggedWord : result) {
       sb.append(taggedWord.getLemma());
@@ -27,13 +47,12 @@ public class CombiningTaggerTest {
       sb.append(taggedWord.getPosTag());
       sb.append("\n");
     }
-    assertTrue(sb.toString().contains("baseform1/POSTAG1"));
-    assertTrue(sb.toString().contains("baseform2/POSTAG2"));
+    return sb.toString();
   }
 
   @Test(expected = IOException.class)
   public void testInvalidFile() throws Exception {
     new ManualTagger(JLanguageTool.getDataBroker().getFromResourceDirAsStream("/xx/added-invalid.txt"));
- }
+  }
 
 }
