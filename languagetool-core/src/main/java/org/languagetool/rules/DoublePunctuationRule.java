c714629f9bd2feac952a8504b8b67d5af2ef8c7b
yakovru <yakovr_st@mail.ru>
2015-01-26 23:03:55 +0300

    [ru] fix for "?.."  and "!.."

--- languagetool-core/src/main/java/org/languagetool/rules/DoublePunctuationRule.java
+++ languagetool-core/src/main/java/org/languagetool/rules/DoublePunctuationRule.java
@@ -59,7 +59,7 @@ public class DoublePunctuationRule extends Rule {
     int startPos = 0;
     int dotCount = 0;
     int commaCount = 0;
-    for (int i = 0; i < tokens.length; i++) {
+    for (int i = 1; i < tokens.length; i++) {
       final String token = tokens[i].getToken();
       String nextToken = null;
       String prevToken = null;
