3c230e6425115fc4da5cf34ab13ef0d71d1133d4
jaumeortola <jaumeortola@gmail.com>
2015-03-04 22:39:30 +0100

    [ca] improved tro/tron

--- languagetool-language-modules/ca/src/test/java/org/languagetool/rules/ca/CatalanWrongWordInContextRuleTest.java
+++ languagetool-language-modules/ca/src/test/java/org/languagetool/rules/ca/CatalanWrongWordInContextRuleTest.java
@@ -49,6 +49,7 @@ public class CatalanWrongWordInContextRuleTest extends TestCase {
     assertEquals(1, rule.match(langTool.getAnalyzedSentence("És un terreny abonat per als problemes.")).length);
     assertEquals(1, rule.match(langTool.getAnalyzedSentence("No li va cosir bé les betes.")).length);
     assertEquals(1, rule.match(langTool.getAnalyzedSentence("Sempre li seguia la beta.")).length);
+    assertEquals(1, rule.match(langTool.getAnalyzedSentence("un any en el qual la reina Victoria encara era al tro britànic")).length);
   }
   
 }
