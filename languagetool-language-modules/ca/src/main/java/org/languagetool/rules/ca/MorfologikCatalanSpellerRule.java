34860bff194e5614c9eb9fd8f4324b0e987608fa
jaumeortola <jaumeortola@gmail.com>
2015-03-09 21:36:43 +0100

    [ca] code cleanup

--- languagetool-language-modules/ca/src/main/java/org/languagetool/rules/ca/MorfologikCatalanSpellerRule.java
+++ languagetool-language-modules/ca/src/main/java/org/languagetool/rules/ca/MorfologikCatalanSpellerRule.java
@@ -103,9 +103,9 @@ public final class MorfologikCatalanSpellerRule extends MorfologikSpellerRule {
     }
     Matcher matcher = wordPattern.matcher(word);
     if (matcher.matches()) {
-      String newSuggestion=matcher.group(suggestionPosition);
+      String newSuggestion = matcher.group(suggestionPosition);
       if (matchPostagRegexp(tagger.tag(Arrays.asList(newSuggestion)).get(0), postagPattern)) {
-        return matcher.group(1)+separator+matcher.group(2);
+        return matcher.group(1) + separator + matcher.group(2);
       }
     }
     return "";
