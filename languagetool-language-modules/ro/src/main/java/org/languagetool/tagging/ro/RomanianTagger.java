408b1ce0070ee2646343a70b70e47056c3241741
Daniel Naber <naber@danielnaber.de>
2015-03-11 20:15:57 +0100

    [ro] clean up tagger code - the logic for using manual additions is now in the base class

--- languagetool-language-modules/ro/src/main/java/org/languagetool/tagging/ro/RomanianTagger.java
+++ languagetool-language-modules/ro/src/main/java/org/languagetool/tagging/ro/RomanianTagger.java
@@ -18,89 +18,28 @@
  */
 package org.languagetool.tagging.ro;
 
-import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
+import org.languagetool.tagging.BaseTagger;
+
 import java.util.Locale;
 
-import morfologik.stemming.DictionaryLookup;
-import morfologik.stemming.IStemmer;
-import morfologik.stemming.WordData;
-
-import org.languagetool.AnalyzedToken;
-import org.languagetool.AnalyzedTokenReadings;
-import org.languagetool.JLanguageTool;
-import org.languagetool.tagging.BaseTagger;
-import org.languagetool.tagging.ManualTagger;
-import org.languagetool.tagging.TaggedWord;
-
 /**
- * Romanian Part-of-speech tagger 
- * 
+ * Romanian Part-of-speech tagger
+ *
  * @author Ionuț Păduraru
  */
 public class RomanianTagger extends BaseTagger {
 
-  private static final String DEFAULT_BINARY_DICT = "/ro/romanian.dict";
-  private static final String DEFAULT_PLAINTEXT_DICT = "/ro/added.txt";
-  private static final Locale RO_LOCALE = new Locale("ro");
-
-  private final String plaintextDictPath;
-
-  private ManualTagger manualTagger;
-
   public RomanianTagger() {
-    this(DEFAULT_BINARY_DICT, DEFAULT_PLAINTEXT_DICT);
+    super("/ro/romanian.dict", new Locale("ro"));
   }
 
-  public RomanianTagger(final String dictFileName, final String userDictFileName) {
-    super(dictFileName, RO_LOCALE);
-    plaintextDictPath = userDictFileName;
+  RomanianTagger(String dictPath) {
+    super(dictPath, new Locale("ro"));
   }
 
   @Override
   public String getManualAdditionsFileName() {
-    return null;  // TODO: make use of this
-  }
-
-  @Override
-  public final List<AnalyzedTokenReadings> tag(
-      final List<String> sentenceTokens) throws IOException {
-    final List<AnalyzedTokenReadings> tokenReadings = new ArrayList<>();
-    int pos = 0;
-    final IStemmer morfologik = new DictionaryLookup(getDictionary());
-    if (manualTagger == null && plaintextDictPath != null) {
-      manualTagger = new ManualTagger(JLanguageTool.getDataBroker().getFromResourceDirAsStream(plaintextDictPath));
-    }
-
-    for (final String word : sentenceTokens) {
-      final List<AnalyzedToken> l = new ArrayList<>();
-      final String lowerCaseWord = word.toLowerCase(RO_LOCALE);
-      final List<WordData> taggerTokens = morfologik.lookup(lowerCaseWord);
-      if (taggerTokens != null) {
-        for (WordData wd : taggerTokens) {
-          final String[] tagsArr = wd.getStem().toString().split("\\+");
-          for (final String currTag : tagsArr) {
-            l.add(new AnalyzedToken(word, 
-                wd.getTag().toString(), currTag));
-          }
-        }
-      }
-      if (manualTagger != null) { // add user tags, if any
-        final List<TaggedWord> manualTags = manualTagger.tag(lowerCaseWord);
-        for (TaggedWord manualTag : manualTags) {
-          l.add(new AnalyzedToken(word, manualTag.getPosTag(), manualTag.getLemma()));
-        }
-      }
-
-      if (l.isEmpty()) {
-        l.add(new AnalyzedToken(word, null, null));
-      }
-      tokenReadings.add(new AnalyzedTokenReadings(l, pos));
-      pos += word.length();
-    }
-
-    return tokenReadings;
+    return "/ro/added.txt";
   }
 
 }
