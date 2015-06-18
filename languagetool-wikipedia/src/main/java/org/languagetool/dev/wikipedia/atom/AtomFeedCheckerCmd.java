4e37c8bc758c323febab38d9eb5ca5fb807da246
Daniel Naber <naber@danielnaber.de>
2015-06-18 22:27:43 +0200

    fix argument parsing

--- languagetool-wikipedia/src/main/java/org/languagetool/dev/wikipedia/atom/AtomFeedCheckerCmd.java
+++ languagetool-wikipedia/src/main/java/org/languagetool/dev/wikipedia/atom/AtomFeedCheckerCmd.java
@@ -64,7 +64,7 @@ final class AtomFeedCheckerCmd {
     System.out.println("Sleep time: " + sleepTimeMillis + "ms (-1 = don't loop)");
     System.out.println("LanguageTool version: " + JLanguageTool.VERSION + " (" + JLanguageTool.BUILD_DATE + ")");
     DatabaseConfig databaseConfig = null;
-    if (args.length == 3) {
+    if (args.length >= 3) {
       String propFile = args[2];
       databaseConfig = new DatabaseConfig(propFile);
       System.out.println("Writing results to database at: " + databaseConfig.getUrl());
