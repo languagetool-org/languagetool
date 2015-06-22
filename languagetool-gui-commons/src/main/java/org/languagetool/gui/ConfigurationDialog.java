01cddc5afb590b4d36cb784637a8ea8aa31d3561
Daniel Naber <naber@danielnaber.de>
2015-06-22 10:37:23 +0200

    cleanup: move code to own methods

--- languagetool-gui-commons/src/main/java/org/languagetool/gui/ConfigurationDialog.java
+++ languagetool-gui-commons/src/main/java/org/languagetool/gui/ConfigurationDialog.java
@@ -18,6 +18,7 @@
  */
 package org.languagetool.gui;
 
+import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 import org.languagetool.JLanguageTool;
 import org.languagetool.Language;
@@ -120,9 +121,6 @@ public class ConfigurationDialog implements ActionListener {
     }
     dialog = new JDialog(owner, true);
     dialog.setTitle(messages.getString("guiConfigWindowTitle"));
-
-    Collections.sort(rules, new CategoryComparator());
-
     // close dialog when user presses Escape key:
     final KeyStroke stroke = KeyStroke.getKeyStroke(KeyEvent.VK_ESCAPE, 0);
     final ActionListener actionListener = new ActionListener() {
@@ -135,7 +133,6 @@ public class ConfigurationDialog implements ActionListener {
     rootPane.registerKeyboardAction(actionListener, stroke,
         JComponent.WHEN_IN_FOCUSED_WINDOW);
 
-    // JPanel
     final JPanel checkBoxPanel = new JPanel();
     checkBoxPanel.setLayout(new GridBagLayout());
     GridBagConstraints cons = new GridBagConstraints();
@@ -144,10 +141,160 @@ public class ConfigurationDialog implements ActionListener {
     cons.weightx = 1.0;
     cons.weighty = 1.0;
     cons.fill = GridBagConstraints.BOTH;
+    Collections.sort(rules, new CategoryComparator());
     DefaultMutableTreeNode rootNode = createTree(rules);
+    configTree = new JTree(getTreeModel(rootNode));
+
+    Language lang = config.getLanguage();
+    if (lang == null) {
+      lang = Languages.getLanguageForLocale(Locale.getDefault());
+    }
+    configTree.applyComponentOrientation(ComponentOrientation.getOrientation(lang.getLocale()));
+
+    configTree.setRootVisible(false);
+    configTree.setEditable(false);
+    configTree.setCellRenderer(new CheckBoxTreeCellRenderer());
+    TreeListener.install(configTree);
+    checkBoxPanel.add(configTree, cons);
+    configTree.addMouseListener(getMouseAdapter());
+    
+    final JPanel portPanel = new JPanel();
+    portPanel.setLayout(new GridBagLayout());
+    cons = new GridBagConstraints();
+    cons.insets = new Insets(0, 4, 0, 0);
+    cons.gridx = 0;
+    cons.gridy = 0;
+    cons.anchor = GridBagConstraints.WEST;
+    cons.fill = GridBagConstraints.NONE;
+    cons.weightx = 0.0f;
+    if (!insideOffice) {
+      createNonOfficeElements(cons, portPanel);
+    }
+
+    final JPanel buttonPanel = new JPanel();
+    buttonPanel.setLayout(new GridBagLayout());
+    okButton = new JButton(Tools.getLabel(messages.getString("guiOKButton")));
+    okButton.setMnemonic(Tools.getMnemonic(messages.getString("guiOKButton")));
+    okButton.addActionListener(this);
+    cancelButton = new JButton(Tools.getLabel(messages.getString("guiCancelButton")));
+    cancelButton.setMnemonic(Tools.getMnemonic(messages.getString("guiCancelButton")));
+    cancelButton.addActionListener(this);
+    cons = new GridBagConstraints();
+    cons.insets = new Insets(0, 4, 0, 0);
+    buttonPanel.add(okButton, cons);
+    buttonPanel.add(cancelButton, cons);
+
+    final Container contentPane = dialog.getContentPane();
+    contentPane.setLayout(new GridBagLayout());
+    cons = new GridBagConstraints();
+    cons.insets = new Insets(4, 4, 4, 4);
+    cons.gridx = 0;
+    cons.gridy = 0;
+    cons.weightx = 10.0f;
+    cons.weighty = 10.0f;
+    cons.fill = GridBagConstraints.BOTH;
+    contentPane.add(new JScrollPane(checkBoxPanel), cons);
+
+    cons.gridx = 0;
+    cons.gridy++;
+    cons.fill = GridBagConstraints.NONE;
+    cons.anchor = GridBagConstraints.LINE_END;
+    contentPane.add(getTreeButtonPanel(), cons);
+    
+    cons.gridy++;
+    cons.anchor = GridBagConstraints.WEST;
+    contentPane.add(getMotherTonguePanel(cons), cons);
+
+    cons.gridy++;
+    cons.anchor = GridBagConstraints.WEST;
+    contentPane.add(portPanel, cons);
+
+    cons.gridy++;
+    cons.anchor = GridBagConstraints.EAST;
+    contentPane.add(buttonPanel, cons);
+
+    dialog.pack();
+    dialog.setSize(500, 500);
+    // center on screen:
+    final Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
+    final Dimension frameSize = dialog.getSize();
+    dialog.setLocation(screenSize.width / 2 - frameSize.width / 2,
+        screenSize.height / 2 - frameSize.height / 2);
+    dialog.setLocationByPlatform(true);
+    dialog.setVisible(true);
+  }
+
+  private void createNonOfficeElements(GridBagConstraints cons, JPanel portPanel) {
+    serverCheckbox = new JCheckBox(Tools.getLabel(messages.getString("guiRunOnPort")));
+    serverCheckbox.setMnemonic(Tools.getMnemonic(messages.getString("guiRunOnPort")));
+    serverCheckbox.setSelected(config.getRunServer());
+    portPanel.add(serverCheckbox, cons);
+    serverCheckbox.addActionListener(new ActionListener() {
+      @Override
+      public void actionPerformed(@SuppressWarnings("unused") ActionEvent e) {
+        serverPortField.setEnabled(serverCheckbox.isSelected());
+        serverSettingsCheckbox.setEnabled(serverCheckbox.isSelected());
+      }
+    });
+    serverCheckbox.addItemListener(new ItemListener() {
+      @Override
+      public void itemStateChanged(ItemEvent e) {
+        config.setRunServer(serverCheckbox.isSelected());
+      }
+    });
+
+    serverPortField = new JTextField(Integer.toString(config.getServerPort()));
+    serverPortField.setEnabled(serverCheckbox.isSelected());
+    serverSettingsCheckbox = new JCheckBox(Tools.getLabel(messages.getString("useGUIConfig")));
+    serverPortField.setMinimumSize(new Dimension(100, 25));  // without this the box is just a few pixels small, but why?
+    cons.gridx = 1;
+    portPanel.add(serverPortField, cons);
+    serverPortField.getDocument().addDocumentListener(new DocumentListener() {
+
+      @Override
+      public void insertUpdate(DocumentEvent e) {
+        changedUpdate(e);
+      }
+
+      @Override
+      public void removeUpdate(DocumentEvent e) {
+        changedUpdate(e);
+      }
+
+      @Override
+      public void changedUpdate(DocumentEvent e) {
+        try {
+          int serverPort = Integer.parseInt(serverPortField.getText());
+          if (serverPort > -1 && serverPort < MAX_PORT) {
+            serverPortField.setForeground(null);
+            config.setServerPort(serverPort);
+          } else {
+            serverPortField.setForeground(Color.RED);
+          }
+        } catch (NumberFormatException ex) {
+          serverPortField.setForeground(Color.RED);
+        }
+      }
+    });
+
+    cons.gridx = 0;
+    cons.gridy = 10;
+    serverSettingsCheckbox.setMnemonic(Tools.getMnemonic(messages.getString("useGUIConfig")));
+    serverSettingsCheckbox.setSelected(config.getUseGUIConfig());
+    serverSettingsCheckbox.setEnabled(config.getRunServer());
+    serverSettingsCheckbox.addItemListener(new ItemListener() {
+      @Override
+      public void itemStateChanged(ItemEvent e) {
+        config.setUseGUIConfig(serverSettingsCheckbox.isSelected());
+      }
+    });
+    portPanel.add(serverSettingsCheckbox, cons);
+  }
+
+  @NotNull
+  private DefaultTreeModel getTreeModel(DefaultMutableTreeNode rootNode) {
     DefaultTreeModel treeModel = new DefaultTreeModel(rootNode);
     treeModel.addTreeModelListener(new TreeModelListener() {
-
       @Override
       public void treeNodesChanged(TreeModelEvent e) {
         DefaultMutableTreeNode node = (DefaultMutableTreeNode) e.getTreePath().getLastPathComponent();
@@ -178,96 +325,80 @@ public class ConfigurationDialog implements ActionListener {
           }
         }
       }
-
       @Override
-      public void treeNodesInserted(TreeModelEvent e) {
-      }
-
+      public void treeNodesInserted(TreeModelEvent e) {}
       @Override
-      public void treeNodesRemoved(TreeModelEvent e) {
-      }
-
+      public void treeNodesRemoved(TreeModelEvent e) {}
       @Override
-      public void treeStructureChanged(TreeModelEvent e) {
-      }
+      public void treeStructureChanged(TreeModelEvent e) {}
     });
-    configTree = new JTree(treeModel);
+    return treeModel;
+  }
 
-    Language lang = config.getLanguage();
-    if (lang == null) {
-      lang = Languages.getLanguageForLocale(Locale.getDefault());
-    }
-    configTree.applyComponentOrientation(
-      ComponentOrientation.getOrientation(lang.getLocale()));
-
-    configTree.setRootVisible(false);
-    configTree.setEditable(false);
-    configTree.setCellRenderer(new CheckBoxTreeCellRenderer());
-    TreeListener.install(configTree);
-    checkBoxPanel.add(configTree, cons);
-
-    MouseAdapter ma = new MouseAdapter() {
-      private void handlePopupEvent(MouseEvent e) {
-        final JTree tree = (JTree) e.getSource();
-
-        TreePath path = tree.getPathForLocation(e.getX(), e.getY());
-        if (path == null) {
-          return;
-        }
-
-        DefaultMutableTreeNode node
-                = (DefaultMutableTreeNode) path.getLastPathComponent();
-
-        TreePath[] paths = tree.getSelectionPaths();
-
-        boolean isSelected = false;
-        if (paths != null) {
-          for (TreePath selectionPath : paths) {
-            if (selectionPath.equals(path)) {
-              isSelected = true;
+  @NotNull
+  private MouseAdapter getMouseAdapter() {
+    return new MouseAdapter() {
+        private void handlePopupEvent(MouseEvent e) {
+          final JTree tree = (JTree) e.getSource();
+          TreePath path = tree.getPathForLocation(e.getX(), e.getY());
+          if (path == null) {
+            return;
+          }
+          DefaultMutableTreeNode node
+                  = (DefaultMutableTreeNode) path.getLastPathComponent();
+          TreePath[] paths = tree.getSelectionPaths();
+          boolean isSelected = false;
+          if (paths != null) {
+            for (TreePath selectionPath : paths) {
+              if (selectionPath.equals(path)) {
+                isSelected = true;
+              }
             }
           }
-        }
-        if (!isSelected) {
-          tree.setSelectionPath(path);
-        }
-        if (node.isLeaf()) {
-          JPopupMenu popup = new JPopupMenu();
-          final JMenuItem aboutRuleMenuItem = new JMenuItem(messages.getString("guiAboutRuleMenu"));
-          aboutRuleMenuItem.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent actionEvent) {
-              RuleNode node = (RuleNode) tree.getSelectionPath().getLastPathComponent();
-              Rule rule = node.getRule();
-              Language lang = config.getLanguage();
-              if(lang == null) {
-                lang = Languages.getLanguageForLocale(Locale.getDefault());
+          if (!isSelected) {
+            tree.setSelectionPath(path);
+          }
+          if (node.isLeaf()) {
+            JPopupMenu popup = new JPopupMenu();
+            final JMenuItem aboutRuleMenuItem = new JMenuItem(messages.getString("guiAboutRuleMenu"));
+            aboutRuleMenuItem.addActionListener(new ActionListener() {
+              @Override
+              public void actionPerformed(ActionEvent actionEvent) {
+                RuleNode node = (RuleNode) tree.getSelectionPath().getLastPathComponent();
+                Rule rule = node.getRule();
+                Language lang = config.getLanguage();
+                if(lang == null) {
+                  lang = Languages.getLanguageForLocale(Locale.getDefault());
+                }
+                Tools.showRuleInfoDialog(tree, messages.getString("guiAboutRuleTitle"),
+                        rule.getDescription(), rule, messages,
+                        lang.getShortNameWithCountryAndVariant());
               }
-              Tools.showRuleInfoDialog(tree, messages.getString("guiAboutRuleTitle"),
-                      rule.getDescription(), rule, messages,
-                      lang.getShortNameWithCountryAndVariant());
-            }
-          });
-          popup.add(aboutRuleMenuItem);
-          popup.show(tree, e.getX(), e.getY());
+            });
+            popup.add(aboutRuleMenuItem);
+            popup.show(tree, e.getX(), e.getY());
+          }
         }
-      }
+  
+        @Override
+        public void mousePressed(MouseEvent e) {
+          if (e.isPopupTrigger()) {
+            handlePopupEvent(e);
+          }
+        }
+  
+        @Override
+        public void mouseReleased(MouseEvent e) {
+          if (e.isPopupTrigger()) {
+            handlePopupEvent(e);
+          }
+        }
+      };
+  }
 
-      @Override
-      public void mousePressed(MouseEvent e) {
-        if (e.isPopupTrigger()) {
-          handlePopupEvent(e);
-        }
-      }
-
-      @Override
-      public void mouseReleased(MouseEvent e) {
-        if (e.isPopupTrigger()) {
-          handlePopupEvent(e);
-        }
-      }
-    };
-    configTree.addMouseListener(ma);
+  @NotNull
+  private JPanel getTreeButtonPanel() {
+    GridBagConstraints cons;
     final JPanel treeButtonPanel = new JPanel();
     cons = new GridBagConstraints();
     cons.gridx = 0;
@@ -305,7 +436,11 @@ public class ConfigurationDialog implements ActionListener {
         }
       }
     });
+    return treeButtonPanel;
+  }
 
+  @NotNull
+  private JPanel getMotherTonguePanel(GridBagConstraints cons) {
     final JPanel motherTonguePanel = new JPanel();
     motherTonguePanel.add(new JLabel(messages.getString("guiMotherTongue")), cons);
     motherTongueBox = new JComboBox<>(getPossibleMotherTongues());
@@ -328,136 +463,7 @@ public class ConfigurationDialog implements ActionListener {
       }
     });
     motherTonguePanel.add(motherTongueBox, cons);
-    
-    final JPanel portPanel = new JPanel();
-    portPanel.setLayout(new GridBagLayout());
-    cons = new GridBagConstraints();
-    cons.insets = new Insets(0, 4, 0, 0);
-    cons.gridx = 0;
-    cons.gridy = 0;
-    cons.anchor = GridBagConstraints.WEST;
-    cons.fill = GridBagConstraints.NONE;
-    cons.weightx = 0.0f;
-    if (!insideOffice) {
-      serverCheckbox = new JCheckBox(Tools.getLabel(messages.getString("guiRunOnPort")));
-      serverCheckbox.setMnemonic(Tools.getMnemonic(messages.getString("guiRunOnPort")));
-      serverCheckbox.setSelected(config.getRunServer());
-      portPanel.add(serverCheckbox, cons);
-      serverCheckbox.addActionListener(new ActionListener() {
-        @Override
-        public void actionPerformed(@SuppressWarnings("unused") ActionEvent e) {
-          serverPortField.setEnabled(serverCheckbox.isSelected());
-          serverSettingsCheckbox.setEnabled(serverCheckbox.isSelected());
-        }
-      });
-      serverCheckbox.addItemListener(new ItemListener() {
-
-        @Override
-        public void itemStateChanged(ItemEvent e) {
-          config.setRunServer(serverCheckbox.isSelected());
-        }
-      });
-
-      serverPortField = new JTextField(Integer.toString(config.getServerPort()));
-      serverPortField.setEnabled(serverCheckbox.isSelected());
-      serverSettingsCheckbox = new JCheckBox(Tools.getLabel(messages.getString("useGUIConfig")));
-      serverPortField.setMinimumSize(new Dimension(100, 25));  // without this the box is just a few pixels small, but why?
-      cons.gridx = 1;
-      portPanel.add(serverPortField, cons);
-      serverPortField.getDocument().addDocumentListener(new DocumentListener() {
-
-        @Override
-        public void insertUpdate(DocumentEvent e) {
-          changedUpdate(e);
-        }
-
-        @Override
-        public void removeUpdate(DocumentEvent e) {
-          changedUpdate(e);
-        }
-
-        @Override
-        public void changedUpdate(DocumentEvent e) {
-          try {
-            int serverPort = Integer.parseInt(serverPortField.getText());
-            if (serverPort > -1 && serverPort < MAX_PORT) {
-              serverPortField.setForeground(null);
-              config.setServerPort(serverPort);
-            } else {
-              serverPortField.setForeground(Color.RED);
-            }
-          } catch (NumberFormatException ex) {
-            serverPortField.setForeground(Color.RED);
-          }
-        }
-      });
-
-      cons.gridx = 0;
-      cons.gridy = 10;      
-      serverSettingsCheckbox.setMnemonic(Tools.getMnemonic(messages
-          .getString("useGUIConfig")));
-      serverSettingsCheckbox.setSelected(config.getUseGUIConfig());
-      serverSettingsCheckbox.setEnabled(config.getRunServer());
-      serverSettingsCheckbox.addItemListener(new ItemListener() {
-        @Override
-        public void itemStateChanged(ItemEvent e) {
-          config.setUseGUIConfig(serverSettingsCheckbox.isSelected());
-        }
-      });
-      portPanel.add(serverSettingsCheckbox, cons);
-    }
-
-    final JPanel buttonPanel = new JPanel();
-    buttonPanel.setLayout(new GridBagLayout());
-    okButton = new JButton(Tools.getLabel(messages.getString("guiOKButton")));
-    okButton.setMnemonic(Tools.getMnemonic(messages.getString("guiOKButton")));
-    okButton.addActionListener(this);
-    cancelButton = new JButton(Tools.getLabel(messages.getString("guiCancelButton")));
-    cancelButton.setMnemonic(Tools.getMnemonic(messages.getString("guiCancelButton")));
-    cancelButton.addActionListener(this);
-    cons = new GridBagConstraints();
-    cons.insets = new Insets(0, 4, 0, 0);
-    buttonPanel.add(okButton, cons);
-    buttonPanel.add(cancelButton, cons);
-
-    final Container contentPane = dialog.getContentPane();
-    contentPane.setLayout(new GridBagLayout());
-    cons = new GridBagConstraints();
-    cons.insets = new Insets(4, 4, 4, 4);
-    cons.gridx = 0;
-    cons.gridy = 0;
-    cons.weightx = 10.0f;
-    cons.weighty = 10.0f;
-    cons.fill = GridBagConstraints.BOTH;
-    contentPane.add(new JScrollPane(checkBoxPanel), cons);
-
-    cons.gridx = 0;
-    cons.gridy++;
-    cons.fill = GridBagConstraints.NONE;
-    cons.anchor = GridBagConstraints.LINE_END;
-    contentPane.add(treeButtonPanel, cons);
-    
-    cons.gridy++;
-    cons.anchor = GridBagConstraints.WEST;
-    contentPane.add(motherTonguePanel, cons);
-
-    cons.gridy++;
-    cons.anchor = GridBagConstraints.WEST;
-    contentPane.add(portPanel, cons);
-
-    cons.gridy++;
-    cons.anchor = GridBagConstraints.EAST;
-    contentPane.add(buttonPanel, cons);
-
-    dialog.pack();
-    dialog.setSize(500, 500);
-    // center on screen:
-    final Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
-    final Dimension frameSize = dialog.getSize();
-    dialog.setLocation(screenSize.width / 2 - frameSize.width / 2,
-        screenSize.height / 2 - frameSize.height / 2);
-    dialog.setLocationByPlatform(true);
-    dialog.setVisible(true);
+    return motherTonguePanel;
   }
 
   private String[] getPossibleMotherTongues() {
