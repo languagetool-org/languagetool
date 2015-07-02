d11749aaac8f11f8f13d0d46d34dcbae1f065379
Daniel Naber <naber@danielnaber.de>
2015-07-02 10:23:21 +0200

    move classes to sub package

--- languagetool-dev/src/main/java/org/languagetool/dev/bigdata/FrequencyIndexCreator.java
+++ languagetool-dev/src/main/java/org/languagetool/dev/bigdata/FrequencyIndexCreator.java
@@ -16,7 +16,7 @@
  * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301
  * USA
  */
-package org.languagetool.dev;
+package org.languagetool.dev.bigdata;
 
 import org.apache.lucene.analysis.Analyzer;
 import org.apache.lucene.analysis.standard.StandardAnalyzer;
--- languagetool-dev/src/main/java/org/languagetool/dev/bigdata/NGramAggregator.java
+++ languagetool-dev/src/main/java/org/languagetool/dev/bigdata/NGramAggregator.java
@@ -16,7 +16,7 @@
  * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301
  * USA
  */
-package org.languagetool.dev.hadoop;
+package org.languagetool.dev.bigdata;
 
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.Path;
