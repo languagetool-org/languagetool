ff474baa3db4ec8215ff1fa055c38e481783e000
Daniel Naber <naber@danielnaber.de>
2015-09-28 17:25:45 +0200

    [de] support ISO date (yyyy-mm-dd) for date consistency check

--- languagetool-language-modules/de/src/main/java/org/languagetool/rules/de/YMDDateCheckFilter.java
+++ languagetool-language-modules/de/src/main/java/org/languagetool/rules/de/YMDDateCheckFilter.java
@@ -16,7 +16,7 @@
  * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301
  * USA
  */
-package org.languagetool.rules.en;
+package org.languagetool.rules.de;
 
 import org.languagetool.AnalyzedTokenReadings;
 import org.languagetool.rules.RuleMatch;
