6586565e37191d5f861533a99cbe2e8a30ad41da
Dominique Pelle <dominique.pelle@gmail.com>
2015-12-20 06:10:09 +0100

    [br] updated script to create Breton FSA spelling dictionary
    The script create-fsa-spell-dictionary.sh now uses
    LanguageTool to create the FSA dictionary instead
    of relying on morfologik-distribution-1.5.4.zip.
    Breton Hunspell dictionaries 0.13 and 0.14 of LibreOffice
    were found to be identical (weird!) so the binary
    dictionary of LanguageTool was not updated.

--- languagetool-language-modules/br/src/main/resources/org/languagetool/resource/br/hunspell/create-fsa-spell-dictionary.sh
+++ languagetool-language-modules/br/src/main/resources/org/languagetool/resource/br/hunspell/create-fsa-spell-dictionary.sh
@@ -3,30 +3,29 @@
 # Create the Breton FSA spelling dictionary from
 # Hunspell dictionary.
 # 
-# Download morforlogik-distribution-1.5.4.zip at:
-# http://sourceforge.net/projects/morfologik/files/morfologik-stemming/1.5.4/morfologik-distribution-1.5.4.zip/download?use_mirror=freefr
-#
-# Run: $ unzip morforlogik-distribution-1.5.4.zip
-#
-# Then run the script without argument, it will
-# create the FSA spelling dictionary br_FR.dict.
+# This creates the FSA spelling dictionary br_FR.dict.
 #
 # Author: Dominique Pelle <dominique.pelle@gmail.com>
 
-file_oxt=difazier-an-drouizig-0_13.oxt
+file_oxt=difazier-an-drouizig-0_14.oxt
 if [ ! -f $file_oxt ]; then
   # Download the Breton Hunspell dictionary, if not already done.
-  wget http://extensions.libreoffice.org/extension-center/an-drouizig-breton-spellchecker/releases/0.13/$file_oxt
+  wget http://extensions.libreoffice.org/extension-center/an-drouizig-breton-spellchecker/releases/0.14/$file_oxt
 fi
 
 # We're only interested in 2 files (*.aff and *.dic) files in the *.oxt zip file. 
 unzip -o $file_oxt dictionaries/br_FR.aff dictionaries/br_FR.dic
 
-# File .info is used by morfologik.
-ln -sf br_FR.info .info
-
+TEMP_FILE=/tmp/lt-dictionary-br.dmp
+ 
 unmunch dictionaries/br_FR.dic dictionaries/br_FR.aff |
 sed -e "s/'/’/g" |
-LC_ALL=C sort |
-java -jar morfologik-distribution-1.5.4/morfologik-tools-1.5.4-standalone.jar \
-     fsa_build --sorted -f cfsa2 -o br_FR.dict
+LC_ALL=C sort > $TEMP_FILE
+
+java -cp ../../../../../../../../../../languagetool-standalone/target/LanguageTool-3.2-SNAPSHOT/LanguageTool-3.2-SNAPSHOT/languagetool.jar \
+  -Xmx2048m \
+  org.languagetool.dev.SpellDictionaryBuilder \
+  br-FR \
+  $TEMP_FILE br_FR.info - -o br_FR.dict
+
+rm $TEMP_FILE
