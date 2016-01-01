09d060f61d5f4aa107dcb374d2c9e7213b928607
Daniel Naber <naber@danielnaber.de>
2016-01-01 22:38:48 +0100

    make class non-public

--- languagetool-commandline/src/main/java/org/languagetool/commandline/UnknownParameterException.java
+++ languagetool-commandline/src/main/java/org/languagetool/commandline/UnknownParameterException.java
@@ -21,7 +21,7 @@ package org.languagetool.commandline;
 /**
  * Thrown when an unknown command-line parameter is specified.
  */
-public class UnknownParameterException extends RuntimeException {
+class UnknownParameterException extends RuntimeException {
 
   public UnknownParameterException(String message) {
     super(message);
