fcebc0d319d5ff1e5816d6b51d812e5e07642661
Daniel Naber <naber@danielnaber.de>
2016-01-23 16:57:46 +0100

    [de] less false alarms for pair war/was, as these words are so common

--- languagetool-core/src/main/java/org/languagetool/rules/ngrams/ConfusionProbabilityRule.java
+++ languagetool-core/src/main/java/org/languagetool/rules/ngrams/ConfusionProbabilityRule.java
@@ -285,7 +285,7 @@ public abstract class ConfusionProbabilityRule extends Rule {
       debug("  Min coverage of %.2f not reached: %.2f, %.2f, %.2f, assuming p=0\n", MIN_COVERAGE, ngram3Left.getCoverage(), ngram3Middle.getCoverage(), ngram3Right.getCoverage());
       return 0.0;
     } else {
-      //debug("  Min coverage of %.2f okay: %.2f, %.2f\n", MIN_COVERAGE, ngram3Left.coverage, ngram3Right.coverage);
+      //debug("  Min coverage of %.2f okay: %.2f, %.2f\n", MIN_COVERAGE, ngram3Left.getCoverage(), ngram3Right.getCoverage());
       return ngram3Left.getProb() * ngram3Middle.getProb() * ngram3Right.getProb();
     }
   }
