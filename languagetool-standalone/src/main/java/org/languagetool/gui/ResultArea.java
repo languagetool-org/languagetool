a05f95e0828f765203282ea2f08be2bfb3a192a1
Daniel Naber <naber@danielnaber.de>
2016-03-15 09:37:50 +0100

    tiny code cleanup to avoid IntelliJ warning

--- languagetool-standalone/src/main/java/org/languagetool/gui/ResultArea.java
+++ languagetool-standalone/src/main/java/org/languagetool/gui/ResultArea.java
@@ -126,7 +126,7 @@ class ResultArea {
         final String replacement = String.join("; ", match.getSuggestedReplacements());
         sb.append("<b>").append(messages.getString("correctionMessage")).append("</b> ").append(replacement).append("<br>\n");
       }
-      if (ITSIssueType.Misspelling.equals(match.getRule().getLocQualityIssueType())) {
+      if (ITSIssueType.Misspelling == match.getRule().getLocQualityIssueType()) {
         contextTools.setErrorMarkerStart(SPELL_ERROR_MARKER_START);
       } else {
         contextTools.setErrorMarkerStart(LT_ERROR_MARKER_START);
