f51cad01bc4e27922db928316ef2b7592a074dab
Daniel Naber <naber@danielnaber.de>
2016-04-05 08:06:04 +0200

    add API report script

---
+++ languagetool-standalone/scripts/report.sh
@@ -0,0 +1,40 @@
+#!/bin/sh
+# Daily API usage reports. To be run as a cronjob at the end of the day.
+# dnaber, 2015-11-14
+
+DATE1=`date +"%Y%m%d"`
+DATE2=`date +"%Y-%m-%d"`
+# for testing:
+#DATE1=20160331
+#DATE2=2016-03-31
+TMPFILE=/tmp/log.temp
+OUTFILE=/tmp/statusmail.txt
+
+echo "Daily LanguageTool API Report $DATE2" >$OUTFILE
+echo "" >>$OUTFILE
+
+grep "$DATE2 " log-[0-9]-$DATE1*.txt >$TMPFILE
+
+TOTAL=`grep -c "Check done:" $TMPFILE`
+printf "Total text checks : %'d\n" $TOTAL >>$OUTFILE
+
+TOTALHOME=`grep "Check done:" $TMPFILE | grep -c languagetool.org`
+printf "Checks from lt.org: %'d\n" $TOTALHOME >>$OUTFILE
+
+FF=`grep -c "languagetoolfx"  $TMPFILE`
+printf "Firefox Requests  : %'d\n" $FF >>$OUTFILE
+
+CHROME=`grep -c "chrome-extension" $TMPFILE`
+printf "Chrome Requests   : %'d\n" $CHROME >>$OUTFILE
+
+ANDROID=`grep -c "androidspell" $TMPFILE`
+printf "Android Requests   : %'d\n" $ANDROID >>$OUTFILE
+
+echo "$DATE2;$TOTAL;$FF;$CHROME;$ANDROID" >>/home/languagetool/api/api-log.csv
+
+echo "" >>$OUTFILE
+echo "An error has occurred      : `grep -c 'An error has occurred' $TMPFILE`" >>$OUTFILE
+echo "too many requests          : `grep -c 'too many requests' $TMPFILE`" >>$OUTFILE
+echo "too many requests (Android): `grep -c 'androidspell.*too many requests' $TMPFILE`" >>$OUTFILE
+
+cat $OUTFILE | mail -a 'Content-Type: text/plain; charset=utf-8' -s "LanguageTool API Report" daniel.naber@languagetool.org
