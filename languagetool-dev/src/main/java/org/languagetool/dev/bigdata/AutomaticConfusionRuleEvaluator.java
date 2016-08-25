599dfd05f18d61b968a311a32eb52f18871f849d
Daniel Naber <naber@danielnaber.de>
2016-08-25 21:01:33 +0200

    set default to English again; don't get all results if not case sensitive (too slow for common words like "the", "to" etc)

--- languagetool-dev/src/main/java/org/languagetool/dev/bigdata/AutomaticConfusionRuleEvaluator.java
+++ languagetool-dev/src/main/java/org/languagetool/dev/bigdata/AutomaticConfusionRuleEvaluator.java
@@ -42,8 +42,8 @@ import java.util.stream.Collectors;
 @SuppressWarnings({"resource", "CallToPrintStackTrace"})
 class AutomaticConfusionRuleEvaluator {
   
-  private static final String LANGUAGE = "de";
-  private static final boolean CASE_SENSITIVE = true;
+  private static final String LANGUAGE = "en";
+  private static final boolean CASE_SENSITIVE = false;
   private static final int MAX_EXAMPLES = 1000;
   private static final List<Long> EVAL_FACTORS = Arrays.asList(10L, 100L, 1_000L, 10_000L, 100_000L, 1_000_000L, 10_000_000L);
   private static final float MIN_PRECISION = 0.99f;
@@ -143,7 +143,7 @@ class AutomaticConfusionRuleEvaluator {
 
   private void findExampleSentences(String word, FileWriter fw) throws IOException {
     Term term = new Term(TextIndexCreator.FIELD, CASE_SENSITIVE ? word.toLowerCase() : word);
-    TopDocs topDocs = searcher.search(new TermQuery(term), Integer.MAX_VALUE);
+    TopDocs topDocs = searcher.search(new TermQuery(term), CASE_SENSITIVE ? Integer.MAX_VALUE : MAX_EXAMPLES);
     int count = 0;
     for (ScoreDoc scoreDoc : topDocs.scoreDocs) {
       String sentence = searcher.doc(scoreDoc.doc).get(TextIndexCreator.FIELD);
