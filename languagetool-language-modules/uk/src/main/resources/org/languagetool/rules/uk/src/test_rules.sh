9eded725cabebe0a96ff9707370b65b42bce0241
Andriy Rysin <arysin@gmail.com>
2016-08-25 15:41:38 -0400

    [uk] script updates

--- languagetool-language-modules/uk/src/main/resources/org/languagetool/rules/uk/src/test_rules.sh
+++ languagetool-language-modules/uk/src/main/resources/org/languagetool/rules/uk/src/test_rules.sh
@@ -1,14 +1,10 @@
 #!/bin/sh
 
-#./generate_replace.sh
 
 export PATH=$PATH:~/bin
 PWD=`pwd`
 BASE="../../../../../../../../../.."
-#cd $BASE
-#./build.sh uk package -DskipTests
-#./build.sh uk test
-#export PATH=/usr/java/jdk1.7.0_75/bin:$PATH
+
 cd $BASE/languagetool-language-modules/uk
 
 if [ "$1" != "" ]; then
