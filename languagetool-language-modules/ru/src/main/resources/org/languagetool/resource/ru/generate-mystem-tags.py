528412849f5af7c4547abd232bb49df4d606bff1
Konstantin Ladutenko <kostyfisik@users.noreply.github.com>
2016-09-03 22:59:14 +0300

    [ru]mystem POS tags generator (#532)
    * [ru] add mystem tag generator initial support
    
    * [ru]updage mystem generator

--- languagetool-language-modules/ru/src/main/resources/org/languagetool/resource/ru/generate-mystem-tags.py
+++ languagetool-language-modules/ru/src/main/resources/org/languagetool/resource/ru/generate-mystem-tags.py
@@ -4,6 +4,191 @@ import os
 from subprocess import call
 import re
 import enchant
+
+pos_LT = {
+    'A':'ADJ',#	прилагательное
+    'ADV':'ADV',#	наречие
+    'ADVPRO':'none',#	местоименное наречие
+    'ANUM':'none',#	числительное-прилагательное
+    'APRO':'none',#	местоимение-прилагательное
+    'COM':'none',#	часть композита - сложного слова
+    'CONJ':'CONJ',#	союз
+    'INTJ':'INTERJECTION',#	междометие
+    'NUM':'NumC',#	числительное
+    'PART':'PARTICLE',#	частица
+    'PR':'PREP',#	предлог
+    'S':'NN',#	существительное
+    'SPRO':'PNN',#	местоимение-существительное
+    'V':'VB'}#	глагол
+
+pos_other_LT=['DPT',
+'PT',
+'ADJ_Short',#	краткая форма
+'ADJ',#	полная форма
+'PADJ',#	притяжательные прилагательные
+'ADJ_Comp',#	превосходная
+'ADJ_Sup',#	сравнительная
+'NNN',#	имя собственное
+'NNP',#	отчество
+'NNF'#	фамилия
+              ]
+
+tense_LT ={
+    'praes':'Real',#	настоящее
+    'inpraes':'Fut',#	непрошедшее
+    'praet':'Past',#	прошедшее
+    'inf':'INF',#	инфинитив
+    'imper':'IMP'#	повелительное наклонение
+}
+
+case_LT={
+    #Падеж
+    'nom':'Nom',#	именительный
+    'gen':'R',#	родительный
+    'dat':'D',#	дательный
+    'acc':'V',#	винительный
+    'ins':'T',#	творительный
+    'abl':'P',#	предложный
+    'part':'R',#	партитив (второй родительный)
+    'loc':'P',#	местный (второй предложный)
+    'voc':'none'#	звательный
+}
+
+number_LT ={
+    #Число
+    'sg':'Sin',#	единственное число
+    'pl':'PL'#	множественное число
+}
+
+person_LT={
+    #Лицо глагола
+    '1p':'P1',#	1-е лицо
+    '2p':'P2',#	2-е лицо
+    '3p':'P3'#	3-е лицо
+}
+
+gender_LT={
+    #Род
+    'm':'Masc',#	мужской род
+    'f':'Fem',#	женский род
+    'n':'Neut'#	средний род
+}
+# PRDC predicate
+# NumC - числительное количественное  - NUMERAL COUNTABLE
+# Ord - числительное порядковое - ORDINAL
+# PT_Short - краткое причастие - SHORT PARTICIPLE
+
+other_LT={
+#Репрезентация и наклонение глагола
+'ger':'DPT',#	деепричастие
+'partcp':'PT',#	причастие
+'indic':'',#	изьявительное наклонение
+#Форма прилагательных
+'brev':'ADJ_Short',#	краткая форма
+'plen':'ADJ',#	полная форма
+'poss':'PADJ',#	притяжательные прилагательные
+#Степень сравнения
+'supr':'ADJ_Comp',#	превосходная
+'comp':'ADJ_S',#	сравнительная
+#Вид
+'ipf':'',#	несовершенный
+'pf':'',#	совершенный
+#Залог
+'act':'',#	действительный залог
+'pass':'',#	страдательный залог
+#Одушевленность
+'anim':'',#	одушевленное
+'inan':'',#	неодушевленное
+#Переходность
+'tran':'',#	переходный глагол
+'intr':'',#	непереходный глагол
+#Прочие обозначения
+'parenth':'',#	вводное слово
+'geo':'',#	географическое название
+'awkw':'',#	образование формы затруднено
+'persn':'NNN',#	имя собственное
+'dist':'',#	искаженная форма
+'mf':'',#	общая форма мужского и женского рода
+'obsc':'',#	обсценная лексика
+'patrn':'NNP',#	отчество
+'praed':'',#	предикатив
+'inform':'Talk',#	разговорная форма
+'rare':'',#	редко встречающееся слово
+'abbr':'ABR',#	сокращение
+'obsol':'',#	устаревшая форма
+'famn':'NNF'#	фамилия
+}
+all_tags=[]
+with open('all_tags.txt', 'r') as data_file:
+    for data_line in data_file:
+        all_tags.append(data_line[:-1])
+#print(all_tags)
+def find_list(split, dic):
+    ret=[]
+    for key in dic.keys():
+        if key in split:
+            ret.append(dic[key])
+    if len(ret)==0: ret.append('none')
+    return ret
+def convert_gramma(gramma):
+    split = re.split(',|=',gramma)
+    # pos = pos_LT[split[0]]
+    pos = find_list(split, pos_LT)
+    case = find_list(split, case_LT)
+    number = find_list(split, number_LT)
+    gender = find_list(split, gender_LT)
+    tense = find_list(split, tense_LT)
+    person = find_list(split, person_LT)
+    other = find_list(split, other_LT)
+    other_pos = set(other) & set(pos_other_LT)
+
+    if len(pos) != 1 or len(case) > 1 or len(number) > 1 or len(gender) > 1 or len(tense) > 1 or len(person) > 1 or len(other_pos)>1:        
+        print("ERROR! Too many pos tags in "+gramma)
+        return 'UNKNOWN'
+    pos = pos[0]
+    if len(other_pos) != 0:
+        pos=other_pos.pop()
+    if  pos == 'none': return 'UNKNOWN'
+      
+    output = pos
+    
+    if pos in ['NN','NNN','NNF','NNP']:
+        output+=":"+gender[0]+":"+number[0]+":"+case[0]
+        if 'Talk' in other:
+            output += ":Talk"
+    if pos in ['ADJ','ADJ_Com','ADJ_Short','PADJ']:
+        output+=":"+gender[0]
+        if number[0] != 'Sin':
+            output += ":"+number[0]
+        output+=":"+case[0]
+    if pos == 'DPT':
+        output+=":"+tense[0]
+    if pos == 'NumC':
+        output+=":"+case[0]
+    if pos in ['PT','PT_Short']:
+        output+=":"+tense[0]+":"+gender[0]
+        if number[0] != 'Sin':
+            output += ":"+number[0]
+        output+=":"+case[0]
+    if pos in ['VB']:
+        output+=":"+tense[0]
+        if tense[0]=='Past':
+            output+=":"+gender[0]
+            if number[0] != 'Sin':
+                output += ":"+number[0]
+        else:
+            output+=":"+number[0]+":"+person[0]
+    
+    output = output.replace('none','')
+    if ('NN::' or '::Talk') not in output:
+        output = output.replace('::',':')
+        output = output.replace('::',':')
+    if output[-1] == ":":
+        output = output[:-1]
+    return output #+" "+str(other)+" <=="+gramma
+
+
+
 d = enchant.Dict("ru_RU")
 words = set()
 i=0
@@ -43,6 +228,7 @@ print(len(news))
 with open('new-tag.txt','w') as new_file:
     for word in news:
         new_file.write(word+"\n")
+
 os.system("./mystem -nwi --eng-gr new-tag.txt mout.txt")
 
 mystems = []
@@ -53,7 +239,27 @@ with open('mout.txt','r') as new_file:
 mystems.sort()
 with open('final-tags.txt','w') as final:
     for word in mystems:
-        final.write(word)
+        token = word.split("{",1)[0]
+        descs = word.split("{",1)[1].split("|")
+        current_lemma = ""
+        # final.write("\n"+word)
+        for desc in descs:
+            if desc[0] != "=":
+                current_lemma = desc.split("=",1)[0]
+            # else: continue #TODO remove debug
+            gramma = desc.split("=",1)[1]
+            if gramma[-2] == "}": gramma = gramma[:-2]
+            pos_tag = convert_gramma(gramma)
+            if pos_tag in all_tags:
+                final.write(token+"\t"+current_lemma+"\t"+pos_tag+"\n")
+            else:
+                print(token+"\t"+current_lemma+"\t"+pos_tag+" <== "+gramma)
+
+# mystems.sort()
+# with open('final-tags.txt','w') as final:
+#     for word in mystems:
+#         final.write(word)
+
 with open('final.txt','w') as final:
     for word in mystems:
         final.write(word.split("{",1)[0]+"\n")
