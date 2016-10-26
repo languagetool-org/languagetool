7b2a6e42407f0e2d57870b6d72def3e07dffe29a
Daniel Naber <naber@danielnaber.de>
2016-10-26 10:38:53 +0200

    force alphabetical order of words in the confusion sets, this makes it easier to detect duplicates or other issues

--- languagetool-standalone/src/test/java/org/languagetool/rules/ConfusionSetLoaderTest.java
+++ languagetool-standalone/src/test/java/org/languagetool/rules/ConfusionSetLoaderTest.java
@@ -38,7 +38,12 @@ public class ConfusionSetLoaderTest {
   public void testConfusionSetLoading() throws IOException {
     int count = 0;
     for (Language language : Languages.get()) {
-      List<Rule> rules = language.getRelevantLanguageModelRules(JLanguageTool.getMessageBundle(), new FakeLanguageModel());
+      List<Rule> rules;
+      try {
+        rules = language.getRelevantLanguageModelRules(JLanguageTool.getMessageBundle(), new FakeLanguageModel());
+      } catch (Exception e) {
+        throw new RuntimeException("Could not load confusion pairs for " + language.getName(), e);
+      }
       if (rules.size() > 0) {
         String path = "/" + language.getShortName() + "/confusion_sets.txt";
         try (InputStream confusionSetStream = JLanguageTool.getDataBroker().getFromResourceDirAsStream(path)) {
