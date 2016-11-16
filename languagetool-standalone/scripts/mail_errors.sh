6ba9454e24eba343ad366ca9972022e7915520c8
Daniel Naber <naber@danielnaber.de>
2016-11-16 22:10:22 +0100

    don't send email if there are only few matches

--- languagetool-standalone/scripts/mail_errors.sh
+++ languagetool-standalone/scripts/mail_errors.sh
@@ -5,9 +5,13 @@
 FIRSTPART=naber
 LASTPART=danielnaber.de
 TMPDATA=/tmp/lt_hourly_error_data.txt
+LIMIT=10
 
 tail -n 50000 log-1.txt log-2.txt | grep "`date +"%Y-%m-%d %H:"`" | egrep "An error has occurred" | grep -v "code 413" >$TMPDATA
 MATCHES=`cat $TMPDATA | wc -l`
 
-cat $TMPDATA | tail -n 100 | \
+if [ "$MATCHES" -gt "$LIMIT" ]
+then
+  cat $TMPDATA | tail -n 100 | \
         ifne mail -a 'Content-Type: text/plain; charset=utf-8' -s "Hourly LT Error Report ($MATCHES total)" ${FIRSTPART}@${LASTPART}
+fi
