601e2d9cc42f9ad33058af9b29a48ae0f9579e86
Jaume Ortolà <jaumeortola@gmail.com>
2016-12-18 08:47:31 +0100

    [ca] update priorities

--- languagetool-language-modules/ca/src/main/java/org/languagetool/language/Catalan.java
+++ languagetool-language-modules/ca/src/main/java/org/languagetool/language/Catalan.java
@@ -163,7 +163,8 @@ public class Catalan extends Language {
   @Override
   public int getPriorityForId(String id) {
     switch (id) {
-      case "DIACRITICS": return 10;
+      case "DIACRITICS": return 20;
+      case "ACCENTUATION_CHECK": return 10;
       case "FALTA_COMA_FRASE_CONDICIONAL": return -20;
       case "FALTA_ELEMENT_ENTRE_VERB": return -10;
       case "MORFOLOGIK_RULE_CA_ES": return -100;
