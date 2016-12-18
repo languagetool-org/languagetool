133922e1b3602fc8a769a768adf89429d97eae52
Daniel Naber <naber@danielnaber.de>
2016-12-18 20:05:24 +0100

    support "ß"

--- languagetool-dev/src/main/java/org/languagetool/dev/wordsimilarity/GermanQwertzKeyboardDistance.java
+++ languagetool-dev/src/main/java/org/languagetool/dev/wordsimilarity/GermanQwertzKeyboardDistance.java
@@ -24,6 +24,7 @@ package org.languagetool.dev.wordsimilarity;
 public class GermanQwertzKeyboardDistance implements KeyboardDistance {
     
     private static final char[][] KEYS = {
+            { '\0', '\0', '\0', '\0', '\0', '\0', '\0', '\0', '\0', '\0', 'ß' },      
             { 'q', 'w', 'e', 'r', 't', 'z', 'u', 'i', 'o', 'p', 'ü' },      
             { 'a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l', 'ö', 'ä' },     
             { 'y', 'x', 'c', 'v', 'b', 'n', 'm' }      
