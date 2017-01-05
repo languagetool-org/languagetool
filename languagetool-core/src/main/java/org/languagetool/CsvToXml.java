e90a729569624ae0bb84f2b7ad30fce8e6541b9e
emaljik <michaljanik26@gmail.com>
2017-01-05 03:32:19 +0100

    Convert glossaries into terminology checks for translation Quality Assessment

--- languagetool-core/src/main/java/org/languagetool/CsvToXml.java
+++ languagetool-core/src/main/java/org/languagetool/CsvToXml.java
@@ -24,108 +24,109 @@ import javax.xml.transform.TransformerException;
 import javax.xml.transform.TransformerFactory;
 import javax.xml.transform.dom.DOMSource;
 import javax.xml.transform.stream.StreamResult;
+
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
 
 public class CsvToXml {
 
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        System.out.print("Choose interface (To choose command line type 'cl', to choose UI type 'ui'): ");
-        System.out.flush();
-        String screen = scanner.nextLine();
+  public static void main(String[] args) {
+    Scanner scanner = new Scanner(System.in);
+    System.out.print("Choose interface (To choose command line type 'cl', to choose UI type 'ui'): ");
+    System.out.flush();
+    String screen = scanner.nextLine();
 
-        if ("cl".equals(screen)) {
-            System.out.print("Enter a file name: ");
-            System.out.flush();
-            String filename = scanner.nextLine();
-            System.out.print("Enter character to seperate csv file: ");
-            System.out.flush();
-            String splitBy = scanner.next();
-            csvToBitext(filename, splitBy, "");
-        } else if ("ui".equals(screen)) {
-            JFrame.setDefaultLookAndFeelDecorated(true);
-            JDialog.setDefaultLookAndFeelDecorated(true);
-            JFrame frame = new JFrame("User Interface");
-            frame.setPreferredSize(new Dimension(400, 100));
-            frame.setLayout(new FlowLayout());
-            frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-            JLabel text = new JLabel("Split words by:");
-            JTextField split = new JTextField(5);
-            JButton btnFile = new JButton("Select Csv File");
-            JButton btnOk = new JButton("Ok");
-            JFileChooser jfc = new JFileChooser(FileSystemView.getFileSystemView().getHomeDirectory());
-            btnFile.addActionListener((ActionEvent ae) -> {
-                jfc.setDialogTitle("Select csv file");
-                jfc.setAcceptAllFileFilterUsed(false);
+    if ("cl".equals(screen)) {
+      System.out.print("Enter a file name: ");
+      System.out.flush();
+      String filename = scanner.nextLine();
+      System.out.print("Enter character to seperate csv file: ");
+      System.out.flush();
+      String splitBy = scanner.next();
+      csvToBitext(filename, splitBy, "");
+    } else if ("ui".equals(screen)) {
+      JFrame.setDefaultLookAndFeelDecorated(true);
+      JDialog.setDefaultLookAndFeelDecorated(true);
+      JFrame frame = new JFrame("User Interface");
+      frame.setPreferredSize(new Dimension(400, 100));
+      frame.setLayout(new FlowLayout());
+      frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+      JLabel text = new JLabel("Split words by:");
+      JTextField split = new JTextField(5);
+      JButton btnFile = new JButton("Select Csv File");
+      JButton btnOk = new JButton("Ok");
+      JFileChooser jfc = new JFileChooser(FileSystemView.getFileSystemView().getHomeDirectory());
+      btnFile.addActionListener((ActionEvent ae) -> {
+        jfc.setDialogTitle("Select csv file");
+        jfc.setAcceptAllFileFilterUsed(false);
 //                FileNameExtensionFilter filter = new FileNameExtensionFilter("csv");
 //                jfc.addChoosableFileFilter(filter);
 
-                int returnValue = jfc.showOpenDialog(null);
+        int returnValue = jfc.showOpenDialog(null);
 //                if (returnValue == JFileChooser.APPROVE_OPTION) {
 //                    
 //                }
-            });
-            btnOk.addActionListener((ActionEvent ae) -> {
-                csvToBitext(jfc.getSelectedFile().getPath(), split.getText(), "");
-            });
-            frame.add(btnFile);
-            frame.add(text);
-            frame.add(split);
-            frame.add(btnOk);
-            frame.pack();
-            frame.setVisible(true);
-        }
+      });
+      btnOk.addActionListener((ActionEvent ae) -> {
+        csvToBitext(jfc.getSelectedFile().getPath(), split.getText(), "");
+      });
+      frame.add(btnFile);
+      frame.add(text);
+      frame.add(split);
+      frame.add(btnOk);
+      frame.pack();
+      frame.setVisible(true);
     }
+  }
 
-    public static void csvToBitext(String csvFile, String cvsSplitBy, String line) {
+  public static void csvToBitext(String csvFile, String cvsSplitBy, String line) {
 
-        try (BufferedReader br = new BufferedReader(new FileReader(csvFile))) {
-            DocumentBuilderFactory docFactory = DocumentBuilderFactory.newInstance();
-            DocumentBuilder docBuilder = docFactory.newDocumentBuilder();
+    try (BufferedReader br = new BufferedReader(new FileReader(csvFile))) {
+      DocumentBuilderFactory docFactory = DocumentBuilderFactory.newInstance();
+      DocumentBuilder docBuilder = docFactory.newDocumentBuilder();
 
-            Document doc = docBuilder.newDocument();
-            Element rootElement = doc.createElement("example");
-            doc.appendChild(rootElement);
+      Document doc = docBuilder.newDocument();
+      Element rootElement = doc.createElement("example");
+      doc.appendChild(rootElement);
 
-            Element example00 = doc.createElement("example00");
-            rootElement.appendChild(example00);
+      Element example00 = doc.createElement("example00");
+      rootElement.appendChild(example00);
 
-            Element example01 = doc.createElement("example01");
-            rootElement.appendChild(example01);
+      Element example01 = doc.createElement("example01");
+      rootElement.appendChild(example01);
 
-            example00.setAttribute("lang", "en");
+      example00.setAttribute("lang", "en");
 
-            example01.setAttribute("lang", "ru");
+      example01.setAttribute("lang", "ru");
 
-            while ((line = br.readLine()) != null) {
+      while ((line = br.readLine()) != null) {
 
-                String[] x = line.split(cvsSplitBy);
+        String[] x = line.split(cvsSplitBy);
 
-                Element example000 = doc.createElement("example000");
-                example000.appendChild(doc.createTextNode(x[4]));
-                example00.appendChild(example000);
+        Element example000 = doc.createElement("example000");
+        example000.appendChild(doc.createTextNode(x[4]));
+        example00.appendChild(example000);
 
-                Element example010 = doc.createElement("example010");
-                example010.appendChild(doc.createTextNode(x[5]));
-                example01.appendChild(example010);
-            }
+        Element example010 = doc.createElement("example010");
+        example010.appendChild(doc.createTextNode(x[5]));
+        example01.appendChild(example010);
+      }
 
-            // write the content into xml file
-            TransformerFactory transformerFactory = TransformerFactory.newInstance();
-            Transformer transformer = transformerFactory.newTransformer();
-            DOMSource source = new DOMSource(doc);
-            StreamResult result = new StreamResult(new File("file.xml"));
+      // write the content into xml file
+      TransformerFactory transformerFactory = TransformerFactory.newInstance();
+      Transformer transformer = transformerFactory.newTransformer();
+      DOMSource source = new DOMSource(doc);
+      StreamResult result = new StreamResult(new File("file.xml"));
 
-            // Output to console for testing
+      // Output to console for testing
 //            StreamResult result = new StreamResult(System.out);
-            transformer.transform(source, result);
+      transformer.transform(source, result);
 
-            System.out.println("File saved!");
+      System.out.println("File saved!");
 
-        } catch (IOException | ParserConfigurationException ex) {
-        } catch (TransformerConfigurationException ex) {
-        } catch (TransformerException ex) {
-        }
+    } catch (IOException | ParserConfigurationException ex) {
+    } catch (TransformerConfigurationException ex) {
+    } catch (TransformerException ex) {
     }
+  }
 }
