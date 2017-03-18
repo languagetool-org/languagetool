a1a6ffb96b9d30e4c970e53d57beb099ddf43512
Andriy Rysin <arysin@gmail.com>
2017-03-17 23:40:15 -0400

    [uk] several new rules

--- languagetool-language-modules/uk/src/main/resources/org/languagetool/rules/uk/src/test_rules.sh
+++ languagetool-language-modules/uk/src/main/resources/org/languagetool/rules/uk/src/test_rules.sh
@@ -11,7 +11,7 @@ if [ "$1" != "" ]; then
   FLAGS="-Dtest=$1"
 fi
 
-#FLAGS="$FLAGS -Dorg.languagetool.rules.uk.TokenInflectionExceptionHelper.debug=true"
+#FLAGS="$FLAGS -Dorg.languagetool.rules.uk.TokenInflectionAgreementRule.debug=true"
 #FLAGS="$FLAGS -Dorg.languagetool.rules.uk.TokenVerbAgreementRule.debug=true"
 
 mvn $FLAGS compile test
