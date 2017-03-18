72a074504c0584efb76922359a7803b333dc5bde
Jan Schreiber <jan.schreiber@languagetool.org>
2017-03-18 19:05:17 +0100

    remove 'rôle' from VALID_WORDS because ô is accepted as character now as per 4709945.

--- languagetool-standalone/src/test/java/org/languagetool/rules/WordListValidatorTest.java
+++ languagetool-standalone/src/test/java/org/languagetool/rules/WordListValidatorTest.java
@@ -47,8 +47,7 @@ public class WordListValidatorTest {
   // Words that are valid but with special characters so are that we don't want to
   // allow them in general:
   private static final Set<String> VALID_WORDS = new HashSet<>(Arrays.asList(
-          "Hidschāb/S",
-          "rôle", "rôles"
+          "Hidschāb/S"
   ));
 
   @Test
