69725749c13ae430c66cdbb8d167b9ffb866a32c
Taha Zerrouki <taha_zerrouki@hotmail.com>
2017-03-25 13:49:20 +0100

    [ar] add arabic support with basic rules

---
+++ languagetool-language-modules/ar/src/main/java/org/languagetool/language/Arabic.java
@@ -0,0 +1,111 @@
+/* LanguageTool, a natural language style checker 
+ * Copyright (C) 2014 Daniel Naber (http://www.danielnaber.de)
+ * 
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301
+ * USA
+ */
+package org.languagetool.language;
+
+import org.languagetool.Language;
+import org.languagetool.rules.*;
+import org.languagetool.rules.spelling.hunspell.HunspellNoSuggestionRule;
+import org.languagetool.tagging.Tagger;
+import org.languagetool.tagging.ar.ArabicTagger;
+import org.languagetool.tokenizers.SentenceTokenizer;
+import org.languagetool.tokenizers.SimpleSentenceTokenizer;
+import org.languagetool.tokenizers.WordTokenizer;
+import org.languagetool.tokenizers.ArabicWordTokenizer;
+
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.List;
+import java.util.ResourceBundle;
+
+/**
+ * Support for Arabic.
+ */
+public class Arabic extends Language {
+
+  private SentenceTokenizer sentenceTokenizer;
+  private WordTokenizer wordTokenizer;
+  private Tagger tagger;
+
+  @Override
+  public String getName() {
+    return "Arabic";
+  }
+
+  @Override
+  public String getShortCode() {
+    return "ar";
+  }
+
+  @Override
+  public String[] getCountries() {
+    return new String[]{"DZ"};
+  }
+  
+  @Override
+  public SentenceTokenizer getSentenceTokenizer() {
+    if (sentenceTokenizer == null) {
+      sentenceTokenizer =  new SimpleSentenceTokenizer();
+    }
+    return sentenceTokenizer;
+  }
+
+  @Override
+  public WordTokenizer getWordTokenizer() {
+    if (wordTokenizer == null) {
+      wordTokenizer = new ArabicWordTokenizer();
+    }
+    return wordTokenizer;
+  }
+
+  @Override
+  public Tagger getTagger() {
+    if (tagger == null) {
+      tagger = new ArabicTagger();
+    }
+    return tagger;
+  }
+  
+  @Override
+  public Contributor[] getMaintainers() {
+    return new Contributor[] {
+            new Contributor("Taha Zerrouki"),
+            new Contributor("KALI Imen"),
+            new Contributor("TCHOKETCH Karima"),
+            new Contributor("Sohaib AFIFI"),
+    };
+  }
+
+  @Override
+  public List<Rule> getRelevantRules(ResourceBundle messages) throws IOException {
+    return Arrays.asList(
+    	new CommaWhitespaceRule(messages,
+    				                 Example.wrong("نعم<marker> ,</marker> لقد نجحنا."),
+    				                 Example.fixed("نعم<marker>,</marker> لقد نجحنا.")),
+        new DoublePunctuationRule(messages),
+        new MultipleWhitespaceRule(messages, this),
+        new LongSentenceRule(messages),
+        new SentenceWhitespaceRule(messages),
+        // specific to Arabic :
+        new HunspellNoSuggestionRule(messages, this, Example.wrong("هذا <marker>الانسان</marker> رائع"), Example.fixed("هذا <marker>الإنسان</marker> رائع"))
+
+	
+    );
+  }
+
+}
