23fd9b9d5439150d17541a9326d3e313204530c1
Sohaib Afifi <me@sohaibafifi.com>
2017-04-04 13:02:52 +0200

    [ar] add nonWordPattern to the LongSentenceRule constructor to reduce code duplication

--- languagetool-language-modules/ar/src/main/java/org/languagetool/rules/ar/ArabicLongSentenceRule.java
+++ languagetool-language-modules/ar/src/main/java/org/languagetool/rules/ar/ArabicLongSentenceRule.java
@@ -1,4 +1,5 @@
 /* LanguageTool, a natural language style checker
+ * Copyright (C) 2017 Sohaib Afifi, Taha Zerrouki
  * 
  * This library is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public
@@ -17,57 +18,22 @@
  */
 package org.languagetool.rules.ar;
 
-import java.io.IOException;
-import java.text.MessageFormat;
-import java.util.ArrayList;
-import java.util.List;
 import java.util.ResourceBundle;
 import java.util.regex.Pattern;
 
-import org.languagetool.AnalyzedSentence;
-import org.languagetool.AnalyzedTokenReadings;
 import org.languagetool.rules.LongSentenceRule;
-import org.languagetool.rules.RuleMatch;
 
 public class ArabicLongSentenceRule extends LongSentenceRule {
 
 	private static final Pattern NON_WORD_REGEX = Pattern.compile("[،؟؛.?!:;,~’'\"„“»«‚‘›‹()\\[\\]-]");
-	private final int maxWords;
 
 	public ArabicLongSentenceRule(ResourceBundle messages, int maxSentenceLength) {
-		super(messages, maxSentenceLength);
-		maxWords = maxSentenceLength;
+		super(messages, maxSentenceLength, NON_WORD_REGEX);
 	}
 
 	@Override
 	public final String getId() {
 		return "ARABIC_TOO_LONG_SENTENCE";
-	}
-
-	@Override
-	public RuleMatch[] match(AnalyzedSentence sentence) throws IOException {
-		List<RuleMatch> ruleMatches = new ArrayList<>();
-		AnalyzedTokenReadings[] tokens = sentence.getTokensWithoutWhitespace();
-		String msg = MessageFormat.format(messages.getString("long_sentence_rule_msg"), maxWords);
-		int numWords = 0;
-		int pos = 0;
-		if (tokens.length < maxWords + 1) { // just a short-circuit
-			return toRuleMatchArray(ruleMatches);
-		} else {
-			for (AnalyzedTokenReadings aToken : tokens) {
-				String token = aToken.getToken();
-				pos += token.length(); // won't match the whole offending
-										// sentence, but much of it
-				if (!aToken.isSentenceStart() && !aToken.isSentenceEnd() && !NON_WORD_REGEX.matcher(token).matches()) {
-					numWords++;
-				}
-			}
-		}
-		if (numWords > maxWords) {
-			RuleMatch ruleMatch = new RuleMatch(this, 0, pos, msg);
-			ruleMatches.add(ruleMatch);
-		}
-		return toRuleMatchArray(ruleMatches);
 	}
 
 }
