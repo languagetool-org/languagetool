0259f64265118f360a03f1068596b63f9dfa33f7
Sohaib Afifi <me@sohaibafifi.com>
2017-04-04 13:00:47 +0200

    [ar] add missing copyright statements

--- languagetool-language-modules/ar/src/main/java/org/languagetool/rules/ar/ArabicCommaWhitespaceRule.java
+++ languagetool-language-modules/ar/src/main/java/org/languagetool/rules/ar/ArabicCommaWhitespaceRule.java
@@ -1,4 +1,5 @@
 /* LanguageTool, a natural language style checker
+ * Copyright (C) 2017 Sohaib Afifi, Taha Zerrouki
  * 
  * This library is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public
