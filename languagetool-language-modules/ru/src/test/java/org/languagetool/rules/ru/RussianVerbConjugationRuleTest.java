fb920a0321e94ccac78acee2217bf6a42e54b44d
evleeena <evleeena@gmail.com>
2017-06-09 22:53:41 +0300

    [ru] add headers to RussianVerbConjugationRule.java and RussianVerbConjugationRuleTest.java

--- languagetool-language-modules/ru/src/test/java/org/languagetool/rules/ru/RussianVerbConjugationRuleTest.java
+++ languagetool-language-modules/ru/src/test/java/org/languagetool/rules/ru/RussianVerbConjugationRuleTest.java
@@ -1,3 +1,21 @@
+/* LanguageTool, a natural language style checker
+ * Copyright (C) 2010 Daniel Naber (http://www.languagetool.org)
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301
+ * USA
+ */
 package org.languagetool.rules.ru;
 
 import com.google.common.collect.ImmutableSet;
@@ -12,9 +30,6 @@ import java.util.Set;
 
 import static org.junit.Assert.*;
 
-/**
- * Created by zaets on 30.05.2017.
- */
 public class RussianVerbConjugationRuleTest {
 
     private Set<String> rightSentences = ImmutableSet.of("Я иду", "Она сидит", "Оно думает",
