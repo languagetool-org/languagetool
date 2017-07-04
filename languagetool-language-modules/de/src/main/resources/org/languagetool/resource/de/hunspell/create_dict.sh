18ffcde76f2906a0c9336ba3d9003ac1a377621b
Daniel Naber <naber@danielnaber.de>
2017-07-04 16:27:55 +0200

    update and fix paths

--- languagetool-language-modules/de/src/main/resources/org/languagetool/resource/de/hunspell/create_dict.sh
+++ languagetool-language-modules/de/src/main/resources/org/languagetool/resource/de/hunspell/create_dict.sh
@@ -14,10 +14,10 @@ then
 fi
 
 REPO=/home/dnaber/.m2/repository
-LT_VERSION=3.7-SNAPSHOT
+LT_VERSION=3.9-SNAPSHOT
 # get frequency data from https://github.com/mozilla-b2g/gaia/tree/master/apps/keyboard/js/imes/latin/dictionaries -
 # this is optional, remove "-freq $FREQ_FILE" below for not using frequencies:
-FREQ_FILE=/lt/de_wordlist.xml
+FREQ_FILE=/home/dnaber/lt/occurrence_counts/de_wordlist.xml
 INPUT_ENCODING=latin1
 OUTPUT_ENCODING=utf8
 TEMP_FILE=/tmp/lt-dictionary.dump
@@ -41,7 +41,7 @@ mvn clean package -DskipTests &&
  # the result using hunspell:
  recode $INPUT_ENCODING..$OUTPUT_ENCODING | grep -v "^#" | hunspell -d $DIC_NO_SUFFIX -G -l >$TEMP_FILE
 
-java -cp $CPATH:languagetool-standalone/target/LanguageTool-*/LanguageTool-*/languagetool.jar \
+java -cp $CPATH:languagetool-standalone/target/LanguageTool-$LT_VERSION/LanguageTool-$LT_VERSION/languagetool.jar:languagetool-standalone/target/LanguageTool-$LT_VERSION/LanguageTool-$LT_VERSION/libs/languagetool-tools.jar \
   org.languagetool.tools.SpellDictionaryBuilder -i $TEMP_FILE -info $INFO_FILE -o $OUTPUT_FILE -freq $FREQ_FILE
 
 rm $TEMP_FILE
