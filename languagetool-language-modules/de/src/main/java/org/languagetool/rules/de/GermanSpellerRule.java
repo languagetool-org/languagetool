a2884dfa5809cdbb616f00ae4f09971a0f59cfb0
Daniel Naber <naber@danielnaber.de>
2017-07-12 13:08:12 +0200

    [de] remove "Barbys"

--- languagetool-language-modules/de/src/main/java/org/languagetool/rules/de/GermanSpellerRule.java
+++ languagetool-language-modules/de/src/main/java/org/languagetool/rules/de/GermanSpellerRule.java
@@ -265,9 +265,6 @@ public class GermanSpellerRule extends CompoundAwareHunspellRule {
       return Collections.singletonList("Partys");
     } else if (word.equals("Babies")) {
       return Collections.singletonList("Babys");
-    // Jan: I don't think the following is correct
-    // } else if (word.equals("Barbies")) {
-      // return Collections.singletonList("Barbys");
     } else if (word.equals("Ladies")) {
       return Collections.singletonList("Ladys");
     } else if (!StringTools.startsWithUppercase(word)) {
