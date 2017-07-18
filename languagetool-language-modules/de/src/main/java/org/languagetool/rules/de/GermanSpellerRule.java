0e08005556fa1ffadb222db83d0b7b90dafe16cb
Jan Schreiber <jan.schreiber@languagetool.org>
2017-07-18 10:39:43 +0200

    [de] encourage "Erdoğan"

--- languagetool-language-modules/de/src/main/java/org/languagetool/rules/de/GermanSpellerRule.java
+++ languagetool-language-modules/de/src/main/java/org/languagetool/rules/de/GermanSpellerRule.java
@@ -265,6 +265,8 @@ public class GermanSpellerRule extends CompoundAwareHunspellRule {
       return Collections.singletonList("Ladys");
     } else if (word.matches("legen[td]lich")) {
       return Collections.singletonList("lediglich");
+    } else if (word.matches("Erdogans?$")) {
+      return Collections.singletonList(word.replaceFirst("^Erdogan", "Erdoğan"));
     } else if (word.matches("Email[a-zäöü]{5,}")) {
       String suffix = word.substring(5);
       if (hunspellDict.misspelled(suffix)) {
