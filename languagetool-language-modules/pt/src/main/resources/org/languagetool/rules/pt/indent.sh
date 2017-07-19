0af279e834ca1877072bda6537178d80b9742673
TiagoSantos <tiagofsantos81@sapo.pt>
2017-07-19 11:50:17 +0100

    [pt] indentation script update

--- languagetool-language-modules/pt/src/main/resources/org/languagetool/rules/pt/indent.sh
+++ languagetool-language-modules/pt/src/main/resources/org/languagetool/rules/pt/indent.sh
@@ -3,12 +3,12 @@ echo 'Please wait...'
 sed -ri 's/^[ \t]*(<\/?category)/ \1/' $@
 sed -ri 's/^[ \t]*(<\/?(rulegroup|!DOCTYPE))/  \1/' $@
 sed -ri 's/^[ \t]*(<\/?(rule |rule>|!--|!ENTITY))/    \1/' $@
-sed -ri 's/^[ \t]*(<\/?(marker|suggestion|equivalence|and|or))/        \1/' $@
+sed -ri 's/^[ \t]*(<\/?(marker|suggestion|equivalence|and|or|wd))/        \1/' $@
 sed -ri 's/^[ \t]*(<\/?(antipattern|pattern|regexp|filter|message|url|short|example|disambig|unification))/      \1/' $@
 sed -ri 's/^[ \t]*(<\/?(token|unify))/          \1/' $@
 sed -ri 's/^[ \t]*(<\/?(exception|feature))/            \1/' $@
 
-sed -ri 's/(<token[^>]*?)><\/token>/\1\/>/' $@
+sed -ri 's/(<(token|exception|suggestion|match|disambig|feature|wd)[^>]*?)><\/\2>/\1\/>/' $@
 sed -ri 's/[ \t]+\r?$/\r/' $@
 
 echo $@' indented'
