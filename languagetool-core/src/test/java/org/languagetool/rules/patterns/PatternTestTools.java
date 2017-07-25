ffcc51d30c3f127f835827b00b17d9b18e619250
f-knorr <florianknorr@web.de>
2017-07-25 19:18:39 +0200

    [de] add exception for German postag to PatterTestTool

--- languagetool-core/src/test/java/org/languagetool/rules/patterns/PatternTestTools.java
+++ languagetool-core/src/test/java/org/languagetool/rules/patterns/PatternTestTools.java
@@ -49,6 +49,14 @@ public final class PatternTestTools {
   private static final Pattern CHAR_SET_PATTERN = Pattern.compile("\\[^?([^\\]]+)\\]");
   private static final Pattern STRICT_CHAR_SET_PATTERN = Pattern.compile("(\\(\\?-i\\))?.*(?<!\\\\)\\[^?([^\\]]+)\\]");
 
+  /*
+   * These strings are not be recognized as a regular expression
+   */
+  private static final Set<String> NO_REGEXP = new HashSet<>(Arrays.asList(
+    "PRP:LOK+TMP+MOD:DAT+AKK"
+    ));
+
+
   private PatternTestTools() {
   }
 
@@ -332,7 +340,7 @@ public final class PatternTestTools {
             : PROBABLE_PATTERN;       // something else than Polish POS tag.
 
     if (!isRegularExpression && stringValue.length() > 1
-            && regexPattern.matcher(stringValue).find()) {
+            && regexPattern.matcher(stringValue).find() && !NO_REGEXP.contains(stringValue)) {
       System.err.println("The " + lang + " rule: "
               + ruleId + ", token [" + tokenIndex + "], contains " + "\"" + stringValue
               + "\" that is not marked as regular expression but probably is one.");
