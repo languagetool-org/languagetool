e7816c4d32baa72c74c5eb79ac60fc699185a723
Strn Git <strngit@gmail.com>
2017-08-27 20:37:37 +0200

    Readded Serbian language module

---
+++ languagetool-language-modules/sr/src/main/java/org/languagetool/language/BosniaSerbian.java
@@ -0,0 +1,15 @@
+package org.languagetool.language;
+
+public class BosniaSerbian extends Serbian {
+
+	@Override
+	public String[] getCountries() {
+		return new String[] { "BA" };
+	}
+
+	@Override
+	public String getName() {
+		return "Serbian (Bosnia)";
+	}
+
+}
