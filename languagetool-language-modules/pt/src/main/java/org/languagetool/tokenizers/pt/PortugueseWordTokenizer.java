8b8e0f0b918830c0d49590af63ca7d97ce770a07
TiagoSantos <tiagofsantos81@sapo.pt>
2017-09-01 21:01:28 +0100

    [pt] tokenize more mathematical operators

--- languagetool-language-modules/pt/src/main/java/org/languagetool/tokenizers/pt/PortugueseWordTokenizer.java
+++ languagetool-language-modules/pt/src/main/java/org/languagetool/tokenizers/pt/PortugueseWordTokenizer.java
@@ -44,7 +44,7 @@ public class PortugueseWordTokenizer extends WordTokenizer {
         + "\u2028\u2029\u202a\u202b\u202c\u202d\u202e\u202f"
         + "\u205F\u2060\u2061\u2062\u2063\u206A\u206b\u206c\u206d"
         + "\u206E\u206F\u3000\u3164\ufeff\uffa0\ufff9\ufffa\ufffb"
-        + "\u002A\u002B×∗·÷:="
+        + "\u002A\u002B×∗·÷:=≠≂≃≄≅≆≇≈≉≤≥≪≫∧∨∩∪∈∉∊∋∌∍"
         + ",.;()[]{}<>!?:/\\\"'«»„”“‘`’…¿¡\t\n\r";
 
   // Section copied from UkranianWordTokenizer.java for handling exceptions
