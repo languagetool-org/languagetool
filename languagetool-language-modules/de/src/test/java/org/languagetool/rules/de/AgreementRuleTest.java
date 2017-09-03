0a865cf623da617285afdd51a4321620fe8f82fe
f-knorr <florianknorr@web.de>
2017-09-03 13:30:09 +0200

    [de] added test / removed tag

--- languagetool-language-modules/de/src/test/java/org/languagetool/rules/de/AgreementRuleTest.java
+++ languagetool-language-modules/de/src/test/java/org/languagetool/rules/de/AgreementRuleTest.java
@@ -88,6 +88,8 @@ public class AgreementRuleTest {
     assertGood("Das viele Geld wird ihr helfen.");
     assertGood("Er verspricht jedem hohe Gewinne.");
     assertGood("Für ihn ist das Alltag.");
+    assertGood("Das ist ein super Tipp.");
+    assertGood("Für ihn ist das Alltag.");
 
     assertGood("Das Dach von meinem Auto.");
     assertGood("Das Dach von meinen Autos.");
