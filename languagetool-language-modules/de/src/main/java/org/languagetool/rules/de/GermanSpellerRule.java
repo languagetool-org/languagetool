fbeccbb035418715eaf5318ba59cecdf1ea08174
Jan Schreiber <jan.schreiber@languagetool.org>
2017-09-06 21:04:29 +0200

    [de] various spelling improvements

--- languagetool-language-modules/de/src/main/java/org/languagetool/rules/de/GermanSpellerRule.java
+++ languagetool-language-modules/de/src/main/java/org/languagetool/rules/de/GermanSpellerRule.java
@@ -348,6 +348,8 @@ public class GermanSpellerRule extends CompoundAwareHunspellRule {
       if (!hunspellDict.misspelled(suggestion)) {
         return Collections.singletonList(suggestion);
       }
+    } else if (word.equals("Ruhigkeit")) {
+      return Collections.singletonList("Ruhe");
     } else if (word.equals("angepreist")) {
       return Collections.singletonList("angepriesen");
     } else if (word.equals("halo")) {
