cf6f15db17d056a130ef1f72dbfcba2858f2e83c
Strn Git <strngit@gmail.com>
2017-09-07 21:29:21 +0200

    [sr] Renamed Serbian synth dictionary file

--- languagetool-language-modules/sr/src/main/resources/org/languagetool/resource/sr/gendicts.sh
+++ languagetool-language-modules/sr/src/main/resources/org/languagetool/resource/sr/gendicts.sh
@@ -1,16 +1,18 @@
 #!/usr/bin/env bash
 
 # This script contains command for quick POS dictionary and SYNTH_ dictionary generation.
-CORPUS_DIR=/opt/app/s/smd/ltagged
+CORPUS_FILE=/opt/app/s/smd/ltagged/serbian-corpus.txt
+
+if [ ! -f ${CORPUS_FILE} ]; then
+    echo "Serbian word corpus file ${CORPUS_FILE} does not exist, aborting ..."
+    exit 1
+fi
 
 echo "Generating POS Serbian dictionary ..."
-
-ltposdic -freq serbian-wordlist.xml -i ${CORPUS_DIR}/serbian-corpus.txt -info serbian.info -o serbian.dict
+ltposdic -freq serbian-wordlist.xml -i ${CORPUS_FILE} -info serbian.info -o serbian.dict
 
 echo "Generating SYNTH Serbian dictionary ..."
-
-ltsyndic -i ${CORPUS_DIR}/serbian-corpus.txt -info serbian-synth.info -o serbian_synth.info
+ltsyndic -i ${CORPUS_FILE} -info serbian_synth.info -o serbian_synth.dict
 
 # echo "Exporting dictionary for HunSpell ..."
-
 # ltexpdic -i serbian.dict -info serbian.info -o /tmp/dictionary.txt
