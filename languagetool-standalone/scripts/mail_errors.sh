ad32143c9ce1d62c55dcaff68d9f60a4bd32bc68
Daniel Naber <naber@danielnaber.de>
2017-09-23 09:14:18 +0200

    improve error filtering

--- languagetool-standalone/scripts/mail_errors.sh
+++ languagetool-standalone/scripts/mail_errors.sh
@@ -10,6 +10,9 @@ DATE=`date +"%Y%m"`
 cat log-[12]-${DATE}*_*.txt log-1.txt log-2.txt | grep "`date +"%Y-%m-%d %H:"`" | \
   egrep "An error has occurred" | \
   grep -v "Could not decode query" | \
+  grep -v "URLDecoder: Illegal hex characters" | \
+  grep -v "Missing 'language' parameter" | \
+  grep -v "Text checking took longer than" | \
   grep -v "Missing 'text' or 'data' parameter" | \
   grep -v "code 413" >$TMPDATA
 #cat log-1.txt log-2.txt | grep "`date +"%Y-%m-%d %H:"`" | egrep "An error has occurred" | grep -v "code 413" >$TMPDATA
