8a0bb1b150446232652e7404e50c6ef72d88227c
gulp21 <support.gulp21@googlemail.com>
2017-09-24 16:52:08 +0200

    add unit tests for Dictionary and Matrix

---
+++ languagetool-core/src/test/java/org/languagetool/rules/neuralnetwork/DictionaryTest.java
@@ -0,0 +1,18 @@
+package org.languagetool.rules.neuralnetwork;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class DictionaryTest {
+
+    @Test
+    public void mapFromStringTest() {
+        String dictionary = "{'a': 3, 'asa': 1, 'UNK': 42}";
+        Dictionary dict = new Dictionary(dictionary);
+        assertEquals(new Integer(1), dict.get("asa"));
+        assertEquals(new Integer(3), dict.safeGet("a"));
+        assertEquals(new Integer(42), dict.safeGet("foo"));
+    }
+
+}
---
+++ languagetool-core/src/test/java/org/languagetool/rules/neuralnetwork/MatrixTest.java
@@ -0,0 +1,35 @@
+package org.languagetool.rules.neuralnetwork;
+
+import org.junit.Test;
+
+import java.util.Arrays;
+
+import static org.junit.Assert.assertEquals;
+
+public class MatrixTest {
+
+    @Test
+    public void matrixFromListTest() {
+        String matrixString = "1 2\n3 4\n5 6";
+        Matrix expectedMatrix = new Matrix(new double[][]{{1,2},{3,4},{5,6}});
+        Matrix matrix = new Matrix(Arrays.asList(matrixString.split("\n")));
+        assertEquals(expectedMatrix, matrix);
+    }
+
+    @Test
+    public void matMulTest() {
+        final Matrix a = new Matrix(new double[][]{{1,2},{3,4},{5,6}});
+        final Matrix b = new Matrix(new double[][]{{1},{2}});
+        final Matrix c = new Matrix(new double[][]{{5},{11},{17}});
+        assertEquals(c, a.mul(b));
+    }
+
+    @Test
+    public void matAddTest() {
+        Matrix a = new Matrix(new double[][]{{1,2},{3,4},{5,6}});
+        Matrix b = new Matrix(new double[][]{{1,2},{3,4},{5,7}});
+        Matrix c = new Matrix(new double[][]{{2,4},{6,8},{10,13}});
+        assertEquals(c, a.add(b));
+    }
+
+}
