58ec54f77681850852a6260aab115a33117f8d02
Jan Schreiber <jan.schreiber@languagetool.org>
2017-09-24 13:57:30 +0200

    [de] tiny spell check improvement

--- languagetool-language-modules/de/src/test/java/org/languagetool/rules/de/GermanSpellerRuleTest.java
+++ languagetool-language-modules/de/src/test/java/org/languagetool/rules/de/GermanSpellerRuleTest.java
@@ -152,6 +152,7 @@ public class GermanSpellerRuleTest {
     assertFirstSuggestion("Wohlfühlsein", "Wellness", rule, lt);
     assertFirstSuggestion("schmetrlinge", "Schmetterlinge", rule, lt);
     assertFirstSuggestion("einlamienirte", "laminierte", rule, lt);
+    assertFirstSuggestion("Assecoires", "Accessoires", rule, lt);
   }
 
   @Test
