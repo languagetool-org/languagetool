78aa7232869aab640b9ad9851c985825bf37f60f
Daniel Naber <naber@danielnaber.de>
2017-10-22 14:38:52 +0200

    fix AuthException detection

--- languagetool-server/src/main/java/org/languagetool/server/LanguageToolHttpHandler.java
+++ languagetool-server/src/main/java/org/languagetool/server/LanguageToolHttpHandler.java
@@ -119,7 +119,7 @@ class LanguageToolHttpHandler implements HttpHandler {
       if (e instanceof TextTooLongException) {
         errorCode = HttpURLConnection.HTTP_ENTITY_TOO_LARGE;
         response = e.getMessage();
-      } else if (e instanceof AuthException) {
+      } else if (e instanceof AuthException ||  e.getCause() != null && e.getCause() instanceof AuthException) {
         errorCode = HttpURLConnection.HTTP_FORBIDDEN;
         response = e.getMessage();
       } else if (e instanceof IllegalArgumentException) {
