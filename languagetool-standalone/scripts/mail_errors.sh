1da49795344456a0e90d42040d40352273799285
Daniel Naber <naber@danielnaber.de>
2017-10-27 08:57:18 +0200

    more mail filtering

--- languagetool-standalone/scripts/mail_errors.sh
+++ languagetool-standalone/scripts/mail_errors.sh
@@ -19,6 +19,7 @@ cat log-[12]-${DATE}*_*.txt log-1.txt log-2.txt | grep "`date +"%Y-%m-%d %H:"`" 
   grep -v "You specified 'preferredVariants' but" | \
   grep -v "Unsupported action: ''" | \
   grep -v "langCode cannot be empty or whitespace only" | \
+  grep -v "Invalid format for 'preferredVariants'" | \
   grep -v "code 413" >$TMPDATA
 #cat log-1.txt log-2.txt | grep "`date +"%Y-%m-%d %H:"`" | egrep "An error has occurred" | grep -v "code 413" >$TMPDATA
 MATCHES=`cat $TMPDATA | wc -l`
