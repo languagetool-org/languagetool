55fdeedf443b75292adce57ef03aab3b1675226a
Fred Kruse <f.kruse@freenet.de>
2017-12-21 11:08:16 +0100

    [de] GermanStyleRepeatedWordRule: some optimizations

--- languagetool-language-modules/de/src/main/java/org/languagetool/rules/de/GermanStyleRepeatedWordRule.java
+++ languagetool-language-modules/de/src/main/java/org/languagetool/rules/de/GermanStyleRepeatedWordRule.java
@@ -41,7 +41,7 @@ import org.languagetool.rules.TextLevelRule;
 
 public class GermanStyleRepeatedWordRule  extends TextLevelRule {
   
-  private static final int MAX_TOKEN_TO_CHECK = 4;
+  private static final int MAX_TOKEN_TO_CHECK = 5;
 
   public GermanStyleRepeatedWordRule(ResourceBundle messages) {
     super(messages);
@@ -62,13 +62,39 @@ public class GermanStyleRepeatedWordRule  extends TextLevelRule {
     return "Wiederholende Worte in aufeinanderfolgenden Sätzen";
   }
   
+  /*
+   * Only substantive, names, verbs and adjectives are checked
+   */
   private static boolean isTokenToCheck(AnalyzedTokenReadings token) {
     return (token.matchesPosTagRegex("(SUB|EIG|VER|ADJ):.*") && !token.matchesPosTagRegex("ART:.*|ADV:.*|VER:(AUX|MOD):.*"));
   }
   
+  /*
+   * Pairs of substantive are excluded like "Arm in Arm", "Seite an Seite", etc.
+   */
+  private static boolean isTokenPair(AnalyzedTokenReadings[] tokens, int n, boolean before) {
+    if(before) {
+      if (tokens[n-2].matchesPosTagRegex("SUB:.*") && tokens[n-1].matchesPosTagRegex("PRP:.*") 
+          && tokens[n].matchesPosTagRegex("SUB:.*")) {
+        return true;
+      }
+    } else {
+      if (tokens[n].matchesPosTagRegex("SUB:.*") && tokens[n+1].matchesPosTagRegex("PRP:.*") 
+          && tokens[n+2].matchesPosTagRegex("SUB:.*")) {
+        return true;
+      }
+    }
+    return false;
+  }
+  
+  /*
+   * listings are excluded
+   */
   private static boolean hasBreakToken(AnalyzedTokenReadings[] tokens) {
     for(int i = 0; i < tokens.length && i < MAX_TOKEN_TO_CHECK; i++) {
-      if(tokens[i].getToken().equals("-") || tokens[i].getToken().equals("–")) return true;
+      if(tokens[i].getToken().equals("-") || tokens[i].getToken().equals("—") || tokens[i].getToken().equals("–")) {
+        return true;
+      }
     }
     return false;
   }
@@ -77,19 +103,38 @@ public class GermanStyleRepeatedWordRule  extends TextLevelRule {
     return isTokenInSentence(testToken, tokens, -1);
   }
   
+  /* 
+   *  true if token is found in sentence
+   */
   private static boolean isTokenInSentence(AnalyzedTokenReadings testToken, AnalyzedTokenReadings[] tokens, int notCheck) {
     if (testToken != null && tokens != null) {
       List<AnalyzedToken> readings = testToken.getReadings();
-      if (readings.size() < 1) return false;
+      if (readings.size() < 1) {
+        return false;
+      }
       String testBase = readings.get(0).getLemma();
-      if (testBase == null) return false;
+      if (testBase == null) {
+        return false;
+      }
       for (int i = 0; i < tokens.length; i++) {
         if (i != notCheck && isTokenToCheck(tokens[i])) {
           readings = tokens[i].getReadings();
           if (readings.size() > 0) {
             String base = readings.get(0).getLemma();
             if (base != null) {
-              if(testBase.equals(base)) return true;
+              if(testBase.equals(base)) {
+                if(notCheck >= 0) {
+                  if(notCheck == i - 2) {
+                    return !isTokenPair(tokens, i, true);
+                  } else if(notCheck == i + 2) {
+                    return !isTokenPair(tokens, i, false);
+                  } else if((notCheck == i + 1 || notCheck == i - 1) 
+                      && testToken.getToken().equals(tokens[i].getToken())) {
+                    return false;
+                  }
+                }
+                return true;
+              }
             }
           }
         }
@@ -106,23 +151,44 @@ public class GermanStyleRepeatedWordRule  extends TextLevelRule {
       AnalyzedSentence sentence = sentences.get(n);
       AnalyzedSentence lastSentence = null;
       AnalyzedSentence nextSentence = null;
-      if (n > 0) lastSentence = sentences.get(n - 1);
-      if (n < sentences.size() - 1) nextSentence = sentences.get(n + 1);
-      AnalyzedTokenReadings[] tokens = sentence.getTokens();
+      if (n > 0) {
+        lastSentence = sentences.get(n - 1);
+      }
+      if (n < sentences.size() - 1) {
+        nextSentence = sentences.get(n + 1);
+      }
+      AnalyzedTokenReadings[] tokens = sentence.getTokensWithoutWhitespace();
       if(!hasBreakToken(tokens)) {
         AnalyzedTokenReadings[] lastTokens = null;
         AnalyzedTokenReadings[] nextTokens = null;
-        if (lastSentence != null) lastTokens = lastSentence.getTokens();
-        if (nextSentence != null) nextTokens = nextSentence.getTokens();
+        if (lastSentence != null) {
+          lastTokens = lastSentence.getTokensWithoutWhitespace();
+        }
+        if (nextSentence != null) {
+          nextTokens = nextSentence.getTokensWithoutWhitespace();
+        }
         for (int i = 0; i < tokens.length; i++) {
           AnalyzedTokenReadings token = tokens[i];
           if (isTokenToCheck(token)) {
-            boolean isRepeated = false;
-            isRepeated = isTokenInSentence(token, tokens, i);
-            if (!isRepeated && lastTokens != null) isRepeated = isTokenInSentence(token, lastTokens);
-            if (!isRepeated && nextTokens != null) isRepeated = isTokenInSentence(token, nextTokens);
-            if (isRepeated) {
-              String msg = "Stilproblem: Wortwiederholung";
+            int isRepeated = 0;
+            if (isTokenInSentence(token, tokens, i)) {
+              isRepeated = 1;
+            }
+            if (isRepeated == 0 && lastTokens != null && isTokenInSentence(token, lastTokens)) {
+              isRepeated = 2;
+            }
+            if (isRepeated == 0 && nextTokens != null && isTokenInSentence(token, nextTokens)) {
+              isRepeated = 3;
+            }
+            if (isRepeated != 0) {
+              String msg;
+              if (isRepeated == 1) {
+                msg = "Stilproblem: Das Wort wird bereits im selben Satz verwendet!";
+              } else if (isRepeated == 2) {
+                msg = "Stilproblem: Das Wort wird bereits im vorhergehenden Satz verwendet!";
+              } else {
+                msg = "Stilproblem: Das Wort wird bereits im nachfolgenden Satz verwendet!";
+              }
               int startPos = pos + token.getStartPos();
               int endPos = pos + token.getEndPos();
               RuleMatch ruleMatch = new RuleMatch(this, startPos, endPos, msg);
