7cb528c390ae3307b4eac888acd24c591a84837a
Daniel Naber <naber@danielnaber.de>
2017-12-28 14:03:14 +0100

    adapt to slightly modified output of neural network rule

--- languagetool-core/src/test/java/org/languagetool/rules/neuralnetwork/NeuralNetworkRuleTest.java
+++ languagetool-core/src/test/java/org/languagetool/rules/neuralnetwork/NeuralNetworkRuleTest.java
@@ -47,7 +47,6 @@ public class NeuralNetworkRuleTest {
   public static void setUp() throws IOException {
     InputStream confusionSetStream = new ByteArrayInputStream("foo|lorem;bar|ipsum;0.8#blabla\nfizz;buzz;0.8".getBytes(StandardCharsets.UTF_8.name()));
     confusionSets = ScoredConfusionSetLoader.loadConfusionSet(confusionSetStream);
-
     language = TestTools.getDemoLanguage();
     lt = new JLanguageTool(language);
   }
@@ -55,39 +54,36 @@ public class NeuralNetworkRuleTest {
   @Test
   public void testNeuralNetworkRule() throws IOException {
     NeuralNetworkRule neuralNetworkRule = new NeuralNetworkRule(TestTools.getEnglishMessages(), language, confusionSets.get(0), context -> new float[]{-0.9f, 1.9f});
-
     assertThat(neuralNetworkRule.getId(), is("XX_foo_VS_bar_NEURALNETWORK"));
 
     AnalyzedSentence analyzedSentence = lt.getAnalyzedSentence("We go to the foo tomorrow.");
     RuleMatch[] ruleMatches = neuralNetworkRule.match(analyzedSentence);
     assertThat(ruleMatches.length, is(1));
-    assertThat(ruleMatches[0].getMessage(), is("Our neural network thinks that 'bar' (ipsum) might be the correct word here, not 'foo' (lorem). Please check. [-0.90, 1.90]"));
+    assertThat(ruleMatches[0].getMessage(), is("Our neural network thinks that 'bar' (ipsum) might be the correct word here, not 'foo' (lorem). Please check."));
     assertThat(ruleMatches[0].getSuggestedReplacements().get(0), is("bar"));
   }
 
   @Test
   public void testSuggestionTextRule() throws IOException {
     NeuralNetworkRule neuralNetworkRule = new NeuralNetworkRule(TestTools.getEnglishMessages(), language, confusionSets.get(0), context -> new float[]{0.9f, -1.9f});
-
     assertThat(neuralNetworkRule.getId(), is("XX_foo_VS_bar_NEURALNETWORK"));
 
     AnalyzedSentence analyzedSentence = lt.getAnalyzedSentence("We go to the bar tomorrow.");
     RuleMatch[] ruleMatches = neuralNetworkRule.match(analyzedSentence);
     assertThat(ruleMatches.length, is(1));
-    assertThat(ruleMatches[0].getMessage(), is("Our neural network thinks that 'foo' (lorem) might be the correct word here, not 'bar' (ipsum). Please check. [0.90, -1.90]"));
+    assertThat(ruleMatches[0].getMessage(), is("Our neural network thinks that 'foo' (lorem) might be the correct word here, not 'bar' (ipsum). Please check."));
     assertThat(ruleMatches[0].getSuggestedReplacements().get(0), is("foo"));
   }
 
   @Test
   public void testSuggestionTextWithoutDescription() throws IOException {
     NeuralNetworkRule neuralNetworkRule = new NeuralNetworkRule(TestTools.getEnglishMessages(), language, confusionSets.get(1), context -> new float[]{0.9f, -1.9f});
-
     assertThat(neuralNetworkRule.getId(), is("XX_fizz_VS_buzz_NEURALNETWORK"));
 
     AnalyzedSentence analyzedSentence = lt.getAnalyzedSentence("We go to the buzz tomorrow.");
     RuleMatch[] ruleMatches = neuralNetworkRule.match(analyzedSentence);
     assertThat(ruleMatches.length, is(1));
-    assertThat(ruleMatches[0].getMessage(), is("Our neural network thinks that 'fizz' might be the correct word here, not 'buzz'. Please check. [0.90, -1.90]"));
+    assertThat(ruleMatches[0].getMessage(), is("Our neural network thinks that 'fizz' might be the correct word here, not 'buzz'. Please check."));
     assertThat(ruleMatches[0].getSuggestedReplacements().get(0), is("fizz"));
   }
 
