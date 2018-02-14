779bd593ce2c37f48d76c2e84747900103ae9c86
Daniel Naber <naber@danielnaber.de>
2018-02-14 16:36:08 +0100

    [en] comment out OpenNMTRule, users activate it by accident and then get confused about what's going on (#903)

--- languagetool-language-modules/en/src/main/java/org/languagetool/language/English.java
+++ languagetool-language-modules/en/src/main/java/org/languagetool/language/English.java
@@ -178,7 +178,7 @@ public class English extends Language implements AutoCloseable {
         new MultipleWhitespaceRule(messages, this),
         new LongSentenceRule(messages),
         new SentenceWhitespaceRule(messages),
-        new OpenNMTRule(),
+        //new OpenNMTRule(),     // commented out because of #903
         new WhiteSpaceBeforeParagraphEnd(messages),
         new WhiteSpaceAtBeginOfParagraph(messages),
         new EmptyLineRule(messages),
