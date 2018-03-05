6305e6f3f6cf7ab22b4a8e7a9c11d3a17177973a
Daniel Naber <naber@danielnaber.de>
2018-03-05 09:14:09 +0100

    more exact measurement for API performance

--- languagetool-standalone/scripts/test-performance.sh
+++ languagetool-standalone/scripts/test-performance.sh
@@ -3,22 +3,21 @@
 DATADIR=/home/languagetool/languagetool.org/languagetool-website/www/regression-tests
 sleep $[ ( $RANDOM % 5 )  + 1 ]s
 
-RUNTIME=`/usr/bin/time --format="%E" curl -s --max-time 30 "https://languagetool.org/api/v2/check?language=en-US&text=my+texd" >/dev/null 2>/tmp/runtime.log`
-RUNTIME=`cat /tmp/runtime.log | sed s/0://`
+# Note: we first access the server and ignore it, as HTTPS has quite some (non-deterministic?) overhead.
+# If we don't, we get peak times that are caused by HTTPS, not by the API itself...
+RUNTIME=`curl -s -w "\nTOTAL_TIME:%{time_total}\n\n" "https://languagetool.org" "https://languagetool.org/api/v2/check?language=en-US&text=my+texd" | grep "TOTAL_TIME" | sed 's/TOTAL_TIME://' | tail -n 1`
 DATE=`date +"%Y-%m-%d %H:%M:%S"`
 echo "$DATE;$RUNTIME" >>$DATADIR/performance-api.log
-# 8640 = 288 checks per day * 30 days:
+# 288 per day * 30 days:
 tail -n 8640 $DATADIR/performance-api.log >$DATADIR/performance-api-recent.log
 /home/languagetool/api/perf-plot-api.pg >$DATADIR/performance-api.png
 
-RUNTIME=`/usr/bin/time --format="%E" curl -s --max-time 30 "https://languagetoolplus.com/api/v2/check?language=en-US&text=my+texd" >/dev/null 2>/tmp/runtime-ltp.log`
-RUNTIME=`cat /tmp/runtime-ltp.log | sed s/0://`
+RUNTIME=`curl -s -w "\nTOTAL_TIME:%{time_total}\n\n" "https://languagetoolplus.com/api/v2/languages" "https://languagetoolplus.com/api/v2/check?language=en-US&text=my+texd" | grep "TOTAL_TIME" | sed 's/TOTAL_TIME://' | tail -n 1`
 DATE=`date +"%Y-%m-%d %H:%M:%S"`
 echo "$DATE;$RUNTIME" >>$DATADIR/performance-ltp.log
 tail -n 8640 $DATADIR/performance-ltp.log >$DATADIR/performance-ltp-recent.log
 
-RUNTIME=`/usr/bin/time --format="%E" curl -s --max-time 30 "https://api.languagetool.org/v2/check?language=en-US&text=my+texd" >/dev/null 2>/tmp/runtime-cloud.log`
-RUNTIME=`cat /tmp/runtime-cloud.log | sed s/0://`
+RUNTIME=`curl -s -w "\nTOTAL_TIME:%{time_total}\n\n" "https://api.languagetool.org/v2/languages" "https://api.languagetool.org/v2/check?language=en-US&text=my+texd" | grep "TOTAL_TIME" | sed 's/TOTAL_TIME://' | tail -n 1`
 DATE=`date +"%Y-%m-%d %H:%M:%S"`
 echo "$DATE;$RUNTIME" >>$DATADIR/performance-api-cloud.log
 # 8640 = 288 checks per day * 30 days:
