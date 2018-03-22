06d61a6580d456741d7fd69873ce073ab056efaa
Laveen Ekka <laveen.ekka@gmail.com>
2018-03-22 12:41:20 -0400

    Update LanguageComboBoxModel.java

--- languagetool-standalone/src/main/java/org/languagetool/gui/LanguageComboBoxModel.java
+++ languagetool-standalone/src/main/java/org/languagetool/gui/LanguageComboBoxModel.java
@@ -23,7 +23,6 @@ import java.util.Collections;
 import java.util.List;
 import java.util.ResourceBundle;
 import javax.swing.DefaultComboBoxModel;
-
 import org.languagetool.JLanguageTool;
 import org.languagetool.Language;
 import org.languagetool.Languages;
