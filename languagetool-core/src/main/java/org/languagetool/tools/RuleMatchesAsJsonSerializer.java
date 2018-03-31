7faef3b828974a2e734ccd7e71c82d28795f9327
Daniel Naber <naber@danielnaber.de>
2018-03-31 15:19:02 +0200

    add information about whether the premium version is active

--- languagetool-core/src/main/java/org/languagetool/tools/RuleMatchesAsJsonSerializer.java
+++ languagetool-core/src/main/java/org/languagetool/tools/RuleMatchesAsJsonSerializer.java
@@ -41,6 +41,7 @@ public class RuleMatchesAsJsonSerializer {
 
   private static final int API_VERSION = 1;
   private static final String STATUS = "";
+  private static final String PREMIUM_HINT = "You might be missing errors only the Premium version can find. Please see https://languagetoolplus.com";
   private static final String START_MARKER = "__languagetool_start_marker";
 
   private final JsonFactory factory = new JsonFactory();
@@ -87,6 +88,10 @@ public class RuleMatchesAsJsonSerializer {
     g.writeStringField("version", JLanguageTool.VERSION);
     g.writeStringField("buildDate", JLanguageTool.BUILD_DATE);
     g.writeNumberField("apiVersion", API_VERSION);
+    g.writeBooleanField("premium", JLanguageTool.isPremiumVersion());
+    if (!JLanguageTool.isPremiumVersion()) {
+      g.writeStringField("premiumHint", PREMIUM_HINT);
+    }
     g.writeStringField("status", STATUS);
     g.writeEndObject();
   }
