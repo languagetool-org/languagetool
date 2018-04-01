f4a6013d0cf1b223079eec58e8f5bfdb3634bde7
Daniel Naber <naber@danielnaber.de>
2018-04-01 16:26:45 +0200

    change premium hint wording

--- languagetool-core/src/main/java/org/languagetool/tools/RuleMatchesAsJsonSerializer.java
+++ languagetool-core/src/main/java/org/languagetool/tools/RuleMatchesAsJsonSerializer.java
@@ -41,7 +41,7 @@ public class RuleMatchesAsJsonSerializer {
 
   private static final int API_VERSION = 1;
   private static final String STATUS = "";
-  private static final String PREMIUM_HINT = "You might be missing errors only the Premium version can find. Please see https://languagetoolplus.com";
+  private static final String PREMIUM_HINT = "You might be missing errors only the Premium version can find. Contact us at support<at>languagetoolplus.com.";
   private static final String START_MARKER = "__languagetool_start_marker";
 
   private final JsonFactory factory = new JsonFactory();
