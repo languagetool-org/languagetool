5f807329b5da8bd0dcea1206c15a4e60bb0c5472
Ze Dang <29113588@qq.com>
2018-05-11 15:27:23 +0800

    create SC and TC language model.

---
+++ languagetool-language-modules/zh/src/main/java/org/languagetool/language/TraditionalChinese.java
@@ -0,0 +1,37 @@
+package org.languagetool.language;
+
+import org.languagetool.tagging.Tagger;
+import org.languagetool.tagging.zh.SChineseTagger;
+import org.languagetool.tokenizers.SentenceTokenizer;
+import org.languagetool.tokenizers.Tokenizer;
+import org.languagetool.tokenizers.zh.SChineseSentenceTokenizer;
+import org.languagetool.tokenizers.zh.SChineseWordTokenizer;
+import org.languagetool.tokenizers.zh.TChinsesWordTokenizer;
+
+public class TraditionalChinese extends Chinese{
+
+    private Tokenizer wordTokenizer;
+
+    @Override
+    public String getName() {
+        return "TraditionalChinese";
+    }
+
+    @Override
+    public Tagger getTagger() {
+        return new SChineseTagger();
+    }
+
+    @Override
+    public Tokenizer getWordTokenizer() {
+        if (wordTokenizer == null) {
+            wordTokenizer = new TChinsesWordTokenizer();
+        }
+        return wordTokenizer;
+    }
+
+    @Override
+    public SentenceTokenizer getSentenceTokenizer() {
+        return new SChineseSentenceTokenizer();
+    }
+}
