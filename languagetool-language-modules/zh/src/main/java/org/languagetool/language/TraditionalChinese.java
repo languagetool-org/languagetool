7d4b6d1e5c8b581ca12964f6ca5348216d5e7631
Ze Dang <29113588@qq.com>
2018-05-18 10:58:41 +0800

    Fix bug in tokenizer and create "语法" rules in zh-CN.grammar

--- languagetool-language-modules/zh/src/main/java/org/languagetool/language/TraditionalChinese.java
+++ languagetool-language-modules/zh/src/main/java/org/languagetool/language/TraditionalChinese.java
@@ -1,11 +1,6 @@
 package org.languagetool.language;
 
-import org.languagetool.tokenizers.Tokenizer;
-import org.languagetool.tokenizers.zh.TChinsesWordTokenizer;
-
 public class TraditionalChinese extends Chinese{
-
-  private Tokenizer wordTokenizer;
 
   @Override
   public String getName() {
@@ -16,13 +11,5 @@ public class TraditionalChinese extends Chinese{
   public String[] getCountries() {
     return new String[]{"TW"};
   }
-
-//  @Override
-//  public Tokenizer getWordTokenizer() {
-//    if (wordTokenizer == null) {
-//      wordTokenizer = new TChinsesWordTokenizer();
-//    }
-//    return wordTokenizer;
-//  }
 
 }
