a40a1b9bb11256a6ff505e7e00ce3b98e1a1dc8b
Ze Dang <29113588@qq.com>
2018-06-11 16:04:09 +0800

    Fix problems reported in `Chinese Module update #1002`.

--- languagetool-language-modules/zh/src/main/java/org/languagetool/rules/zh/TraditionalChineseAmbiguityRule.java
+++ languagetool-language-modules/zh/src/main/java/org/languagetool/rules/zh/TraditionalChineseAmbiguityRule.java
@@ -1,8 +1,26 @@
+/* LanguageTool, a natural language style checker
+ * Copyright (C) 2013 Daniel Naber (http://www.danielnaber.de)
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301
+ * USA
+ */
+
 package org.languagetool.rules.zh;
 
 import com.hankcs.hanlp.HanLP;
 import org.languagetool.AnalyzedSentence;
-import org.languagetool.AnalyzedToken;
 import org.languagetool.AnalyzedTokenReadings;
 import org.languagetool.rules.Rule;
 import org.languagetool.rules.RuleMatch;
@@ -32,7 +50,7 @@ public class TraditionalChineseAmbiguityRule extends Rule {
 
   @Override
   public String getDescription() {
-    return "A rule detects ambiguities in words.";
+    return "A rule detects ambiguities in TC words.";
   }
 
   @Override
