2d8844663bfd3ad41cd0cea757571fd81c84c0b5
Fred Kruse <f.kruse@freenet.de>
2018-06-11 21:33:33 +0200

    abstractFillerWordsRule correction of copyright

--- languagetool-core/src/main/java/org/languagetool/rules/AbstractFillerWordsRule.java
+++ languagetool-core/src/main/java/org/languagetool/rules/AbstractFillerWordsRule.java
@@ -1,6 +1,6 @@
-/* LanguageTool, a natural language style checker
- * Copyright (C) 2011 Michael Bryant
- *
+/* LanguageTool, a natural language style checker 
+ * Copyright (C) 2005 Daniel Naber (http://www.danielnaber.de)
+ * 
  * This library is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public
  * License as published by the Free Software Foundation; either
