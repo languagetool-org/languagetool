037e989ca1dfdf0447194f78fc1e2d367c445c41
Daniel Naber <naber@danielnaber.de>
2018-08-08 16:06:20 +0200

    adapt to SuggestionsOrdererConfig (still commented out)

--- languagetool-standalone/src/test/java/org/languagetool/rules/patterns/PerformanceTest2.java
+++ languagetool-standalone/src/test/java/org/languagetool/rules/patterns/PerformanceTest2.java
@@ -22,6 +22,7 @@ import org.languagetool.Language;
 import org.languagetool.Languages;
 import org.languagetool.MultiThreadedJLanguageTool;
 import org.languagetool.rules.RuleMatch;
+import org.languagetool.rules.spelling.morfologik.suggestions_ordering.SuggestionsOrdererConfig;
 import org.languagetool.tools.StringTools;
 
 import java.io.File;
@@ -54,7 +55,9 @@ final class PerformanceTest2 {
       String subText = text.substring(beginIndex, endIndex);
       long startTime = System.currentTimeMillis();
       MultiThreadedJLanguageTool lt = new MultiThreadedJLanguageTool(language);
-      //lt.activateLanguageModelRules(new File("/home/dnaber/data/google-ngram-index"));
+      //String ngramPath = "/home/dnaber/data/google-ngram-index";
+      //lt.activateLanguageModelRules(new File(ngramPath));
+      //SuggestionsOrdererConfig.setNgramsPath(ngramPath);
       //lt.activateWord2VecModelRules(new File("/home/dnaber/lt/word2vec"));
       List<RuleMatch> matches = lt.check(subText);
       //System.out.println(matches);
