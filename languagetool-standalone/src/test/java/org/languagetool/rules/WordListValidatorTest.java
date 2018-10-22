ef7d279a89784a2d3d0b2b62a6107e054b473d7e
Jan Schreiber <jan.schreiber@languagetool.org>
2018-10-22 21:51:16 +0200

    [de/all] fix failing test

--- languagetool-standalone/src/test/java/org/languagetool/rules/WordListValidatorTest.java
+++ languagetool-standalone/src/test/java/org/languagetool/rules/WordListValidatorTest.java
@@ -52,6 +52,8 @@ public class WordListValidatorTest {
   private static final Set<String> VALID_WORDS = new HashSet<>(Arrays.asList(
           "Mondelēz",
           "Brač",
+          "Djuveč",
+          "Djuvečreis",
           "Hidschāb/S",
           "Dvořák/S",
           "Erdoğan/S",
