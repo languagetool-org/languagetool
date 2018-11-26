6a51039faba18dae8ab8d5f5c4d2a708f75e826d
R. Baars <baarsrj@xs4all.nl>
2018-11-26 10:37:55 +0100

    [nl] and another letter

--- languagetool-standalone/src/test/java/org/languagetool/rules/WordListValidatorTest.java
+++ languagetool-standalone/src/test/java/org/languagetool/rules/WordListValidatorTest.java
@@ -42,7 +42,7 @@ public class WordListValidatorTest {
           "ŚśōżúïÎôêâû" +
           "õ" +   // for Portuguese
           "·" +   // for Catalan
-          "'ÅıøğåšĝÇİŞŠśćč" +   // for Dutch proper names
+          "'ÅıøğåšĝÇİŞŠśčžć" +   // for Dutch (inhabitants) proper names mostly
           "./-]+" + 
           "|[khmcdµ]?m[²³]|°[CFR]|CO₂"
   );
