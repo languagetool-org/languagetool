b31b6515332c46bb14543e55b5cd7b8e6254b891
F. Knorr <florianknorr@web.de>
2019-02-03 21:00:29 +0100

    [de] minor improvement

--- languagetool-language-modules/de/src/main/java/org/languagetool/rules/de/AgreementRule.java
+++ languagetool-language-modules/de/src/main/java/org/languagetool/rules/de/AgreementRule.java
@@ -25,7 +25,6 @@ import java.util.HashSet;
 import java.util.List;
 import java.util.ResourceBundle;
 import java.util.Set;
-import java.util.regex.Pattern;
 
 import org.apache.commons.lang3.StringUtils;
 import org.jetbrains.annotations.Nullable;
