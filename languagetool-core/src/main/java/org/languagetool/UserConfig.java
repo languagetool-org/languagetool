4b18ce370c51fb5a385f5abdb52c0e20df32f694
Fabian Richter <fabianrichter@fastmail.com>
2019-02-27 10:47:37 +0100

    Fixed bug in A/B test logic caused by caching
    UserConfig.equals ignored textSessionId and abTest properties
    Because of pipeline caching, a pipeline could have saved a spelling rule with
    a UserConfig with a different textSessionId (and thus different group for A/B testing)
    than what the client would later submit along with the result of the test

--- languagetool-core/src/main/java/org/languagetool/UserConfig.java
+++ languagetool-core/src/main/java/org/languagetool/UserConfig.java
@@ -133,7 +133,10 @@ public class UserConfig {
       .append(configurableRuleValues, other.configurableRuleValues)
       .append(userDictName, other.userDictName)
       .append(userSpecificSpellerWords, other.userSpecificSpellerWords)
-      // skipping abTest and textSessionId on purpose - not relevant for caching
+      // omitting these distorts A/B tests, as UserConfig is cached by the pipeline pool
+      // -> (cached) textSessionId on server may say group A, but ID on client (relevant for saved correction) says B
+      .append(textSessionId, other.textSessionId)
+      .append(abTest, other.abTest)
       .isEquals();
   }
 
