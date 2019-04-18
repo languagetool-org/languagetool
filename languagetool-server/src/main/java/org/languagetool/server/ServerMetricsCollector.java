9fad210bca9a21b807e188171fff76529538b14b
Daniel Naber <naber@danielnaber.de>
2019-04-18 16:14:23 +0200

    adding license header

--- languagetool-server/src/main/java/org/languagetool/server/ServerMetricsCollector.java
+++ languagetool-server/src/main/java/org/languagetool/server/ServerMetricsCollector.java
@@ -1,3 +1,21 @@
+/* LanguageTool, a natural language style checker
+ * Copyright (C) 2019 Fabian Richter
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301
+ * USA
+ */
 package org.languagetool.server;
 
 import io.prometheus.client.Counter;
@@ -15,11 +33,10 @@ public class ServerMetricsCollector {
     MAX_TEXT_SIZE,
     INVALID_REQUEST
   }
-
-
+  
   private static final ServerMetricsCollector collector = new ServerMetricsCollector();
+  
   private static HTTPServer server;
-
 
   private final Counter checkCounter = Counter
     .build("languagetool_checks_total", "Total text checks").register();
