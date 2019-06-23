3e56a8548d2841f4628b7c4b8e1e29751d0b8156
Daniel Naber <naber@danielnaber.de>
2019-06-23 16:01:14 +0200

    [de] whitespace cleanup only

--- languagetool-language-modules/de/src/main/java/org/languagetool/tagging/de/SwissGermanTagger.java
+++ languagetool-language-modules/de/src/main/java/org/languagetool/tagging/de/SwissGermanTagger.java
@@ -32,19 +32,19 @@ public class SwissGermanTagger extends GermanTagger {
   /* (non-Javadoc)
    * @see org.languagetool.tagging.de.GermanTagger#tag(java.util.List, boolean)
    */
-	@Override
-  public List<AnalyzedTokenReadings> tag(List<String> sentenceTokens, boolean ignoreCase) throws IOException {
-  	List<AnalyzedTokenReadings> tokens = super.tag(sentenceTokens, ignoreCase);
+  @Override
+  public List<AnalyzedTokenReadings> tag(List<String> sentenceTokens, boolean ignoreCase) throws IOException { 
+    List<AnalyzedTokenReadings> tokens = super.tag(sentenceTokens, ignoreCase);
     for (int i = 0; i < tokens.size(); i++) {
-    	AnalyzedTokenReadings reading = tokens.get(i);
-      if (reading != null &&
-      		reading.getToken() != null &&
-      		reading.getToken().contains("ss") &&
-      		!reading.isTagged()) {
+      AnalyzedTokenReadings reading = tokens.get(i);
+      if (reading != null && 
+          reading.getToken() != null &&
+          reading.getToken().contains("ss") && 
+          !reading.isTagged()) {
         AnalyzedTokenReadings replacementReading = lookup(reading.getToken().replace("ss", "ß"));
         if(replacementReading != null) {
           for(AnalyzedToken at : replacementReading.getReadings()) {
-          	reading.addReading(new AnalyzedToken(reading.getToken(), at.getPOSTag(), at.getLemma()));
+            reading.addReading(new AnalyzedToken(reading.getToken(), at.getPOSTag(), at.getLemma()));
           }
         }
       }
