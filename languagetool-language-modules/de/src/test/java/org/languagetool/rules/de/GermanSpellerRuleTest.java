449a53a5881bc49c121fe0654aa955a46a62006a
Jan Schreiber <jan.schreiber@languagetool.org>
2019-07-20 15:49:36 +0200

    [de] fix build

--- languagetool-language-modules/de/src/test/java/org/languagetool/rules/de/GermanSpellerRuleTest.java
+++ languagetool-language-modules/de/src/test/java/org/languagetool/rules/de/GermanSpellerRuleTest.java
@@ -386,7 +386,6 @@ public class GermanSpellerRuleTest {
     assertFirstSuggestion("rethorisch", "rhetorisch", rule, lt);
     assertFirstSuggestion("anschliessliche", "anschließende", rule, lt);
     assertFirstSuggestion("Überstreitung", "Überschreitung", rule, lt);
-    assertFirstSuggestion("spätmöglichster", "spätestmöglicher", rule, lt);
   }
 
   @Test
