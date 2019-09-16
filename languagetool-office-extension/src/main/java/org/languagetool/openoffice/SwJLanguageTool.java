0b5ef90a8d7a4e2a06ffab16da4923691897a7c5
Fred Kruse <f.kruse@freenet.de>
2019-09-16 13:34:14 +0200

    [LO extension] remote access: bug at switch between textlevel and other rules fixed

--- languagetool-office-extension/src/main/java/org/languagetool/openoffice/SwJLanguageTool.java
+++ languagetool-office-extension/src/main/java/org/languagetool/openoffice/SwJLanguageTool.java
@@ -195,6 +195,7 @@ public class SwJLanguageTool {
     private boolean initDone = false;
     private URL serverBaseUrl;
     private Language language;
+    private Language motherTongue;
     private RemoteLanguageTool remoteLanguageTool;
     private List<String> enabledRules = new ArrayList<>();
     private List<String> disabledRules = new ArrayList<>();
@@ -207,8 +208,7 @@ public class SwJLanguageTool {
     LORemoteLanguageTool(Language language, Language motherTongue, UserConfig userConfig
         , List<Rule> extraRemoteRules) throws MalformedURLException {
       this.language = language;
-      configBuilder = new CheckConfigurationBuilder(language.getShortCodeWithCountryAndVariant());
-      configBuilder.setMotherTongueLangCode(motherTongue.getShortCodeWithCountryAndVariant());
+      this.motherTongue = motherTongue;
       serverBaseUrl = new URL(serverUrl == null ? SERVER_URL : serverUrl);
       remoteLanguageTool = new RemoteLanguageTool(serverBaseUrl);
       lt = new JLanguageTool(language, motherTongue, null, userConfig);
@@ -227,18 +227,25 @@ public class SwJLanguageTool {
           maxTextLength = SERVER_LIMIT;
           MessageHandler.printToLogFile("Server doesn't support maxTextLength, Limit text length set to: " + maxTextLength);
         }
+      }
+      configBuilder = new CheckConfigurationBuilder(language.getShortCodeWithCountryAndVariant());
+      configBuilder.setMotherTongueLangCode(motherTongue.getShortCodeWithCountryAndVariant());
+      if(paraMode == ParagraphHandling.ONLYPARA) {
+        if(!useServerConfig) {
+          configBuilder.enabledRuleIds(enabledRules);
+          configBuilder.ruleValues(getRuleValues());
+          configBuilder.enabledOnly();
+        }
+        configBuilder.mode("textLevelOnly");
+      } else {
         if(!useServerConfig) {
           configBuilder.enabledRuleIds(enabledRules);
           configBuilder.disabledRuleIds(disabledRules);
           configBuilder.ruleValues(getRuleValues());
         }
-        if(paraMode == ParagraphHandling.ONLYPARA) {
-          configBuilder.mode("textLevelOnly");
-        } else {
-          configBuilder.mode("allButTextLevelOnly");
-        }
-        remoteConfig = configBuilder.build();
+        configBuilder.mode("allButTextLevelOnly");
       }
+      remoteConfig = configBuilder.build();
       List<RuleMatch> ruleMatches = new ArrayList<>();
       int limit = maxTextLength;
       for (int nStart = 0; text.length() > nStart; nStart += limit) {
