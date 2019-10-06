6df7610e31bfbed04204be8da80c08b0be7555d7
Christopher Blum <christopher.blum@spellboy.com>
2019-10-06 17:32:51 +0200

    [en] add POS, words and improve rules

--- languagetool-language-modules/en/src/main/java/org/languagetool/language/English.java
+++ languagetool-language-modules/en/src/main/java/org/languagetool/language/English.java
@@ -281,6 +281,7 @@ public class English extends Language implements AutoCloseable {
       case "EN_DIACRITICS_REPLACE":     return 9;   // prefer over spell checker
       case "A_INFINITIVE":              return -1;  // prefer other more specific rules (with suggestions)
       case "PRP_VB":                    return -1;  // prefer other more specific rules (with suggestions)
+      case "CD_NN":                     return -1;  // prefer other more specific rules (with suggestions)
       case "PRONOUN_NOUN":              return -2;  // prefer other rules (e.g. PRP_VB)
       case "MORFOLOGIK_RULE_EN_US":     return -10;  // more specific rules (e.g. L2 rules) have priority
       case "MORFOLOGIK_RULE_EN_GB":     return -10;  // more specific rules (e.g. L2 rules) have priority
