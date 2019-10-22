7dfab61a4cbd4c13e5dc87fa15e71ba0e2c44e4b
Daniel Naber <naber@danielnaber.de>
2019-10-22 14:24:33 +0200

    fix test

--- languagetool-core/src/test/java/org/languagetool/rules/AbstractSimpleReplaceRule2Test.java
+++ languagetool-core/src/test/java/org/languagetool/rules/AbstractSimpleReplaceRule2Test.java
@@ -38,15 +38,15 @@ public class AbstractSimpleReplaceRule2Test {
     MyCaseSensitiveRule csRule = new MyCaseSensitiveRule(JLanguageTool.getMessageBundle(), lang);
     JLanguageTool lt = new JLanguageTool(lang);
     assertThat(csRule.match(lt.getAnalyzedSentence("But a propos")).length, is(1));
-    assertThat(csRule.match(lt.getAnalyzedSentence("A propos")).length, is(1));
-    //assertThat(csRule.match(lt.getAnalyzedSentence("But A propos")).length, is(0));  // TODO: should not be found
-    //assertThat(csRule.match(lt.getAnalyzedSentence("A Pokemon")).length, is(1));  // TODO: should be found
+    assertThat(csRule.match(lt.getAnalyzedSentence("But A propos")).length, is(0));
+    assertThat(csRule.match(lt.getAnalyzedSentence("A propos")).length, is(0));
+    assertThat(csRule.match(lt.getAnalyzedSentence("A Pokemon")).length, is(1));
     assertThat(csRule.match(lt.getAnalyzedSentence("A pokemon")).length, is(0));
 
     MyCaseInsensitiveRule ciRule = new MyCaseInsensitiveRule(JLanguageTool.getMessageBundle(), lang);
     assertThat(ciRule.match(lt.getAnalyzedSentence("But a propos")).length, is(1));
-    assertThat(ciRule.match(lt.getAnalyzedSentence("A propos")).length, is(1));
     assertThat(ciRule.match(lt.getAnalyzedSentence("But A propos")).length, is(1));
+    assertThat(ciRule.match(lt.getAnalyzedSentence("A propos")).length, is(1));
     //assertThat(ciRule.match(lt.getAnalyzedSentence("A Pokemon")).length, is(1));  // TODO: should be found
     //assertThat(ciRule.match(lt.getAnalyzedSentence("A pokemon")).length, is(1));  // TODO: should be found
   }
@@ -85,7 +85,7 @@ public class AbstractSimpleReplaceRule2Test {
     }
     @Override
     public boolean isCaseSensitive() {
-      return false;
+      return true;
     }
   }
 
