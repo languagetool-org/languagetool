ae1c91b102075ffe04b2c08d15b14a945ce2d3fa
Jim O'Regan <jaoregan@tcd.ie>
2019-10-27 10:46:11 +0100

    convert some more gramadoir rules

---
+++ languagetool-language-modules/ga/src/main/resources/org/languagetool/resource/ga/conv-gramadoir.pl
@@ -0,0 +1,62 @@
+#!/usr/bin/perl
+
+use warnings;
+use strict;
+use utf8;
+
+binmode(STDIN, ":utf8");
+binmode(STDOUT, ":utf8");
+
+while(<>) {
+	chomp;
+	s/\[Aa\]/a/g;
+	s/\[Áá\]/á/g;
+	s/\[Bb\]/b/g;
+	s/\[Cc\]/c/g;
+	s/\[Dd\]/d/g;
+	s/\[Ee\]/e/g;
+	s/\[Ff\]/f/g;
+	s/\[Gg\]/g/g;
+	s/\[Hh\]/h/g;
+	s/\[Ii\]/i/g;
+	s/\[Ll\]/l/g;
+	s/\[Mm\]/m/g;
+	s/\[Nn\]/n/g;
+	s/\[Oo\]/o/g;
+	s/\[Óó\]/ó/g;
+	s/\[Pp\]/p/g;
+	s/\[Rr\]/r/g;
+	s/\[Ss\]/s/g;
+	s/\[Tt\]/t/g;
+	s/\[Uu\]/u/g;
+	s/\[Úú\]/ú/g;
+
+	if(/([^ ]+) <N[^>]+>([^<]+)<\/[^>]*>:BACHOIR\{([^\}]+)\}/) {
+		my $num = $1;
+		my $word = $2;
+		my $repl = $3;
+
+		my $titlenum = uc($num);
+		$titlenum =~ s/.\?//;
+		my $titleword = uc($word);
+		my $title = $titlenum . '_' . $titleword;
+
+		my $egnum = $num;
+		$egnum =~ s/.\?//;
+
+my $out=<<__END__;
+        <rule id="$title" name="$egnum $word">
+            <pattern>
+                <token regexp="yes">$num</token>
+                <marker>
+                    <token>$word</token>
+                </marker>
+            </pattern>
+            <message>Ba chóir duit <suggestion>$repl</suggestion> a scríobh.</message>
+            <example correction='$repl'>$egnum <marker>$word</marker></example>
+        </rule>
+__END__
+
+	print $out;
+	}
+}
