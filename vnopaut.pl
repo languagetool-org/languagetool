322c3f4ec957654a5097483cbd5e9b6c19e99b33
Jim O'Regan <jaoregan@tcd.ie>
2019-11-05 18:25:54 +0100

    more

--- vnopaut.pl
+++ vnopaut.pl
@@ -15,18 +15,18 @@ sub unfada {
     $in =~ s/ú/u/g;
     return $in;
 }
-for my $word (qw/ár bhur dár faoinár inár lenár ónár sula trínár/) {
+for my $word (qw/a an cá dá go mura nach/) {
     my $upper = uc(unfada($word));
 my $out=<<__END__;
-        <rule id="${upper}_UNECLIPSED" name="$word UNECLIPSED">
+        <rule id="${upper}_VERB" name="$word VERB">
             <pattern>
                 <token>$word</token>
                 <marker>
-                    <token><exception postag=".*:Ecl" postag_regexp="yes"/></token>
+                    <token postag=".*Verb.*" postag_regexp="yes"><exception postag=".*:Ecl" postag_regexp="yes"/></token>
                 </marker>
             </pattern>
             <message>Urú ar iarraidh: <suggestion><match no="2" postag="([^:]*):(.*)" postag_regexp="yes" postag_replace="\$1:\$2:Ecl"/></suggestion></message>
-            <example correction='dteach'>$word <marker>teach</marker></example>
+            <example correction='gcantar'>$word <marker>cantar</marker></example>
         </rule>
 __END__
     print $out;
