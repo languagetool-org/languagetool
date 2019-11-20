c35b9bdf8e4874396d360def7c2a60d2508303f0
Daniel Naber <naber@danielnaber.de>
2019-11-20 17:07:47 +0100

    [en] avoid false alarm (https://forum.languagetool.org/t/en-english-possible-false-positive-2019-11-20/5075)

--- languagetool-language-modules/en/src/main/java/org/languagetool/rules/en/EnglishConfusionProbabilityRule.java
+++ languagetool-language-modules/en/src/main/java/org/languagetool/rules/en/EnglishConfusionProbabilityRule.java
@@ -49,6 +49,11 @@ public class EnglishConfusionProbabilityRule extends ConfusionProbabilityRule {
       "on point",
       "chapter one",
       "usb port",
+      // The quote in this case in management means: "Know the competition and your software, and you will win.":
+      "know the competition and",
+      "know the competition or",
+      "know your competition and",
+      "know your competition or",
       "G Suite",
       "your look is" // Really, your look is
     );
