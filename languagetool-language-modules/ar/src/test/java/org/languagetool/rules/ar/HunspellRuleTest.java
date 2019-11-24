3c7c7e1fe6b13d22f209725c8168d74f6e26a0e5
Sohaib LAFIFI <sohaib.lafifi@univ-artois.fr>
2019-11-24 12:01:36 +0100

    [ar] add HunspellRuleTest

---
+++ languagetool-language-modules/ar/src/test/java/org/languagetool/rules/ar/HunspellRuleTest.java
@@ -0,0 +1,43 @@
+/* LanguageTool, a natural language style checker 
+ * Copyright (C) 2012 Marcin Miłkowski (http://www.languagetool.org)
+ * 
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301
+ * USA
+ */
+package org.languagetool.rules.ar;
+
+import org.junit.Test;
+import org.languagetool.JLanguageTool;
+import org.languagetool.Languages;
+import org.languagetool.TestTools;
+import org.languagetool.rules.spelling.hunspell.HunspellRule;
+
+import static org.junit.Assert.assertEquals;
+
+public class HunspellRuleTest {
+
+  @Test
+  public void testRuleWithArabic() throws Exception {
+    HunspellRule rule = new HunspellRule(TestTools.getMessages("ar"), Languages.getLanguageForShortCode("ar"), null);
+    JLanguageTool langTool = new JLanguageTool(Languages.getLanguageForShortCode("ar"));
+    assertEquals(0, rule.match(langTool.getAnalyzedSentence("السلام عليكم.")).length);
+    assertEquals(1, rule.match(langTool.getAnalyzedSentence("السلام علييكم.")).length);
+    // ignore URLs:
+    assertEquals(0, rule.match(langTool.getAnalyzedSentence("تصفح http://foo.org/bar.")).length);
+
+  }
+
+  
+}
