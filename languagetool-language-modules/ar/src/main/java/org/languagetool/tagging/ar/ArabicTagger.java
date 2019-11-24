cd4c4e71a747f4a0609651a0e9565bcdff1baa76
Sohaib LAFIFI <sohaib.lafifi@univ-artois.fr>
2019-11-24 02:15:37 +0100

    [ar] update copyright statements

--- languagetool-language-modules/ar/src/main/java/org/languagetool/tagging/ar/ArabicTagger.java
+++ languagetool-language-modules/ar/src/main/java/org/languagetool/tagging/ar/ArabicTagger.java
@@ -1,5 +1,5 @@
 /* LanguageTool, a natural language style checker
- * Copyright (C) 2006 Daniel Naber (http://www.danielnaber.de)
+ * Copyright (C) 2019 Sohaib Afifi, Taha Zerrouki
  *
  * This library is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public
