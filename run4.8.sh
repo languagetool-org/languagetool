8d11dd7a2794ba4a2d6f4cb11ead03bbbebff3a3
linuxscout <taha_zerrouki@hotmail.com>
2019-12-08 16:17:41 +0100

    add more rules

---
+++ run4.8.sh
@@ -0,0 +1,2 @@
+java -jar languagetool-standalone/target/LanguageTool-4.8-SNAPSHOT/LanguageTool-4.8-SNAPSHOT/languagetool.jar 
+
---
+++ testrules-arabic.sh
@@ -0,0 +1,2 @@
+cd languagetool-standalone/target/LanguageTool-4.8-SNAPSHOT/LanguageTool-4.8-SNAPSHOT/; ./testrules.sh ar
+
