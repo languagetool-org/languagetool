e9e75cda38df689430e71929c1f36bc862f858e1
Taha Zerrouki <taha_zerrouki@hotmail.com>
2019-12-10 09:52:44 +0100

    [ar] Add new rules

--- languagetool-language-modules/ar/src/main/resources/org/languagetool/resource/ar/grammar_csv2xml.py
+++ languagetool-language-modules/ar/src/main/resources/org/languagetool/resource/ar/grammar_csv2xml.py
@@ -96,6 +96,7 @@ def main():
         line = line.strip('\n')
         if not line.startswith("#"):
             liste=line.split("\t");
+            liste = [x.strip() for x in liste]
             if len(liste) >= nb_field:
                 rule_table.append(liste);
         line=fl.readline().decode("utf8");
@@ -103,7 +104,7 @@ def main():
     #limit=MAX_LINES_TREATED;
     idrule = 1
     for tuple_rule in rule_table[:limit]:
-    #   
+    #
         rule ={}
         rule['pattern']        = tuple_rule[0].strip();
         rule['suggestions']    = tuple_rule[1].strip();
@@ -113,30 +114,30 @@ def main():
 
         print treat_rule(rule, idrule).encode('utf8')
         idrule += 1
-        
+
 def treat_rule(rule, idr):
     """ treat rule to be displayed as LT grammar XML
-    
+
     XML format as:
         <rule>
         <pattern>
         <marker><token>ثلاثة</token></marker>
-        <token postag='NFP'/>           
-        </pattern>          
+        <token postag='NFP'/>
+        </pattern>
         <message>أتقصد <suggestion>ثلاث</suggestion>؟</message>
         الاسم المؤنث يسبق بعدد مذكر
         <example correction="ثلاثة"><marker>ثلاث</marker>أولاد</example>
         <example correction="ثلاث"><marker>ثلاثة</marker>بنات</example>
     </rule>
-    
-    input format is 
+
+    input format is
         rule['pattern']      ;
-        rule['suggestions']    
+        rule['suggestions']
         rule['message']        ;
         rule['wrong_example']  ;
-        rule['correct_example'];    
+        rule['correct_example'];
     """
-    
+
     pattern, message = treat_pattern(rule['pattern'], rule['suggestions'], rule['message'])
     example = treat_example(rule['wrong_example'], rule['correct_example'])
     text = u"""\t<rule id ='unsorted%03.d'>
@@ -150,7 +151,7 @@ def treat_rule(rule, idr):
 \t</rule>
     """%(idr, pattern, message, example, rule['wrong_example'], rule['correct_example'])
     return text
-    
+
 def treat_pattern(pattern, suggestions, message):
     """
     Extract infos and fields from input
@@ -164,22 +165,18 @@ def treat_pattern(pattern, suggestions, message):
 
 def treat_example(wrong_example, correct_example):
     """ create an element to represent an example of error """
-    
+
     # split tokens
     correct_example = correct_example.split('/')[0]
     correct_tokens = araby.tokenize(correct_example)
     wrong_tokens   = araby.tokenize(wrong_example)
-    
+
     correct_word ,   wrong_tokens = diff(wrong_tokens, correct_tokens)
-    correct_word = u" ".join(correct_word )
-    wrong_output  = u" ".join(wrong_tokens  )
-
-            
-
-    
-    example = u"<example correct='%s'>%s </example>"%(correct_word, wrong_output)
+    correct_word = u" ".join(correct_word)
+    wrong_output  = u" ".join(wrong_tokens)
+    example = u"<example correction='%s'>%s</example>\n"%(correct_word, wrong_output)
     return example
-    
+
 def diff(wrong, correct):
     """ diff two lists"""
     i = 0
@@ -193,12 +190,12 @@ def diff(wrong, correct):
     if  i >= start  and j >= start and correct[i] == wrong[j]:
         i -= 1
         j -= 1
-        
+
     end_correct = i
     end_wrong   = j
     correct_word = correct[start:end_correct]
     wrong = wrong[:start] +['<marker>',] + wrong[start:end_wrong] +['</marker>',]+ wrong[end_wrong:]
-    
+
     return correct_word, wrong
 if __name__ == "__main__":
   main()
