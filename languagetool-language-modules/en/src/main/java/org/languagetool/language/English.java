a846896db96cdd0b027afdf52452162b0fe863ec
Christopher Blum <christopher.blum@spellboy.com>
2019-12-16 16:13:19 +0100

    [en] add and improve rules

--- languagetool-language-modules/en/src/main/java/org/languagetool/language/English.java
+++ languagetool-language-modules/en/src/main/java/org/languagetool/language/English.java
@@ -299,6 +299,7 @@ public class English extends Language implements AutoCloseable {
       case "MISSING_PREPOSITION":       return -1;  // prefer other more specific rules (with suggestions)
       case "PRP_RB_NO_VB":              return -1;  // prefer other more specific rules (with suggestions)
       case "NON3PRS_VERB":              return -1;  // prefer other more specific rules (with suggestions)
+      case "VB_NN":                     return -1;  // prefer other more specific rules (with suggestions)
       case "A_INFINITIVE":              return -2;  // prefer other more specific rules (with suggestions, e.g. PREPOSITION_VERB)
       case "I_MOVING":                  return -2;  // prefer other more specific rules (e.g. PRP_VBG)
       case "HE_VERB_AGR":               return -2;  // prefer other more specific rules (e.g. PRP_VBG)
