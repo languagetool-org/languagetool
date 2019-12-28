d64e78af26b949de4c05238824fe7ad6cd9690aa
Jim O'Regan <jaoregan@tcd.ie>
2019-12-29 00:59:58 +0100

    fix message

--- rule.pl
+++ rule.pl
@@ -6,7 +6,14 @@ use utf8;
 binmode(STDOUT,":utf8");
 
 my @things = qw/
-DO;n?[Dd]h?ó;dó;dhá
+AON;h?aon;haon;aon
+TRI;d?th?rí;trí;trí
+CEATHAIR;g?ch?eathair;ceathair;ceithre
+CUIG;g?ch?úig;cúig;cúig
+SE;sh?é;sé;sé
+SEACHT;sh?eacht;seacht;seacht
+OCHT;h?ocht;hocht;ocht
+NAOI;naoi;naoi;naoi
 /;
 
 for my $th (@things) {
@@ -17,7 +24,7 @@ print<<__END__;
             <antipattern>
                 <token>a</token>
                 <token regexp="yes">$c</token>
-                <token regexp="yes">dhéag</token>
+                <token regexp="yes">déag</token>
             </antipattern>
             <antipattern>
                 <token>$d</token>
@@ -34,20 +41,20 @@ print<<__END__;
                     <token regexp="yes">$b</token>
                     <token regexp="yes">dh?éag</token>
                 </pattern>
-                <message>Ní úsáidtear an focal seo ach sna abairtíní <suggestion>a $c dhéag<suggestion> nó <suggestion>$d X déag</suggestion> de ghnáth</message>
+                <message>Ní úsáidtear an focal seo ach sna abairtíní <suggestion>a $c déag</suggestion> agus <suggestion>$d X déag</suggestion> de ghnáth</message>
                 <short>In abairt</short>
-                <example correction="a $c dhéag|$d X déag"><marker>$c déag</marker></example>
+                <example correction="a $c déag|$d X déag"><marker>$c déag</marker></example>
             </rule>
             <rule>
                 <pattern>
                     <token>a</token>
-                    <token regexp="yes">$b</token>
+                    <token regexp="yes">$c</token>
                     <marker>
                     <token>déag</token>
                     </marker>
                 </pattern>
-                <message>Ba chóir duit <suggestion>dhéag</suggestion> a scríobh.</message>
-                <example correction="dhéag">a $c <marker>déag</marker></example>
+                <message>Ba chóir duit <suggestion>déag</suggestion> a scríobh.</message>
+                <example correction="déag">a $c <marker>dhéag</marker></example>
             </rule>
         </rulegroup>
 __END__
