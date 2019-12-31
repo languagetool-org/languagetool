42079c561667c840269ccc9f34c7c31ad9990231
Sohaib LAFIFI <sohaib.lafifi@univ-artois.fr>
2019-12-31 11:31:24 +0100

    [ar] factorize Tashkeel code

--- languagetool-language-modules/ar/src/main/java/org/languagetool/language/Arabic.java
+++ languagetool-language-modules/ar/src/main/java/org/languagetool/language/Arabic.java
@@ -43,6 +43,21 @@ public class Arabic extends Language implements AutoCloseable {
 
   private static final Language DEFAULT_ARABIC = new AlgerianArabic();
 
+  public static final String TASHKEEL_CHARS =
+    "\u064B"    // Fathatan
+    + "\u064C"  // Dammatan
+    + "\u064D"  // Kasratan
+    + "\u064E"  // Fatha
+    + "\u064F"  // Damma
+    + "\u0650"  // Kasra
+    + "\u0651"  // Shadda
+    + "\u0652"  // Sukun
+    + "\u0653"  // Maddah Above
+    + "\u0654"  // Hamza Above
+    + "\u0655"  // Hamza Below
+    + "\u0656"  // Subscript Alef
+    + "\u0640"; // Tatweel
+
   private LanguageModel languageModel;
 
   @Override
