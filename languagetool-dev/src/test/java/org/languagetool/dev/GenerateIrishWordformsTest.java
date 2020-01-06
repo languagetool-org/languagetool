a3f65c44216f9ef8cbca6578a545f76194f590eb
Jim O'Regan <jaoregan@tcd.ie>
2020-01-07 00:26:49 +0100

    delete unneeded

--- languagetool-dev/src/test/java/org/languagetool/dev/GenerateIrishWordformsTest.java
+++ languagetool-dev/src/test/java/org/languagetool/dev/GenerateIrishWordformsTest.java
@@ -33,10 +33,4 @@ public class GenerateIrishWordformsTest {
     assertEquals("b", aMap.get("stem"));
     assertEquals("ádóirí", aMap.get("pl.gen"));
   }
-
-  @Test
-  public void foo() {
-    GenerateIrishWordforms.writeFromGuess("blagadóir");
-    GenerateIrishWordforms.writeFromGuess("blagálaí");
-  }
 }
