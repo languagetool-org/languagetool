39d13dcb89478cd5631a0745435812ff7c81f000
Jan Schreiber <jan.schreiber@languagetool.org>
2020-01-12 17:36:32 +0100

    [de] spell-check: suggestion for "Kautsch"

--- languagetool-language-modules/de/src/main/java/org/languagetool/rules/de/GermanSpellerRule.java
+++ languagetool-language-modules/de/src/main/java/org/languagetool/rules/de/GermanSpellerRule.java
@@ -500,6 +500,7 @@ public class GermanSpellerRule extends CompoundAwareHunspellRule {
     put("[jJ]ahundert", "Jahrhundert");
     put("Gesochse", "Gesocks");
     put("[vV]orgensweise", "Vorgehensweise");
+    put("[kK]autsch", "Couch");
     put("guterletzt", "guter Letzt");
     putRepl("[zZ]uguterletzt", "guterletzt", " guter Letzt");
     putRepl("Nootbooks?", "Noot", "Note");
