ee6c5e5339e861fa7acd4cac5261ee915d2598fa
Christopher Blum <christopher.blum@spellboy.com>
2020-01-14 21:20:26 +0100

    [en] improve spelling suggestions

--- languagetool-language-modules/en/src/main/java/org/languagetool/rules/en/AbstractEnglishSpellerRule.java
+++ languagetool-language-modules/en/src/main/java/org/languagetool/rules/en/AbstractEnglishSpellerRule.java
@@ -204,8 +204,22 @@ public abstract class AbstractEnglishSpellerRule extends MorfologikSpellerRule {
       return Arrays.asList("Account", "Accusative");
     } else if ("Adhoc".equals(word)) {
       return Arrays.asList("Ad hoc");
-    } else if ("ad hoc".equals(word)) {
+    } else if ("adhoc".equals(word)) {
       return Arrays.asList("ad hoc");
+    } else if ("ios".equals(word)) {
+      return Arrays.asList("iOS");
+    } else if ("standup".equals(word)) {
+      return Arrays.asList("stand-up");
+    } else if ("standups".equals(word)) {
+      return Arrays.asList("stand-ups");
+    } else if ("Standup".equals(word)) {
+      return Arrays.asList("Stand-up");
+    } else if ("Standups".equals(word)) {
+      return Arrays.asList("Stand-ups");
+    } else if ("Playdough".equals(word)) {
+      return Arrays.asList("Play-Doh");
+    } else if ("playdough".equals(word)) {
+      return Arrays.asList("Play-Doh");
     } else if ("biggy".equals(word)) {
       return Arrays.asList("biggie");
     } else if ("lieing".equals(word)) {
@@ -284,8 +298,12 @@ public abstract class AbstractEnglishSpellerRule extends MorfologikSpellerRule {
       return Arrays.asList("Today's");
     } else if ("todo".equals(word)) {
       return Arrays.asList("to-do", "to do");
-    } else if ("Todo".equals(word)) {
+    } else if ("todos".equals(word)) {
+      return Arrays.asList("to-dos", "to do");
+    } else if ("Todo".equalsIgnoreCase(word)) {
       return Arrays.asList("To-do", "To do");
+    } else if ("Todos".equalsIgnoreCase(word)) {
+      return Arrays.asList("To-dos");
     } else if ("heres".equals(word)) {
       return Arrays.asList("here's");
     } else if ("Heres".equals(word)) {
