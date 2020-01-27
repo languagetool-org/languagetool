18b40b5179659137cf8e74272349d85aced9d13b
Daniel Naber <naber@danielnaber.de>
2020-01-27 15:16:57 +0100

    [de] adding "Trockner", so e.g. "Haartrockner" gets split

--- languagetool-language-modules/de/src/main/java/org/languagetool/tokenizers/de/GermanCompoundTokenizer.java
+++ languagetool-language-modules/de/src/main/java/org/languagetool/tokenizers/de/GermanCompoundTokenizer.java
@@ -48,6 +48,7 @@ public class GermanCompoundTokenizer implements Tokenizer {
     static Set<String> extendedList() {
       Set<String> words = new HashSet<>(EmbeddedGermanDictionary.getWords());
       // add compound parts here so we don't need to update JWordSplitter for every missing word we find:
+      words.add("trockner");
       words.add("thermostat");
       words.add("thermostats");
       words.add("fehl");
