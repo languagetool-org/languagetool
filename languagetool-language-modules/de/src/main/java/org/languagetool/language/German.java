8e7a83714850adb59bab3b173a78ab598d2cb155
Christopher Blum <christopher.blum@spellboy.com>
2020-02-13 18:30:16 +0100

    [de] change PRIO of COMMA_IN_FRONT_RELATIVE_CLAUSE

--- languagetool-language-modules/de/src/main/java/org/languagetool/language/German.java
+++ languagetool-language-modules/de/src/main/java/org/languagetool/language/German.java
@@ -295,6 +295,7 @@ public class German extends Language implements AutoCloseable {
       case "DASS_MIT_VERB": return 1; // prefer over SUBJUNKTION_KOMMA ("Dass wird Konsequenzen haben.")
       // default is 0
       case "DE_AGREEMENT": return -1;  // prefer RECHT_MACHEN, MONTAGS, KONJUNKTION_DASS_DAS and other
+      case "COMMA_IN_FRONT_RELATIVE_CLAUSE": return -1; // prefer other rules (KONJUNKTION_DASS_DAS)
       case "CONFUSION_RULE": return -1;  // probably less specific than the rules from grammar.xml
       case "MODALVERB_FLEKT_VERB": return -1;
       case "AKZENT_STATT_APOSTROPH": return -1;  // lower prio than PLURAL_APOSTROPH
