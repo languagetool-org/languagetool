8fe0259558bfc19ddbeb9fcf3b6e7302313393d9
Fabian Richter <fabianrichter@fastmail.com>
2020-02-21 14:03:54 +0100

    fixed DatabaseLoggerTest

--- languagetool-server/src/main/java/org/languagetool/server/DatabaseLogger.java
+++ languagetool-server/src/main/java/org/languagetool/server/DatabaseLogger.java
@@ -37,14 +37,11 @@ class DatabaseLogger {
   // package private for mocking in tests
   static DatabaseLogger instance = null;
 
-/*  // smaller numbers for tests
-  static final int SQL_BATCH_SIZE = 10;
-  static final int SQL_BATCH_WAITING_TIME = 5000; // milliseconds to wait until batch gets committed anyway
-  static final int POLLING_TIME = 1000; */
-  static final int SQL_BATCH_SIZE = 1000;
-  static final int SQL_BATCH_WAITING_TIME = 10000; // milliseconds to wait until batch gets committed anyway
+  // smaller numbers used for tests
+  static final int POLLING_TIME = 1000;
+  static int SQL_BATCH_SIZE = 1000;
+  static int SQL_BATCH_WAITING_TIME = 10000; // milliseconds to wait until batch gets committed anyway
   
-  private static final int POLLING_TIME = 1000;
   private static final int MAX_QUEUE_SIZE = 50000; // drop entries after limit is reached, to avoid running out of memory
 
   /**
