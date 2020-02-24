ab8f67b355987dd773738ef4d0d53aca48e24f9e
Christopher Blum <christopher.blum@spellboy.com>
2020-02-24 16:29:26 +0100

    [en] improve hyphen rules

--- languagetool-language-modules/en/src/main/java/org/languagetool/language/English.java
+++ languagetool-language-modules/en/src/main/java/org/languagetool/language/English.java
@@ -275,6 +275,7 @@ public class English extends Language implements AutoCloseable {
   @Override
   public int getPriorityForId(String id) {
     switch (id) {
+      case "I_E":                       return 10; // needs higher prio than EN_COMPOUNDS ("i.e learning")
       case "MISSING_HYPHEN":            return 5;
       case "TRANSLATION_RULE":          return 5;   // Premium
       case "WRONG_APOSTROPHE":          return 5;
