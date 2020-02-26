ed04b082b9056dda7455a8e67cda45441d24fb01
Daniel Naber <naber@danielnaber.de>
2020-02-26 12:19:07 +0100

    make 'triggers_error' check behave more like the real LT by splitting sentences

--- languagetool-core/src/test/java/org/languagetool/rules/patterns/PatternRuleTest.java
+++ languagetool-core/src/test/java/org/languagetool/rules/patterns/PatternRuleTest.java
@@ -56,7 +56,6 @@ import org.languagetool.rules.Rule;
 import org.languagetool.rules.RuleMatch;
 import org.languagetool.rules.spelling.SpellingCheckRule;
 import org.languagetool.tagging.disambiguation.rules.DisambiguationPatternRule;
-import org.languagetool.tools.RuleMatchAsXmlSerializer;
 
 /**
  * @author Daniel Naber
@@ -349,7 +348,7 @@ public class PatternRuleTest extends AbstractPatternRuleTest {
       // necessary for XML Pattern rules containing <or>
       List<RuleMatch> matches = new ArrayList<>();
       for (Rule auxRule : rules) { 
-        matches.addAll(getMatches(auxRule, badSentence, lt));
+        matches.addAll(getMatchesForSingleSentence(auxRule, badSentence, lt));
       }
       
       if (rule instanceof RegexPatternRule || rule instanceof PatternRule && !((PatternRule)rule).isWithComplexPhrase()) {
@@ -394,7 +393,7 @@ public class PatternRuleTest extends AbstractPatternRuleTest {
           for (String replacement : matches.get(0).getSuggestedReplacements()) {
             String fixedSentence = badSentence.substring(0, fromPos)
                 + replacement + badSentence.substring(toPos);
-            matches = getMatches(rule, fixedSentence, lt);
+            matches = getMatchesForSingleSentence(rule, fixedSentence, lt);
             if (matches.size() > 0) {
                 fail("Incorrect input:\n"
                         + "  " + badSentence
@@ -409,7 +408,7 @@ public class PatternRuleTest extends AbstractPatternRuleTest {
         }
       } else { // for multiple rules created with complex phrases
 
-        matches = getMatches(rule, badSentence, lt);
+        matches = getMatchesForSingleSentence(rule, badSentence, lt);
         if (matches.isEmpty()
             && !complexRules.containsKey(rule.getId() + badSentence)) {
           complexRules.put(rule.getId() + badSentence, rule);
@@ -455,7 +454,7 @@ public class PatternRuleTest extends AbstractPatternRuleTest {
                                             AbstractPatternRule rule) throws IOException {
     for (ErrorTriggeringExample example : rule.getErrorTriggeringExamples()) {
       String sentence = cleanXML(example.getExample());
-      List<RuleMatch> matches = getMatches(rule, sentence, lt);
+      List<RuleMatch> matches = getMatchesForText(rule, sentence, lt);
       if (matches.isEmpty()) {
         fail(lang + ": " + rule.getFullId() + ": Example sentence marked with 'triggers_error' didn't actually trigger an error: '" + sentence + "'");
       }
@@ -503,7 +502,7 @@ public class PatternRuleTest extends AbstractPatternRuleTest {
       for (String replacement : matches.get(0).getSuggestedReplacements()) {
         String fixedSentence = badSentence.substring(0, fromPos)
             + replacement + badSentence.substring(toPos);
-        List<RuleMatch> tempMatches = getMatches(rule, fixedSentence, lt);
+        List<RuleMatch> tempMatches = getMatchesForSingleSentence(rule, fixedSentence, lt);
         assertEquals("Corrected sentence for rule " + rule.getFullId()
             + " triggered error: " + fixedSentence, 0, tempMatches.size());
       }
@@ -565,7 +564,17 @@ public class PatternRuleTest extends AbstractPatternRuleTest {
     return matchCount > 0;
   }
 
-  private List<RuleMatch> getMatches(Rule rule, String sentence, JLanguageTool lt) throws IOException {
+  // Unlike getMatchesForSingleSentence() this splits the text at sentence boundaries
+  private List<RuleMatch> getMatchesForText(Rule rule, String sentence, JLanguageTool lt) throws IOException {
+    List<AnalyzedSentence> analyzedSentences = lt.analyzeText(sentence);
+    List<RuleMatch> matches = new ArrayList<>();
+    for (AnalyzedSentence analyzedSentence : analyzedSentences) {
+      matches.addAll(Arrays.asList(rule.match(analyzedSentence)));
+    }
+    return matches;
+  }
+
+  private List<RuleMatch> getMatchesForSingleSentence(Rule rule, String sentence, JLanguageTool lt) throws IOException {
     AnalyzedSentence analyzedSentence = lt.getAnalyzedSentence(sentence);
     RuleMatch[] matches = rule.match(analyzedSentence);
     if (CHECK_WITH_SENTENCE_SPLITTING) {
