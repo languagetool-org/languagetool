7bf3b470016907f67afab05e4e1f30ba7ad4004d
jaumeortola <jaumeortola@gmail.com>
2020-03-26 22:42:53 +0100

    [es] tag well-formed adverbs: -mente

--- languagetool-language-modules/es/src/main/java/org/languagetool/tagging/es/SpanishTagger.java
+++ languagetool-language-modules/es/src/main/java/org/languagetool/tagging/es/SpanishTagger.java
@@ -18,14 +18,29 @@
  */
 package org.languagetool.tagging.es;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Locale;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
+import org.jetbrains.annotations.Nullable;
+import org.languagetool.AnalyzedToken;
+import org.languagetool.AnalyzedTokenReadings;
 import org.languagetool.tagging.BaseTagger;
+import org.languagetool.tools.StringTools;
 
-/** Spanish Tagger.
- * @author Marcin Milkowski
+import morfologik.stemming.DictionaryLookup;
+import morfologik.stemming.IStemmer;
+
+/**
+ * Spanish Tagger.
+ * 
+ * @author Jaume Ortolà
  */
 public class SpanishTagger extends BaseTagger {
+
+  private static final Pattern ADJ_PART_FS = Pattern.compile("VMP00SF|A[QO].[FC][SN].");
 
   @Override
   public String getManualAdditionsFileName() {
@@ -34,5 +49,85 @@ public class SpanishTagger extends BaseTagger {
 
   public SpanishTagger() {
     super("/es/spanish.dict", new Locale("es"));
+  }
+
+  @Override
+  public List<AnalyzedTokenReadings> tag(final List<String> sentenceTokens) {
+
+    final List<AnalyzedTokenReadings> tokenReadings = new ArrayList<>();
+    int pos = 0;
+    final IStemmer dictLookup = new DictionaryLookup(getDictionary());
+
+    for (String word : sentenceTokens) {
+      final List<AnalyzedToken> l = new ArrayList<>();
+      final String lowerWord = word.toLowerCase(conversionLocale);
+      final boolean isLowercase = word.equals(lowerWord);
+      final boolean isMixedCase = StringTools.isMixedCase(word);
+      final boolean isAllUpper = StringTools.isAllUppercase(word);
+      List<AnalyzedToken> taggerTokens = asAnalyzedTokenListForTaggedWords(word, getWordTagger().tag(word));
+
+      // normal case:
+      addTokens(taggerTokens, l);
+      // tag non-lowercase (alluppercase or startuppercase), but not mixedcase
+      // word with lowercase word tags:
+      if (!isLowercase && !isMixedCase) {
+        List<AnalyzedToken> lowerTaggerTokens = asAnalyzedTokenListForTaggedWords(word, getWordTagger().tag(lowerWord));
+        addTokens(lowerTaggerTokens, l);
+      }
+      // tag all-uppercase proper nouns (ex. FRANCIA)
+      if (l.isEmpty() && isAllUpper) {
+        final String firstUpper = StringTools.uppercaseFirstChar(lowerWord);
+        List<AnalyzedToken> firstupperTaggerTokens = asAnalyzedTokenListForTaggedWords(word,
+            getWordTagger().tag(firstUpper));
+        addTokens(firstupperTaggerTokens, l);
+      }
+
+      // additional tagging with prefixes
+      if (l.isEmpty() && !isMixedCase) {
+        addTokens(additionalTags(word, dictLookup), l);
+      }
+
+      if (l.isEmpty()) {
+        l.add(new AnalyzedToken(word, null, null));
+      }
+
+      AnalyzedTokenReadings atr = new AnalyzedTokenReadings(l, pos);
+
+      tokenReadings.add(atr);
+      pos += word.length();
+    }
+
+    return tokenReadings;
+  }
+
+  @Nullable
+  protected List<AnalyzedToken> additionalTags(String word, IStemmer stemmer) {
+    final IStemmer dictLookup = new DictionaryLookup(getDictionary());
+    List<AnalyzedToken> additionalTaggedTokens = new ArrayList<>();
+    // Any well-formed adverb with suffix -ment is tagged as an adverb (RG)
+    // adjective o participle feminine singular + -ment
+    if (word.endsWith("mente")) {
+      final String lowerWord = word.toLowerCase(conversionLocale);
+      final String possibleAdj = lowerWord.replaceAll("^(.+)mente$", "$1");
+      List<AnalyzedToken> taggerTokens;
+      taggerTokens = asAnalyzedTokenList(possibleAdj, dictLookup.lookup(possibleAdj));
+      for (AnalyzedToken taggerToken : taggerTokens) {
+        final String posTag = taggerToken.getPOSTag();
+        if (posTag != null) {
+          final Matcher m = ADJ_PART_FS.matcher(posTag);
+          if (m.matches()) {
+            additionalTaggedTokens.add(new AnalyzedToken(word, "RG", lowerWord));
+            return additionalTaggedTokens;
+          }
+        }
+      }
+    }
+    return null;
+  }
+
+  private void addTokens(final List<AnalyzedToken> taggedTokens, final List<AnalyzedToken> l) {
+    if (taggedTokens != null) {
+      l.addAll(taggedTokens);
+    }
   }
 }
