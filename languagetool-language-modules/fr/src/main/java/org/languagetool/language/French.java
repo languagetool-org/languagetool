730dba4086823c787660d3c94f32db0d7dcbe16f
Christopher Blum <christopher.blum@spellboy.com>
2020-03-31 21:05:15 +0200

    [fr] prefer ESPACE_UNITES over spelling rule

--- languagetool-language-modules/fr/src/main/java/org/languagetool/language/French.java
+++ languagetool-language-modules/fr/src/main/java/org/languagetool/language/French.java
@@ -171,6 +171,7 @@ public class French extends Language implements AutoCloseable {
   @Override
   public int getPriorityForId(String id) {
     switch (id) {
+      case "ESPACE_UNITES": return 1; // needs to have higher priority than spell checker
       case "FRENCH_WHITESPACE_STRICT": return 1;  // default off, but if on, it should overwrite FRENCH_WHITESPACE 
       case "FRENCH_WHITESPACE": return 0;
     }
