9ab9190c860eedcb1a7a783cf05ac8e636caba1e
Christopher Blum <christopher.blum@spellboy.com>
2020-03-31 21:06:25 +0200

    [fr] disable two more grammalecte typography rules (will be activated once we have a picky mode)

--- languagetool-language-modules/fr/src/main/java/org/languagetool/rules/fr/GrammalecteRule.java
+++ languagetool-language-modules/fr/src/main/java/org/languagetool/rules/fr/GrammalecteRule.java
@@ -86,6 +86,8 @@ public class GrammalecteRule extends Rule {
     "typo_points_suspension3",
     "typo_tiret_incise", // picky
     "esp_avant_après_tiret", // picky
+    "nbsp_après_tiret1", // picky
+    "nbsp_après_tiret2", // picky
     "tab_début_ligne",
     "esp_milieu_ligne" // we already have WHITESPACE_RULE
   ));
