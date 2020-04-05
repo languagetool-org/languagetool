bf516ccecf36ddf2e112890c8143dc273ffaac54
jaumeortola <jaumeortola@gmail.com>
2020-04-05 15:25:51 +0200

    [ca] fix false alarms, improve rule tan/tant

--- languagetool-language-modules/ca/src/main/java/org/languagetool/rules/ca/MorfologikCatalanSpellerRule.java
+++ languagetool-language-modules/ca/src/main/java/org/languagetool/rules/ca/MorfologikCatalanSpellerRule.java
@@ -36,7 +36,7 @@ public final class MorfologikCatalanSpellerRule extends MorfologikSpellerRule {
   private static final String SPELLING_FILE = "/ca/spelling.txt";
   
   private static final Pattern PARTICULA_INICIAL = Pattern.compile("^(els?|als?|pels?|dels?|de|per|uns?|una|unes|la|les|[tms]eus?) (..+)$",Pattern.CASE_INSENSITIVE|Pattern.UNICODE_CASE);
-  private static final Pattern PREFIX_AMB_ESPAI = Pattern.compile("^(avant|auto|ex|extra|macro|mega|meta|micro|multi|mono|mini|post|retro|semi|super|trans) (..+)$",Pattern.CASE_INSENSITIVE|Pattern.UNICODE_CASE);
+  private static final Pattern PREFIX_AMB_ESPAI = Pattern.compile("^(des|avant|auto|ex|extra|macro|mega|meta|micro|multi|mono|mini|post|retro|semi|super|trans) (..+)$",Pattern.CASE_INSENSITIVE|Pattern.UNICODE_CASE);
   
   private static final Pattern APOSTROF_INICI_VERBS = Pattern.compile("^([lnmts])(h?[aeiouàéèíòóú].*)$",Pattern.CASE_INSENSITIVE|Pattern.UNICODE_CASE);
   private static final Pattern APOSTROF_INICI_NOM_SING = Pattern.compile("^([ld])(h?[aeiouàéèíòóú].+)$",Pattern.CASE_INSENSITIVE|Pattern.UNICODE_CASE);
