9719cfe71e3c2f6f798d83a2fb51a541820f370a
Daniel Naber <naber@danielnaber.de>
2020-04-10 11:10:46 +0200

    remove unmaintained experimental code

--- languagetool-language-modules/en/src/main/java/org/languagetool/language/English.java
+++ languagetool-language-modules/en/src/main/java/org/languagetool/language/English.java
@@ -200,7 +200,6 @@ public class English extends Language implements AutoCloseable {
         new EmptyLineRule(messages, this),
         new LongSentenceRule(messages, userConfig),
         new LongParagraphRule(messages, this, userConfig),
-        //new OpenNMTRule(),     // commented out because of #903
         new ParagraphRepeatBeginningRule(messages, this),
         new PunctuationMarkAtParagraphEnd(messages, this),
         new PunctuationMarkAtParagraphEnd2(messages, this),
