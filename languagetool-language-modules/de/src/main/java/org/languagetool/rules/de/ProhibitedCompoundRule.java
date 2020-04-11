5a734e5b3ac7b43c6f86032e86d508093d0c608d
Daniel Naber <naber@danielnaber.de>
2020-04-11 09:36:38 +0200

    [de] adding gel/geld

--- languagetool-language-modules/de/src/main/java/org/languagetool/rules/de/ProhibitedCompoundRule.java
+++ languagetool-language-modules/de/src/main/java/org/languagetool/rules/de/ProhibitedCompoundRule.java
@@ -46,6 +46,7 @@ public class ProhibitedCompoundRule extends Rule {
   private static final List<Pair> lowercasePairs = Arrays.asList(
           // NOTE: words here must be all-lowercase
           // NOTE: no need to add words from confusion_sets.txt, they will be used automatically (if starting with uppercase char)
+          new Pair("gel", "dickflüssige Masse", "geld", "Zahlungsmittel"),
           new Pair("kamp", "Flurname für ein Stück Land", "kampf", "Auseinandersetzung"),
           new Pair("obst", "Frucht", "ost", "Himmelsrichtung"),
           new Pair("beeren", "Früchte", "bären", "Raubtiere"),
