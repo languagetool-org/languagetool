fd2bb9d8014d6fff117b779e130612e7994c765c
baarsrj <baarsrj@xs4all.nl>
2020-04-24 09:20:20 +0200

    [nl] added flexibility for number of paramters to compound filter

--- languagetool-language-modules/nl/src/main/java/org/languagetool/rules/nl/CompoundFilter.java
+++ languagetool-language-modules/nl/src/main/java/org/languagetool/rules/nl/CompoundFilter.java
@@ -32,32 +32,48 @@ import static java.lang.Character.isUpperCase;
 
 public class CompoundFilter extends RuleFilter {
 
-  private final static String spelledWords = "(abc|adv|aed|apk|b2b|bh|bhv|bso|btw|bv|cao|cd|cfk|ckv|cv|dc|dj|dtp|dvd|fte|gft|ggo|ggz|gm|gmo|gps|gsm|hbo|" +
-           "hd|hiv|hr|hrm|hst|ic|ivf|kmo|lcd|lp|lpg|lsd|mbo|mdf|mkb|mms|msn|mt|ngo|nv|ob|ov|ozb|p2p|pc|pcb|pdf|pk|pps|" +
-           "pr|pvc|roc|rvs|sms|tbc|tbs|tl|tv|uv|vbo|vj|vmbo|vsbo|vwo|wc|wo|xtc|zzp)";
+  private static String glueParts(String s) {
+    String spelledWords = "(abc|adv|aed|apk|b2b|bh|bhv|bso|btw|bv|cao|cd|cfk|ckv|cv|dc|dj|dtp|dvd|fte|gft|ggo|ggz|gm|gmo|gps|gsm|hbo|" +
+            "hd|hiv|hr|hrm|hst|ic|ivf|kmo|lcd|lp|lpg|lsd|mbo|mdf|mkb|mms|msn|mt|ngo|nv|ob|ov|ozb|p2p|pc|pcb|pdf|pk|pps|" +
+            "pr|pvc|roc|rvs|sms|tbc|tbs|tl|tv|uv|vbo|vj|vmbo|vsbo|vwo|wc|wo|xtc|zzp)";
+    //System.out.print("******"+s+"****** => ");
+    String[] parts = s.split(" ");
+    String compound = parts[0];
+    for (int i = 1; i < parts.length; i++) {
+      String word2 = parts[i];
+      char lastChar = compound.charAt(compound.length() - 1);
+      char firstChar = word2.charAt(0);
+      String connection = lastChar + String.valueOf(firstChar);
+      if (StringUtils.containsAny(connection, "aa", "ae", "ai", "ao", "au", "ee", "ei", "eu", "ie", "ii", "oe", "oi", "oo", "ou", "ui", "uu", "ij")) {
+        compound = compound + '-' + word2;
+      } else if (isUpperCase(firstChar) && isLowerCase(lastChar)) {
+        compound = compound + '-' + word2;
+      } else if (isUpperCase(lastChar) && isLowerCase(firstChar)) {
+        compound = compound + '-' + word2;
+      } else if (compound.matches("(^|.+-)?" + spelledWords) || word2.matches(spelledWords + "(-.+|$)?")) {
+        compound = compound + '-' + word2;
+      } else if (compound.matches(".+-[a-z]$") || word2.matches("^[a-z]-.+")) {
+        compound = compound + '-' + word2;
+      } else {
+        compound = compound + word2;
+      }
+    }
+    //System.out.println("******"+compound+"******");
+    return compound;
+  }
 
   @Nullable
   @Override
   public RuleMatch acceptRuleMatch(RuleMatch match, Map<String, String> arguments, int patternTokenPos, AnalyzedTokenReadings[] patternTokens) {
-    String word1 = arguments.get("word1");
-    String word2 = arguments.get("word2");
-    char lastChar = word1.charAt(word1.length() - 1);
-    char firstChar = word2.charAt(0);
-    String connection = lastChar + String.valueOf(firstChar);
-    String repl;
-    if (StringUtils.containsAny(connection, "aa", "ae", "ai", "ao", "au", "ee", "ei", "eu", "ie", "ii", "oe", "oi", "oo", "ou", "ui", "uu", "ij")) {
-      repl = word1 + '-' + word2;
-    } else if (isUpperCase(firstChar) && isLowerCase(lastChar)) {
-      repl = word1 + '-' + word2;
-    } else if (isUpperCase(lastChar) && isLowerCase(firstChar)) {
-      repl = word1 + '-' + word2;
-    } else if (word1.matches("(^|.+-)?" + spelledWords) || word2.matches(spelledWords + "(-.+|$)?")) {
-      repl = word1 + '-' + word2;
-    } else if (word1.matches(".+-[a-z]$") || word2.matches("^[a-z]-.+")) {
-      repl = word1 + '-' + word2;
-    } else {
-      repl = word1 + word2;
+    String words="";
+    for (int i = 1; i < 6; i++) {
+      String arg=arguments.get("word"+i);
+        if(null != arg) {
+          words = words+" "+arguments.get("word"+i);
+        }
     }
+    words=words.substring(1);
+    String repl=glueParts(words);
     String message = match.getMessage().replaceAll("<suggestion>.*?</suggestion>", "<suggestion>" + repl + "</suggestion>");
     String shortMessage = match.getShortMessage().replaceAll("<suggestion>.*?</suggestion>", "<suggestion>" + repl + "</suggestion>");
     RuleMatch newMatch = new RuleMatch(match.getRule(), match.getSentence(), match.getFromPos(), match.getToPos(), message, shortMessage);
