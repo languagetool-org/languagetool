8e8a707cf4934e2a0f1fdfd4a3110e92ef76d5af
jaumeortola <jaumeortola@gmail.com>
2020-05-05 20:37:29 +0200

    [es] improve sentence tokenization

--- languagetool-language-modules/es/src/test/java/org/languagetool/tokenizers/es/SpanishSentenceTokenizerTest.java
+++ languagetool-language-modules/es/src/test/java/org/languagetool/tokenizers/es/SpanishSentenceTokenizerTest.java
@@ -73,6 +73,7 @@ public class SpanishSentenceTokenizerTest {
     testSplit("Ahora p. ej. esto.");
     testSplit("Ahora p. e. esto.");
     testSplit("Son las 5hrs. del domingo.");
+    testSplit("Son las 2as. jornadas.");
 
     // Exception to abbreviations    
     testSplit("Esto pasa el PP. ", "Pero, por otra parte,");
