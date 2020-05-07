6a3254d1c52228d327e20a9b05ece03df0d6629e
Sohaib LAFIFI <sohaib.lafifi@univ-artois.fr>
2020-05-07 20:49:00 +0200

    [ar] use upstream synthesizer (to support manual tags)

--- languagetool-language-modules/ar/src/main/java/org/languagetool/synthesis/ar/ArabicSynthesizer.java
+++ languagetool-language-modules/ar/src/main/java/org/languagetool/synthesis/ar/ArabicSynthesizer.java
@@ -18,28 +18,13 @@
  */
 package org.languagetool.synthesis.ar;
 
-import morfologik.stemming.IStemmer;
-import morfologik.stemming.WordData;
-import org.languagetool.AnalyzedToken;
 import org.languagetool.Language;
 import org.languagetool.synthesis.BaseSynthesizer;
-
-import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 
 /**
  * Arabic word form synthesizer.
  * Based on part-of-speech lists in Public Domain. See readme.txt for details,
- * the POS tagset is described in tagset.txt.
- * <p>
- * There are two special additions:
- * <ol>
- * <li>+GF - tag that adds  feminine gender to word</li>
- * <li>+GM - a tag that adds masculine gender to word</li>
- * </ol>
+ * the POS tagset is described in arabic_tags_description.txt.
  *
  * @author Taha Zerrouki
  * @since 4.9
@@ -48,64 +33,8 @@ public class ArabicSynthesizer extends BaseSynthesizer {
 
   private static final String RESOURCE_FILENAME = "/ar/arabic_synth.dict";
   private static final String TAGS_FILE_NAME = "/ar/arabic_tags.txt";
-
   public ArabicSynthesizer(Language lang) {
     super(RESOURCE_FILENAME, TAGS_FILE_NAME, lang);
-  }
-
-  /**
-   * Get a form of a given AnalyzedToken, where the form is defined by a
-   * part-of-speech tag.
-   *
-   * @param token  AnalyzedToken to be inflected.
-   * @param posTag A desired part-of-speech tag.
-   * @return String value - inflected word.
-   */
-  @Override
-  public String[] synthesize(AnalyzedToken token, String posTag) {
-    IStemmer synthesizer = createStemmer();
-    List<WordData> wordData = synthesizer.lookup(token.getLemma() + "|" + posTag);
-    List<String> wordForms = new ArrayList<>();
-    for (WordData wd : wordData) {
-      wordForms.add(wd.getStem().toString());
-    }
-    return wordForms.toArray(new String[0]);
-  }
-
-  /**
-   * Special Arabic regexp based synthesizer that allows adding articles
-   * when the regexp-based tag ends with a special signature {@code \\+GM} or {@code \\+GF}.
-   */
-  @Override
-  public String[] synthesize(AnalyzedToken token, String posTag,
-                             boolean posTagRegExp) throws IOException {
-
-    if (posTag != null && posTagRegExp) {
-      String myPosTag = posTag;
-      String det = "";
-      initPossibleTags();
-      Pattern p = Pattern.compile(myPosTag);
-      List<String> results = new ArrayList<>();
-
-      for (String tag : possibleTags) {
-        Matcher m = p.matcher(tag);
-        if (m.matches()) {
-          lookup(token.getLemma(), tag, results, det);
-        }
-      }
-      return results.toArray(new String[0]);
-    }
-
-    return synthesize(token, posTag);
-  }
-
-  private void lookup(String lemma, String posTag, List<String> results, String determiner) {
-    synchronized (this) { // the stemmer is not thread-safe
-      List<WordData> wordForms = getStemmer().lookup(lemma + "|" + posTag);
-      for (WordData wd : wordForms) {
-        results.add(determiner + wd.getStem());
-      }
-    }
   }
 
 }
