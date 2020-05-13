e5ffcf81eb4e1c4fb7adc66ccbfa7233e11c0e2b
Christopher Blum <christopher.blum@spellboy.com>
2020-05-13 14:19:14 +0200

    [en] add rules (start a replacement rule for A_PLURAL that is more reliable)

--- languagetool-language-modules/en/src/main/java/org/languagetool/language/English.java
+++ languagetool-language-modules/en/src/main/java/org/languagetool/language/English.java
@@ -258,7 +258,11 @@ public class English extends Language implements AutoCloseable {
       case "WON_T_TO":                  return 1;   // higher prio than DON_T_AREN_T
       case "WAN_T":                     return 1;   // higher prio than DON_T_AREN_T
       case "THE_US":                    return 1;   // higher prio than DT_PRP
+      case "NOW_A_DAYS":                return 1;   // higher prio than A_PLURAL/A_NNS
+      case "COUPLE_OF_TIMES":           return 1;   // higher prio than A_PLURAL/A_NNS
       case "A_WINDOWS":                 return 1;   // higher prio than A_PLURAL/A_NNS
+      case "A_SCISSOR":                 return 1;   // higher prio than A_PLURAL/A_NNS
+      case "ROUND_A_BOUT":              return 1;   // higher prio than A_PLURAL/A_NNS
       case "SEEM_SEEN":                 return 1;   // higher prio than HAVE_PART_AGREEMENT and PRP_HAVE_VB
       case "BORN_IN":                   return 1;   // higher prio than PRP_PAST_PART
       case "DO_TO":                     return 1;   // higher prio than HAVE_PART_AGREEMENT
