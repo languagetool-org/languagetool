369e2751c5260ecdc5319939a1dc9e36f9752b0f
Daniel Naber <naber@danielnaber.de>
2020-05-15 11:35:16 +0200

    [de] improve category name

--- languagetool-language-modules/de/src/main/java/org/languagetool/rules/de/GermanReadabilityRule.java
+++ languagetool-language-modules/de/src/main/java/org/languagetool/rules/de/GermanReadabilityRule.java
@@ -54,7 +54,7 @@ public class GermanReadabilityRule extends ReadabilityRule {
   public GermanReadabilityRule(ResourceBundle messages, Language lang, UserConfig userConfig, 
       boolean tooEasyTest, int level, boolean defaultOn) {
     super(messages, lang, userConfig, tooEasyTest, level, defaultOn);
-    super.setCategory(new Category(new CategoryId("TEXT_ANALYSIS"), "Textanalyse", Location.INTERNAL, false));
+    super.setCategory(new Category(new CategoryId("TEXT_ANALYSIS"), "Textanalyse/Lesbarkeit", Location.INTERNAL, false));
     this.tooEasyTest = tooEasyTest;
   }
   
