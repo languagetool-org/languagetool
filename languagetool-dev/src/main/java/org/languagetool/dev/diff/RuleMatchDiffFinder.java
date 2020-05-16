4e3e7b6e70db3dcc4861cb5f9426b3b9550c14c6
Daniel Naber <naber@danielnaber.de>
2020-05-16 08:51:08 +0200

    use CDN url for tablefiler, so we don't have to deal with relative/absolute path issues

--- languagetool-dev/src/main/java/org/languagetool/dev/diff/RuleMatchDiffFinder.java
+++ languagetool-dev/src/main/java/org/languagetool/dev/diff/RuleMatchDiffFinder.java
@@ -202,7 +202,7 @@ public class RuleMatchDiffFinder {
       fw.write("<head>\n");
       fw.write("  <title>" + title + "</title>\n");
       fw.write("  <meta charset='utf-8'>\n");
-      fw.write("  <script src='/regression-tests/tablefilter/tablefilter.js'></script>\n");  // https://github.com/koalyptus/TableFilter/
+      fw.write("  <script src='https://unpkg.com/tablefilter@0.7.0/dist/tablefilter/tablefilter.js'></script>\n");  // https://github.com/koalyptus/TableFilter/
       fw.write("  <style>\n");
       fw.write("    .sentence { color: #666; }\n");
       fw.write("    .marker { text-decoration: underline; }\n");
