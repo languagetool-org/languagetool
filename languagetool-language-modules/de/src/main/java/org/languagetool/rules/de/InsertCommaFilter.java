b43d4be54fb8f1bfc955a40fdc0419f2cc5b8ab1
Daniel Naber <naber@danielnaber.de>
2020-05-17 12:00:31 +0200

    [de] small code simplification

--- languagetool-language-modules/de/src/main/java/org/languagetool/rules/de/InsertCommaFilter.java
+++ languagetool-language-modules/de/src/main/java/org/languagetool/rules/de/InsertCommaFilter.java
@@ -66,25 +66,25 @@ public class InsertCommaFilter extends RuleFilter {
           List<AnalyzedTokenReadings> tags1 = tagger.tag(singletonList(parts[0]));
           List<AnalyzedTokenReadings> tags2 = tagger.tag(singletonList(parts[1]));
           List<AnalyzedTokenReadings> tags3 = tagger.tag(singletonList(parts[2]));
-          List<String> rest1 = Arrays.asList(parts).subList(1, parts.length);
+          String rest1 = String.join(" ", Arrays.asList(parts).subList(1, parts.length));
           if (parts.length == 4 && patternTokenPos == 2 &&
             patternTokens[0].hasPosTagStartingWith("VER:") &&
             patternTokens[1].getToken().matches("der|die|das|seine|ihre|deine|unsere|meine|folgender|dieser")) {
             // "Aristoteles meint(,) das Genussleben führe nicht zum Glück."
-            suggestions.add(parts[0] + ", " + String.join(" ", rest1));
+            suggestions.add(parts[0] + ", " + rest1);
           } else if (hasTag(tags1, "VER:") && hasTag(tags2, "PRO:POS:") && hasTag(tags3, "SUB:")) {
             // "Ich glaube(,) eure Premium-Accounts sind noch aktiv."
-            suggestions.add(parts[0] + ", " + String.join(" ", rest1));
+            suggestions.add(parts[0] + ", " + rest1);
           } else if (hasTag(tags1, "VER:") && hasTag(tags2, "PRO:PER:") && hasTag(tags3, "ADV:INR")) {
             // "Weißt du(,) warum diese Regel aus ist?"
-            List<String> rest2 = Arrays.asList(parts).subList(2, parts.length);
-            suggestions.add(parts[0] + " " + parts[1] + ", " + String.join(" ", rest2));
+            String rest2 = String.join(" ",Arrays.asList(parts).subList(2, parts.length));
+            suggestions.add(parts[0] + " " + parts[1] + ", " + rest2);
           } else if (hasTag(tags1, "VER:") && hasTag(tags2, "PRO:POS:") && hasTag(tags3, "ADJ:")) {
             // "Ich glaube(,) eure individuellen Premium-Accounts sind noch aktiv."
-            suggestions.add(parts[0] + ", " + String.join(" ", rest1));
+            suggestions.add(parts[0] + ", " + rest1);
           } else if (hasTag(tags1, "VER:") && hasTag(tags2, "PRO:DEM:") && hasTag(tags3, "SUB:")) {
             // "Ich schätze(,) diese Krawatte passt gut zum Anzug."
-            suggestions.add(parts[0] + ", " + String.join(" ", rest1));
+            suggestions.add(parts[0] + ", " + rest1);
           }
         }
       } catch (IOException e) {
