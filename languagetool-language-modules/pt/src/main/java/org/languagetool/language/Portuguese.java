e721f8d9b19529e8103b3416f3027f1452ee7fe3
Daniel Naber <naber@danielnaber.de>
2020-05-28 14:55:13 +0200

    [pt] make barbarism.txt specific to language variant (#2984)

--- languagetool-language-modules/pt/src/main/java/org/languagetool/language/Portuguese.java
+++ languagetool-language-modules/pt/src/main/java/org/languagetool/language/Portuguese.java
@@ -130,7 +130,7 @@ public class Portuguese extends Language implements AutoCloseable {
             //Specific to Portuguese:
             new PostReformPortugueseCompoundRule(messages),
             new PortugueseReplaceRule(messages),
-            new PortugueseBarbarismsRule(messages),
+            new PortugueseBarbarismsRule(messages, "/pt/barbarisms-pt.txt"),
             new PortugueseClicheRule(messages),
             new PortugueseFillerWordsRule(messages, this, userConfig),
             new PortugueseRedundancyRule(messages),
