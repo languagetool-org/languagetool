8a1fcd7f074b896758b3a243aabeee2499f5fece
Daniel Naber <naber@danielnaber.de>
2020-05-31 23:06:05 +0200

    [de] fix false alarms caused by POS update / noticed after POS update

--- languagetool-language-modules/de/src/test/java/org/languagetool/rules/de/AgreementRuleTest.java
+++ languagetool-language-modules/de/src/test/java/org/languagetool/rules/de/AgreementRuleTest.java
@@ -143,6 +143,7 @@ public class AgreementRuleTest {
     assertGood("Den 2019er Wert hatten sie geschätzt.");
     assertGood("Andere formale Systeme, deren Semantiken jeweils...");
     assertGood("Gesetz zur Änderung des Kündigungsrechts und anderer arbeitsrechtlicher Vorschriften");
+    //assertGood("... der zu dieser Zeit aber ohnehin schon allen Einfluss verloren hatte.");
 
     assertGood("Wir machen das Januar.");
     assertGood("Wir teilen das Morgen mit.");
