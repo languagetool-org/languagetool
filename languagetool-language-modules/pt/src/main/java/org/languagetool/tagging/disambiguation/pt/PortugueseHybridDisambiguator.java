ec7646b6ffae837cae54b69db60a0a905ffafae6
jaumeortola <jaumeortola@gmail.com>
2020-06-15 14:02:19 +0200

    [pt] simplify multiword tagging

--- languagetool-language-modules/pt/src/main/java/org/languagetool/tagging/disambiguation/pt/PortugueseHybridDisambiguator.java
+++ languagetool-language-modules/pt/src/main/java/org/languagetool/tagging/disambiguation/pt/PortugueseHybridDisambiguator.java
@@ -1,5 +1,5 @@
 /* LanguageTool, a natural language style checker
- * Copyright (C) 2007 Daniel Naber (http://www.danielnaber.de)
+ * Copyright (C) 2020 Daniel Naber (http://www.danielnaber.de)
  *
  * This library is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public
@@ -20,8 +20,13 @@
 package org.languagetool.tagging.disambiguation.pt;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 import org.languagetool.AnalyzedSentence;
+import org.languagetool.AnalyzedToken;
+import org.languagetool.AnalyzedTokenReadings;
 import org.languagetool.language.Portuguese;
 import org.languagetool.tagging.disambiguation.AbstractDisambiguator;
 import org.languagetool.tagging.disambiguation.Disambiguator;
@@ -29,24 +34,91 @@ import org.languagetool.tagging.disambiguation.MultiWordChunker;
 import org.languagetool.tagging.disambiguation.rules.XmlRuleDisambiguator;
 
 /**
- * Hybrid chunker-disambiguator for Portuguese
- *
- * @author Marcin Miłkowski
- * l18n by Tiago F. Santos
+ * Hybrid chunker-disambiguator for Portuguese.
+ * 
  */
 public class PortugueseHybridDisambiguator extends AbstractDisambiguator {
 
-    private final Disambiguator chunker = new MultiWordChunker("/pt/multiwords.txt");
-    private final Disambiguator disambiguator = new XmlRuleDisambiguator(new Portuguese());
+  private final Disambiguator chunker = new MultiWordChunker("/pt/multiwords.txt", true, true);
+  private final Disambiguator disambiguator = new XmlRuleDisambiguator(new Portuguese());
 
-    /**
-     * Calls two disambiguator classes: (1) a chunker; (2) a rule-based
-     * disambiguator.
-     */
-    @Override
-    public final AnalyzedSentence disambiguate(AnalyzedSentence input)
-            throws IOException {
-        return disambiguator.disambiguate(chunker.disambiguate(input));
+  /**
+   * Calls two disambiguator classes: (1) a chunker; (2) a rule-based
+   * disambiguator.
+   * 
+   * Put the results of the MultiWordChunker in a more appropriate and useful way.
+   *   <NN></NN> becomes NN NN
+   *   The individual original tags are removed. 
+   *   Add spell ignore
+   */
+  @Override
+  public final AnalyzedSentence disambiguate(AnalyzedSentence input)
+      throws IOException {
+    AnalyzedSentence analyzedSentence = chunker.disambiguate(input);
+           
+    AnalyzedTokenReadings[] aTokens = analyzedSentence.getTokens();
+    int i=0;
+    String POSTag = "";
+    String lemma = "";
+    String nextPOSTag = "";
+    AnalyzedToken analyzedToken = null;
+    while (i < aTokens.length) {
+      if (!aTokens[i].isWhitespace()) {  
+        if (!nextPOSTag.isEmpty()) {
+          AnalyzedToken newAnalyzedToken = new AnalyzedToken(aTokens[i].getToken(), nextPOSTag, lemma);
+          if (aTokens[i].hasPosTagAndLemma("</" + POSTag + ">", lemma)) {
+            nextPOSTag = "";
+            lemma = "";
+          }
+          aTokens[i] = new AnalyzedTokenReadings(aTokens[i], Arrays.asList(newAnalyzedToken), "EN_HybridDisambiguator");
+          aTokens[i].ignoreSpelling();
+        } else if ((analyzedToken = getMultiWordAnalyzedToken(aTokens, i)) != null) {
+          POSTag = analyzedToken.getPOSTag().substring(1, analyzedToken.getPOSTag().length() - 1);
+          lemma = analyzedToken.getLemma();
+          AnalyzedToken newAnalyzedToken = new AnalyzedToken(analyzedToken.getToken(), POSTag, lemma);
+          aTokens[i] = new AnalyzedTokenReadings(aTokens[i], Arrays.asList(newAnalyzedToken), "EN_HybridDisambiguator");
+          aTokens[i].ignoreSpelling();
+          nextPOSTag = POSTag;
+        }
+      }
+      i++;
     }
+
+    return disambiguator.disambiguate(new AnalyzedSentence(aTokens));
+  }
+  
+  private AnalyzedToken getMultiWordAnalyzedToken(AnalyzedTokenReadings[] aTokens, Integer i) {
+    List<AnalyzedToken> l = new ArrayList<AnalyzedToken>();
+    for (AnalyzedToken reading : aTokens[i]) {
+      String POSTag = reading.getPOSTag();
+      if (POSTag != null) {
+        if (POSTag.startsWith("<") && POSTag.endsWith(">") && !POSTag.startsWith("</")) {
+          l.add(reading);
+        }
+      }
+    }
+    // choose the longest one
+    if (l.size() > 0) { 
+      AnalyzedToken selectedAT = null;
+      int maxDistance = 0;
+      for (AnalyzedToken at : l) {
+        String tag = "</" + at.getPOSTag().substring(1);
+        String lemma = at.getLemma();
+        int distance = 1;
+        while (i + distance < aTokens.length) {
+          if (aTokens[i + distance].hasPosTagAndLemma(tag, lemma)) {
+            if (distance > maxDistance) {
+              distance = maxDistance;
+              selectedAT = at;
+            }
+            break;
+          }
+          distance++;
+        }
+      }
+      return selectedAT;
+    }
+    return null;
+  }
 
 }
---
+++ languagetool-language-modules/pt/src/test/java/org/languagetool/tagging/disambiguation/pt/PortugueseDisambiguationRuleTest.java
@@ -0,0 +1,64 @@
+/* LanguageTool, a natural language style checker 
+ * Copyright (C) 2020 Daniel Naber (http://www.danielnaber.de)
+ * 
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301
+ * USA
+ */
+package org.languagetool.tagging.disambiguation.pt;
+
+import java.io.IOException;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.languagetool.TestTools;
+import org.languagetool.language.Portuguese;
+//import org.languagetool.tagging.disambiguation.rules.XmlRuleDisambiguator;
+//import org.languagetool.tagging.disambiguation.xx.DemoDisambiguator;
+import org.languagetool.tagging.disambiguation.pt.PortugueseHybridDisambiguator;
+import org.languagetool.tagging.pt.PortugueseTagger;
+import org.languagetool.tokenizers.SRXSentenceTokenizer;
+import org.languagetool.tokenizers.SentenceTokenizer;
+import org.languagetool.tokenizers.WordTokenizer;
+
+public class PortugueseDisambiguationRuleTest {
+  
+  private PortugueseTagger tagger;
+  private WordTokenizer tokenizer;
+  private SentenceTokenizer sentenceTokenizer;
+  //private XmlRuleDisambiguator disambiguator;
+  //private DemoDisambiguator disamb2;
+  private PortugueseHybridDisambiguator hybridDisam;
+  
+  @Before
+  public void setUp() {
+    tagger = new PortugueseTagger();
+    tokenizer = new WordTokenizer();
+    sentenceTokenizer = new SRXSentenceTokenizer(new Portuguese());
+    //disambiguator = new XmlRuleDisambiguator(new Portuguese());
+    //disamb2 = new DemoDisambiguator(); 
+    hybridDisam = new PortugueseHybridDisambiguator();
+  }
+
+  @Test
+  public void testChunker() throws IOException {
+    TestTools.myAssert("A cada semana.",
+        "/[null]SENT_START A/[a cada semana]RG  /[null]null cada/[a cada semana]RG  /[null]null semana/[a cada semana]RG ./[.]_PUNCT", 
+        tokenizer, sentenceTokenizer, tagger, hybridDisam);
+    
+  }
+
+}
+
+
