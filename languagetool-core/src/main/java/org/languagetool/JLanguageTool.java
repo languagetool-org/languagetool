dd6daef067352005677aac58cdab686fbad3dfd5
Daniel Naber <naber@danielnaber.de>
2020-06-17 08:26:20 +0200

    remove word count from exception, it's useful only as debugging information

--- languagetool-core/src/main/java/org/languagetool/JLanguageTool.java
+++ languagetool-core/src/main/java/org/languagetool/JLanguageTool.java
@@ -1483,7 +1483,8 @@ public class JLanguageTool {
             CommonWords commonWords = new CommonWords();
             throw new ErrorRateTooHighException("Text checking was stopped due to too many errors (more than " + String.format("%.0f", maxErrorsPerWordRate*100) +
                     "% of words seem to have an error). Are you sure you have set the correct text language? Language set: " + JLanguageTool.this.language.getName() +
-                    ", text length: " + annotatedText.getPlainText().length() + ", common word count: " + commonWords.getKnownWordsPerLanguage(annotatedText.getPlainText()));
+                    ", text length: " + annotatedText.getPlainText().length());
+            //        ", text length: " + annotatedText.getPlainText().length() + ", common word count: " + commonWords.getKnownWordsPerLanguage(annotatedText.getPlainText()));
           }
           charCount += sentence.length();
           lineCount += countLineBreaks(sentence);
