45755c806c0c62e9310553de1e08c08458a6223c
jaumeortola <jaumeortola@gmail.com>
2020-06-26 13:15:28 +0200

    [pt] temp_off rules #3158

--- languagetool-language-modules/pt/src/main/java/org/languagetool/language/Portuguese.java
+++ languagetool-language-modules/pt/src/main/java/org/languagetool/language/Portuguese.java
@@ -115,7 +115,7 @@ public class Portuguese extends Language implements AutoCloseable {
                     Arrays.asList("[", "(", "{", "\"", "“" /*, "«", "'", "‘" */),
                     Arrays.asList("]", ")", "}", "\"", "”" /*, "»", "'", "’" */)),
             new HunspellRule(messages, this, userConfig, altLanguages),
-            new LongSentenceRule(messages, userConfig, -1, true),
+            new LongSentenceRule(messages, userConfig, -1, false),
             new LongParagraphRule(messages, this, userConfig),
             new UppercaseSentenceStartRule(messages, this,
                 Example.wrong("Esta casa é velha. <marker>foi</marker> construida em 1950."),
