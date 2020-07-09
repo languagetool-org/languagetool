ecff790978e4de33fe0bf98e0854a01c2dd3ee34
Daniel Naber <naber@danielnaber.de>
2020-07-09 21:57:14 +0200

    [de] fix splitting of Alpinforum

--- languagetool-language-modules/de/src/main/java/org/languagetool/tokenizers/de/GermanCompoundTokenizer.java
+++ languagetool-language-modules/de/src/main/java/org/languagetool/tokenizers/de/GermanCompoundTokenizer.java
@@ -139,6 +139,7 @@ public class GermanCompoundTokenizer implements Tokenizer {
     wordSplitter.addException("Siebengestirnen", asList("Sieben", "gestirnen"));
     wordSplitter.addException("Siebengestirns", asList("Sieben", "gestirns"));
     wordSplitter.addException("Siebengestirnes", asList("Sieben", "gestirnes"));
+    wordSplitter.addException("Alpinforum", asList("Alpin", "forum"));
     wordSplitter.setStrictMode(strictMode);
     wordSplitter.setMinimumWordLength(3);
   }
