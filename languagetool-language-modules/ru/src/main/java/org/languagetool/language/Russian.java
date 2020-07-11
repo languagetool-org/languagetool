e1b5bf5167a5f346c21de0f3afeedc290f33b088
yakovru <yakovr_st@mail.ru>
2020-07-11 20:07:28 +0200

    [ru] activate some common java rules

--- languagetool-language-modules/ru/src/main/java/org/languagetool/language/Russian.java
+++ languagetool-language-modules/ru/src/main/java/org/languagetool/language/Russian.java
@@ -105,18 +105,18 @@ public class Russian extends Language implements AutoCloseable {
             new WordRepeatRule(messages, this),
             new MultipleWhitespaceRule(messages, this),
 	    new SentenceWhitespaceRule(messages),
-        //  new WhiteSpaceBeforeParagraphEnd(messages, this),
+            new WhiteSpaceBeforeParagraphEnd(messages, this),  //    
             new WhiteSpaceAtBeginOfParagraph(messages),
-        //  new EmptyLineRule(messages, this),
+            new EmptyLineRule(messages, this),  //
             new LongSentenceRule(messages, userConfig),
             new LongParagraphRule(messages, this, userConfig),
             new ParagraphRepeatBeginningRule(messages, this),
             new RussianFillerWordsRule(messages, this, userConfig),
         //  new PunctuationMarkAtParagraphEnd(messages, this),
-        //  new PunctuationMarkAtParagraphEnd2(messages, this),
+            new PunctuationMarkAtParagraphEnd2(messages, this),  //
         //  new ReadabilityRule(messages, this, userConfig, false), // need use localise rule
         //  new ReadabilityRule(messages, this, userConfig, true),  // need use localise rule
-     
+      
             
                 // specific to Russian :
             new MorfologikRussianYOSpellerRule(messages, this, userConfig, altLanguages), // This rule must set off by default!!!
