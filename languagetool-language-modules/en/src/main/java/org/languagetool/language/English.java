bf57218ad797e2bc4450e37f0253373fe962ef76
Daniel Naber <naber@danielnaber.de>
2020-07-25 11:03:44 +0200

    [en] fix SimpleReplaceRule the pipe symbol actually works as documented

--- languagetool-language-modules/en/src/main/java/org/languagetool/language/English.java
+++ languagetool-language-modules/en/src/main/java/org/languagetool/language/English.java
@@ -193,7 +193,7 @@ public class English extends Language implements AutoCloseable {
         new EnglishDiacriticsRule(messages),
         new EnglishPlainEnglishRule(messages),
         new EnglishRedundancyRule(messages),
-        new SimpleReplaceRule(messages),
+        new SimpleReplaceRule(messages, this),
         new ReadabilityRule(messages, this, userConfig, false),
         new ReadabilityRule(messages, this, userConfig, true)
     ));
