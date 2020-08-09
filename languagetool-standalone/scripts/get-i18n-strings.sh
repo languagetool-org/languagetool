672717182356dfaa8da8d6b37efe946d834cca7f
Daniel Naber <naber@danielnaber.de>
2020-08-09 19:24:01 +0200

    prints the i18n strings as (mostly) plain text, e.g. to run it through LT

---
+++ languagetool-standalone/scripts/get-i18n-strings.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+# prints the i18n strings for one language as (mostly) plain text, e.g. to run it through LT
+
+if [ $# -eq 0 ]
+  then
+    echo "No arguments supplied, expected language code"
+    exit
+fi
+
+native2ascii -reverse ../../languagetool-language-modules/$1/src/main/resources/org/languagetool/MessagesBundle_$1.properties | \
+  sed 's/.*= //' | grep -v "^#" | awk ' {print;} { print ""; }'| sed 's/&//'
