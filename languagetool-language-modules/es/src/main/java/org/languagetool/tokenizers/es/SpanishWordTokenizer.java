5682db3fb6dd5b400375a5b7a08c1f85773ed5fd
jaumeortola <jaumeortola@gmail.com>
2020-08-26 10:24:56 +0200

    [es] wrong dashes, [ca] mesa/taula

--- languagetool-language-modules/es/src/main/java/org/languagetool/tokenizers/es/SpanishWordTokenizer.java
+++ languagetool-language-modules/es/src/main/java/org/languagetool/tokenizers/es/SpanishWordTokenizer.java
@@ -66,8 +66,11 @@ public class SpanishWordTokenizer extends WordTokenizer {
     auxText = matcher.replaceAll("$1\u0001\u0001CA_DECIMALCOMMA\u0001\u0001$2");
 
     StringTokenizer st = new StringTokenizer(auxText, "\u0020\u00A0\u115f\u1160\u1680"
-        + "\u2000\u2001\u2002\u2003\u2004\u2005\u2006\u2007" + "\u2008\u2009\u200A\u200B\u200c\u200d\u200e\u200f"
-        + "\u2013\u2014\u2015" + "\u2028\u2029\u202a\u202b\u202c\u202d\u202e\u202f"
+        + "\u2000\u2001\u2002\u2003\u2004\u2005\u2006\u2007" 
+        + "\u2008\u2009\u200A\u200B\u200c\u200d\u200e\u200f"
+        + "\u2013\u2014\u2015"
+        + "\u2500\u3161" // other dashes
+        + "\u2028\u2029\u202a\u202b\u202c\u202d\u202e\u202f"
         + "\u203C\u205F\u2060\u2061\u2062\u2063\u206A\u206b\u206c\u206d"
         + "\u206E\u206F\u3000\u3164\ufeff\uffa0\ufff9\ufffa\ufffb" + ",.;()[]{}<>!?:=*#∗×+÷/\\\"'«»„”“‘`’…¿¡\t\n\r",
         true);
