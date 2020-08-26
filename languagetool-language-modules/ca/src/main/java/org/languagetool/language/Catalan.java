1283b31fcd2bd187256bb8cba1a20ba5eb8cade9
jaumeortola <jaumeortola@gmail.com>
2020-08-26 09:44:19 +0200

    [ca] improve rule for incorrect dashes

--- languagetool-language-modules/ca/src/main/java/org/languagetool/language/Catalan.java
+++ languagetool-language-modules/ca/src/main/java/org/languagetool/language/Catalan.java
@@ -198,6 +198,7 @@ public class Catalan extends Language {
       case "FALTA_COMA_FRASE_CONDICIONAL": return -20;
       case "MUNDAR": return -50;
       case "MORFOLOGIK_RULE_CA_ES": return -100;
+      case "EXIGEIX_ACCENTUACIO_VALENCIANA": return -120;
       case "SUBSTANTIUS_JUNTS": return -150;
       case "FALTA_ELEMENT_ENTRE_VERBS": return -200;
       case "NOMBRES_ROMANS": return -400;
