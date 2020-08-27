8587b1f2b28f5ef73120bfe29287a74e0a2527cf
Fabian Richter <fabianrichter@fastmail.com>
2020-08-27 14:52:23 +0200

    mark METRIC_UNITS_EN_* as picky

--- languagetool-language-modules/en/src/main/java/org/languagetool/rules/en/UnitConversionRule.java
+++ languagetool-language-modules/en/src/main/java/org/languagetool/rules/en/UnitConversionRule.java
@@ -21,10 +21,12 @@
 
 package org.languagetool.rules.en;
 
+import org.languagetool.Tag;
 import org.languagetool.rules.AbstractUnitConversionRule;
 
 import java.math.RoundingMode;
 import java.text.NumberFormat;
+import java.util.Arrays;
 import java.util.Locale;
 import java.util.ResourceBundle;
 
@@ -40,6 +42,7 @@ public class UnitConversionRule extends AbstractUnitConversionRule {
 
   public UnitConversionRule(ResourceBundle messages) {
     super(messages);
+    setTags(Arrays.asList(Tag.picky));
     format = NumberFormat.getNumberInstance(Locale.ENGLISH);
     format.setMaximumFractionDigits(2);
     format.setRoundingMode(RoundingMode.HALF_UP);
