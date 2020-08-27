bb239db40bfbf6ce44e1bcce6ab86d2cd506ae08
Daniel Naber <naber@danielnaber.de>
2020-08-27 17:11:07 +0200

    improve highlighting

--- languagetool-standalone/scripts/check-messages.sh
+++ languagetool-standalone/scripts/check-messages.sh
@@ -14,8 +14,16 @@ cd /home/languagetool/regression-test/message-spelling
 for LAN in de-DE de-AT de-CH en-US en-GB fr pl ca it br nl pt-PT pt-BR ru ast be zh da eo gl el ja km ro sk sl es sv tl uk fa ta ga ar de-DE-x-simple-language
 do
 
-    mv $target/$LAN.txt $target/${LAN}_old.txt
+    mv $target/${LAN}_new.txt $target/${LAN}_old.txt
     java -cp languagetool-dev.jar org.languagetool.dev.LTMessageChecker.LTMessageChecker $LAN >$target/$LAN.txt
-    diff -u $target/${LAN}_old.txt $target/${LAN}.txt >$target/${LAN}_diff.txt
+    grep "LTM: " $target/$LAN.txt | sed 's/LTM: //' >$target/${LAN}_new.txt
+    diff -u $target/${LAN}_old.txt $target/${LAN}_new.txt >$target/${LAN}_diff.txt
+    vim -n $target/${LAN}_diff.txt -c TOhtml -c ":saveas vim.tmp" -c ":q" -c ":q"
+    mv vim.tmp $target/result/${LAN}_diff.html
+    sed -i 's/\*\*/<b>/' $target/result/${LAN}_diff.html
+    sed -i 's/\*\*/<\/b>/' $target/result/${LAN}_diff.html
+    sed -i 's/monospace; color: #....../monospace; color: #cccccc/' $target/result/${LAN}_diff.html
+    sed -i 's/Identifier { color: #....../Identifier { color: #000000/' $target/result/${LAN}_diff.html
+    sed -i 's/Statement { color: #....../Statement { color: #cccccc/' $target/result/${LAN}_diff.html
 
 done
