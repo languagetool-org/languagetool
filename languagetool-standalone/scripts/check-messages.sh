91f787939c23336efa72d34e9540c377c183756b
Daniel Naber <naber@danielnaber.de>
2020-08-28 09:47:03 +0200

    store result in subdirectory

--- languagetool-standalone/scripts/check-messages.sh
+++ languagetool-standalone/scripts/check-messages.sh
@@ -2,6 +2,7 @@
 # run message spell check, to be called via cronjob
 
 target=/home/languagetool/regression-test/results/open-source/message-spelling
+date=`date +%Y-%m-%d`
 
 cd /home/languagetool/languagetool
 mvn install -DskipTests
@@ -19,11 +20,12 @@ do
     grep "LTM: " $target/$LAN.txt | sed 's/LTM: //' >$target/${LAN}_new.txt
     diff -u $target/${LAN}_old.txt $target/${LAN}_new.txt >$target/${LAN}_diff.txt
     vim -n $target/${LAN}_diff.txt -c TOhtml -c ":saveas vim.tmp" -c ":q" -c ":q"
-    mv vim.tmp $target/result/${LAN}_diff.html
-    sed -i 's/\*\*/<b>/' $target/result/${LAN}_diff.html
-    sed -i 's/\*\*/<\/b>/' $target/result/${LAN}_diff.html
-    sed -i 's/monospace; color: #....../monospace; color: #cccccc/' $target/result/${LAN}_diff.html
-    sed -i 's/Identifier { color: #....../Identifier { color: #000000/' $target/result/${LAN}_diff.html
-    sed -i 's/Statement { color: #....../Statement { color: #cccccc/' $target/result/${LAN}_diff.html
+    mkdir $target/result/$date
+    mv vim.tmp $target/result/$date/${LAN}_diff.html
+    sed -i 's/\*\*/<b>/' $target/result/$date/${LAN}_diff.html
+    sed -i 's/\*\*/<\/b>/' $target/result/$date/${LAN}_diff.html
+    sed -i 's/monospace; color: #....../monospace; color: #cccccc/' $target/result/$date/${LAN}_diff.html
+    sed -i 's/Identifier { color: #....../Identifier { color: #000000/' $target/result/$date/${LAN}_diff.html
+    sed -i 's/Statement { color: #....../Statement { color: #cccccc/' $target/result/$date/${LAN}_diff.html
 
 done
