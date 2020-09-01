adb32a69493f142495f61113325b74100eb9a950
Daniel Naber <naber@danielnaber.de>
2020-09-01 09:59:07 +0200

    [fr] deactivate eepi_écriture_épicène_singulier

--- languagetool-language-modules/fr/src/main/java/org/languagetool/rules/fr/GrammalecteRule.java
+++ languagetool-language-modules/fr/src/main/java/org/languagetool/rules/fr/GrammalecteRule.java
@@ -95,6 +95,7 @@ public class GrammalecteRule extends Rule {
     "typo_espace_manquant_après2", // false alarm in urls (e.g. '&rk=...')
     "typo_espace_manquant_après3", // false alarm in file names (e.g. 'La teaser.zip')
     "typo_tiret_incise2",  // picky
+    "eepi_écriture_épicène_singulier",
     "g1__eleu_élisions_manquantes__b1_a1_1" // picky
   ));
 
