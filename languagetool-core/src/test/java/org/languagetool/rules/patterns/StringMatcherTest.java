85d89d7a6193b480e9bcbedb24b2738957d3a8af
Daniel Naber <naber@danielnaber.de>
2020-11-18 17:59:50 +0100

    add license headers

--- languagetool-core/src/test/java/org/languagetool/rules/patterns/StringMatcherTest.java
+++ languagetool-core/src/test/java/org/languagetool/rules/patterns/StringMatcherTest.java
@@ -1,3 +1,21 @@
+/* LanguageTool, a natural language style checker
+ * Copyright (C) 2020 Peter Gromov
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301
+ * USA
+ */
 package org.languagetool.rules.patterns;
 
 import com.google.common.collect.Sets;
@@ -8,6 +26,7 @@ import static org.junit.Assert.assertNull;
 import static org.languagetool.rules.patterns.StringMatcher.getPossibleRegexpValues;
 
 public class StringMatcherTest {
+
   @Test
   public void testGetPossibleValues() {
     assertNull(getPossibleRegexpValues("x.*"));
