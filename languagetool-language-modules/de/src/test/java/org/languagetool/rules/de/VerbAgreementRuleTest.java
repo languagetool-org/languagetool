7037be364d7299bf31aff99a26c3c990e5e6e462
f-knorr <florianknorr@web.de>
2020-12-23 08:25:46 +0100

    [de] added exceptions/rule to disambiguation.xml

--- languagetool-language-modules/de/src/test/java/org/languagetool/rules/de/VerbAgreementRuleTest.java
+++ languagetool-language-modules/de/src/test/java/org/languagetool/rules/de/VerbAgreementRuleTest.java
@@ -127,6 +127,7 @@ public class VerbAgreementRuleTest {
     assertGood("Tom werde ich nicht noch mal um Hilfe bitten.");
     assertGood("Tom konnte ich überzeugen, nicht aber Maria.");
     assertGood("Mach du mal!");
+    assertGood("Das bekomme ich nicht hin.");
     // incorrect sentences:
     assertBad("Als Borcarbid weißt es eine hohe Härte auf.");
     assertBad("Das greift auf Vorläuferinstitutionen bist auf die Zeit von 1234 zurück.");
