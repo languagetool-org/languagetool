0607fbe659f5635d6c170f05d707ae2dcddbc3ef
Daniel Naber <naber@danielnaber.de>
2020-12-30 09:59:34 +0100

    avoid warning

--- languagetool-language-modules/de/src/test/java/org/languagetool/synthesis/GermanSynthesizerTest.java
+++ languagetool-language-modules/de/src/test/java/org/languagetool/synthesis/GermanSynthesizerTest.java
@@ -26,8 +26,8 @@ import org.languagetool.Languages;
 import java.io.IOException;
 import java.util.Arrays;
 
+import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.core.Is.is;
-import static org.junit.Assert.assertThat;
 
 public class GermanSynthesizerTest {
 
