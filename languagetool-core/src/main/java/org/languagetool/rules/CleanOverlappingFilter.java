8197446bd2924c1e639b110d64a477821c12ddbf
Daniel Naber <naber@danielnaber.de>
2020-12-30 10:31:48 +0100

    fix copyright

--- languagetool-core/src/main/java/org/languagetool/rules/CleanOverlappingFilter.java
+++ languagetool-core/src/main/java/org/languagetool/rules/CleanOverlappingFilter.java
@@ -1,5 +1,5 @@
 /* LanguageTool, a natural language style checker
- * Copyright (C) 2016 Jaume Ortolà (http://www.languagetool.org)
+ * Copyright (C) 2016 Jaume Ortolà
  *
  * This library is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public
--- languagetool-language-modules/fr/src/main/java/org/languagetool/rules/fr/SuggestionsFilter.java
+++ languagetool-language-modules/fr/src/main/java/org/languagetool/rules/fr/SuggestionsFilter.java
@@ -1,5 +1,5 @@
 /* LanguageTool, a natural language style checker 
- * Copyright (C) 2020 Jaume Ortolà (http://www.danielnaber.de)
+ * Copyright (C) 2020 Jaume Ortolà
  * 
  * This library is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public
