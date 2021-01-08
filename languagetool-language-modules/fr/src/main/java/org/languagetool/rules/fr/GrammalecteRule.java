9f13cd403309904915a9b69d9dc85ccede7eebda
Daniel Naber <naber@danielnaber.de>
2021-01-08 13:14:27 +0100

    [fr] turn off rule considered too political

--- languagetool-language-modules/fr/src/main/java/org/languagetool/rules/fr/GrammalecteRule.java
+++ languagetool-language-modules/fr/src/main/java/org/languagetool/rules/fr/GrammalecteRule.java
@@ -96,6 +96,7 @@ public class GrammalecteRule extends Rule {
     "typo_espace_manquant_après3", // false alarm in file names (e.g. 'La teaser.zip')
     "typo_tiret_incise2",  // picky
     "eepi_écriture_épicène_singulier",
+    "g1__bs_vidéoprotection__b1_a1_1",
     "g1__eleu_élisions_manquantes__b1_a1_1" // picky
   ));
 
