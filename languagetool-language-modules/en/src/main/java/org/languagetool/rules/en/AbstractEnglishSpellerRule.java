ef2723a838c3e03bb69a1a0e9a345661bbae49d9
Daniel Naber <naber@danielnaber.de>
2021-01-08 13:47:07 +0100

    [en] improve suggestions for "constits" (#2814)

--- languagetool-language-modules/en/src/main/java/org/languagetool/rules/en/AbstractEnglishSpellerRule.java
+++ languagetool-language-modules/en/src/main/java/org/languagetool/rules/en/AbstractEnglishSpellerRule.java
@@ -327,6 +327,7 @@ public abstract class AbstractEnglishSpellerRule extends MorfologikSpellerRule {
 
   protected static Map<String, List<String>> getTopSuggestions() {
     Map<String, List<String>> s = new HashMap<>();
+    s.put("constits", Arrays.asList("consists"));
     s.put("morphium", Arrays.asList("morphine"));
     s.put("Morphium", Arrays.asList("Morphine"));
     s.put("morphiums", Arrays.asList("morphines"));
