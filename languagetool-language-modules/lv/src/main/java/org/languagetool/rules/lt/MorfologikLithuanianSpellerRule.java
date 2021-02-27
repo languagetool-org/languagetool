07d2c61c1184bb2577f92645ca3df2b21f6de142
Aidan Macdonald <aidan.plenert.macdonald@gmail.com>
2021-02-27 12:34:11 -0800

    Base copy fro lt

---
+++ languagetool-language-modules/lv/src/main/java/org/languagetool/rules/lt/MorfologikLithuanianSpellerRule.java
@@ -0,0 +1,49 @@
+/* LanguageTool, a natural language style checker 
+ * Copyright (C) 2012 Marcin Miłkowski (http://www.languagetool.org)
+ * 
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301
+ * USA
+ */
+
+package org.languagetool.rules.lt;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.ResourceBundle;
+
+import org.languagetool.Language;
+import org.languagetool.UserConfig;
+import org.languagetool.rules.spelling.morfologik.MorfologikSpellerRule;
+
+public final class MorfologikLithuanianSpellerRule extends MorfologikSpellerRule {
+
+  private static final String RESOURCE_FILENAME = "/lt/hunspell/lt_LT.dict";
+
+  public MorfologikLithuanianSpellerRule(ResourceBundle messages,
+                                         Language language, UserConfig userConfig, List<Language> altLanguages) throws IOException {
+    super(messages, language, userConfig, altLanguages);
+  }
+
+  @Override
+  public String getFileName() {
+    return RESOURCE_FILENAME;
+  }
+
+  @Override
+  public String getId() {
+    return "MORFOLOGIK_RULE_LT_LT";
+  }
+
+}
