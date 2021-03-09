904d852f1af92a14d1b3e0600ca425e4ee1411e9
yakovru <yakovr_st@mail.ru>
2021-03-09 21:04:31 +0300

    [en]  Update en_GB spell dictionary

--- languagetool-language-modules/en/src/main/resources/org/languagetool/resource/en/hunspell/make_en_gb_dict.sh
+++ languagetool-language-modules/en/src/main/resources/org/languagetool/resource/en/hunspell/make_en_gb_dict.sh
@@ -3,5 +3,5 @@
 # Get en_us_wordlist.xml from https://github.com/mozilla-b2g/gaia/raw/master/apps/keyboard/js/imes/latin/dictionaries/en_gb_wordlist.xml
 
 unmunch en-GB.dic en-GB.aff  >  en_GB1.txt
-cat en_GB1.txt spelling_merged.txt| java -cp languagetool.jar:languagetool-dev-5.1-SNAPSHOT.jar org.languagetool.dev.archive.WordTokenizer en | sort -u > en_GB.txt
+cat en_GB1.txt spelling_merged.txt| java -cp languagetool.jar:languagetool-dev-5.3-SNAPSHOT.jar org.languagetool.dev.archive.WordTokenizer en | sort -u > en_GB.txt
 java -cp languagetool.jar org.languagetool.tools.SpellDictionaryBuilder -i en_GB.txt -info en_GB.info -freq en_gb_wordlist.xml  -o en_GB.dict
