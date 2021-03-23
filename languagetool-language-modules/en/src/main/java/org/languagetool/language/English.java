13967cd28a99623939973e5273160f57a0b41301
Christopher Blum <christopher.blum@spellboy.com>
2021-03-23 18:18:57 +0100

    [en] improve rules

--- languagetool-language-modules/en/src/main/java/org/languagetool/language/English.java
+++ languagetool-language-modules/en/src/main/java/org/languagetool/language/English.java
@@ -317,6 +317,7 @@ public class English extends Language implements AutoCloseable {
       case "ABBREVIATION_PUNCTUATION":  return 2;
       case "FEDEX":                     return 2;   // higher prio than many verb rules (e.g. MD_BASEFORM)
       case "LIFE_COMPOUNDS":            return 1;
+      case "HER_S":                     return 1;   // higher prio than THEIR_S
       case "COVID_19":                  return 1;
       case "OTHER_WISE_COMPOUND":       return 1;
       case "ON_EXCEL":                  return 1;
