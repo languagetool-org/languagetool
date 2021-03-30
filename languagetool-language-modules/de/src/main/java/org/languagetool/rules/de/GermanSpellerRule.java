b480807e02978ea1cc9e23945ab2d169db219131
Daniel Naber <naber@danielnaber.de>
2021-03-30 10:02:55 +0200

    [de] frägst -> fragst

--- languagetool-language-modules/de/src/main/java/org/languagetool/rules/de/GermanSpellerRule.java
+++ languagetool-language-modules/de/src/main/java/org/languagetool/rules/de/GermanSpellerRule.java
@@ -63,6 +63,7 @@ public class GermanSpellerRule extends CompoundAwareHunspellRule {
   private static final Map<Pattern, Function<String,List<String>>> ADDITIONAL_SUGGESTIONS = new HashMap<>();
   static {
     put("lieder", w -> Arrays.asList("leider", "Lieder"));
+    put("frägst", "fragst");
     put("Wandererin", "Wanderin");
     put("daß", "dass");
     put("eien", "eine");
