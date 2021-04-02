6b2d0972abab98f9cc8ec5112ca2fc7f35b31aab
jaumeortola <jaumeortola@gmail.com>
2021-04-02 14:58:14 +0200

    [fr] fix FPs

--- languagetool-language-modules/fr/src/main/java/org/languagetool/language/French.java
+++ languagetool-language-modules/fr/src/main/java/org/languagetool/language/French.java
@@ -246,6 +246,7 @@ public class French extends Language implements AutoCloseable {
       //case "ACCORD_COULEUR": return 1; // needs to have higher priority than agreement postponed adj
       case "CONFUSION_PAR_PART": return -1;  // turn off completely when PART_OU_PAR is activated
       case "FR_SIMPLE_REPLACE": return -10;
+      case "IMP_PRON": return -10; // less than D_N
       case "TOO_LONG_PARAGRAPH": return -15;
       case "VERB_PRONOUN": return -50; // greater than FR_SPELLING_RULE; less than ACCORD_V_QUESTION
       case "AGREEMENT_POSTPONED_ADJ": return -50;
