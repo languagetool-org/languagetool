33152de882ecd33c21aeccd0b9664568d7ddae2a
Rebecca-auque <76939989+Rebecca-auque@users.noreply.github.com>
2021-05-03 17:07:14 +0200

    non v rule improved (#4956)
    * [fr] non v rule improved
    
    * [fr] non v rule improved

--- languagetool-language-modules/fr/src/main/java/org/languagetool/language/French.java
+++ languagetool-language-modules/fr/src/main/java/org/languagetool/language/French.java
@@ -256,6 +256,7 @@ public class French extends Language implements AutoCloseable {
       case "UPPERCASE_SENTENCE_START": return -300;
       case "FRENCH_WHITESPACE_STRICT": return -350; // picky; if on, it should overwrite FRENCH_WHITESPACE
       case "FRENCH_WHITESPACE": return -400; // lesser than UPPERCASE_SENTENCE_START and FR_SPELLING_RULE
+      case "NON_V": return -300;
     }
     if (id.startsWith("grammalecte_")) {
       return -150;
