8e9f13725ab031b75e080ad8115ffc0ee4900cbf
jaumeortola <jaumeortola@gmail.com>
2021-05-14 11:57:15 +0200

    [fr] avoid splitting prefixes

--- languagetool-language-modules/fr/src/main/java/org/languagetool/rules/fr/MorfologikFrenchSpellerRule.java
+++ languagetool-language-modules/fr/src/main/java/org/languagetool/rules/fr/MorfologikFrenchSpellerRule.java
@@ -39,7 +39,7 @@ public final class MorfologikFrenchSpellerRule extends MorfologikSpellerRule {
       "^(non|en|a|le|la|les|pour|de|du|des|un|une|mon|ma|mes|ton|ta|tes|son|sa|ses|leur|leurs|ce|cet) (..+)$",
       Pattern.CASE_INSENSITIVE | Pattern.UNICODE_CASE);
   private static final Pattern PREFIX_AMB_ESPAI = Pattern.compile(
-      "^(auto|ex|extra|macro|mega|meta|micro|multi|mono|mini|post|retro|semi|super|trans|l|d) (..+)$",
+      "^(agro|anti|archi|auto|aéro|cardio|co|cyber|demi|ex|extra|grand|géo|haut|hospitalo|hydro|hyper|hypo|infra|inter|macro|mega|meta|mi|micro|mini|mono|multi|musculo|méga|méta|nord|néo|omni|pan|para|pluri|poly|post|prim|pro|proto|pré|pseudo|psycho|péri|re|retro|ré|semi|simili|socio|sous|sud|super|supra|sur|sus|trans|tri|télé|ultra|uni|vice|éco|l|d) (..+)$",
       Pattern.CASE_INSENSITIVE | Pattern.UNICODE_CASE);
 
   private static final Pattern APOSTROF_INICI_VERBS = Pattern.compile("^([lnts])(h?[aeiouàéèíòóú].*[^è])$",
