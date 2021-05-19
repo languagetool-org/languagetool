deed79a60f5b7189760f6f8216ec776257c849b2
Fabian Richter <fabianrichter@fastmail.com>
2021-05-19 17:58:56 +0200

    added testing RemoteRuleFilters w/ stored matches

--- languagetool-core/src/main/java/org/languagetool/rules/RemoteRuleFilters.java
+++ languagetool-core/src/main/java/org/languagetool/rules/RemoteRuleFilters.java
@@ -21,9 +21,12 @@
 
 package org.languagetool.rules;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.google.common.cache.CacheBuilder;
 import com.google.common.cache.CacheLoader;
 import com.google.common.cache.LoadingCache;
+import com.google.common.collect.Streams;
 import org.jetbrains.annotations.NotNull;
 import org.languagetool.AnalyzedSentence;
 import org.languagetool.JLanguageTool;
@@ -33,9 +36,13 @@ import org.languagetool.broker.ResourceDataBroker;
 import org.languagetool.rules.patterns.AbstractPatternRule;
 
 import java.io.IOException;
+import java.nio.charset.Charset;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.*;
 import java.util.concurrent.ExecutionException;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 /**
  * rules in remote-rule-filter.xml, same syntax as pattern rules
@@ -44,13 +51,16 @@ import java.util.stream.Collectors;
  * the match is discarded
  * filtering uses the IDs from the matches, so dynamically created rules using different IDs will work
  */
-public class RemoteRuleFilters {
+public final class RemoteRuleFilters {
   
   public static final String RULE_FILE = "remote-rule-filters.xml";
 
   private static final LoadingCache<Language, Map<String, List<AbstractPatternRule>>> rules =
     CacheBuilder.newBuilder()
       .build(CacheLoader.from(RemoteRuleFilters::load));
+
+  private RemoteRuleFilters() {
+  }
 
 
   public static List<RuleMatch> filterMatches(@NotNull Language lang, @NotNull AnalyzedSentence sentence, @NotNull List<RuleMatch> matches) throws ExecutionException, IOException {
@@ -78,9 +88,6 @@ public class RemoteRuleFilters {
         // is there a filter match with the right ID at this position?
         boolean matched = filterRulesByPosition.getOrDefault(pos, Collections.emptySet())
           .stream().anyMatch(rule -> rule.getId().equals(match.getRule().getId()));
-        if (matched) {
-          System.out.println("Removing match " + match + " - matched filter");
-        }
         return !matched;
       })
       .collect(Collectors.toList());
@@ -88,19 +95,90 @@ public class RemoteRuleFilters {
   }
 
 
-  public static void main(String[] args) throws ExecutionException {
-    String langCode;
-    if (args.length > 0) {
-      langCode = args[0];
-    } else {
-      langCode = "en";
+  static class ExpectedMatches {
+    public String sentence;
+    public List<ExpectedMatch> matches;
+  }
+
+  static class ExpectedMatch {
+    public int offset;
+    public int length;
+    public String rule_id;
+  }
+
+  static class ExpectedRule extends Rule{
+    private final String id;
+    public ExpectedRule(String id)  {
+      this.id = id;
     }
+    @Override
+    public String getId() {
+      return id;
+    }
+
+    @Override
+    public String getDescription() {
+      return null;
+    }
+
+    @Override
+    public RuleMatch[] match(AnalyzedSentence sentence) throws IOException {
+      throw new IllegalStateException();
+    }
+  }
+
+  /**
+   * Print matches remaining after filtering; arguments are language code and JSON file with matches
+   * e.g. java [...] org.languagetool.rules.RemoteRuleFilters en matches.json
+   */
+  public static void main(String[] args) throws Exception {
+    String langCode = args[0];
+    String matchesFile = args[1];
     Language lang = Languages.getLanguageForShortCode(langCode);
-    rules.get(lang).forEach((id, rules) -> {
-      System.out.println("=== " + id + " ===");
-      rules.forEach(System.out::println);
-      System.out.println();
-    });
+    List<AbstractPatternRule> rules = RemoteRuleFilters.load(lang)
+      .values().stream().flatMap(Collection::stream).collect(Collectors.toList());
+    Stream<String> lines = Files.lines(Paths.get(matchesFile), Charset.forName("UTF-8"));
+    ObjectMapper mapper = new ObjectMapper();
+    JLanguageTool lt = new JLanguageTool(lang);
+
+    Map<String, List<AbstractMap.SimpleEntry<Boolean, RuleMatch>>> result = lines.parallel()
+      .map(s -> {
+        try {
+          return mapper.readValue(s, ExpectedMatches.class);
+        } catch (JsonProcessingException e) {
+          throw new RuntimeException(e);
+        }
+      })
+      .flatMap(matches -> {
+        try {
+          AnalyzedSentence s = lt.getAnalyzedSentence(matches.sentence);
+          List<RuleMatch> ruleMatches = matches.matches.stream().map(
+            m -> new RuleMatch(new ExpectedRule(m.rule_id), s, m.offset, m.offset + m.length, "")
+          ).collect(Collectors.toList());
+          List<RuleMatch> remaining = RemoteRuleFilters.filterMatches(lang, s, ruleMatches);
+          ruleMatches.removeAll(remaining);
+          return Streams.concat(
+            remaining.stream().map(m -> new AbstractMap.SimpleEntry<>(true, m)),
+            ruleMatches.stream().map(m -> new AbstractMap.SimpleEntry<>(false, m)));
+        } catch (IOException | ExecutionException e) {
+          throw new RuntimeException(e);
+        }
+      })
+      .collect(Collectors.groupingBy(key ->
+        (key.getKey() ? "Remaining" : "Removed" ) + " - " + key.getValue().getRule().getId()));
+
+    result.forEach((section, matches) -> {
+        System.out.println(section);
+        System.out.println("---");
+        for (Map.Entry<Boolean, RuleMatch> entry : matches) {
+          RuleMatch match = entry.getValue();
+          String s = match.getSentence().getText();
+          String marked = s.substring(0, match.getFromPos()) + "<marker>" +
+            s.substring(match.getFromPos(), match.getToPos()) + "</marker>" + s.substring(match.getToPos());
+          System.out.println(marked);
+        }
+        System.out.println("---");
+      });
   }
 
   static Map<String, List<AbstractPatternRule>> load(Language lang) {
