366dc70ba0b1d976b2efe117bacbfd114a4b9318
Daniel Naber <naber@danielnaber.de>
2021-05-21 14:15:04 +0200

    [en] adapt priority

--- languagetool-language-modules/en/src/main/java/org/languagetool/language/English.java
+++ languagetool-language-modules/en/src/main/java/org/languagetool/language/English.java
@@ -499,7 +499,7 @@ public class English extends Language implements AutoCloseable {
       return -11;
     }
     if (id.startsWith("AI_EN_G_")) { // prefer more specific rules (also speller)
-      return -11;
+      return -21;
     }
     if (id.matches("EN_FOR_[A-Z]+_SPEAKERS_FALSE_FRIENDS.*")) {
       return -21;
