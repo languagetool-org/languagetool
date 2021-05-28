e663f0951b89eb28cc1a26d29700916bd7a86ee5
Christopher Blum <christopher.blum@spellboy.com>
2021-05-28 14:21:21 +0200

    [en] improve rules

--- languagetool-language-modules/en/src/main/java/org/languagetool/language/English.java
+++ languagetool-language-modules/en/src/main/java/org/languagetool/language/English.java
@@ -404,6 +404,7 @@ public class English extends Language implements AutoCloseable {
       case "THERE_THEIR":               return 1;   // prefer over GO_TO_HOME
       case "IT_IS_DEPENDING_ON":        return 1;   // prefer over PROGRESSIVE_VERBS
       case "IRREGARDLESS":              return 1;   // prefer over spell checker
+      case "LOOK_FORWARD_TO":           return 1;   // prefer over LOOK_FORWARD_NOT_FOLLOWED_BY_TO
       case "MISSING_GENITIVE":          return -1;  // prefer over spell checker (like EN_SPECIFIC_CASE)
       case "EN_UNPAIRED_BRACKETS":      return -1;  // less priority than rules that suggest the correct brackets
       case "NEEDS_FIXED":               return -1;  // less priority than MISSING_TO_BEFORE_A_VERB
