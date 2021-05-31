478ebf616035037884127b49dbdf14fb52327aaa
Daniel Naber <naber@danielnaber.de>
2021-05-31 18:25:45 +0200

    improve warning text

--- languagetool-core/src/test/java/org/languagetool/rules/patterns/PatternRuleTest.java
+++ languagetool-core/src/test/java/org/languagetool/rules/patterns/PatternRuleTest.java
@@ -432,7 +432,7 @@ public class PatternRuleTest extends AbstractPatternRuleTest {
       String marker = origBadSentence.substring(expectedMatchStart+"<marker>".length(), origBadSentence.indexOf("</marker>"));
       if (marker.startsWith(", ") && origBadExample.getCorrections().stream().anyMatch(k -> !k.startsWith(" ") && !k.startsWith(",") && !k.startsWith("."))) {
         System.err.println("*** WARNING: " + lang.getName() + " rule " + rule.getFullId() + " removes ', ' but " +
-          "doesn't have a space in the suggestion: " + origBadSentence + " => " + origBadExample.getCorrections());
+          "doesn't have a space, comma or dot at the start of the suggestion: " + origBadSentence + " => " + origBadExample.getCorrections());
       }
 
       // necessary for XML Pattern rules containing <or>
