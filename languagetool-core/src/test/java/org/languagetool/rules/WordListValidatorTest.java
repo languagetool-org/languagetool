219c37cde148615c50d881fd5162f87b769fc5e1
Christopher Blum <christopher.blum@spellboy.com>
2021-06-11 17:25:55 +0200

    [en] improve rules

--- languagetool-core/src/test/java/org/languagetool/rules/WordListValidatorTest.java
+++ languagetool-core/src/test/java/org/languagetool/rules/WordListValidatorTest.java
@@ -44,6 +44,8 @@ public class WordListValidatorTest {
   private static final Set<String> VALID_WORDS = new HashSet<>(Arrays.asList(
           "Będzin",
           "Aydın",
+          "Čeferin",
+          "Čeferin/S",
           "Bhagavad-gītā",
           "Sønderjylland/S",
           "Božena/S",
