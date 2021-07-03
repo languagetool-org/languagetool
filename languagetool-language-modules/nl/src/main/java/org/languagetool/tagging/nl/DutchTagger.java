8a9d6f28613ce141fa4267aedec19c7538ab25fd
baarsrj <baarsrj@xs4all.nl>
2021-07-03 13:27:03 +0200

    [nl] rearranged specific code in tagger to correct bug.

--- languagetool-language-modules/nl/src/main/java/org/languagetool/tagging/nl/DutchTagger.java
+++ languagetool-language-modules/nl/src/main/java/org/languagetool/tagging/nl/DutchTagger.java
@@ -43,6 +43,7 @@ public class DutchTagger extends BaseTagger {
 
     for (String word : sentenceTokens) {
       boolean ignoreSpelling = false;
+
       // make treatment of weird apostrophes same as in tokenizer (R. Baars, 2020-11-06)
       String originalWord=word;
       word=word.replace("`","'").replace("’","'").replace("‘","'").replace("´","'");
@@ -52,9 +53,10 @@ public class DutchTagger extends BaseTagger {
       final boolean isLowercase = word.equals(lowerWord);
       final boolean isMixedCase = StringTools.isMixedCase(word);
       final boolean isAllUpper = StringTools.isAllUppercase(word);
+
+      // assign tokens for flattened word to original word
       List<AnalyzedToken> taggerTokens = asAnalyzedTokenListForTaggedWords(originalWord, getWordTagger().tag(word));
       //List<AnalyzedToken> taggerTokens = asAnalyzedTokenListForTaggedWords(word, getWordTagger().tag(word));
-
       // normal case:
       addTokens(taggerTokens, l);
       // tag non-lowercase (alluppercase or startuppercase), but not mixedcase
@@ -73,47 +75,55 @@ public class DutchTagger extends BaseTagger {
       }
 
       if (l.isEmpty()) {
+        // there is still not postag found
         String word2 = lowerWord;
         // remove single accented characters
-        word2 = word2.replace('á', 'a').replace('é', 'e').replace('í', 'i').replace('ó', 'o').replace('ú', 'u');
-        
-        // TODO: remove optional hyphens one at a time; for now just all will be removed
+        word2 = word2.replaceAll("([^aeiouáéíóú])(á)([^aeiouáéíóú])", "$1a$3");
+        word2 = word2.replaceAll("([^aeiouáéíóú])(é)([^aeiouáéíóú])", "$1e$3");
+        word2 = word2.replaceAll("([^aeiouáéíóú])(í)([^aeiouáéíóú])", "$1i$3");
+        word2 = word2.replaceAll("([^aeiouáéíóú])(ó)([^aeiouáéíóú])", "$1o$3");
+        word2 = word2.replaceAll("([^aeiouáéíóú])(ú)([^aeiouáéíóú])", "$1u$3");
+
+        // remove allowed accented characters
+        word2 = word2.replace("áá", "aa");
+        word2 = word2.replace("áé", "ae");
+        word2 = word2.replace("áí", "ai");
+        word2 = word2.replace("áú", "au");
+        word2 = word2.replace("éé", "ee");
+        word2 = word2.replace("éí", "ei");
+        word2 = word2.replace("éú", "eu");
+        word2 = word2.replace("íé", "ie");
+        word2 = word2.replace("óé", "oe");
+        word2 = word2.replace("óí", "oi");
+        word2 = word2.replace("óó", "oo");
+        word2 = word2.replace("óú", "ou");
+        word2 = word2.replace("úí", "ui");
+        word2 = word2.replace("úú", "uu");
+        word2 = word2.replace("íj", "ij");
+
+        word2 = word2.replaceAll("(^|[^aeiou])á([^aeiou]|$)", "$1a$2");
+        word2 = word2.replaceAll("(^|[^aeiou])é([^aeiou]|$)", "$1e$2");
+        word2 = word2.replaceAll("(^|[^aeiou])í([^aeiou]|$)", "$1i$2");
+        word2 = word2.replaceAll("(^|[^aeiou])ó([^aeiou]|$)", "$1o$2");
+        word2 = word2.replaceAll("(^|[^aeiou])ú([^aeiou]|$)", "$1u$2");
+
         // best would be to check the parts as well (uncompound)
-        word2 = word2.replaceAll("([a-z])-([a-z])", "$1$2");
-        
+        if (word2.contains("-")) {
+          String part1=word2.replaceAll("(^.*)-(.*$)", "$1");
+          List<AnalyzedToken> p1 = asAnalyzedTokenListForTaggedWords(originalWord, getWordTagger().tag(part1));
+          String part2=word2.replaceAll("(^.*)-(.*$)", "$2");
+          List<AnalyzedToken> p2 = asAnalyzedTokenListForTaggedWords(originalWord, getWordTagger().tag(part2));
+          if (!(p1.isEmpty()||p2.isEmpty())) {
+            word2 = word2.replaceAll("([a-z])-([a-z])", "$1$2");
+          }
+        }
+
         if (!word2.equals(word)) {
           List<AnalyzedToken> l2 = asAnalyzedTokenListForTaggedWords(originalWord, getWordTagger().tag(word2));
-          if (l2 != null) {
+          if (!l2.isEmpty()) {
+            // woord bestaat
             addTokens(l2, l);
-
-            String word3 = word;
-            word3 = word.replaceAll("([a-z])-([a-z])", "$1$2");
-            // remove allowed accented characterd
-
-            word3 = word3.replace("áá", "aa");
-            word3 = word3.replace("áé", "ae");
-            word3 = word3.replace("áí", "ai");
-            word3 = word3.replace("áú", "au");
-            word3 = word3.replace("éé", "ee");
-            word3 = word3.replace("éí", "ei");
-            word3 = word3.replace("éú", "eu");
-            word3 = word3.replace("íé", "ie");
-            word3 = word3.replace("óé", "oe");
-            word3 = word3.replace("óí", "oi");
-            word3 = word3.replace("óó", "oo");
-            word3 = word3.replace("óú", "ou");
-            word3 = word3.replace("úí", "ui");
-            word3 = word3.replace("úú", "uu");
-            word3 = word3.replace("íj", "ij");
-            
-            word3 = word3.replaceAll("(^|[^aeiou])á([^aeiou]|$)", "$1a$2");
-            word3 = word3.replaceAll("(^|[^aeiou])é([^aeiou]|$)", "$1e$2");
-            word3 = word3.replaceAll("(^|[^aeiou])í([^aeiou]|$)", "$1i$2");
-            word3 = word3.replaceAll("(^|[^aeiou])ó([^aeiou]|$)", "$1o$2");
-            word3 = word3.replaceAll("(^|[^aeiou])ú([^aeiou]|$)", "$1u$2");
-            if (word3.equals(word2)) {
-              ignoreSpelling = true;
-            }
+            ignoreSpelling=true;
           }
         }
       }
