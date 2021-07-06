7c86e1fd4a2557bf2d093a4d0b6bfa3b647d8d4e
baarsrj <baarsrj@xs4all.nl>
2021-07-06 08:21:31 +0200

    [nl] first verison of Dutch uncompounder added. Needs a good look at by professional Java programmer.

--- languagetool-language-modules/nl/src/main/java/org/languagetool/tagging/nl/DutchTagger.java
+++ languagetool-language-modules/nl/src/main/java/org/languagetool/tagging/nl/DutchTagger.java
@@ -35,7 +35,7 @@ public class DutchTagger extends BaseTagger {
   public DutchTagger() {
     super("/nl/dutch.dict", new Locale("nl"));
   }
-  // custom code to deal with words carrying optional accents
+    // custom code to deal with words carrying optional accents
   @Override
   public List<AnalyzedTokenReadings> tag(final List<String> sentenceTokens) {
     final List<AnalyzedTokenReadings> tokenReadings = new ArrayList<>();
@@ -69,14 +69,14 @@ public class DutchTagger extends BaseTagger {
       // tag all-uppercase proper nouns
       if (l.isEmpty() && isAllUpper) {
         final String firstUpper = StringTools.uppercaseFirstChar(lowerWord);
-        List<AnalyzedToken> firstupperTaggerTokens = asAnalyzedTokenListForTaggedWords(originalWord,
-            getWordTagger().tag(firstUpper));
+        List<AnalyzedToken> firstupperTaggerTokens = asAnalyzedTokenListForTaggedWords(originalWord, getWordTagger().tag(firstUpper));
         addTokens(firstupperTaggerTokens, l);
       }
 
       if (l.isEmpty()) {
-        // there is still not postag found
-        String word2 = lowerWord;
+        // there is still no postag found
+        //String word2 = lowerWord;
+        String word2 = word; // why the lowerword?
         // remove single accented characters
         word2 = word2.replaceAll("([^aeiouáéíóú])(á)([^aeiouáéíóú])", "$1a$3");
         word2 = word2.replaceAll("([^aeiouáéíóú])(é)([^aeiouáéíóú])", "$1e$3");
@@ -128,6 +128,177 @@ public class DutchTagger extends BaseTagger {
             ignoreSpelling=true;
           }
         }
+        //*************** START OF ADDED UNCOMPOUNDER CODE ****************** //
+        // (still too) simple uncompounder
+        // it needs check for postags and substring
+        // nevertheless, 5 is rather safe
+        // TODO :
+        // - optimize code
+        // - move code to separate file/function/class
+        // - add more safe word types
+        // wordExceptions TODO: implement this as textfile
+        if (l.isEmpty()) {
+          String wordExceptions="weerszijden|bijenkomst|.*[a-z]{3}[aeiouy]s|stats.*|bronzen.*|(paars|rood)(groene?|rood|rode)";
+          // TODO make riskyParts a list or even a file
+          String riskyParts="vergoding|bijbel|tegens";
+          if (!word.matches(wordExceptions)) {
+            int size =word.length();
+            String trueCollisions=".*(a~[aeéiu]|[eé]~[eéiu]|i~[e]|o~[eiou]|i~j|[A-Z]~[a-z]|[a-z]~[A-Z]|[0-9]~[a-zA-Z]|[a-z][A-Z]~[0-9]).*";
+            // other false patterns
+            trueCollisions+="|(tegen|voor|achter|midden|open)~(s|s-)~.*";
+            for (int i = 5; i <= size-5; i++) {
+              // end is most significant, so check that
+              String end=word.substring(i);
+              // betther make riskyparts a list
+              if (!end.matches(riskyParts)) {
+                List<AnalyzedToken> e = asAnalyzedTokenListForTaggedWords(originalWord, getWordTagger().tag(end));
+                String front=word.substring(0,i);
+                if (!front.matches(riskyParts)) {
+                  if (!e.isEmpty()) {
+                    // is a word
+                    //System.out.println("front:"+front);
+                    // check front
+                    List<AnalyzedToken> f = asAnalyzedTokenListForTaggedWords(originalWord, getWordTagger().tag(front));
+                    if (!f.isEmpty()) {
+                      // front is a valid word
+                      String option=front+"~"+end;
+                      if (!option.matches(trueCollisions)) {
+                        // there is no character collision
+                        // get the tags now, and check the combinations
+                        for(int j=0;j<e.size();j++){
+                          String eTag=e.get(j).getPOSTag();
+                          String eWord=e.get(j).getLemma();
+                          for(int k=0;k<f.size();k++){
+                            String fTag=f.get(k).getPOSTag();
+                            String fWord=f.get(k).getLemma();
+
+                            String tagCombi=(fTag+"~"+eTag);
+                            //System.out.println(option+":"+tagCombi);
+
+                            if (tagCombi.matches("^(ZNW:EKV|ZNW:EKV:DE_|ZNW:EKV:HET|ZNW:MRV:VRK:HET|WKW:TGW:1EP)~ZNW:.*$")) {
+                              // the end tag determines the total tag
+                              l.add(new AnalyzedToken(word, eTag, front+eWord));
+                              ignoreSpelling=true;
+                              l.add (new AnalyzedToken(word, "LIKELY_SPELLING", word));
+                            } else if (tagCombi.matches("^ZNW:MRV:DE_~ZNW:.*$")&&(front.matches(".*en$"))) {
+                              // the end tag determines the total tag
+                              // compounding with -n when there is also a plural with -s is forbidden\
+                              String otherPlural=front.replaceAll("n$","s");
+                              System.out.println(otherPlural);
+                              List<AnalyzedToken> o = asAnalyzedTokenListForTaggedWords(otherPlural, getWordTagger().tag(otherPlural));
+                              if (o.isEmpty()) {
+                                // in fact, all tags should be checked to be ZNW:MRV:DE_, if so, then reject
+                                AnalyzedToken temp = new AnalyzedToken(word, eTag, front+eWord);
+                                l.add( new AnalyzedToken(word, eTag, front+eWord) );
+                                l.add (new AnalyzedToken(word, "LIKELY_SPELLING", word));
+                                ignoreSpelling=true;
+                              }
+                            }
+                          }
+                        }
+                      }
+                    }
+                  }
+                }
+                if (word.substring(i-2,i).equals("s-")) {
+                  //System.out.println("s-"+front);
+                  // front could have an compounding s and dash
+                  front=word.substring(0,i-2);
+                  if (!front.matches(riskyParts)) {
+                    List<AnalyzedToken> f = asAnalyzedTokenListForTaggedWords(originalWord, getWordTagger().tag(front));
+                    if (!f.isEmpty()) {
+                      String option=front+"~s-~"+end;
+                      for(int j=0;j<e.size();j++){
+                        String eTag=e.get(j).getPOSTag();
+                        String eWord=e.get(j).getLemma();
+                        for(int k=0;k<f.size();k++){
+                          String fTag=f.get(k).getPOSTag();
+                          String fWord=f.get(k).getLemma();
+
+                          String tagCombi=(fTag+"~s-~"+eTag);
+                          //System.out.println(option+":"+tagCombi);
+                          if (tagCombi.matches("^(ZNW:EKV|ZNW:EKV:DE_|ZNW:EKV:HET|ZNW:MRV:VRK:HET)~s-~ZNW:.*$")&&(!option.matches(".*e~s-~.*"))) {
+                            // the end tag determines the total tag
+                            l.add( new AnalyzedToken(word, eTag, front+"s-"+eWord) );
+                            l.add (new AnalyzedToken(word, "LIKELY_SPELLING", word));
+                            ignoreSpelling=true;
+                          }
+                        }
+                      }
+                    }
+                  }
+                }
+                if (word.substring(i-1,i).equals("s")) {
+                  //System.out.println("s$");
+                  // front could have an compounding s and dash
+                  front=word.substring(0,i-1);
+                  if (!front.matches(riskyParts)) {
+                    List<AnalyzedToken> f = asAnalyzedTokenListForTaggedWords(originalWord, getWordTagger().tag(front));
+                    if (!f.isEmpty()) {
+                      String option=front+"~s~"+end;
+                      for(int j=0;j<e.size();j++){
+                        String eTag=e.get(j).getPOSTag();
+                        String eWord=e.get(j).getLemma();
+                        for(int k=0;k<f.size();k++){
+                          String fTag=f.get(k).getPOSTag();
+                          String fWord=f.get(k).getLemma();
+                          String tagCombi=(fTag+"~s~"+eTag);
+                          //System.out.println(option+":"+tagCombi);
+                          if (tagCombi.matches("^(ZNW:EKV|ZNW:EKV:DE_|ZNW:EKV:HET|ZNW:MRV:VRK:HET)~s~ZNW:.*$")&&(!option.matches(".*e~s~.*"))) {
+                            l.add( new AnalyzedToken(word, eTag, front+"s"+eWord) );
+                            l.add (new AnalyzedToken(word, "LIKELY_SPELLING", word));
+                            ignoreSpelling=true;
+                          }
+                        }
+                      }
+                    }
+                  }
+                }
+                if (word.substring(i-1,i).equals("-")) {
+                  // front could have an compounding s and dash
+                  front=word.substring(0,i-1);
+                  if (!front.matches(riskyParts)) {
+                    List<AnalyzedToken> f = asAnalyzedTokenListForTaggedWords(originalWord, getWordTagger().tag(front));
+                    if (!f.isEmpty()) {
+                      String option=front+"~-~"+end;
+                      for(int j=0;j<e.size();j++){
+                        String eTag=e.get(j).getPOSTag();
+                        String eWord=e.get(j).getLemma();
+                        for(int k=0;k<f.size();k++){
+                          String fTag=f.get(k).getPOSTag();
+                          String fWord=f.get(k).getLemma();
+                          String tagCombi=(fTag+"~-~"+eTag);
+                          //System.out.println(option+":"+tagCombi);
+                          if (tagCombi.matches("^(ZNW:EKV|ZNW:EKV:DE_|ZNW:EKV:HET|ZNW:MRV:VRK:HET|WKW:TGW:1EP)~-~ZNW:.*$")) {
+                            // the end tag determines the total tag
+                            // this could be added
+                            l.add( new AnalyzedToken(word, eTag, front+"-"+eWord) );
+                            l.add (new AnalyzedToken(word, "LIKELY_SPELLING", word));
+                            ignoreSpelling=true;
+                          } else if (tagCombi.matches("^ZNW:MRV:DE_~ZNW:.*$")&&(front.matches(".*en$"))) {
+                            String otherPlural=front.replaceAll("n$","s");
+                            //System.out.println(otherPlural);
+                            //compounding with -n when there is also a plural with -s is forbidden
+                            List<AnalyzedToken> o = asAnalyzedTokenListForTaggedWords(otherPlural, getWordTagger().tag(otherPlural));
+                            if (o.isEmpty()) {
+                              // in fact, all tags should be checked to be ZNW:MRV:DE_, if so, then reject
+                              System.out.println(o.toString());
+                              AnalyzedToken temp = new AnalyzedToken(word, eTag, front+eWord);
+                              l.add( new AnalyzedToken(word, eTag, front+"-"+eWord) );
+                              l.add (new AnalyzedToken(word, "LIKELY_SPELLING", word));
+                              ignoreSpelling=true;
+                            }
+                          }
+                        }
+                      }
+                    }
+                  }
+                }
+              }
+            }
+          }
+        }
+        // ********* END OF UNCOMPOUNDER CODE *************** //
       }
 
       // set word to original
@@ -139,7 +310,23 @@ public class DutchTagger extends BaseTagger {
 
       AnalyzedTokenReadings atr = new AnalyzedTokenReadings(l, pos);
       if (ignoreSpelling) {
-        atr.ignoreSpelling();
+        // it might be a word that needs captials! Check this in dictionary
+        if (isLowercase) {
+          List<AnalyzedToken> fu = asAnalyzedTokenListForTaggedWords(StringTools.uppercaseFirstChar(originalWord), getWordTagger().tag(StringTools.uppercaseFirstChar(originalWord)));
+          if (fu.isEmpty()) {
+            // does not exist in dictionary having firstupper
+            atr.ignoreSpelling();
+          } else {
+            // there is an uppercased form in the dictionary; so this one is probably wrong
+            //System.out.println("=>"+l.toString());
+            // TODO clearing the l list does not work herethe 'LIKELY_SPELLING' tag should be removed! But somehow, this does not work when done here.
+            l.clear();
+            l.add(new AnalyzedToken(originalWord, null, null));
+            //System.out.println("=>"+l.toString());
+          }
+        } else {
+            atr.ignoreSpelling();
+        }
       }
 
       tokenReadings.add(atr);
