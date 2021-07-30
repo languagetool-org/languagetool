d2638d1275d64bd3cdacfb1354cd24763d3b5135
Daniel Naber <naber@danielnaber.de>
2021-07-30 14:44:38 +0200

    build languagetool-tools

--- build-zip.sh
+++ build-zip.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 # fast way to build the ZIP for testing an deployment
 
-mvn -pl '!languagetool-http-client,!languagetool-wikipedia,!languagetool-office-extension,!languagetool-dev,!languagetool-rpm-package,!languagetool-tools' clean package -DskipTests
+mvn -pl '!languagetool-http-client,!languagetool-wikipedia,!languagetool-office-extension,!languagetool-dev,!languagetool-rpm-package' clean package -DskipTests
