7933859723c0e8d9e0dea4deaa93d4735232a04a
Daniel Naber <naber@danielnaber.de>
2021-08-02 16:51:14 +0200

    add exception to pattern test

--- languagetool-core/src/test/java/org/languagetool/rules/patterns/PatternTestTools.java
+++ languagetool-core/src/test/java/org/languagetool/rules/patterns/PatternTestTools.java
@@ -55,7 +55,8 @@ public final class PatternTestTools {
    * These strings are not be recognized as a regular expression
    */
   private static final Set<String> NO_REGEXP = new HashSet<>(Arrays.asList(
-    "PRP:LOK+TMP+MOD:DAT+AKK", "AUX:ind+pres+3+p", "PRP:TMP+MOD+CAU:DAT", "PRP:LOK+TMP:DAT", "PRP:LOK+TMP+CAU:DAT+AKK", "PRP:MOD:GEN+DAT"
+    "PRP:LOK+TMP+MOD:DAT+AKK", "AUX:ind+pres+3+p", "PRP:TMP+MOD+CAU:DAT", "PRP:LOK+TMP:DAT", "PRP:LOK+TMP+CAU:DAT+AKK",
+    "PRP:MOD:GEN+DAT", "PRP:LOK+TMP+CAU:DAT"
     ));
 
 
