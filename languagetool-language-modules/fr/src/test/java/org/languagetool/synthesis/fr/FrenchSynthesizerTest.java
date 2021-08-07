07056aae14b426a6a46e93b0dc1f587fd9fc2627
jaumeortola <jaumeortola@gmail.com>
2021-08-07 19:27:47 +0200

    [fr] synthesis tests

--- languagetool-language-modules/fr/src/test/java/org/languagetool/synthesis/fr/FrenchSynthesizerTest.java
+++ languagetool-language-modules/fr/src/test/java/org/languagetool/synthesis/fr/FrenchSynthesizerTest.java
@@ -37,6 +37,9 @@ public class FrenchSynthesizerTest {
     
     assertEquals("[nagent]", Arrays.toString(synth.synthesize(dummyToken("nager"), "V ind pres 3 p")));
     assertEquals("[les]", Arrays.toString(synth.synthesize(dummyToken("le"), "D (m|e) (p|sp)", true)));
+    assertEquals("[marins]", Arrays.toString(synth.synthesize(dummyToken("marin"), "J [me] (p|sp)", true)));
+    assertEquals("[marins]", Arrays.toString(synth.synthesize(new AnalyzedToken("marine", "J f s", "marin"), "J [me] (p|sp)", true)));
+    assertEquals("[marinés]", Arrays.toString(synth.synthesize(new AnalyzedToken("marine", "V ind pres 1 s", "mariner"), "V ppa [me] (p|sp)", true)));
   }
 
   private AnalyzedToken dummyToken(String tokenStr) {
