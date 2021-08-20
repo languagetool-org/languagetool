81efab383c73713f451aa6359d26bf187aa2dd91
jaumeortola <jaumeortola@gmail.com>
2021-08-20 10:20:08 +0200

    [fr] higher priority CAT_TOURS_CRITIQUES

--- languagetool-language-modules/fr/src/main/java/org/languagetool/language/French.java
+++ languagetool-language-modules/fr/src/main/java/org/languagetool/language/French.java
@@ -249,6 +249,7 @@ public class French extends Language implements AutoCloseable {
       case "A_VERBE_INFINITIF": return 20; // greater than PRONSUJ_NONVERBE
       case "CAT_TYPOGRAPHIE": return 20; // greater than PRONSUJ_NONVERBE or agreement rules
       case "CAT_HOMONYMES_PARONYMES": return 20;
+      case "CAT_TOURS_CRITIQUES": return 20;
       case "CONFUSION_PARLEZ_PARLER": return 10; // greater than N_V
       case "AGREEMENT_TOUT_LE": return 10; // compare to TOUT_LES
       case "ESPACE_UNITES": return 10; // needs to have higher priority than spell checker
