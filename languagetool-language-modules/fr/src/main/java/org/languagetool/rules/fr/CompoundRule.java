0f1f2ea13b9089c5e83f2de2f818f2a1cfad1a8f
jaumeortola <jaumeortola@gmail.com>
2021-08-20 11:06:49 +0200

    [fr] new rules LEDIT, LAQUELLE

--- languagetool-language-modules/fr/src/main/java/org/languagetool/rules/fr/CompoundRule.java
+++ languagetool-language-modules/fr/src/main/java/org/languagetool/rules/fr/CompoundRule.java
@@ -49,6 +49,11 @@ public class CompoundRule extends AbstractCompoundRule {
   public String getDescription() {
     return "Mots avec trait d’union";
   }
+  
+  @Override
+  public boolean isHyphenIgnored() {
+    return false;
+  }
 
   @Override
   protected CompoundRuleData getCompoundRuleData() {
