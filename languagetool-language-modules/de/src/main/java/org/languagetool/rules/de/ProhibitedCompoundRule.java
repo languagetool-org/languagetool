7096bac66bd4cd08c2643866bd1ddc5c84d1e1d2
Daniel Naber <naber@danielnaber.de>
2021-08-21 15:53:42 +0200

    [de] adding words; false alarm fix

--- languagetool-language-modules/de/src/main/java/org/languagetool/rules/de/ProhibitedCompoundRule.java
+++ languagetool-language-modules/de/src/main/java/org/languagetool/rules/de/ProhibitedCompoundRule.java
@@ -120,6 +120,7 @@ public class ProhibitedCompoundRule extends Rule {
     "Gra(ph|f)it"   // Grafit/Graphit
   );
   private static final Set<String> blacklist = new HashSet<>(Arrays.asList(
+          "Lichtkunstwerk",  // vs Dichtkunstwerk
           "Infektionslage",  // vs Infektionstage
           "Teilspiegel",  // vs Heilspiegel
           "Preiseseite",  // vs Presseseite
