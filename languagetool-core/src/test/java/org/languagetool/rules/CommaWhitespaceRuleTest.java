4d67eadb9810c327b6a4fcfe44afbb1c9ccf6b5c
Udo Mai <udo.mai@languagetoolplus.com>
2021-08-24 17:07:27 +0200

    [core] add positions to tests

--- languagetool-core/src/test/java/org/languagetool/rules/CommaWhitespaceRuleTest.java
+++ languagetool-core/src/test/java/org/languagetool/rules/CommaWhitespaceRuleTest.java
@@ -99,9 +99,13 @@ public class CommaWhitespaceRuleTest {
     matches = rule.match(lt.getAnalyzedSentence("You \" fixed\" it."));
     assertEquals(1, matches.length);
     assertEquals("\"", matches[0].getSuggestedReplacements().get(0));
+    assertEquals(4, matches[0].getFromPos());
+    assertEquals(6, matches[0].getToPos());
     matches = rule.match(lt.getAnalyzedSentence("You \"fixed \" it."));
     assertEquals(1, matches.length);
     assertEquals("\"", matches[0].getSuggestedReplacements().get(0));
+    assertEquals(11, matches[0].getFromPos());
+    assertEquals(13, matches[0].getToPos());
 
     assertMatches("Ellipsis . . . as suggested by The Chicago Manual of Style", 3);
     assertMatches("Ellipsis . . . . as suggested by The Chicago Manual of Style", 4);
