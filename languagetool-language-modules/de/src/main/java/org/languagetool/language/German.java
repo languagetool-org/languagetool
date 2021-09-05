64d8a2c4aed6e3327eda9ac09fc07c6bbaca69c5
Christopher Blum <christopher.blum@spellboy.com>
2021-09-05 15:25:41 +0200

    [de] improve rules

--- languagetool-language-modules/de/src/main/java/org/languagetool/language/German.java
+++ languagetool-language-modules/de/src/main/java/org/languagetool/language/German.java
@@ -360,7 +360,6 @@ public class German extends Language implements AutoCloseable {
       case "KOMMA_VOR_RELATIVSATZ": return -10;
       case "KOMMA_ZWISCHEN_HAUPT_UND_NEBENSATZ": return -10;
       case "COMMA_BEHIND_RELATIVE_CLAUSE": return -10;
-      case "SUBJUNKTION_KOMMA_2": return -11; // lower prio than KOMMA_ZWISCHEN_HAUPT_UND_NEBENSATZ
       case "TOO_LONG_PARAGRAPH": return -15;
       // Category ids - make sure style issues don't hide overlapping "real" errors:
       case "COLLOQUIALISMS": return -15;
@@ -370,8 +369,9 @@ public class German extends Language implements AutoCloseable {
       case "TYPOGRAPHY": return -15;
       case "ALL_UPPERCASE": return -15;
       case "AI_HYDRA_LEO_MISSING_COMMA": return -51; // prefer comma style rules.
-      case "KOMMA_ZWISCHEN_HAUPT_UND_NEBENSATZ_2": return -52;
-      case "VERB_FEM_SUBST": return -53; // prefer comma rules (including AI)
+      case "VERB_FEM_SUBST": return -52; // prefer comma rules (including AI)
+      case "KOMMA_ZWISCHEN_HAUPT_UND_NEBENSATZ_2": return -53;
+      case "SUBJUNKTION_KOMMA_2": return -54; // lower prio than KOMMA_ZWISCHEN_HAUPT_UND_NEBENSATZ and KOMMA_ZWISCHEN_HAUPT_UND_NEBENSATZ_2
     }
     if (id.startsWith("CONFUSION_RULE_")) {
       return -1;
