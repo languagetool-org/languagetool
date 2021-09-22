2c6c88cb9578d19ad6ee3cd81160a177f5e5b602
Christopher Blum <christopher.blum@spellboy.com>
2021-09-22 15:04:24 +0200

    [en] improve rules

--- languagetool-core/src/test/java/org/languagetool/rules/patterns/PatternRuleTest.java
+++ languagetool-core/src/test/java/org/languagetool/rules/patterns/PatternRuleTest.java
@@ -419,7 +419,7 @@ public class PatternRuleTest extends AbstractPatternRuleTest {
       }
       
       String marker = origBadSentence.substring(expectedMatchStart+"<marker>".length(), origBadSentence.indexOf("</marker>"));
-      if (marker.startsWith(", ") && origBadExample.getCorrections().stream().anyMatch(k -> !k.startsWith(" ") && !k.startsWith(",") && !k.startsWith(".") && !k.startsWith(";") && !k.startsWith("…"))) {
+      if (marker.startsWith(", ") && origBadExample.getCorrections().stream().anyMatch(k -> !k.startsWith(" ") && !k.startsWith(",") && !k.startsWith("?") && !k.startsWith(".") && !k.startsWith(";") && !k.startsWith("…"))) {
         System.err.println("*** WARNING: " + lang.getName() + " rule " + rule.getFullId() + " removes ', ' but " +
           "doesn't have a space, comma, semicolon, or dot at the start of the suggestion: " + origBadSentence + " => " + origBadExample.getCorrections());
       }
