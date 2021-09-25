8227ab5b0ad728cc23efed9f5c6e1a122648bcd6
Jan Schreiber <jan.schreiber@languagetool.org>
2021-09-25 12:13:17 +0200

    [de] add "Poreč" to exceptions

--- languagetool-core/src/test/java/org/languagetool/rules/WordListValidatorTest.java
+++ languagetool-core/src/test/java/org/languagetool/rules/WordListValidatorTest.java
@@ -45,6 +45,7 @@ public class WordListValidatorTest {
           "Varaždin/S",
           "Będzin",
           "Aydın",
+          "Poreč",
           "Čeferin",
           "Čeferin/S",
           "Perišić",
