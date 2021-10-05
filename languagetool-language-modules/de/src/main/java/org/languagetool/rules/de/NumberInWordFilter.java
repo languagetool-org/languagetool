95292e2096553aa9f274eeb8fd53519df4e9b71c
SteVio89 <stefan@stevio.de>
2021-10-05 16:32:19 +0200

    - Add license - Improve regex in grammar.xml - Use GermanSpellerRule direct

--- languagetool-language-modules/de/src/main/java/org/languagetool/rules/de/NumberInWordFilter.java
+++ languagetool-language-modules/de/src/main/java/org/languagetool/rules/de/NumberInWordFilter.java
@@ -1,3 +1,21 @@
+/* LanguageTool, a natural language style checker
+ * Copyright (C) 2021 Stefan Viol
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301
+ * USA
+ */
 package org.languagetool.rules.de;
 
 import org.jetbrains.annotations.Nullable;
@@ -5,54 +23,40 @@ import org.languagetool.AnalyzedTokenReadings;
 import org.languagetool.JLanguageTool;
 import org.languagetool.language.German;
 import org.languagetool.language.GermanyGerman;
-import org.languagetool.rules.Rule;
 import org.languagetool.rules.RuleMatch;
 import org.languagetool.rules.patterns.RuleFilter;
 
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 import java.util.regex.Pattern;
 
 public class NumberInWordFilter extends RuleFilter {
 
   private final German language = new GermanyGerman();
-  private JLanguageTool lt;
+  private final static Pattern typoPattern = Pattern.compile("[0-9]");
 
   @Nullable
   @Override
   public RuleMatch acceptRuleMatch(RuleMatch match, Map<String, String> arguments, int patternTokenPos, AnalyzedTokenReadings[] patternTokens) throws IOException {
-    initLt();
+    ResourceBundle messages = JLanguageTool.getDataBroker().getResourceBundle(JLanguageTool.MESSAGE_BUNDLE, new Locale(language.getShortCode()));
     String word = arguments.get("word");
-    Pattern typoPattern = Pattern.compile("[0-9]");
     String wordWithoutNumberCharacter = typoPattern.matcher(word).replaceAll("");
-
     List<String> replacements = new ArrayList<>();
-    List<RuleMatch> matches = lt.check(wordWithoutNumberCharacter);
-    if (matches.isEmpty()) {
-      replacements.add(wordWithoutNumberCharacter);
+
+    GermanSpellerRule germanSpellerRule = new GermanSpellerRule(messages, language);
+    boolean misspelled = germanSpellerRule.isMisspelled(wordWithoutNumberCharacter);
+
+    if (misspelled) {
+      List<String> suggestions = germanSpellerRule.getSuggestions(wordWithoutNumberCharacter);
+      replacements.addAll(suggestions);
     } else {
-      replacements.addAll(matches.get(0).getSuggestedReplacements());
+      replacements.add(wordWithoutNumberCharacter);
     }
     if (!replacements.isEmpty()) {
-      RuleMatch ruleMatch = new RuleMatch(match.getRule(), match.getSentence(), match.getFromPos(), match.getToPos(),
-        match.getMessage(), match.getShortMessage());
-      ruleMatch.setType(match.getType());
+      RuleMatch ruleMatch = new RuleMatch(match);
       ruleMatch.setSuggestedReplacements(replacements);
       return ruleMatch;
     }
     return null;
-  }
-
-  private void initLt() {
-    if (lt == null) {
-      lt = new JLanguageTool(language);
-      for (Rule rule : lt.getAllActiveRules()) {
-        if (!rule.getId().equals("GERMAN_SPELLER_RULE")) {
-          lt.disableRule(rule.getId());
-        }
-      }
-    }
   }
 }
--- languagetool-language-modules/de/src/test/java/org/languagetool/rules/de/NumberInWordFilterTest.java
+++ languagetool-language-modules/de/src/test/java/org/languagetool/rules/de/NumberInWordFilterTest.java
@@ -1,3 +1,21 @@
+/* LanguageTool, a natural language style checker
+ * Copyright (C) 2021 Stefan Viol
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301
+ * USA
+ */
 package org.languagetool.rules.de;
 
 import org.junit.Test;
@@ -19,6 +37,7 @@ public class NumberInWordFilterTest {
     JLanguageTool lt = new JLanguageTool(Languages.getLanguageForShortCode("de"));
     runFilter("Schöne Grüß0e aus Potsdam.", "Grüß0e", "Grüße", 2, 0, 5, lt);
     runFilter("Schöne Grü0e aus Potsdam.", "Grü0e", "Grüße", 2, 0, 5, lt);
+    runFilter("Der Aut0r schreibt ein Buch.", "Aut0r", "Autor", 2, 0, 5, lt);
   }
 
   private void runFilter(String input, String arg, String newRepl, int patternTokenPos, int fromPos, int toPos, JLanguageTool lt) throws IOException {
@@ -28,7 +47,6 @@ public class NumberInWordFilterTest {
     HashMap<String, String> args = new HashMap<>();
     args.put("word", arg);
     RuleMatch matchTmp = filter.acceptRuleMatch(match, args, patternTokenPos, sentence.getTokensWithoutWhitespace());
-
     assertNotNull(matchTmp);
     assertTrue(matchTmp.getSuggestedReplacements().contains(newRepl));
   }
