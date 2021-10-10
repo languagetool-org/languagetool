73a41f2d48d67ba5ef5e5deb1a8c58593dc0165b
jaumeortola <jaumeortola@gmail.com>
2021-10-10 10:17:16 +0200

    [ca] improve synthesizer

--- languagetool-language-modules/ca/src/main/java/org/languagetool/rules/ca/CatalanRepeatedWordsRule.java
+++ languagetool-language-modules/ca/src/main/java/org/languagetool/rules/ca/CatalanRepeatedWordsRule.java
@@ -41,7 +41,7 @@ public class CatalanRepeatedWordsRule extends AbstractRepeatedWordsRule {
 
   @Override
   protected String getMessage() {
-    return "Ja heu usat aquesta paraula abans. Podeu usar un sinònim per a fer més interessant el text, llevat que la repetició sigui intencionada.";
+    return "Ja heu usat aquesta paraula abans. Podeu substituir-la per un sinònim per a fer més variat el text, llevat que la repetició sigui intencionada.";
   }
 
   @Override
