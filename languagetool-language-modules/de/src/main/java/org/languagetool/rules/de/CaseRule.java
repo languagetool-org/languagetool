feef07876c05714281c17ec09d070d934d1b858a
Christopher Blum <christopher.blum@spellboy.com>
2021-10-13 14:11:33 +0200

    [de] improve rules

--- languagetool-language-modules/de/src/main/java/org/languagetool/rules/de/CaseRule.java
+++ languagetool-language-modules/de/src/main/java/org/languagetool/rules/de/CaseRule.java
@@ -1046,6 +1046,10 @@ public class CaseRule extends Rule {
       regex("nur"),
       csRegex("Schlechtes"),
       csRegex("[a-zäöü…\\.!\\?].*")
+    ),
+    Arrays.asList(
+      regex("[\\ud83c\\udc00-\\ud83c\\udfff]|[\\ud83d\\udc00-\\ud83d\\udfff]|[\\u2600-\\u27ff]+"),
+      csRegex("[A-ZÄÖÜ].+")
     )
   );
 
