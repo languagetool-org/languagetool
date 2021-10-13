a6c5f02f127ec245a0e38ca8cff75eabe9f66fc2
Daniel Naber <naber@danielnaber.de>
2021-10-13 13:54:22 +0200

    rename for consistency

--- languagetool-server/src/main/java/org/languagetool/server/ErrorRequestLimiter.java
+++ languagetool-server/src/main/java/org/languagetool/server/ErrorRequestLimiter.java
@@ -31,7 +31,7 @@ import java.util.Map;
  */
 class ErrorRequestLimiter extends RequestLimiter {
 
-  private static final Logger log = LoggerFactory.getLogger(ErrorRequestLimiter.class);
+  private static final Logger logger = LoggerFactory.getLogger(ErrorRequestLimiter.class);
 
   /**
    * @param requestLimit the maximum number of request per <tt>requestLimitPeriodInSeconds</tt>
@@ -50,7 +50,7 @@ class ErrorRequestLimiter extends RequestLimiter {
       checkLimit(ipAddress, parameters, httpHeader);
       return true;
     } catch (TooManyRequestsException e) {
-      log.info("wouldAccessBeOkay -> false: " + e.getMessage());
+      logger.info("wouldAccessBeOkay -> false: " + e.getMessage());
       return false;
     }
   }
