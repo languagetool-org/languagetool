721be614818b8b61a583ac167bf8f8c99189ff44
jaumeortola <jaumeortola@gmail.com>
2021-11-10 11:37:09 +0100

    [de] fix suggestions for PRAEP_AKK[2]

--- languagetool-core/src/main/java/org/languagetool/rules/AbstractAdvancedSynthesizerFilter.java
+++ languagetool-core/src/main/java/org/languagetool/rules/AbstractAdvancedSynthesizerFilter.java
@@ -48,6 +48,11 @@ public abstract class AbstractAdvancedSynthesizerFilter extends RuleFilter {
   @Override
   public RuleMatch acceptRuleMatch(RuleMatch match, Map<String, String> arguments, int patternTokenPos,
       AnalyzedTokenReadings[] patternTokens) throws IOException {
+    
+    //if (match.getSentence().getText().contains("Das ist wider der Vernunft")) {
+    //  int ii=0;
+    //  ii++;
+    //}
 
     String postagSelect = getRequired("postagSelect", arguments);
     String lemmaSelect = getRequired("lemmaSelect", arguments);
