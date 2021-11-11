30ecb3cc24f66f536ddad2cc24556828e0d3d021
jaumeortola <jaumeortola@gmail.com>
2021-11-11 12:49:03 +0100

    [ca] improve comma rules

--- languagetool-core/src/main/java/org/languagetool/tools/StringTools.java
+++ languagetool-core/src/main/java/org/languagetool/tools/StringTools.java
@@ -65,7 +65,7 @@ public final class StringTools {
   private static final Pattern XML_PATTERN = Pattern.compile("(?<!<)<[^<>]+>", Pattern.DOTALL);
   public static final Set<String> UPPERCASE_GREEK_LETTERS = Collections.unmodifiableSet(new HashSet<>(Arrays.asList("Α","Β","Γ","Δ","Ε","Ζ","Η","Θ","Ι","Κ","Λ","Μ","Ν","Ξ","Ο","Π","Ρ","Σ","Τ","Υ","Φ","Χ","Ψ","Ω")));
   public static final Set<String> LOWERCASE_GREEK_LETTERS = Collections.unmodifiableSet(new HashSet<>(Arrays.asList("α","β","γ","δ","ε","ζ","η","θ","ι","κ","λ","μ","ν","ξ","ο","π","ρ","σ","τ","υ","φ","χ","ψ","ω")));
-  private static final Pattern PUNCTUATION_PATTERN = Pattern.compile("\\p{Punct}", Pattern.DOTALL);
+  private static final Pattern PUNCTUATION_PATTERN = Pattern.compile("[\\p{IsPunctuation}']", Pattern.DOTALL);
 
   private StringTools() {
     // only static stuff
