4d67d3bb8ff7122747bf85aa13c81e42e84f894a
Daniel Naber <naber@danielnaber.de>
2021-11-25 22:14:12 +0100

    [de] avoid false alarm (#6080)

--- languagetool-language-modules/de/src/main/java/org/languagetool/rules/de/CaseRule.java
+++ languagetool-language-modules/de/src/main/java/org/languagetool/rules/de/CaseRule.java
@@ -89,6 +89,7 @@ public class CaseRule extends Rule {
    * workaround to avoid false alarms, these words can be added here.
    */
   private static final String[] exceptions = {
+    "Vertrauter",
     "Out", // eng
     "Packet", // misspelling of "Paket" (caught by spell checker)
     "Adult", // eng
--- languagetool-language-modules/de/src/test/java/org/languagetool/rules/de/CaseRuleTest.java
+++ languagetool-language-modules/de/src/test/java/org/languagetool/rules/de/CaseRuleTest.java
@@ -185,6 +185,7 @@ public class CaseRuleTest {
     assertGood(":D Auf dieses Frl.");
     assertGood("Das Gedicht “Der Panther”.");  // quotes are not correct, but leave that to the quotes rule
     assertGood("Klar, dass wir das brauchen.");
+    assertGood("Das wird Scholz' engster Vertrauter Wolfgang Schmidt übernehmen.");
 
     assertGood("Ist das eine Frage ? Müsste das nicht anders sein?");
     assertGood("Das ist ein Satz !!! Das auch.");
