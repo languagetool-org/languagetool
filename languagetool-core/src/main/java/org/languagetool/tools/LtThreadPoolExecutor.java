5065311d6b5e0abaf82643808c089095647e38b9
Stefan Viol <stefan@stevio.de>
2021-11-25 12:23:58 +0100

    [core] Update gauge collectors

--- languagetool-core/src/main/java/org/languagetool/tools/LtThreadPoolExecutor.java
+++ languagetool-core/src/main/java/org/languagetool/tools/LtThreadPoolExecutor.java
@@ -26,18 +26,31 @@ import lombok.Getter;
 import org.jetbrains.annotations.NotNull;
 
 import java.util.Queue;
+import java.util.Set;
 import java.util.concurrent.*;
+import java.util.stream.Stream;
 
 /**
  * ThreadPoolExecutor with some stopping logic for OOM and metrics tracking
- *
  */
 class LtThreadPoolExecutor extends ThreadPoolExecutor {
 
-  private static final Gauge activeThreads = Gauge.build("languagetool_threadpool_active_threads", "Running threads by threadpool")
-      .labelNames("pool").register();
-  private static final Gauge queueSize = Gauge.build("languagetool_threadpool_queue_size", "Queue size by threadpool").labelNames("pool").register();
-  private static final Gauge maxQueueSize = Gauge.build("languagetool_threadpool_max_queue_size", "Queue capacity by threadpool").labelNames("pool").register();
+//  private static final Gauge activeThreads = Gauge.build("languagetool_threadpool_active_threads", "Running threads by threadpool")
+//    .labelNames("pool").register();
+  private static final Gauge queueSize = Gauge.build("languagetool_threadpool_queue_size", "Queue size by threadpool")
+    .labelNames("pool").register();
+  private static final Gauge maxQueueSize = Gauge.build("languagetool_threadpool_max_queue_size", "Queue capacity by threadpool")
+    .labelNames("pool").register();
+  private static final Gauge largestPoolSize = Gauge.build("languagetool_threadpool_largest_queue_size", "The largest number of threads that have ever simultaneously been in the pool")
+    .labelNames("pool").register();
+  private static final Gauge waitingThreads = Gauge.build("languagetool_threadpool_waiting_threads", "Waiting threads by threadpool")
+    .labelNames("pool").register();
+  private static final Gauge timedWaitingThreads = Gauge.build("languagetool_threadpool_timed_waiting_threads", "Timed_Waiting threads by threadpool")
+    .labelNames("pool").register();
+  private static final Gauge blockingThreads = Gauge.build("languagetool_threadpool_blocking_threads", "Blocking threads by threadpool")
+    .labelNames("pool").register();
+  private static final Gauge runningThreads = Gauge.build("languagetool_threadpool_running_threads", "Running threads by threadpool")
+    .labelNames("pool").register();
 
   @Getter
   private final String name;
@@ -66,7 +79,9 @@ class LtThreadPoolExecutor extends ThreadPoolExecutor {
   @Override
   protected void afterExecute(Runnable r, Throwable t) {
     super.afterExecute(r, t);
-    activeThreads.labels(name).dec();
+    updateThreadGauges();
+//    activeThreads.labels(name).dec();
+//    waitingThreads.labels(name).inc();
     queueSize.labels(name).set(queue.size());
     // inherited from removed StoppingThreadPoolExecutor in org.languagetool.server.Server
     if (t != null && t instanceof OutOfMemoryError) {
@@ -80,7 +95,38 @@ class LtThreadPoolExecutor extends ThreadPoolExecutor {
   @Override
   protected void beforeExecute(Thread t, Runnable r) {
     super.beforeExecute(t, r);
-    activeThreads.labels(name).inc();
-    queueSize.labels(name).set(queue.size());
+    updateThreadGauges();
+//    activeThreads.labels(name).inc();
+//    waitingThreads.labels(name).dec();
+  }
+
+  @NotNull
+  @Override
+  public Future<?> submit(@NotNull Runnable runnable) {
+    largestPoolSize.labels(name).set(getLargestPoolSize());
+    return super.submit(runnable);
+  }
+
+  @NotNull
+  @Override
+  public <T> Future<T> submit(@NotNull Runnable runnable, T t) {
+    largestPoolSize.labels(name).set(getLargestPoolSize());
+    return super.submit(runnable, t);
+  }
+
+  @NotNull
+  @Override
+  public <T> Future<T> submit(@NotNull Callable<T> callable) {
+    largestPoolSize.labels(name).set(getLargestPoolSize());
+    return super.submit(callable);
+  }
+
+  private void updateThreadGauges() {
+    Set<Thread> threads = Thread.getAllStackTraces().keySet();
+    Stream<Thread> filtered = threads.stream().filter(thread -> thread.getName().startsWith(name));
+    blockingThreads.labels(name).set(filtered.filter(thread -> thread.getState() == Thread.State.BLOCKED).count());
+    waitingThreads.labels(name).set(filtered.filter(thread -> thread.getState() == Thread.State.WAITING).count());
+    timedWaitingThreads.labels(name).set(filtered.filter(thread -> thread.getState() == Thread.State.TIMED_WAITING).count());
+    runningThreads.labels(name).set(filtered.filter(thread -> thread.getState() == Thread.State.RUNNABLE).count());
   }
 }
