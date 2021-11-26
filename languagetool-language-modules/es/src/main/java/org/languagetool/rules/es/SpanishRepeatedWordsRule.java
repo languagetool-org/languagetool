aaf30d7eaa405ac97d006567befd48bbb21e28a7
jaumeortola <jaumeortola@gmail.com>
2021-11-26 12:56:37 +0100

    [es] repeated words: add APs

--- languagetool-language-modules/es/src/main/java/org/languagetool/rules/es/SpanishRepeatedWordsRule.java
+++ languagetool-language-modules/es/src/main/java/org/languagetool/rules/es/SpanishRepeatedWordsRule.java
@@ -38,7 +38,9 @@ public class SpanishRepeatedWordsRule extends AbstractRepeatedWordsRule {
   private final Supplier<List<DisambiguationPatternRule>> antiPatterns;
   
   private static final List<List<PatternToken>> ANTI_PATTERNS = Arrays
-      .asList(Arrays.asList(token("también"), csRegex(".+")), Arrays.asList(csRegex(".+"), token("también")));
+      .asList(Arrays.asList(token("también"), csRegex(".+")), 
+          Arrays.asList(csRegex(".+"), token("también")),
+          Arrays.asList(csRegex("[Aa]ntes|[Dd]espués"), csRegex("de|del")));
   
   @Override
   public List<DisambiguationPatternRule> getAntiPatterns() {
