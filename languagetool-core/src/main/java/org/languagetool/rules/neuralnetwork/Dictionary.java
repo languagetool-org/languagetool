87302f2b527782865069f603c05fbf6eb89e87bc
Felicia Wang <49826049+feliciawang08@users.noreply.github.com>
2021-12-06 17:10:20 -0600

    Import Collections to remove compilation error

--- languagetool-core/src/main/java/org/languagetool/rules/neuralnetwork/Dictionary.java
+++ languagetool-core/src/main/java/org/languagetool/rules/neuralnetwork/Dictionary.java
@@ -22,6 +22,7 @@ import java.io.InputStream;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
+import java.util.Collections;
 
 public class Dictionary extends HashMap<String, Integer> {
 
