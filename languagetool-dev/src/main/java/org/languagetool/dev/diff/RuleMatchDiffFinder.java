534c397d66bbb07b612b0fa8cbdbe8c80fa8a3e5
Daniel Naber <naber@danielnaber.de>
2021-12-07 12:28:34 +0100

    fix "picky" column

--- languagetool-dev/src/main/java/org/languagetool/dev/diff/RuleMatchDiffFinder.java
+++ languagetool-dev/src/main/java/org/languagetool/dev/diff/RuleMatchDiffFinder.java
@@ -541,6 +541,7 @@ public class RuleMatchDiffFinder {
       "    col_2: 'none',\n" +
       "    col_3: 'none',\n" +
       "    col_4: 'select',\n" +
+      "    col_5: 'none',\n" +
       "    grid_layout: false,\n" +
       "    col_types: ['number', 'number', 'number', 'number', 'string', 'string'],\n" +
       "    extensions: [{ name: 'sort' }]\n" +
