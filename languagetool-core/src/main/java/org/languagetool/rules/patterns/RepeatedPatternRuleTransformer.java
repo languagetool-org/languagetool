fe087c5255b291909a1dfeb2a632d050794e208f
jaumeortola <jaumeortola@gmail.com>
2021-12-17 22:33:04 +0100

    do RepetitionMatchFilter inside text level rule

--- languagetool-core/src/main/java/org/languagetool/rules/patterns/RepeatedPatternRuleTransformer.java
+++ languagetool-core/src/main/java/org/languagetool/rules/patterns/RepeatedPatternRuleTransformer.java
@@ -1,29 +1,25 @@
-/*
- *  LanguageTool, a natural language style checker
- *  * Copyright (C) 2021 Fabian Richter
- *  *
- *  * This library is free software; you can redistribute it and/or
- *  * modify it under the terms of the GNU Lesser General Public
- *  * License as published by the Free Software Foundation; either
- *  * version 2.1 of the License, or (at your option) any later version.
- *  *
- *  * This library is distributed in the hope that it will be useful,
- *  * but WITHOUT ANY WARRANTY; without even the implied warranty of
- *  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- *  * Lesser General Public License for more details.
- *  *
- *  * You should have received a copy of the GNU Lesser General Public
- *  * License along with this library; if not, write to the Free Software
- *  * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301
- *  * USA
+/* LanguageTool, a natural language style checker 
+ * Copyright (C) 2021 Fabian Richter
+ * 
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
  *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301
+ * USA
  */
 
 package org.languagetool.rules.patterns;
 
 import org.languagetool.AnalyzedSentence;
-import org.languagetool.Language;
-import org.languagetool.rules.RepetitionMatchFilter;
 import org.languagetool.rules.Rule;
 import org.languagetool.rules.RuleMatch;
 import org.languagetool.rules.TextLevelRule;
@@ -34,12 +30,8 @@ import java.util.List;
 import java.util.Optional;
 
 public class RepeatedPatternRuleTransformer implements PatternRuleTransformer {
-
-  public RepeatedPatternRuleTransformer(Language lang) {
-    matchFilter = new RepetitionMatchFilter(lang);
-  }
-
-  private final RepetitionMatchFilter matchFilter;
+  
+  protected int maxDistance = 350; // numer of tokens
 
   /**
    * Wrapper for loaded {@link AbstractPatternRule} instances to act as text-level rules
@@ -66,18 +58,24 @@ public class RepeatedPatternRuleTransformer implements PatternRuleTransformer {
     public RuleMatch[] match(List<AnalyzedSentence> sentences) throws IOException {
       List<RuleMatch> matches = new ArrayList<>();
       int offset = 0;
+      int prevFromPos = 0;
+      int prevMatches = 0;
       // we need to adjust offsets since each pattern rule returns offsets relative to the sentence, not text
       for (AnalyzedSentence s : sentences) {
         RuleMatch[] sentenceMatches = rule.match(s);
         for (RuleMatch m : sentenceMatches) {
-          int from = m.getFromPos() + offset;
-          int to = m.getToPos() + offset;
-          m.setOffsetPosition(from, to);
-          matches.add(m);
+          int fromPos = m.getFromPos() + offset;
+          int toPos = m.getToPos() + offset;
+          m.setOffsetPosition(fromPos, toPos);
+          if (fromPos - prevFromPos <= maxDistance && prevMatches > getMinPrevMatches()) {
+            matches.add(m);
+          }
+          prevFromPos = fromPos;
+          prevMatches++;
         }
         offset += s.getText().length();
       }
-      return matchFilter.filter(matches).toArray(RuleMatch.EMPTY_ARRAY);
+      return matches.toArray(new RuleMatch[0]); 
     }
 
     @Override
