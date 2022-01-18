7f1446f85df6e9bc15d2cd5849dcd5d8a27aec64
rebecca-auque <rebecca.auque@gmail.com>
2022-01-18 15:58:58 +0100

    [fr] LA_VERBE_ADJ

--- languagetool-language-modules/fr/src/main/java/org/languagetool/language/French.java
+++ languagetool-language-modules/fr/src/main/java/org/languagetool/language/French.java
@@ -320,6 +320,7 @@ public class French extends Language implements AutoCloseable {
       case "ET_SENT_START": return -151; // lower than grammalecte rules
       case "MAIS_SENT_START": return -151; // lower than grammalecte rules
       case "ELISION": return -200; // should be lower in priority than spell checker
+      case "POINT": return -200; // should be lower in priority than spell checker
       case "UPPERCASE_SENTENCE_START": return -300;
       case "FRENCH_WHITESPACE_STRICT": return -350; // picky; if on, it should overwrite FRENCH_WHITESPACE
       case "FRENCH_WHITESPACE": return -400; // lesser than UPPERCASE_SENTENCE_START and FR_SPELLING_RULE
