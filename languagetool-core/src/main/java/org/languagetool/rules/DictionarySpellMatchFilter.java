dbdbec37be1a2ccf6eec337a41c0a942eab4bb6b
Fabian Richter <fabianrichter@fastmail.com>
2022-02-15 14:25:58 +0100

    fix: partial matches of accepted phrases
    if only part of an accepted phrase was marked as a spelling error,
    it wouldn't be filtered out by DictionarySpellMatchFilter

--- languagetool-core/src/main/java/org/languagetool/rules/DictionarySpellMatchFilter.java
+++ languagetool-core/src/main/java/org/languagetool/rules/DictionarySpellMatchFilter.java
@@ -18,19 +18,51 @@
  */
 package org.languagetool.rules;
 
+import com.google.common.cache.Cache;
+import com.google.common.cache.CacheBuilder;
+import com.google.common.cache.CacheLoader;
+import com.google.common.cache.LoadingCache;
+import com.hankcs.algorithm.AhoCorasickDoubleArrayTrie;
+import lombok.extern.slf4j.Slf4j;
 import org.languagetool.UserConfig;
 import org.languagetool.markup.AnnotatedText;
 
 import java.util.*;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.TimeUnit;
+import java.util.function.Function;
 
 /**
  * Filter spelling error with phrases the users wants to have accepted.
  * Needed so words with spaces (i.e. phrases) can be added to a user's dictionary
  * without LT creating internal anti patterns for each phrase.
  */
+@Slf4j
 public class DictionarySpellMatchFilter implements RuleMatchFilter {
 
   private final UserConfig userConfig;
+
+  private static final int CACHE_SIZE = 100;
+  private static final long CACHE_TTL_SECONDS = 600;
+
+  private static class PhraseSearchLoader extends CacheLoader<UserConfig, AhoCorasickDoubleArrayTrie<String>> {
+    @Override
+    public AhoCorasickDoubleArrayTrie<String> load(UserConfig key) throws Exception {
+      AhoCorasickDoubleArrayTrie<String> searcher = new AhoCorasickDoubleArrayTrie<>();
+      Map<String, String> phrases = new HashMap<>();
+      for (String phrase : key.getAcceptedPhrases()) {
+        phrases.put(phrase, phrase);
+      }
+      searcher.build(phrases);
+      return searcher;
+    }
+  }
+
+  private static final LoadingCache<UserConfig, AhoCorasickDoubleArrayTrie<String>> phraseSearcher =
+    CacheBuilder.newBuilder().recordStats()
+      .maximumSize(CACHE_SIZE)
+      .expireAfterAccess(CACHE_TTL_SECONDS, TimeUnit.SECONDS)
+      .build(new PhraseSearchLoader());
 
   public DictionarySpellMatchFilter(UserConfig userConfig) {
     this.userConfig = userConfig;
@@ -40,14 +72,30 @@ public class DictionarySpellMatchFilter implements RuleMatchFilter {
   public List<RuleMatch> filter(List<RuleMatch> ruleMatches, AnnotatedText text) {
     Set<String> dictionary = userConfig.getAcceptedPhrases();
     if (dictionary.size() > 0) {
-      Map<String, List<RuleMatch>> phraseToMatches = getPhrases(ruleMatches, text);
       List<RuleMatch> cleanMatches = new ArrayList<>(ruleMatches);
-      for (Map.Entry<String, List<RuleMatch>> entry : phraseToMatches.entrySet()) {
-        if (dictionary.contains(entry.getKey())) {
-          cleanMatches.removeAll(entry.getValue());
+      try {
+        AhoCorasickDoubleArrayTrie<String> searcher = phraseSearcher.get(userConfig);
+
+        List<AhoCorasickDoubleArrayTrie.Hit<String>> phrases = searcher.parseText(text.getPlainText());
+
+        for (AhoCorasickDoubleArrayTrie.Hit<String> phrase : phrases) {
+          Iterator<RuleMatch> iter = cleanMatches.iterator();
+
+          while (iter.hasNext()) {
+            RuleMatch match = iter.next();
+            if (match.getRule().isDictionaryBasedSpellingRule() &&
+                match.getFromPos() >= phrase.begin &&
+                match.getToPos() <= phrase.end) {
+              // remove all spelling matches that are (subsets) of accepted phrases
+              iter.remove();
+            }
+          }
         }
+        return cleanMatches;
+      } catch (ExecutionException e) {
+        log.error("Couldn't set up phrase search, accepted phrases won't work.", e);
+        return ruleMatches;
       }
-      return cleanMatches;
     }
     return ruleMatches;
   }
