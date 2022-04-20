1a4df40a6e530520b4a47e27678e3a8ba42bc942
SteVio89 <stefan@stevio.de>
2022-04-20 11:03:26 +0200

    update dependencies

--- languagetool-core/src/main/java/org/languagetool/rules/ml/MLServerGrpc.java
+++ languagetool-core/src/main/java/org/languagetool/rules/ml/MLServerGrpc.java
@@ -5,7 +5,7 @@ import static io.grpc.MethodDescriptor.generateFullMethodName;
 /**
  */
 @javax.annotation.Generated(
-    value = "by gRPC proto compiler (version 1.45.0)",
+    value = "by gRPC proto compiler (version 1.45.1)",
     comments = "Source: ml_server.proto")
 @io.grpc.stub.annotations.GrpcGenerated
 public final class MLServerGrpc {
