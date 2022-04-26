24d3438fbcdae8ef3196e37691483e39176efc69
jaumeortola <jaumeortola@gmail.com>
2022-04-26 16:31:50 +0200

    [be] allow words with character \u2019

--- languagetool-language-modules/be/src/main/java/org/languagetool/tokenizers/be/BelarusianWordTokenizer.java
+++ languagetool-language-modules/be/src/main/java/org/languagetool/tokenizers/be/BelarusianWordTokenizer.java
@@ -18,22 +18,45 @@
  */
 package org.languagetool.tokenizers.be;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.StringTokenizer;
+
 import org.languagetool.tokenizers.WordTokenizer;
 
 /**
- * Specific to Belarusian: apostrophes (\u0027, \u2019, \u02BC) are part of the word.
+ * Specific to Belarusian: apostrophes (\u0027, \u2019, \u02BC) are part of the
+ * word.
  * 
  * @author Aleś Bułojčyk (alex73mail@gmail.com)
  */
 public class BelarusianWordTokenizer extends WordTokenizer {
-    private final String tokenizingCharacters;
+  private final String tokenizingCharacters;
 
-    public BelarusianWordTokenizer() {
-        tokenizingCharacters = super.getTokenizingCharacters().replace("\u0027", "").replace("\u2019", "").replace("\u02BC", "");
-    }
+  public BelarusianWordTokenizer() {
+    tokenizingCharacters = super.getTokenizingCharacters().replace("\u0027", "").replace("\u2019", "").replace("\u02BC",
+        "");
+  }
 
-    @Override
-    public String getTokenizingCharacters() {
-        return tokenizingCharacters;
+  @Override
+  public String getTokenizingCharacters() {
+    return tokenizingCharacters;
+  }
+
+  @Override
+  public List<String> tokenize(String text) {
+    List<String> l = new ArrayList<>();
+    StringTokenizer st = new StringTokenizer(text, getTokenizingCharacters(), true);
+    while (st.hasMoreElements()) {
+      l.add(st.nextToken());
     }
+    List<String> outputlist = new ArrayList<>();
+    for (String token : joinEMailsAndUrls(l)) {
+      if (token.length() > 1) {
+        outputlist.add(token.replaceAll("\u2019", "'"));
+      } else
+        outputlist.add(token);
+    }
+    return outputlist;
+  }
 }
