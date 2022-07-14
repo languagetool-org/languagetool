65eda0935ffb619fb2d9c89415999be95a2fddb3
Daniel Naber <naber@danielnaber.de>
2022-07-14 08:44:36 +0200

    [de] robe/probe

--- languagetool-language-modules/de/src/main/java/org/languagetool/rules/de/ProhibitedCompoundRule.java
+++ languagetool-language-modules/de/src/main/java/org/languagetool/rules/de/ProhibitedCompoundRule.java
@@ -50,6 +50,7 @@ public class ProhibitedCompoundRule extends Rule {
   private static final List<Pair> lowercasePairs = Arrays.asList(
           // NOTE: words here must be all-lowercase
           // NOTE: no need to add words from confusion_sets.txt, they will be used automatically (if starting with uppercase char)
+          new Pair("robe", "Kleidungsstück", "probe", "Test, Kontrolle"),
           new Pair("mode", "Kleidung", "monde", "Begleiter eines Planeten"),
           new Pair("eigen", "'selbst', z.B. 'Eigenzitat'", "eingen", "Möglicher Tippfehler"),
           new Pair("stümpfe", "Rest eines Körpergliedes", "strümpfe", "Bekleidungsstück für den Fuß"),
