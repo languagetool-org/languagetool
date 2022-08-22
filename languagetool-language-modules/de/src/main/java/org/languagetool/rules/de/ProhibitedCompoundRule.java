87ba8cbe61196b0251b0482d49462d8e95aefc26
Daniel Naber <naber@danielnaber.de>
2022-08-22 19:41:41 +0200

    [de] himmel/hummel

--- languagetool-language-modules/de/src/main/java/org/languagetool/rules/de/ProhibitedCompoundRule.java
+++ languagetool-language-modules/de/src/main/java/org/languagetool/rules/de/ProhibitedCompoundRule.java
@@ -50,6 +50,7 @@ public class ProhibitedCompoundRule extends Rule {
   private static final List<Pair> lowercasePairs = Arrays.asList(
           // NOTE: words here must be all-lowercase
           // NOTE: no need to add words from confusion_sets.txt, they will be used automatically (if starting with uppercase char)
+          new Pair("himmel", "Bereich über der Erde", "hummel", "Insekt"),
           new Pair("server", "Computer", "servier", "zu 'servieren'"),
           new Pair("ziege", "Tier", "ziegel", "Ziegelstein"),
           new Pair("robe", "Kleidungsstück", "probe", "Test, Kontrolle"),
@@ -151,6 +152,9 @@ public class ProhibitedCompoundRule extends Rule {
     ".+gra(ph|f)its?"   // ...grafit/graphit
   );
   private static final Set<String> blacklist = new HashSet<>(Arrays.asList(
+          "Schimmelns",
+          "Oberhummel", "Oberhummels",
+          "Alpenhummel", "Alpenhummeln",
           "Futterrecht", "Futterrechts",
           "Waschtasche", "Waschtaschen",
           "Fiebersaft", "Fiebersafts",
