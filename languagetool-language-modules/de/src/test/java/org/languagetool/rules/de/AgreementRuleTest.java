db12a6d2eb0dc3f2555d38f1bdb38808fdadc1dc
Christopher Blum <christopher.blum@spellboy.com>
2022-11-17 14:48:57 +0100

    [de] improve rules

--- languagetool-language-modules/de/src/test/java/org/languagetool/rules/de/AgreementRuleTest.java
+++ languagetool-language-modules/de/src/test/java/org/languagetool/rules/de/AgreementRuleTest.java
@@ -656,6 +656,10 @@ public class AgreementRuleTest {
     assertGood("Dieser eine Schritt hat gedauert.");
     assertGood("Es besteht durchaus die Gefahr, dass die Telekom eine solch starke monopolistische Stellung auf dem Markt hat, dass sich kaum Wettbewerb entfalten kann.");
     assertBad("Ich habe keine Zeit für solche kleinlichen Belangen.");
+    assertGood("Wenn ein Tiger einen Menschen tötet, ist das Grausamkeit.");
+    assertBad("Einen Dämonen wird er nicht aufhalten.");
+    assertBad("Das versetzte den Kronprinz in Schrecken.");
+    assertGood("Erst später wurde Kritik hauptsächlich an den Plänen zu einem Patriot Act II laut.");
     // TODO:
     // assertBad("Solch hartes Worte!");
     // unsure:
