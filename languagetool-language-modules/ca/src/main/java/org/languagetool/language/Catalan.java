4c4d3a5e45bdd1d77b472fbd413d5bc0fdfd9d07
jaumeortola <jaumeortola@gmail.com>
2022-12-08 08:45:25 +0100

    [ca] enable rules

--- languagetool-language-modules/ca/src/main/java/org/languagetool/language/Catalan.java
+++ languagetool-language-modules/ca/src/main/java/org/languagetool/language/Catalan.java
@@ -225,6 +225,7 @@ public class Catalan extends Language {
       case "DOS_ARTICLES": return 10; // greater than apostrophation rules
       case "MOTS_GUIONET": return 10; // greater than CONCORDANCES_DET_NOM
       case "ZERO_O": return 10; //greater than SPELLING
+      case "URL": return 10; //greater than SPELLING
       case "CONCORDANCES_DET_NOM": return 5;
       case "DET_GN": return 5; // greater than DE_EL_S_APOSTROFEN
       case "SPELLING": return 5;
