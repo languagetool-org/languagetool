527ea765ca28c034717dd74c8d4b36f9ee419d5b
Daniel Naber <naber@danielnaber.de>
2023-01-01 21:15:12 +0100

    [de] avoid false alarm (fixes #5043)

--- languagetool-language-modules/de/src/main/java/org/languagetool/rules/de/CaseRule.java
+++ languagetool-language-modules/de/src/main/java/org/languagetool/rules/de/CaseRule.java
@@ -92,6 +92,8 @@ public class CaseRule extends Rule {
    * workaround to avoid false alarms, these words can be added here.
    */
   private static final String[] exceptions = {
+    "Bedienstete",
+    "Bediensteter",
     "Feierwütiger",
     "Feierwütige",
     "Feierwütigen",
