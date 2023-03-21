954b0f597c6fe77512060066aab1f7af03125557
yakovru <yakovr_st@mail.ru>
2023-03-21 21:19:07 +0300

    [ru] improve algoritm

--- languagetool-language-modules/ru/src/main/java/org/languagetool/rules/ru/INNNumberFilter.java
+++ languagetool-language-modules/ru/src/main/java/org/languagetool/rules/ru/INNNumberFilter.java
@@ -55,6 +55,7 @@ public class INNNumberFilter extends RuleFilter {
               switch (intTab.length) {
                   case 10:
                       kz1 = (intTab[0]*2 + intTab[1]*4 + intTab[2]*10 + intTab[3]*3 + intTab[4]*5+ intTab[5]*9 + intTab[6]*4 + intTab[7]*6 + intTab[8]*8)%11;
+                      if (kz1>9) {kz1=kz1-10;};
                       if (intTab[9] == kz1)  {
                           return null;
                       } else {
@@ -63,6 +64,9 @@ public class INNNumberFilter extends RuleFilter {
                   case 12:
                       kz1 = (intTab[0]*7 + intTab[1]*2 + intTab[2]*4 + intTab[3]*10 + intTab[4]*3 + intTab[5]*5+ intTab[6]*9 + intTab[7]*4 + intTab[8]*6 + intTab[9]*8)%11;
                       kz2 = (intTab[0]*3 + intTab[1]*7 + intTab[2]*2 + intTab[3]*4 + intTab[4]*10 + intTab[5]*3 + intTab[6]*5+ intTab[7]*9 + intTab[8]*4 + intTab[9]*6 + intTab[10]*8)%11;
+                      if (kz1>9) {kz1=kz1-10;};
+                      if (kz2>9) {kz2=kz2-10;};
+                      
                       if ((intTab[10] == kz1) && (intTab[11] == kz2)) {
                           return null;
                       } else {
