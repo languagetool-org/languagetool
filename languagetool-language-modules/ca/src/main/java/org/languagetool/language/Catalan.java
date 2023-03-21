ceae9f74b6fcc76e58d1b0485d181a57054de799
jaumeortola <jaumeortola@gmail.com>
2023-03-21 02:17:51 +0100

    [ca] improve VERBS_PRONOMINALS

--- languagetool-language-modules/ca/src/main/java/org/languagetool/language/Catalan.java
+++ languagetool-language-modules/ca/src/main/java/org/languagetool/language/Catalan.java
@@ -243,6 +243,7 @@ public class Catalan extends Language {
       case "FALTA_COMA_FRASE_CONDICIONAL": return -20;
       case "ESPAIS_QUE_FALTEN_PUNTUACIO": return -20;
       case "VERBS_NOMSPROPIS": return -20;
+      case "VERBS_PRONOMINALS": return -25;
       case "REPETITIONS_STYLE": return -50;
       case "MUNDAR": return -50;
       case "NOMBRES_ROMANS": return -90;
