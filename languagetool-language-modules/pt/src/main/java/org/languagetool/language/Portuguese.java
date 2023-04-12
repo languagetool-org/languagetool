b423320289944e6a3e12b6f608a12817b1c78709
Susana <susana.boatto@languagetool.org>
2023-04-12 13:43:39 +0200

    [pt] change rule priority, add words to entity

--- languagetool-language-modules/pt/src/main/java/org/languagetool/language/Portuguese.java
+++ languagetool-language-modules/pt/src/main/java/org/languagetool/language/Portuguese.java
@@ -245,6 +245,7 @@ public class Portuguese extends Language implements AutoCloseable {
       case "PT_DIACRITICS_REPLACE":     return -45;   // prefer over spell checker
       case "DIACRITICS":     return -45;
       case "PT_COMPOUNDS_POST_REFORM":     return -45;
+      case "AUX_VERBO":     return -45;
       case "HUNSPELL_RULE":             return -50;
       case "CRASE_CONFUSION":           return -54;
       case "NAO_MILITARES":           return -54;
