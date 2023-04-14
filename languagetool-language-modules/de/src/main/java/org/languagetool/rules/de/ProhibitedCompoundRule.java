bbaad7dbb27fffa09026b5aa17cede75ca39119a
Daniel Naber <naber@danielnaber.de>
2023-04-14 14:54:50 +0200

    [de] more file/filet

--- languagetool-language-modules/de/src/main/java/org/languagetool/rules/de/ProhibitedCompoundRule.java
+++ languagetool-language-modules/de/src/main/java/org/languagetool/rules/de/ProhibitedCompoundRule.java
@@ -50,6 +50,7 @@ public class ProhibitedCompoundRule extends Rule {
   private static final List<Pair> lowercasePairs = Arrays.asList(
           // NOTE: words here must be all-lowercase
           // NOTE: no need to add words from confusion_sets.txt, they will be used automatically (if starting with uppercase char)
+          new Pair("file", "engl. 'Datei'", "filet", "ein Stück Fleisch oder Fisch"),
           new Pair("zecke", "blutsaugender Parasit", "zwecke", "Dativ von 'Zweck' (Ziel)"),
           new Pair("frucht", "Teil einer Pflanze; Obst", "furcht", "Angst"),
           new Pair("rate", "Verhältnis zwischen zwei Größen", "ratte", "Nagetier"),
