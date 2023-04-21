2a2eafff15c00adfe06f7cf327b0af138a1937a7
jaumeortola <jaumeortola@gmail.com>
2023-04-21 19:53:36 +0200

    [ca] improve rules

--- languagetool-language-modules/ca/src/main/java/org/languagetool/language/Catalan.java
+++ languagetool-language-modules/ca/src/main/java/org/languagetool/language/Catalan.java
@@ -179,6 +179,24 @@ public class Catalan extends Language {
   public LanguageMaintainedState getMaintainedState() {
     return LanguageMaintainedState.ActivelyMaintained;
   }
+  
+  public int getRulePriority(Rule rule) {
+    int categoryPriority = this.getPriorityForId(rule.getCategory().getId().toString());
+    int rulePriority = this.getPriorityForId(rule.getId());
+    // if there is a priority defined for the rule,
+    // it takes precedence over category priority
+    if (rulePriority != 0) {
+      return rulePriority;
+    }
+    if (categoryPriority != 0) {
+      return categoryPriority;
+    }
+    if (rule.getLocQualityIssueType().equals(ITSIssueType.Style)) {
+      // don't let style issues hide more important errors
+      return -50;
+    }
+    return 0;
+  }
 
   @Override
   protected int getPriorityForId(String id) {
