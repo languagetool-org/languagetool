e1e2ef3496ce4b764988d58e8cff24fd216b1f71
mark-baas <mark.baas@languagetool.org>
2023-04-24 11:54:50 +0700

    [nl] changed priority for rule KOMMA_KOMMA

--- languagetool-language-modules/nl/src/main/java/org/languagetool/language/Dutch.java
+++ languagetool-language-modules/nl/src/main/java/org/languagetool/language/Dutch.java
@@ -186,8 +186,9 @@ public class Dutch extends Language {
       case "HOOFDLETTERS_OVERBODIG_A": return 1; // needs higher priority than MORFOLOGIK_RULE_NL_NL
       case "STAM_ZONDER_IK": return -1;  // sse https://github.com/languagetool-org/languagetool/issues/7644
       case "KOMMA_ONTBR": return -1;   // see https://github.com/languagetool-org/languagetool/issues/7644
-      case "WIJ_ZIJ_MIJ": return -2;  // needs higher priority than JOU_JOUW
       case "KOMMA_AANH": return -2; // needs higher priority than DOUBLE_PUNCTUATION
+      case "KOMMA_KOMMA": return -2; // needs higher priority than DOUBLE_PUNCTUATION
+      case "WIJ_ZIJ_MIJ": return -2;  // needs higher priority than JOU_JOUW
       case "JOU_JOUW": return -3;
       case "DOUBLE_PUNCTUATION": return -3;
       case "KORT_1": return -5;
