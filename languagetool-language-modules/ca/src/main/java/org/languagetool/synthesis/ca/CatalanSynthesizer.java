90444be453c23c2039003e51f6d8234278693eeb
jaumeortola <jaumeortola@gmail.com>
2023-05-13 00:23:11 +0200

    [ca] improve rules

--- languagetool-language-modules/ca/src/main/java/org/languagetool/synthesis/ca/CatalanSynthesizer.java
+++ languagetool-language-modules/ca/src/main/java/org/languagetool/synthesis/ca/CatalanSynthesizer.java
@@ -281,6 +281,12 @@ public class CatalanSynthesizer extends BaseSynthesizer {
       int len0 = arg0.length();
       int len1 = arg1.length();
       if (len0 > 4 && len1 > 4) {
+        if (arg0.equals("VMIS3S00") && arg1.equals("VMIS1S00")) {
+          return 150;
+        }
+        if (arg0.equals("VMIS1S00") && arg1.equals("VMIS3S00")) {
+          return -150;
+        }
         if (arg0.equals("VMIP2P00") && arg1.equals("VMIS3S00")) {
           //feu (present/passat)
           return 150;
