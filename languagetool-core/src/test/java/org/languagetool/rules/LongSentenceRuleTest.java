7a9215c810f73f8f727474666ca8a5e9eb47a1fe
SteVio89 <stefan@stevio.de>
2023-06-01 13:40:52 +0200

    fix offsets too_long_sentence_rule

--- languagetool-core/src/test/java/org/languagetool/rules/LongSentenceRuleTest.java
+++ languagetool-core/src/test/java/org/languagetool/rules/LongSentenceRuleTest.java
@@ -34,37 +34,42 @@ public class LongSentenceRuleTest {
   public void testMatch() throws Exception {
     JLanguageTool lt = new JLanguageTool(TestTools.getDemoLanguage());
     
-    LongSentenceRule rule = new LongSentenceRule(TestTools.getEnglishMessages(), new UserConfig(), 50);
+    LongSentenceRule rule = new LongSentenceRule(TestTools.getEnglishMessages(), new UserConfig(), 40);
     assertNoMatch(" is a rather short text.", rule, lt);
     assertMatch("Now this is not " +
             "a a a a a a a a a a a " +
             "a a a a a a a a a a a " +
             "a a a a a a a a a a a " +
             "a a a a a a a a a a a " +
-            "rather that short text.", 0, 126, rule, lt);
+            "rather that short text.", 0, 127, rule, lt);
     
     assertMatch("Now this is not " +
             "a a a a a a a a a a a " +
             "a a a a a a a a a a a " +
             "a a a a a a a a a a a " +
             "a a a a a a a a a a a " +
-            "rather that short text", 0, 125, rule, lt);
+            "rather that short text", 0, 126, rule, lt);
+    
+    assertMatch("The sun slowly set behind the majestic mountains, " +
+      "casting a warm golden glow over the tranquil valley below, where a " +
+      "gentle breeze rustled the leaves of the trees, and the sound of a " +
+      "distant stream provided a soothing backdrop to the peaceful scene.", 0, 249, rule, lt);
     
     LongSentenceRule shortRule = new LongSentenceRule(TestTools.getEnglishMessages(), new UserConfig(), 6);
 //    shortRule.setDefaultValue(6);
     assertNoMatch("This is a rather short text.", shortRule, lt);
-    assertMatch("This is also a rather short text.", 0, 32, shortRule, lt);
+    assertMatch("This is also a rather short text.", 0, 33, shortRule, lt);
     assertNoMatch("These ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ don't count.", shortRule, lt);
     assertNoMatch("one two three four five six.", shortRule, lt);
     assertNoMatch("one two three (four) five six.", shortRule, lt);
-    assertMatch("one two three four five six seven.", 0, 33, shortRule, lt);
+    assertMatch("one two three four five six seven.", 0, 34, shortRule, lt);
     assertNoMatch("Eins zwei drei vier fünf sechs.", shortRule, lt);
-    assertMatch("\n\n\nEins zwei drei vier fünf sechs seven", 3, 38, shortRule, lt);
-    assertMatch("Eins zwei drei vier fünf sechs seven\n\n\n", 0, 35, shortRule, lt);
-    assertMatch("\n\n\nEins zwei drei vier fünf sechs seven\n\n\n", 3, 38, shortRule, lt);
-    assertMatch("\n\n\nEins zwei drei vier fünf sechs seven.", 3, 39, shortRule, lt);
-    assertMatch("Eins zwei drei vier fünf sechs seven.\n\n\n", 0, 36, shortRule, lt);
-    assertMatch("\n\n\nEins zwei drei vier fünf sechs seven.\n\n\n", 3, 39, shortRule, lt);
+    assertMatch("\n\n\nEins zwei drei vier fünf sechs seven", 3, 39, shortRule, lt);
+    assertMatch("Eins zwei drei vier fünf sechs seven\n\n\n", 0, 36, shortRule, lt);
+    assertMatch("\n\n\nEins zwei drei vier fünf sechs seven\n\n\n", 3, 39, shortRule, lt);
+    assertMatch("\n\n\nEins zwei drei vier fünf sechs seven.", 3, 40, shortRule, lt);
+    assertMatch("Eins zwei drei vier fünf sechs seven.\n\n\n", 0, 37, shortRule, lt);
+    assertMatch("\n\n\nEins zwei drei vier fünf sechs seven.\n\n\n", 3, 40, shortRule, lt);
 
   }
 
