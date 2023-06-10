1995e80366c0ed7ff78603b3d48d81786d0ad74c
jaumeortola <jaumeortola@gmail.com>
2023-06-10 14:40:50 +0200

    disambiguation: add chunktags

--- languagetool-language-modules/ca/src/test/java/org/languagetool/JLanguageToolTest.java
+++ languagetool-language-modules/ca/src/test/java/org/languagetool/JLanguageToolTest.java
@@ -40,6 +40,15 @@ public class JLanguageToolTest {
     matches = tool.check("Potser siga el millor");
     assertEquals(1, matches.size());
     assertEquals("POTSER_SIGUI", matches.get(0).getRule().getId());
+    
+    //ChunkTags
+ 
+    assertEquals("[<S> Ho[ho/PP3NN000] deu[deure/VMIP3S00,GV] haver[haver/VAN00000,haver/_GV_,haver/_perfet,GV] tornat[tornar/VMP00SM0,GV] a[a/SPS00,GV] fer[fer/VMN00000,fer/complement,GV].[</S>./_PUNCT,<P/>]]",
+        tool.analyzeText("Ho deu haver tornat a fer.").toString());
+
+    
+    assertEquals("[<S> Ho[ho/PP3NN000] he[haver/VAIP1S00,haver/_obligacio,GV] de[de/SPS00,GV] continuar[continuar/VMN00000,continuar/_GV_,GV] fent[fer/VMG00000,fent/_GV_,GV] així[així/RG].[</S>./_PUNCT,<P/>]]",
+        tool.analyzeText("Ho he de continuar fent així.").toString());
 
   }
 
