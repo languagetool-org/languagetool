429f88dfce1326775a94e483f78c7c2e41b35cf7
Fabian Richter <fabianrichter@fastmail.com>
2023-07-06 09:22:54 +0200

    feat(server): add new fields for group information

--- languagetool-server/src/main/java/org/languagetool/server/DatabaseAccessOpenSource.java
+++ languagetool-server/src/main/java/org/languagetool/server/DatabaseAccessOpenSource.java
@@ -140,7 +140,7 @@ class DatabaseAccessOpenSource extends DatabaseAccess {
   @Override
   UserInfoEntry getUserInfoWithApiKey(String username, String apiKey) {
     Long userId = getUserId(username, apiKey);
-    UserInfoEntry user = new UserInfoEntry(userId, username, null, null, null, null, null, null, null, null,  apiKey, null);
+    UserInfoEntry user = new UserInfoEntry(userId, username, null, null, null, null, null, null, null, null, apiKey, null, null, null);
     return user;
   }
 
