85e6c344403b59e51db943c58211f99afb40660f
Christopher Blum <christopher.blum@spellboy.com>
2023-08-28 15:35:36 +0200

    [de] improve rules

--- languagetool-language-modules/de/src/main/java/org/languagetool/rules/de/GermanSpellerRule.java
+++ languagetool-language-modules/de/src/main/java/org/languagetool/rules/de/GermanSpellerRule.java
@@ -3496,6 +3496,7 @@ public class GermanSpellerRule extends CompoundAwareHunspellRule {
       case "gestriffen": return topMatch("gestreift");
       case "Regattas": return topMatch("Regatten");
       case "Segelregattas": return topMatch("Segelregatten");
+      case "freigeschalten": return topMatch("freigeschaltet");
     }
     return Collections.emptyList();
   }
