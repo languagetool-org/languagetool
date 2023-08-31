78ed25a4c252225d87bd67ae390d0062456a9080
Pedro Goulart <50704700+p-goulart@users.noreply.github.com>
2023-08-31 16:09:59 +0200

    [pt] Improve compounding rules (#9213)
    * [pt] Remove contra-ataques from txt and add rule
    
    * [pt] Add BOTA_FORA
    
    * [pt] Add SEM_ABRIGO
    
    * [pt] Add CORTA_PAPEL
    
    * [pt] Comment out bem-parecido from compounds
    
    * [pt] Add DEUS_DARA rule
    
    * [pt] Add PT_COLOUR_HYPHENATION rule
    
     - extract all colour names into a separate TXT file;
    
     - add new Java rule for the colour compounding.

--- languagetool-language-modules/pt/src/main/java/org/languagetool/language/Portuguese.java
+++ languagetool-language-modules/pt/src/main/java/org/languagetool/language/Portuguese.java
@@ -126,6 +126,7 @@ public class Portuguese extends Language implements AutoCloseable {
             new PunctuationMarkAtParagraphEnd(messages, this, true),
             //Specific to Portuguese:
             new PostReformPortugueseCompoundRule(messages, this, userConfig),
+            new PortugueseColourHyphenationRule(messages, this, userConfig),
             new PortugueseReplaceRule(messages, this),
             new PortugueseBarbarismsRule(messages, "/pt/barbarisms.txt", this),
             //new PortugueseArchaismsRule(messages, "/pt/archaisms-pt.txt"),   // see https://github.com/languagetool-org/languagetool/issues/3095
