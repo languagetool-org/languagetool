3002d08586ac35bff1e5b9594760179f65f70b25
jaumeortola <jaumeortola@gmail.com>
2023-09-01 14:01:24 +0200

    [en] update instructions to build en_GB spelling dict

--- languagetool-language-modules/en/src/main/resources/org/languagetool/resource/en/hunspell/make_en_gb_dict.sh
+++ languagetool-language-modules/en/src/main/resources/org/languagetool/resource/en/hunspell/make_en_gb_dict.sh
@@ -2,6 +2,11 @@
 # Get dictionaries from https://github.com/marcoagpinto/aoo-mozilla-en-dict
 # Get en_us_wordlist.xml from https://github.com/mozilla-b2g/gaia/raw/master/apps/keyboard/js/imes/latin/dictionaries/en_gb_wordlist.xml
 
-unmunch en-GB.dic en-GB.aff  >  en_GB1.txt
-cat en_GB1.txt spelling_merged.txt| java -cp languagetool.jar:languagetool-dev-5.3-SNAPSHOT.jar org.languagetool.dev.archive.WordTokenizer en | sort -u > en_GB.txt
+# `unmunch` is a command from the Hunspell library: https://github.com/hunspell/hunspell
+# `languagetool-dev-6.3-SNAPSHOT-jar-with-dependencies.jar` is built with 'mvn clean compile assembly:single' in languagetool-dev
+# `languagetool.jar` is built with 'mvn clean package -DskipTests' in languagetool
+# use full paths in all Java files
+
+unmunch en-GB.dic en-GB.aff > en_GB1.txt
+cat en_GB1.txt spelling_merged.txt | java -cp languagetool.jar:languagetool-dev-6.3-SNAPSHOT-jar-with-dependencies.jar en | sort -u > en_GB.txt
 java -cp languagetool.jar org.languagetool.tools.SpellDictionaryBuilder -i en_GB.txt -info en_GB.info -freq en_gb_wordlist.xml  -o en_GB.dict
