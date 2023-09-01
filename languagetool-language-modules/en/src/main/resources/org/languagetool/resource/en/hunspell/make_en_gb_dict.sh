19b7e125f9b36e4541e5d01794e809e358c0fcc3
jaumeortola <jaumeortola@gmail.com>
2023-09-01 14:07:01 +0200

    [en] update instructions to build en_GB spelling dict

--- languagetool-language-modules/en/src/main/resources/org/languagetool/resource/en/hunspell/make_en_gb_dict.sh
+++ languagetool-language-modules/en/src/main/resources/org/languagetool/resource/en/hunspell/make_en_gb_dict.sh
@@ -8,5 +8,5 @@
 # use full paths in all Java files
 
 unmunch en-GB.dic en-GB.aff > en_GB1.txt
-cat en_GB1.txt spelling_merged.txt | java -cp languagetool.jar:languagetool-dev-6.3-SNAPSHOT-jar-with-dependencies.jar en | sort -u > en_GB.txt
-java -cp languagetool.jar org.languagetool.tools.SpellDictionaryBuilder -i en_GB.txt -info en_GB.info -freq en_gb_wordlist.xml  -o en_GB.dict
+cat en_GB1.txt spelling_merged.txt | java -cp /full/path/to/languagetool.jar:/full/path/to/languagetool-dev-6.3-SNAPSHOT-jar-with-dependencies.jar org.languagetool.dev.archive.WordTokenizer en | sort -u > en_GB.txt
+java -cp /full/path/to/languagetool.jar org.languagetool.tools.SpellDictionaryBuilder -i en_GB.txt -info en_GB.info -freq en_gb_wordlist.xml  -o en_GB.dict
