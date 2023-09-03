e99e4a210ef6d01128820d31fa91bf0297a919f7
Christopher Blum <christopher.blum@spellboy.com>
2023-09-03 17:15:26 +0200

    [en] improve rules

--- languagetool-language-modules/en/src/main/java/org/languagetool/language/English.java
+++ languagetool-language-modules/en/src/main/java/org/languagetool/language/English.java
@@ -328,6 +328,7 @@ public class English extends Language implements AutoCloseable {
       case "READ_ONLY_ACCESS_HYPHEN":   return 2;   // higher priority than agreement rules
       case "MAKE_OR_BREAK_HYPHEN":   return 2;   // higher priority than agreement rules
       case "LINKED_IN":                 return 2;   // higher prio than agreement rules
+      case "T_HE":                      return 1;   // higher prio than agreement rules
       case "I_A_M":                     return 1;   // higher prio than EN_A_VS_AN
       case "ACCESS_EXCESS":             return 1;   // higher prio than A_UNCOUNTABLE
       case "PRP_ABLE_TO":               return 1;   // higher prio than AI_HYDRA_LEO_CP_YOU.*
@@ -495,7 +496,6 @@ public class English extends Language implements AutoCloseable {
       case "MANY_NN":                   return -1;  // less priority than PUSH_UP_HYPHEN, SOME_FACULTY
       case "WE_BE":                     return -1;
       case "A_LOT_OF_NN":               return -1;
-      case "IT_VBZ":                    return -1;
       case "REPETITIONS_STYLE":         return -51;  // repetition style rules, usually with prefix REP_
       case "ORDER_OF_WORDS_WITH_NOT":   return -1;  // less prio than punctuation rules
       case "ADVERB_WORD_ORDER_10_TEMP": return 1;
@@ -590,6 +590,7 @@ public class English extends Language implements AutoCloseable {
       case "MORFOLOGIK_RULE_EN_AU":     return -10;  // more specific rules (e.g. L2 rules) have priority
       case "MD_PRP_QUESTION_MARK":   return -11;  // speller needs higher priority
       case "HE_VERB_AGR":               return -12;  // prefer other more specific rules (e.g. AI models, PRP_VBG)
+      case "IT_VBZ":                    return -12;  // prefer other more specific rules (e.g. AI models)
       case "PRP_THE":                   return -12;  // prefer other rules (e.g. AI models, I_A, PRP_JJ, IF_YOU_ANY, I_AN)
       case "PRP_JJ":                    return -12;  // prefer other rules (e.g. AI models, PRP_VBG, IT_IT and ADJECTIVE_ADVERB, PRP_ABLE, PRP_NEW, MD_IT_JJ)
       case "BE_VBP_IN":                 return -12;  // prefer over BEEN_PART_AGREEMENT but not over AI_EN_LECTOR
