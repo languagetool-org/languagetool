ff6c66a9e9add7cb4ebbf1b0ada9304b37f35865
Daniel Naber <naber@danielnaber.de>
2023-09-06 16:12:08 +0200

    fix broken links to Morphy doc

--- languagetool-dev/src/main/java/org/languagetool/dev/ExportGermanNouns.java
+++ languagetool-dev/src/main/java/org/languagetool/dev/ExportGermanNouns.java
@@ -95,7 +95,7 @@ public class ExportGermanNouns {
     System.out.println("# Export date: " + new Date());
     System.out.println("# LanguageTool: " + JLanguageTool.VERSION + " (" + JLanguageTool.BUILD_DATE + ")");
     System.out.println("# Potential German compound parts.");
-    System.out.println("# Data from Morphy (http://www.wolfganglezius.de/doku.php?id=cl:morphy)");
+    System.out.println("# Data from Morphy (https://danielnaber.de/download/wklassen.pdf)");
     System.out.println("# with extensions by LanguageTool (https://languagetool.org)");
     System.out.println("# License: Creative Commons Attribution-Share Alike 4.0, http://creativecommons.org/licenses/by-sa/4.0/");
     for (String word : words) {
