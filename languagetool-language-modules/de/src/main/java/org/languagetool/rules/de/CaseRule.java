9582e3011c3d923b64d623b54a053f3bf2fc1de3
Christopher Blum <christopher.blum@spellboy.com>
2023-09-20 13:51:43 +0200

    [de] remove detection of capitalized writing from DE_CASE (many FPs, we have XML rules who cover this better)

--- languagetool-language-modules/de/src/main/java/org/languagetool/rules/de/CaseRule.java
+++ languagetool-language-modules/de/src/main/java/org/languagetool/rules/de/CaseRule.java
@@ -1025,13 +1025,6 @@ public class CaseRule extends Rule {
         !isInvisibleSeparator(i-1, tokens) &&
         !language.getDefaultSpellingRule().isMisspelled(lcWord)) {
       if (":".equals(tokens[i - 1].getToken())) {
-        AnalyzedTokenReadings[] subarray = new AnalyzedTokenReadings[i];
-        System.arraycopy(tokens, 0, subarray, 0, i);
-        if (isVerbFollowing(i, tokens, lowercaseReadings) || getTokensWithPosTagStartingWithCount(subarray, "VER") == 0) {
-          // no error
-        } else {
-          addRuleMatch(ruleMatches, sentence, COLON_MESSAGE, tokens[i], lcWord);
-        }
         return;
       }
       addRuleMatch(ruleMatches, sentence, UPPERCASE_MESSAGE, tokens[i], lcWord);
