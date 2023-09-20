36a200c80860a7cf0bf45de76daa0486d2f67909
Christopher Blum <christopher.blum@spellboy.com>
2023-09-20 13:50:41 +0200

    [de] reduce priority of 'AI_DE_GGEC_MISSING_PUNCTUATION_PERIOD' to not interfere with spell checker

--- languagetool-language-modules/de/src/main/java/org/languagetool/language/German.java
+++ languagetool-language-modules/de/src/main/java/org/languagetool/language/German.java
@@ -535,6 +535,10 @@ public class German extends Language implements AutoCloseable {
       return -52; // prefer comma style rules and AI_DE_HYDRA_LEO_MISSING_COMMA
     }
     if (id.startsWith("AI_DE_GGEC")) {
+      if (id == "AI_DE_GGEC_MISSING_PUNCTUATION_PERIOD") {
+        // less prio than spell checker
+        return -4;
+      }
       return 1;
     }
     return super.getPriorityForId(id);
