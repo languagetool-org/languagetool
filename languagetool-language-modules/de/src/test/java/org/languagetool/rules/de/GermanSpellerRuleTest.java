e264e6e20d0b60ae7c8ae8a90dfc8a1b82679884
agneskleinhans <kleinhansagnes@gmail.com>
2023-10-11 11:54:18 +0200

    [de] improve GermanSpellerRule + add Test Cases

--- languagetool-language-modules/de/src/test/java/org/languagetool/rules/de/GermanSpellerRuleTest.java
+++ languagetool-language-modules/de/src/test/java/org/languagetool/rules/de/GermanSpellerRuleTest.java
@@ -61,6 +61,8 @@ public class GermanSpellerRuleTest {
   public void testIgnoreMisspelledWord() throws IOException {
     GermanSpellerRule rule = new GermanSpellerRule(TestTools.getMessages("de"), GERMAN_DE);
     assertTrue(rule.ignorePotentiallyMisspelledWord("Weinkühlschrank")); 
+    assertFalse(rule.ignorePotentiallyMisspelledWord("Weinskühlschrank")); 
+    assertFalse(rule.ignorePotentiallyMisspelledWord("Weinsskühlschrank"));
     assertTrue(rule.ignorePotentiallyMisspelledWord("Hundefutterschachtel")); 
     assertTrue(rule.ignorePotentiallyMisspelledWord("Leistungsversuchstest"));
     assertTrue(rule.ignorePotentiallyMisspelledWord("Nachuntersuchungstest"));  // needs extension in ExtendedGermanWordSplitter.extendedList (as of 2023-10-02)
