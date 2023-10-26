34083b85c32e983672c45b4f7a39e0e3d4e87775
SteVio89 <stefan@stevio.de>
2023-10-26 15:40:30 +0200

    add more javadoc

--- languagetool-core/src/main/java/org/languagetool/Language.java
+++ languagetool-core/src/main/java/org/languagetool/Language.java
@@ -537,6 +537,7 @@ public abstract class Language {
    * Create a shared instance of JLanguageTool to use in rules for further processing
    * Instances are shared by Language
    * As this is a shared instance, do not modify (add or remove) any rules or filters.
+   * The alternative to disabling/enabling rules is to select the desired rules from getAllActiveRules(), and run them separately with rule.match(analizedSentence).
    *
    * Do not call this in a static block or to initialize a static JLanguageTool field in rules or filters classes, this could lead to a deadlock during initialization.
    *
