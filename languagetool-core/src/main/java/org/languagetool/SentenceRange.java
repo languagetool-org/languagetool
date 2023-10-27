3b98a66ad62095268fa83c98734958d92a580bf1
Fabian Richter <fabianrichter@fastmail.com>
2023-10-27 14:13:16 +0200

    fix: avoid computing textWithMarkup multiple times

--- languagetool-core/src/main/java/org/languagetool/SentenceRange.java
+++ languagetool-core/src/main/java/org/languagetool/SentenceRange.java
@@ -41,7 +41,8 @@ public class SentenceRange {
   public static List<SentenceRange> getRangesFromSentences(AnnotatedText annotatedText, List<String> sentences) {
     List<SentenceRange> sentenceRanges = new ArrayList<>();
     int pos = 0;
-    int diff = annotatedText.getTextWithMarkup().length() - annotatedText.getPlainText().length();
+    int markupTextLength = annotatedText.getTextWithMarkup().length();
+    int diff = markupTextLength - annotatedText.getPlainText().length();
     for (String sentence : sentences) {
       if (sentence.trim().isEmpty()) {
         //No content no sentence
@@ -57,10 +58,10 @@ public class SentenceRange {
 
       int fromPosOrig = fromPos + diff;
       int toPosOrig = toPos + diff;
-      if (fromPosOrig != annotatedText.getTextWithMarkup().length()) {
+      if (fromPosOrig != markupTextLength) {
         fromPosOrig = annotatedText.getOriginalTextPositionFor(fromPos, false);
       }
-      if (toPosOrig != annotatedText.getTextWithMarkup().length()) {
+      if (toPosOrig != markupTextLength) {
         toPosOrig = annotatedText.getOriginalTextPositionFor(toPos, true);
       }
       sentenceRanges.add(new SentenceRange(fromPosOrig, toPosOrig));
