17a6151cce64b91081a70756187324190df9ef2c
Christopher Blum <christopher.blum@spellboy.com>
2023-10-30 12:30:58 +0100

    [de] improve rules

--- languagetool-language-modules/de/src/main/java/org/languagetool/rules/de/GermanSpellerRule.java
+++ languagetool-language-modules/de/src/main/java/org/languagetool/rules/de/GermanSpellerRule.java
@@ -3640,6 +3640,7 @@ public class GermanSpellerRule extends CompoundAwareHunspellRule {
       case "Aquaplanning": return topMatch("Aquaplaning");
       case "Aquaplannings": return topMatch("Aquaplanings");
       case "Kibbutz": return topMatch("Kibbuz");
+      case "Strebergarten": return topMatch("Schrebergarten");
     }
     return Collections.emptyList();
   }
