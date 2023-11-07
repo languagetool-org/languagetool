a906393e45d6ffcf8079903904ed6d53ea68d79e
jaumeortola <jaumeortola@gmail.com>
2023-11-07 20:50:33 +0100

    [ca] multiwords

--- languagetool-language-modules/ca/src/main/java/org/languagetool/language/Catalan.java
+++ languagetool-language-modules/ca/src/main/java/org/languagetool/language/Catalan.java
@@ -425,7 +425,7 @@ public class Catalan extends Language {
   }
 
   private List<String> spellerExceptions = Arrays.asList("San Juan", "Copa América", "Colección Jumex", "Banco Santander",
-  "San Marcos");
+  "San Marcos", "Santa Ana");
   @Override
   public String prepareLineForSpeller(String line) {
     String parts[] = line.split("#");
