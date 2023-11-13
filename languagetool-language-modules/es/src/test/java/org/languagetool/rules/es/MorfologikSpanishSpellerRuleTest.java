007ba2abd6a9fa6ea5242a3946bec534b7569295
jaumeortola <jaumeortola@gmail.com>
2023-11-13 15:31:25 +0100

    [es,ca] more words

--- languagetool-language-modules/es/src/test/java/org/languagetool/rules/es/MorfologikSpanishSpellerRuleTest.java
+++ languagetool-language-modules/es/src/test/java/org/languagetool/rules/es/MorfologikSpanishSpellerRuleTest.java
@@ -134,6 +134,9 @@ public class MorfologikSpanishSpellerRuleTest {
     matches = rule.match(lt.getAnalyzedSentence("sigloXXI"));
     assertEquals("siglo XXI", matches[0].getSuggestedReplacements().get(0));
 
+    matches = rule.match(lt.getAnalyzedSentence("gustarÃ\u00ADa"));
+    assertEquals("gustaría", matches[0].getSuggestedReplacements().get(0));
+
     // currencies
     matches = rule.match(lt.getAnalyzedSentence("$100"));
     assertEquals(0, matches.length);
