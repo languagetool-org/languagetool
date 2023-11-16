01c4df8b15109d042d8be788134421e4a1fbfd28
jaumeortola <jaumeortola@gmail.com>
2023-11-16 16:13:09 +0100

    [de] set priority for multitoken spelling

--- languagetool-language-modules/de/src/main/java/org/languagetool/language/German.java
+++ languagetool-language-modules/de/src/main/java/org/languagetool/language/German.java
@@ -532,6 +532,9 @@ public class German extends Language implements AutoCloseable {
     if (id.startsWith("DE_PROHIBITED_COMPOUNDS_")) {   // don't hide spelling mistakes
       return -4;
     }
+    if (id.startsWith("DE_MULTITOKEN_SPELLING")) {
+      return -2;
+    }
     if (id.startsWith("CONFUSION_RULE_")) {
       return -1;
     }
