612dc2134dac6a04dc4465076e11c9c65718ec0a
Jaume Ortolà <jaumeortola@gmail.com>
2023-12-06 10:16:40 +0100

    [pt] Migrate spelling dictionaries to Morfologik format (#9424)
    * new rule ID is `MORFOLOGIK_RULE_PT`, with sub-ids for each variant;
    * adjust rule priorities
    * add spelling tests
    * use same chars as everyone else for PT tokeniser, and add tests;
    * tokenise currency expressions and ignore spelling of currencies
    * stop spellchecking Nº666 abbreviations
    * make AO and MZ variants use PT-45 dict
    * add abbreviation and do_not_suggest logic to morfologik
    * add dialect alternations in Morfologik speller
    * disable trema rule from grammar and update diaeresis Morfologik speller message
    * update pt/br replacement txts
    * add grammar rules for elided verb forms (and speller tests)
    * remove Hunspell data
    
    ---------
    
    Co-authored-by: p-goulart <pedro.goulart@languagetool.org>

---
+++ languagetool-language-modules/pt/src/main/java/org/languagetool/rules/pt/MorfologikPortugueseSpellerRule.java
@@ -0,0 +1,275 @@
+/* LanguageTool, a natural language style checker
+ * Copyright (C) 2012 Marcin Miłkowski (http://www.languagetool.org)
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301
+ * USA
+ */
+package org.languagetool.rules.pt;
+
+import org.languagetool.*;
+import org.languagetool.rules.RuleMatch;
+import org.languagetool.rules.SuggestedReplacement;
+import org.languagetool.rules.spelling.SpellingCheckRule;
+import org.languagetool.rules.spelling.morfologik.MorfologikSpellerRule;
+import org.languagetool.synthesis.pt.PortugueseSynthesizer;
+import org.languagetool.tagging.pt.PortugueseTagger;
+import org.languagetool.tools.StringTools;
+
+import javax.annotation.Nullable;
+import java.io.IOException;
+import java.util.*;
+import java.util.function.Predicate;
+import java.util.stream.Collectors;
+
+import static org.languagetool.JLanguageTool.getDataBroker;
+
+public class MorfologikPortugueseSpellerRule extends MorfologikSpellerRule {
+
+  private final Language spellerLanguage;
+  private final String dictFilepath;
+  // Path, in pt/resources, where the list of words to be removed from the suggestion list is to be found.
+  private static final String doNotSuggestWordsFilepath = "/pt/do_not_suggest.txt";
+  // Set of words that we do not want to add to the suggestions, despite being correctly spelt. Mostly profanity.
+  private static final Set<String> doNotSuggestWords = getDoNotSuggestWords();
+  // Path, in pt/resources, where a list of abbreviations is found. These are simple abbreviations of the shape \w+\.
+  private static final String abbreviationFilepath = "/pt/abbreviations.txt";
+  private static final Set<String> abbreviations = getAbbreviations();
+  private static final String dialectAlternationsFilepath = "/pt/dialect_alternations.txt";
+  private final Map<String, String> dialectAlternationMapping;
+  private static final PortugueseTagger tagger = new PortugueseTagger();
+  private static final PortugueseSynthesizer synth = PortugueseSynthesizer.INSTANCE;
+  // Moved out of hunspell/spelling.txt for Morfologik.
+  private static final String SPELLING_FILE = "pt/spelling.txt";
+  private static final String MULTIWORDS_FILE = "pt/multiwords.txt";
+  private static final String SPELLING_IGNORE_FILE = "pt/ignore.txt";
+  private static final String SPELLING_PROHIBIT_FILE = "pt/prohibit.txt";
+
+
+  @Override
+  public String getFileName() {
+    return dictFilepath;
+  }
+
+  public static Set<String> getDoNotSuggestWords() {
+    return getWordSetFromResources(doNotSuggestWordsFilepath);
+  }
+
+  public static Set<String> getAbbreviations() {
+    return getWordSetFromResources(abbreviationFilepath);
+  }
+
+  public static Set<String> getWordSetFromResources(String filepath) {
+    return new HashSet<String>(getDataBroker().getFromResourceDirAsLines(filepath));
+  }
+
+  @Override
+  public String getIgnoreFileName() {
+    return SPELLING_IGNORE_FILE;
+  }
+
+  @Override
+  public String getProhibitFileName() {
+    return SPELLING_PROHIBIT_FILE;
+  }
+
+  @Override
+  public String getId() {
+    return "MORFOLOGIK_RULE_"
+      + language.getShortCodeWithCountryAndVariant().replace("-", "_").toUpperCase();
+  }
+
+  protected String getIdForDialectIssue() {
+    return getId() + "_DIALECT";
+  }
+
+  // TODO: document this, as it's about to get messy
+  @Nullable
+  protected String dialectAlternative(String word) throws IOException {
+    // Naive check: word is identical to lemma present on the TXT list
+    String lemmaCheckResult = dialectAlternationMapping.get(word.toLowerCase());
+    if (lemmaCheckResult != null) {
+      return lemmaCheckResult;
+    }
+    List<String> wordAsList = Collections.singletonList(word);
+    List<AnalyzedTokenReadings> readings = tagger.tag(wordAsList);
+    // Annoying check: the word's lemma exists on the TXT list
+    for (AnalyzedTokenReadings reading : readings) {
+      for (AnalyzedToken token : reading.getReadings()) {
+        String lemma = token.getLemma();
+        String tag = token.getPOSTag();
+        if (tag != null && dialectAlternationMapping.containsKey(lemma)) {
+          String candidate = dialectAlternationMapping.get(lemma);
+          Predicate<String> tagPredicate = tagStr -> tagStr.contentEquals(tag);
+          String[] forms = synth.synthesizeForPosTags(candidate, tagPredicate);
+          // the assumption is these words are almost identical, their tagging also ought to be; so even if it returns
+          // many POS tags, the synthesiser should always yield the same form for the same set of tags
+          if (forms.length > 0) {
+            return forms[0];
+          }
+        }
+      }
+    }
+    return null;
+  }
+
+  @Nullable
+  private String checkDiaeresis(String word) {
+    if (word.indexOf('ü') >= 0) {
+      return word.replace('ü', 'u');
+    }
+    return null;
+  }
+
+  @Nullable
+  private String checkEuropeanStyle1PLPastTense(String word) {
+    if (Objects.equals(spellerLanguage.getShortCodeWithCountryAndVariant(), "pt-BR") && word.endsWith("ámos")) {
+      return word.replace('á', 'a');
+    }
+    return null;
+  }
+
+  private Map<String,String> getDialectAlternationMapping() {
+    Map<String,String> wordMap = new HashMap<>();
+    List<String> lines = JLanguageTool.getDataBroker().getFromResourceDirAsLines(dialectAlternationsFilepath);
+    String fullLanguageCode = spellerLanguage.getShortCodeWithCountryAndVariant();
+    int column = -1;
+    switch(fullLanguageCode) {
+      case "pt-BR": column = 1; break;  // hash from pt-PT to pt-BR
+      case "pt-PT": column = 0; break;  // hash from pt-BR to pt-PT
+    }
+    if (column == -1) { // not supported for pre-45 dictionaries
+      return Collections.emptyMap();
+    }
+    for (String line : lines) {
+      line = line.trim();
+      if (line.isEmpty() || line.startsWith("#")) {
+        continue;
+      }
+      String[] parsedLine = line.split("=");
+      if (parsedLine.length != 2) {
+        throw new RuntimeException("Unexpected format in " + dialectAlternationsFilepath + ": " + line +
+          " - expected two parts delimited by '='");
+      }
+      wordMap.put(parsedLine[column].toLowerCase(), parsedLine[column == 1 ? 0 : 1]);
+    }
+    return wordMap;
+  }
+
+  public MorfologikPortugueseSpellerRule(ResourceBundle messages, Language language, UserConfig userConfig,
+                                      List<Language> altLanguages) throws IOException {
+    super(messages, language, userConfig, altLanguages);
+    // the tagger tags pt-PT and pt-BR words all the same, as it should, but they're still incorrect if they belong
+    // to the wrong dialect, commenting this out
+    // this.setIgnoreTaggedWords();
+    if (language.getShortCodeWithCountryAndVariant().equals("pt")) {
+      spellerLanguage = language.getDefaultLanguageVariant();
+    } else {
+      spellerLanguage = language;
+    }
+    dictFilepath = "/pt/spelling/" + getDictFilename() + JLanguageTool.DICTIONARY_FILENAME_EXTENSION;
+    dialectAlternationMapping = getDialectAlternationMapping();
+  }
+
+  @Override
+  protected List<SuggestedReplacement> filterNoSuggestWords(List<SuggestedReplacement> suggestedReplacements) {
+    return suggestedReplacements.stream().filter(
+      suggestedReplacement -> !doNotSuggestWords.contains(
+        suggestedReplacement.getReplacement().toLowerCase()
+      )).collect(Collectors.toList());
+  }
+
+  @Override
+  protected List<SuggestedReplacement> getAdditionalTopSuggestions(List<SuggestedReplacement> suggestions, String word)
+    throws IOException {
+    List<String> suggestionsList = suggestions.stream().map(SuggestedReplacement::getReplacement)
+      .collect(Collectors.toList());
+    return SuggestedReplacement.convert(getAdditionalTopSuggestionsString(suggestionsList, word));
+  }
+
+  private List<String> getAdditionalTopSuggestionsString(List<String> suggestions, String word) throws IOException {
+    if (isAbbreviation(word)) {
+      return Collections.singletonList(word + ".");
+    }
+    return Collections.emptyList();
+  }
+
+  // Check if the word we're checking is in our list of abbreviations.
+  protected boolean isAbbreviation(String word) {
+    // regular case (since we do have some abbreviations with weird casing) as well as downcased
+    return abbreviations.contains(word + ".") || abbreviations.contains(word.toLowerCase() + ".");
+  }
+
+  private String getDictFilename() {
+    String dictFilename = "pt-BR";  // default dict is pt-BR with 1990 spelling
+    String fullLanguageCode = this.spellerLanguage.getShortCodeWithCountryAndVariant();
+    switch (fullLanguageCode) {
+      case "pt-BR":               dictFilename = "pt-BR";    break;
+      case "pt-PT":               dictFilename = "pt-PT-90"; break;
+      case "pt-AO": case "pt-MZ": dictFilename = "pt-PT-45"; break;
+    }
+    return dictFilename;
+  }
+
+  @Override
+  public List<String> getAdditionalSpellingFileNames() {
+    return Arrays.asList(SpellingCheckRule.GLOBAL_SPELLING_FILE, SPELLING_FILE, MULTIWORDS_FILE);
+  }
+
+  private void replaceFormsOfFirstMatch(String message, AnalyzedSentence sentence, List<RuleMatch> ruleMatches,
+                                        String suggestion, boolean dialectIssue) {
+    // recreating match, might overwrite information by SuggestionsRanker;
+    // this has precedence
+    RuleMatch oldMatch = ruleMatches.get(0);
+    RuleMatch newMatch = new RuleMatch(this, sentence, oldMatch.getFromPos(), oldMatch.getToPos(), message);
+    newMatch.setType(oldMatch.getType());
+    if (dialectIssue) {
+      newMatch.setSpecificRuleId(getIdForDialectIssue());
+    }
+    SuggestedReplacement sugg = new SuggestedReplacement(suggestion);
+    sugg.setShortDescription(language.getName());
+    newMatch.setSuggestedReplacementObjects(Collections.singletonList(sugg));
+    ruleMatches.set(0, newMatch);
+  }
+
+  @Override
+  public List<RuleMatch> getRuleMatches(String word, int startPos, AnalyzedSentence sentence,
+                                        List<RuleMatch> ruleMatchesSoFar, int idx,
+                                        AnalyzedTokenReadings[] tokens) throws IOException {
+    List<RuleMatch> ruleMatches = super.getRuleMatches(word, startPos, sentence, ruleMatchesSoFar, idx, tokens);
+    if (!ruleMatches.isEmpty()) {
+      String wordWithBrazilianStylePastTense = checkEuropeanStyle1PLPastTense(word);
+      if (wordWithBrazilianStylePastTense != null) {
+        String message = "No Brasil, o pretérito perfeito da primeira pessoa do plural escreve-se sem acento.";
+        replaceFormsOfFirstMatch(message, sentence, ruleMatches, wordWithBrazilianStylePastTense, true);
+      }
+      String wordWithoutDiaeresis = checkDiaeresis(word);
+      if (wordWithoutDiaeresis != null) {
+        String message = "No mais recente acordo ortográfico, não se usa mais o trema no português.";
+        replaceFormsOfFirstMatch(message, sentence, ruleMatches, wordWithoutDiaeresis, false);
+      }
+      String dialectAlternative = this.dialectAlternative(word);
+      if (dialectAlternative != null) {
+        String otherVariant = "europeu";
+        if (Objects.equals(spellerLanguage.getShortCodeWithCountryAndVariant(), "pt-PT")) {
+          otherVariant = "brasileiro";
+        }
+        String message = "Possível erro de ortografia: esta é a grafia utilizada no português " + otherVariant + ".";
+        String suggestion = StringTools.startsWithUppercase(word) ? StringTools.uppercaseFirstChar(dialectAlternative) : dialectAlternative;
+        replaceFormsOfFirstMatch(message, sentence, ruleMatches, suggestion, true);
+      }
+    }
+    return ruleMatches;
+  }
+}
--- languagetool-language-modules/pt/src/test/java/org/languagetool/tokenizers/pt/PortugueseWordTokenizerTest.java
+++ languagetool-language-modules/pt/src/test/java/org/languagetool/tokenizers/pt/PortugueseWordTokenizerTest.java
@@ -20,8 +20,10 @@
 package org.languagetool.tokenizers.pt;
 
 import org.junit.Test;
-import java.util.List;
 
+import java.sql.Struct;
+
+import static org.junit.Assert.assertArrayEquals;
 import static org.junit.Assert.assertEquals;
 
 /**
@@ -30,38 +32,133 @@ import static org.junit.Assert.assertEquals;
  */
 
 public class PortugueseWordTokenizerTest {
+  final PortugueseWordTokenizer wordTokenizer = new PortugueseWordTokenizer();
+
+  private void testTokenise(String sentence, String[] tokens) {
+    assertArrayEquals(tokens, wordTokenizer.tokenize(sentence).toArray());
+  }
 
   @Test
-  public void testTokenize() {
-    final PortugueseWordTokenizer wordTokenizer = new PortugueseWordTokenizer();
-    final List <String> tokens = wordTokenizer.tokenize("Isto é\u00A0um teste");
-    assertEquals(tokens.size(), 7);
-    assertEquals("[Isto,  , é, \u00A0, um,  , teste]", tokens.toString());
-    final List <String> tokens2 = wordTokenizer.tokenize("Isto\rquebra");
-    assertEquals(3, tokens2.size());
-    assertEquals("[Isto, \r, quebra]", tokens2.toString());
-    //hyphen with no whitespace
-    final List <String> tokens3 = wordTokenizer.tokenize("Agora isto sim é-mesmo!-um teste.");
-    assertEquals(tokens3.size(), 15);
-    assertEquals("[Agora,  , isto,  , sim,  , é, -, mesmo, !, -, um,  , teste, .]", tokens3.toString());
-    //hyphen at the end of the word
-    final List <String> tokens4 = wordTokenizer.tokenize("Agora isto é- realmente!- um teste.");
-    assertEquals(tokens4.size(), 15);
-    assertEquals("[Agora,  , isto,  , é, -,  , realmente, !, -,  , um,  , teste, .]", tokens4.toString());
-    //mdash
-    final List <String> tokens5 = wordTokenizer.tokenize("Agora isto é—realmente!—um teste.");
-    assertEquals(tokens5.size(), 13);
-    assertEquals("[Agora,  , isto,  , é, —, realmente, !, —, um,  , teste, .]", tokens5.toString());
-    
-    final List <String> tokens6 = wordTokenizer.tokenize("sex-appeal");
-    assertEquals(tokens6.size(), 1);
-    assertEquals("[sex-appeal]", tokens6.toString());
-    final List<String> tokens7 = wordTokenizer.tokenize("Aix-en-Provence");
-    assertEquals(tokens7.size(), 1);
-    assertEquals("[Aix-en-Provence]", tokens7.toString());
-    final List<String> tokens8 = wordTokenizer.tokenize("Montemor-o-Novo");
-    assertEquals(tokens8.size(), 1);
-    assertEquals("[Montemor-o-Novo]", tokens8.toString());
-    
+  public void testTokeniseBreakChars() {
+    testTokenise("Isto é\u00A0um teste", new String[]{"Isto", " ", "é", " ", "um", " ", "teste"});
+    testTokenise("Isto\rquebra", new String[]{"Isto", "\r", "quebra"});
+  }
+
+  @Test
+  public void testTokeniseHyphenNoWhiteSpace() {
+    testTokenise("Agora isto sim é-mesmo!-um teste.",
+      new String[]{"Agora", " ", "isto", " ", "sim", " ", "é", "-", "mesmo", "!", "-", "um", " ", "teste", "."});
+  }
+
+  @Test
+  public void testTokeniseWordFinalHyphen() {
+    testTokenise("Agora isto é- realmente!- um teste.",
+      new String[]{"Agora", " ", "isto", " ", "é", "-", " ", "realmente", "!", "-", " ", "um", " ", "teste", "."});
+  }
+
+  @Test
+  public void testTokeniseMDash() {
+    testTokenise("Agora isto é—realmente!—um teste.",
+      new String[]{"Agora", " ", "isto", " ", "é", "—", "realmente", "!", "—", "um", " ", "teste", "."});
+  }
+
+  @Test
+  public void testTokeniseHyphenatedSingleToken() {
+    testTokenise("sex-appeal", new String[]{"sex-appeal"});
+    testTokenise("Aix-en-Provence", new String[]{"Aix-en-Provence"});
+    testTokenise("Montemor-o-Novo", new String[]{"Montemor-o-Novo"});
+  }
+
+  @Test
+  public void testTokeniseHyphenatedSplit() {
+    testTokenise("Paris-São Paulo", new String[]{"Paris", "-", "São", " ", "Paulo"});
+    // this word exists in the speller but not the tagger dict; this may become a problem
+    testTokenise("Sem-Peixe", new String[]{"Sem", "-", "Peixe"});
+  }
+
+  @Test
+  public void testTokeniseHyphenatedClitics() {
+    testTokenise("diz-se", new String[]{"diz", "-", "se"});
+  }
+
+  @Test
+  public void testTokeniseMesoclisis() {
+    testTokenise("fá-lo-á", new String[]{"fá", "-", "lo", "-", "á"});
+    testTokenise("dir-lhe-ia", new String[]{"dir", "-", "lhe", "-", "ia"});
+    testTokenise("banhar-nos-emos", new String[]{"banhar", "-", "nos", "-", "emos"});
+  }
+
+  @Test
+  public void testTokeniseApostrophe() {
+    // is
+    testTokenise("d'água", new String[]{"d", "'", "água"});
+    testTokenise("d’água", new String[]{"d", "’", "água"});
+    // should be
+    // testTokenise("d’água", new String[]{"d’", "água"});
+    // testTokenise("d'água", new String[]{"d'", "água"});
+  }
+
+  @Test
+    public void testTokeniseHashtags() {
+    // Twitter and whatnot; same as English
+    testTokenise("#CantadasDoBem", new String[]{"#", "CantadasDoBem"});
+  }
+
+  @Test
+  public void testDoNotTokeniseUserMentions() {
+    // Twitter and whatnot; same as English
+    testTokenise("@user", new String[]{"@user"});
+  }
+
+  @Test
+  public void testTokeniseCurrency() {
+    testTokenise("R$45,00", new String[]{"R$", "45,00"});
+    testTokenise("5£", new String[]{"5", "£"});
+    testTokenise("US$249,99", new String[]{"US$", "249,99"});
+    testTokenise("€2.000,00", new String[]{"€", "2.000,00"});
+  }
+
+  @Test
+  public void testTokeniseNumberAbbreviation() {
+    testTokenise("Nº666", new String[]{"Nº666"});  // superscript 'o'
+    testTokenise("N°666", new String[]{"N°666"});  // degree symbol
+    testTokenise("Nº 420", new String[]{"Nº", " ", "420"});
+    testTokenise("N.º69", new String[]{"N", ".", "º69"});  // the '.' char splits it
+    testTokenise("N.º 80085", new String[]{"N", ".", "º", " ", "80085"});  // the '.' char splits it
+  }
+
+  @Test
+  public void testDoNotTokeniseOrdinalSuperscript() {
+    testTokenise("6º", new String[]{"6º"});  // superscript 'o'
+    testTokenise("100°", new String[]{"100°"});  // degree symbol
+    testTokenise("21ª", new String[]{"21ª"});
+  }
+
+  @Test
+  public void testTokeniseExponent() {
+    testTokenise("km²", new String[]{"km", "²"});
+  }
+
+  @Test public void testTokeniseCopyrightAndSimilarSymbols() {
+    testTokenise("Copyright©", new String[]{"Copyright", "©"});
+    testTokenise("Bacana®", new String[]{"Bacana", "®"});
+    testTokenise("Legal™", new String[]{"Legal", "™"});
+  }
+
+  @Test
+  public void testDoNotTokeniseEmoji() {
+    testTokenise("☺☺☺Só", new String[]{"☺☺☺Só"});
+  }
+
+  @Test
+  public void testDoNotTokeniseModifierDiacritics() {
+    // the tilde here is a unicode modifier char; normally, the unicode a-tilde (ã) is used
+    testTokenise("Não", new String[]{"Não"});
+  }
+
+  @Test
+  public void testTokeniseRarePunctuation() {
+    testTokenise("⌈Herói⌋", new String[]{"⌈", "Herói", "⌋"});
+    testTokenise("″Santo Antônio do Manga″", new String[]{"″", "Santo", " ", "Antônio", " ", "do", " ", "Manga", "″"});
   }
 }
