35f8e48b9693db2c01196b775ba5c76eacc43401
jaumeortola <jaumeortola@gmail.com>
2023-12-15 18:44:11 +0100

    [pt] multitoken spelling suggestions

--- languagetool-language-modules/pt/src/main/java/org/languagetool/language/Portuguese.java
+++ languagetool-language-modules/pt/src/main/java/org/languagetool/language/Portuguese.java
@@ -25,6 +25,7 @@ import org.languagetool.languagemodel.LanguageModel;
 import org.languagetool.rules.*;
 import org.languagetool.rules.pt.*;
 import org.languagetool.rules.spelling.SpellingCheckRule;
+import org.languagetool.rules.spelling.multitoken.MultitokenSpeller;
 import org.languagetool.synthesis.Synthesizer;
 import org.languagetool.synthesis.pt.PortugueseSynthesizer;
 import org.languagetool.tagging.Tagger;
@@ -294,5 +295,28 @@ public class Portuguese extends Language implements AutoCloseable {
       return -51;
     }
     return super.getPriorityForId(id);
+  }
+
+  @Override
+  public String prepareLineForSpeller(String line) {
+    String[] parts = line.split("#");
+    if (parts.length == 0) {
+      return line;
+    }
+    String[] formTag = parts[0].split("[\t;]");
+    String form = formTag[0].trim();
+    if (formTag.length > 1) {
+      String tag = formTag[1].trim();
+      if (tag.startsWith("N") || tag.equals("_Latin_")) {
+        return form;
+      } else {
+        return "";
+      }
+    }
+    return line;
+  }
+
+  public MultitokenSpeller getMultitokenSpeller() {
+    return PortugueseMultitokenSpeller.INSTANCE;
   }
 }
