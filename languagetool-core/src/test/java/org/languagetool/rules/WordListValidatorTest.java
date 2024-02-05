4412f5dc46a5402478f77de31693c88f3b448fa8
Jaume Ortolà <jaumeortola@gmail.com>
2024-02-05 11:51:22 +0100

    [ca] new rule FALLADA_FALLIDA

--- languagetool-core/src/test/java/org/languagetool/rules/WordListValidatorTest.java
+++ languagetool-core/src/test/java/org/languagetool/rules/WordListValidatorTest.java
@@ -45,6 +45,8 @@ public class WordListValidatorTest {
   // Words that are valid but with special characters so that we don't want to
   // allow them in general:
   private static final Set<String> VALID_WORDS = new HashSet<>(Arrays.asList(
+          "Şanlıurfa",
+          "Žižek",
           "Háček",
           "Varaždin/S",
           "Będzin",
