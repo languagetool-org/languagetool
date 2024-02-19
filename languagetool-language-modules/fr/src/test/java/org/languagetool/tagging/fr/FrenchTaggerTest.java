e4ccdac64a9e5c69d032e23eae146d9e2f3b22ce
jaumeortola <jaumeortola@gmail.com>
2024-02-19 11:50:46 +0100

    [fr] fix tokenizer: -le/-Le

--- languagetool-language-modules/fr/src/test/java/org/languagetool/tagging/fr/FrenchTaggerTest.java
+++ languagetool-language-modules/fr/src/test/java/org/languagetool/tagging/fr/FrenchTaggerTest.java
@@ -85,6 +85,11 @@ public class FrenchTaggerTest {
     TestTools.myAssert("Aéro-club","Aéro-club/[aéro-club]N m s", tokenizer, tagger);
     TestTools.myAssert("Aéro-Club","Aéro-Club/[aéro-club]N m s", tokenizer, tagger);
     TestTools.myAssert("AÉRO-CLUB","AÉRO-CLUB/[aéro-club]N m s", tokenizer, tagger);
+
+    TestTools.myAssert("ex-Le Pen","ex/[ex]A|ex/[ex]N e sp -- Le/[le]D m s|Le/[le]R pers obj 3 m s -- Pen/[null]null", tokenizer, tagger);
+    TestTools.myAssert("Saint-Côme-Le Mesnil","Saint-Côme/[Saint-Côme]Z e sp -- Le/[le]D m s|Le/[le]R pers obj 3 m s -- Mesnil/[null]null", tokenizer, tagger);
+    TestTools.myAssert("Dieppe-Le Tréport","Dieppe/[Dieppe]Z e sp|Dieppe/[Dieppe]Z e sp -- Le/[le]D m s|Le/[le]R pers obj 3 m s -- Tréport/[null]null", tokenizer, tagger);
+
   }
 
 }
