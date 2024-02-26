511ef42b080eb25b56521b827968e983f4139858
Luisa-LT <luisa.rettinger@languagetool.org>
2024-02-26 14:25:54 +0100

    [de] changed prio + moved ABK + modified gGEC APs + added words

--- languagetool-language-modules/de/src/main/java/org/languagetool/language/German.java
+++ languagetool-language-modules/de/src/main/java/org/languagetool/language/German.java
@@ -435,6 +435,7 @@ public class German extends Language implements AutoCloseable {
     id2prio.put("SCHOENE_WETTER", -2); // prefer more specific rules that offer a suggestion (e.g. DE_AGREEMENT)
     id2prio.put("MEIN_KLEIN_HAUS", -2); // prefer more specific rules that offer a suggestion (e.g. DIES_BEZÜGLICH)
     id2prio.put("UNPAIRED_BRACKETS", -2);
+    id2prio.put("DE_UNPAIRED_QUOTES", -2); // less prio than FALSCHES_ANFUEHRUNGSZEICHEN
     id2prio.put("ICH_GLAUBE_FUER_EUCH", -2); // prefer agreement rules
     id2prio.put("OBJECT_AGREEMENT", -2); // less prio than DE_AGREEMENT
     id2prio.put("ICH_INF_PREMIUM", -2); // prefer more specific rules that offer a suggestion (e.g. SUBJECT_VERB_AGREEMENT)
