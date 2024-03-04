12c921050e88edf5aefe209437708bac6961d300
esbardu <esbardu@softastur.org>
2024-03-04 01:24:13 +0100

    [ast] ...

--- languagetool-language-modules/ast/src/test/java/org/languagetool/language/rules/ast/MorfologikAsturianSpellerRuleTest.java
+++ languagetool-language-modules/ast/src/test/java/org/languagetool/language/rules/ast/MorfologikAsturianSpellerRuleTest.java
@@ -34,7 +34,7 @@ public class MorfologikAsturianSpellerRuleTest {
   @Test
   public void testMorfologikSpeller() throws IOException {
     Asturian language = new Asturian();
-    MorfologikAsturianSpellerRule rule = new MorfologikAsturianSpellerRule(TestTools.getMessages("en"), language, null, Collections.emptyList());
+    MorfologikAsturianSpellerRule rule = new MorfologikAsturianSpellerRule(TestTools.getMessages("ast"), language, null, Collections.emptyList());
     JLanguageTool lt = new JLanguageTool(language);
 
     assertEquals(0, rule.match(lt.getAnalyzedSentence("¿Tas avezáu colos correutores gramaticales?")).length);
