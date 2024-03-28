7f7a9d48f59cd73ec6717c668044be1847fbad5d
Daniel Naber <naber@danielnaber.de>
2024-03-28 11:52:29 +0100

    prepare v6.4: don't publish languagetool-http-client anymore, it has too many dependencies as isn't useful with those (it basically has all of LT as a dependency)

--- languagetool-standalone/scripts/stage-artifacts.sh
+++ languagetool-standalone/scripts/stage-artifacts.sh
@@ -27,8 +27,8 @@ else
 fi
 
 # The list of projects was copied from the top-level pom.xml:
-# Note that we don't stage: languagetool-office-extension, languagetool-standalone, languagetool-commandline
-for PROJECT in languagetool-core en fr de pl ca it br nl pt ru ast be zh da eo gl el ja km ro sk sl es sv tl uk fa ta ga ar de-DE-x-simple-language all languagetool-gui-commons languagetool-server languagetool-http-client
+# Note that we don't stage: languagetool-office-extension, languagetool-standalone, languagetool-commandline, languagetool-wikipedia, languagetool-http-client
+for PROJECT in languagetool-core en fr de pl ca it br nl pt ru ast be zh da eo gl el ja km ro sk sl es sv tl uk fa ta ga ar de-DE-x-simple-language all languagetool-gui-commons languagetool-server
 do
 
     if [ -d "../../languagetool-language-modules/$PROJECT" ]
