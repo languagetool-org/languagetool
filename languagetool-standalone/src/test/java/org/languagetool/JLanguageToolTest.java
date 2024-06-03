a71b17904a6c1af1e8e2089b3c857ecd8cbc3060
jaumeortola <jaumeortola@gmail.com>
2024-06-03 15:36:04 +0200

    [de] fix test

--- languagetool-standalone/src/test/java/org/languagetool/JLanguageToolTest.java
+++ languagetool-standalone/src/test/java/org/languagetool/JLanguageToolTest.java
@@ -214,7 +214,8 @@ public class JLanguageToolTest {
     assertThat(cache.hitCount(), is(2L));
 
     JLanguageTool ltGerman = new JLanguageTool(new GermanyGerman(), null, cache);
-    assertTrue(ltGerman.check("This is an test").size() >= 3);
+    // only one error because English words are ignored
+    assertTrue(ltGerman.check("This is an test.").size() >= 1);
     assertThat(cache.hitCount(), is(2L));
 
     assertThat(ltEnglish.check("This is an test").size(), is(1));
