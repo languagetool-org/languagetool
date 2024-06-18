b7d15db46db7a2d7215e947da294aedff55fef66
Daniel Naber <naber@danielnaber.de>
2024-06-18 17:46:50 +0200

    [de] adding words

--- languagetool-language-modules/de/src/main/java/org/languagetool/tokenizers/de/GermanCompoundTokenizer.java
+++ languagetool-language-modules/de/src/main/java/org/languagetool/tokenizers/de/GermanCompoundTokenizer.java
@@ -72,6 +72,9 @@ public class GermanCompoundTokenizer implements Tokenizer {
       // Add compound parts here so we don't need to update JWordSplitter for every missing word we find.
       // Note: adding words, especially short ones, can also cause incorrect splits. E.g. if "sport"
       // is in the list and you add "tran", without "transport" being in the list, it would split "transport".
+      words.add("pensionisten");
+      words.add("graduierten");
+      words.add("beladung");
       words.add("controller");
       words.add("resilienz");
       words.add("mitführ");
