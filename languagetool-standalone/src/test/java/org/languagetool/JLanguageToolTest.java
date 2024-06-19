e08db9e99f66aac906e5042b79b84ac6ecc66975
St-ac-y <stacy.lattimore@languagetool.org>
2024-06-19 10:28:36 +0200

    [de] ignore english words - add working more java tests

--- languagetool-standalone/src/test/java/org/languagetool/JLanguageToolTest.java
+++ languagetool-standalone/src/test/java/org/languagetool/JLanguageToolTest.java
@@ -566,8 +566,13 @@ public class JLanguageToolTest {
 
     List<RuleMatch> matches = lt.check("Ich weiß nicht, ob today passt.");
     assertEquals(1, matches.size());
+
     matches = lt.check("Komm schon, let us do this!");
     assertEquals(0, matches.size());
+
+    matches = lt.check("let us do this!");
+    assertEquals(1, matches.size());
+    assertEquals("[Let us do this!]", matches.get(0).getSuggestedReplacements().toString());
   }
 
   @Test
