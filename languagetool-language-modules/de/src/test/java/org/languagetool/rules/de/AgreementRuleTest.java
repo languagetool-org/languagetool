597ba00bb7ae32781f240fb8b9a14a4845b67f42
Christopher Blum <christopher.blum@spellboy.com>
2024-08-15 09:21:46 +0200

    [de] improve rules

--- languagetool-language-modules/de/src/test/java/org/languagetool/rules/de/AgreementRuleTest.java
+++ languagetool-language-modules/de/src/test/java/org/languagetool/rules/de/AgreementRuleTest.java
@@ -660,6 +660,7 @@ public class AgreementRuleTest {
     assertBad("Die Idee des Werbekaufmann kam gut an.");
     assertGood("Die Idee des Werbekaufmanns kam gut an.");
     assertGood("Solch harte Worte!");
+    assertGood("Ich habe es an unseren amerikanischen Commercial Lawyer geschickt.");
     assertGood("Dieser eine Schritt hat gedauert.");
     assertGood("Es besteht durchaus die Gefahr, dass die Telekom eine solch starke monopolistische Stellung auf dem Markt hat, dass sich kaum Wettbewerb entfalten kann.");
     assertBad("Ich habe keine Zeit für solche kleinlichen Belangen.");
