74bd500ede63bcb6c20497d8d11b6908d6c0f754
wsw-stack <1770115879@qq.com>
2025-02-10 20:04:18 -0800

    add spell checking and pos tagging FSM tests

---
+++ languagetool-language-modules/en/src/test/java/org/languagetool/PosTaggingWorkflow.java
@@ -0,0 +1,128 @@
+package org.languagetool;
+
+import org.junit.Test;
+import org.languagetool.language.AmericanEnglish;
+
+import java.io.IOException;
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+public class PosTaggingWorkflow {
+  // List<AnalyzedTokenReadings> aTokens = language.getTagger().tag(tokens);
+  // initialize language tool
+  private final Language language = new AmericanEnglish();
+
+  @Test
+  public void testTagging01() throws IOException {
+    // empty space should get a "null" pos tag
+    String sentence = " ";
+    List<String> tokens = language.getWordTokenizer().tokenize(sentence);
+    List<AnalyzedTokenReadings> aTokens = language.getTagger().tag(tokens);
+    assertEquals(" /null", aTokens.get(0).getAnalyzedToken(0).toString());
+  }
+
+  @Test
+  public void testTagging02() throws IOException {
+    String sentence = "dog";
+    List<String> tokens = language.getWordTokenizer().tokenize(sentence);
+    List<AnalyzedTokenReadings> aTokens = language.getTagger().tag(tokens);
+    AnalyzedTokenReadings token = aTokens.get(0);
+
+    boolean isSuccessful = false;
+    for (AnalyzedToken analyzedToken : token.getReadings()) {
+      if("NN".equals(analyzedToken.getPOSTag())) { // should be at least one referring to noun
+        isSuccessful = true;
+        break;
+      }
+    }
+    assertTrue(isSuccessful);
+  }
+
+  @Test
+  public void testTagging03() throws IOException {
+    String sentence = "fight";
+    List<String> tokens = language.getWordTokenizer().tokenize(sentence);
+    List<AnalyzedTokenReadings> aTokens = language.getTagger().tag(tokens);
+    AnalyzedTokenReadings token = aTokens.get(0);
+
+    boolean isSuccessful = false;
+    for (AnalyzedToken analyzedToken : token.getReadings()) {
+      if("VB".equals(analyzedToken.getPOSTag())) { // should be at least one referring to verb
+        isSuccessful = true;
+        break;
+      }
+    }
+    assertTrue(isSuccessful);
+  }
+
+  @Test
+  public void testTagging04() throws IOException {
+    String sentence = "is";
+    List<String> tokens = language.getWordTokenizer().tokenize(sentence);
+    List<AnalyzedTokenReadings> aTokens = language.getTagger().tag(tokens);
+    AnalyzedTokenReadings token = aTokens.get(0);
+
+    boolean isSuccessful = false;
+    for (AnalyzedToken analyzedToken : token.getReadings()) {
+      if("VBZ".equals(analyzedToken.getPOSTag())) { // should be at least one referring to Third-Person Singular verb
+        isSuccessful = true;
+        break;
+      }
+    }
+    assertTrue(isSuccessful);
+  }
+
+  @Test
+  public void testTagging05() throws IOException {
+    String sentence = "good";
+    List<String> tokens = language.getWordTokenizer().tokenize(sentence);
+    List<AnalyzedTokenReadings> aTokens = language.getTagger().tag(tokens);
+    AnalyzedTokenReadings token = aTokens.get(0);
+
+    boolean isSuccessful = false;
+    for (AnalyzedToken analyzedToken : token.getReadings()) {
+      if("JJ".equals(analyzedToken.getPOSTag())) { // should be at least one referring to adjective
+        isSuccessful = true;
+        break;
+      }
+    }
+    assertTrue(isSuccessful);
+  }
+
+  @Test
+  public void testTagging06() throws IOException {
+    String sentence = "France";
+    List<String> tokens = language.getWordTokenizer().tokenize(sentence);
+    List<AnalyzedTokenReadings> aTokens = language.getTagger().tag(tokens);
+    AnalyzedTokenReadings token = aTokens.get(0);
+
+    boolean isSuccessful = false;
+    for (AnalyzedToken analyzedToken : token.getReadings()) {
+      if("NNP".equals(analyzedToken.getPOSTag())) { // should be considered a proper noun
+        isSuccessful = true;
+        break;
+      }
+    }
+    assertTrue(isSuccessful);
+  }
+
+  @Test
+  public void testTagging07() throws IOException {
+    String sentence = "between";
+    List<String> tokens = language.getWordTokenizer().tokenize(sentence);
+    List<AnalyzedTokenReadings> aTokens = language.getTagger().tag(tokens);
+    AnalyzedTokenReadings token = aTokens.get(0);
+
+    boolean isSuccessful = false;
+    for (AnalyzedToken analyzedToken : token.getReadings()) {
+      System.out.println(analyzedToken.getPOSTag());
+      if("IN".equals(analyzedToken.getPOSTag())) { // should be considered a preposition
+        isSuccessful = true;
+        break;
+      }
+    }
+    assertTrue(isSuccessful);
+  }
+}
--- languagetool-language-modules/en/src/test/java/org/languagetool/SpellCheckingWorkFlowTest.java
+++ languagetool-language-modules/en/src/test/java/org/languagetool/SpellCheckingWorkFlowTest.java
@@ -13,11 +13,37 @@ public class SpellCheckingWorkFlowTest {
   private final JLanguageTool lt = new JLanguageTool(Languages.getLanguageForShortCode("en-US"));
 
   @Test
-  public void testSpelling() throws IOException {
+  public void testSpelling1() throws IOException {
     List<RuleMatch> matches = lt.check("Aple");
     System.out.println(matches.get(0).getSentence());
     System.out.println(matches.get(0));
     System.out.println(matches.get(0).getSuggestedReplacements().toString());
     assertEquals(1, matches.size());    // One error, plural subject is mismatched with a singular verb
+  }
+
+  @Test
+  public void testSpelling2() throws IOException {
+    // This word does exist, the matches variable should be empty
+    List<RuleMatch> matches = lt.check("great");
+    assertEquals(0, matches.size());
+  }
+
+  @Test
+  public void testSpelling3() throws IOException {
+    // Facebook does not exist in a dictionary, but here is considered a noun
+    List<RuleMatch> matches = lt.check("Facebook");
+    assertEquals(0, matches.size());
+  }
+
+  @Test
+  public void testSpelling4() throws IOException {
+    // "is" should be replaced by "am"
+    // "tak" should be "take"
+    // "wlk" should be "walk"
+    List<RuleMatch> matches = lt.check("I is going to tak a wlk");
+    for (RuleMatch match : matches) {
+      System.out.println(match);
+    }
+    assertEquals(3, matches.size());
   }
 }
