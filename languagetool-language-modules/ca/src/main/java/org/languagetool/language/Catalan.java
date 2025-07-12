9ea3f27bc26f5b5fec1d49b1ed6f4c6c183f10c2
jaumeortola <jaumeortola@gmail.com>
2025-07-12 16:14:03 +0200

    [ca] improve rules

--- languagetool-language-modules/ca/src/main/java/org/languagetool/language/Catalan.java
+++ languagetool-language-modules/ca/src/main/java/org/languagetool/language/Catalan.java
@@ -265,6 +265,7 @@ public class Catalan extends Language {
       case "CONFUSIONS_ACCENT": return 20;
       case "CONFUSIO_PASSAT_INFINITIU": return 20; // greater than ACCENTUATION_CHECK
       case "DIACRITICS": return 20;
+      case "COMMA_ENTRE_DALTRES": return 20; //greater than CONCORDANCES_DET_NOM
       case "CAP_GENS": return 20; //greater than CAP_ELS_CAP_ALS, CONCORDANCES_DET_NOM
       case "MOTS_SENSE_GUIONETS": return 20; // greater than CONCORDANCES_NUMERALS
       case "ORDINALS": return 20; // greater than SEPARAT
