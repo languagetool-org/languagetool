4a80fdd7fe164a9c147198c62d4f0da1489614f6
jaumeortola <jaumeortola@gmail.com>
2026-01-23 12:26:21 +0100

    [ca] more words

--- languagetool-language-modules/ca/src/main/java/org/languagetool/tagging/ca/CatalanTagger.java
+++ languagetool-language-modules/ca/src/main/java/org/languagetool/tagging/ca/CatalanTagger.java
@@ -48,9 +48,9 @@ public class CatalanTagger extends BaseTagger {
   private static final Pattern TRES_ADJECTIUS = Pattern.compile("(.*)o-(.*)o-(.*.*)",
     Pattern.CASE_INSENSITIVE | Pattern.UNICODE_CASE);
   private final List<String> ALTRES_PREFIXOS = Arrays.asList("greco", "sino", "italo", "franco", "gal·lo", "luso",
-    "germano", "hispano", "anglo", "arabo");
+    "germano", "hispano", "anglo", "àrabo");
   private final List<String> NO_ALTRES_PREFIXOS = Arrays.asList("grego", "xineso", "italiano", "franceso",
-    "portugueso", "angleso", "espanyolo", "alemanyo");
+    "portugueso", "angleso", "espanyolo", "alemanyo", "arabo");
   private static final List<String> ALLUPPERCASE_EXCEPTIONS = Arrays.asList("ARNAU", "CRISTIAN", "TOMÀS");
   private String variant;
 
